ca2305de20a6d77cbc67207d50c9a7f095565993
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-06-21 20:41:35 -0400

    More - hopefully FINAL cleanups of Stroiak Base64 code

--- Library/Sources/Stroika/Foundation/Cryptography/Base64.cpp
+++ Library/Sources/Stroika/Foundation/Cryptography/Base64.cpp
@@ -30,6 +30,10 @@ using	namespace	Stroika::Foundation::Memory;
  *		The public domain (private) code in this file - is designed to operate in a STREAM mode. I've preserved that
  *	internally, evne though my current API doesnt work that way, because we will soon support a STREAM based API here,
  *	and that will fit perfectly.
+ *
+ *		See also http://libb64.sourceforge.net/ - which explains the design of the routines, and the queer nested loop / switch
+ * construct.
+ *
  *		-- LGP 2011-06-21
  */
 
@@ -44,58 +48,57 @@ using	namespace	Stroika::Foundation::Memory;
  ********************************************************************************
  */
 namespace	{
-	enum base64_decodestep {
+	enum base64_decodestep_ {
 		step_a, step_b, step_c, step_d
 	};
-	struct base64_decodestate {
-		base64_decodestep	step;
+	struct base64_decodestate_ {
+		base64_decodestep_	step;
 		char				plainchar;
+
+		base64_decodestate_ ()
+			: step (step_a)
+			, plainchar ('\0')
+			{
+			}
 	};
-	void base64_init_decodestate(base64_decodestate* state_in)
+	int base64_decode_value_ (char value_in)
 		{
-			state_in->step = step_a;
-			state_in->plainchar = 0;
-		}
-	int base64_decode_value(char value_in)
-		{
-			static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
-			static const char decoding_size = sizeof(decoding);
+			static const char kDecoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
 			value_in -= 43;
-			if (value_in < 0 || value_in > decoding_size) return -1;
-			return decoding[(int)value_in];
+			if (value_in < 0 || value_in > NEltsOf (kDecoding)) return -1;
+			return kDecoding[(int)value_in];
 		}
-	int base64_decode_block (const char* code_in, const int length_in, char* plaintext_out, base64_decodestate* state_in)
+	size_t base64_decode_block_ (const char* code_in, size_t length_in, char* plaintext_out, base64_decodestate_* state)
 		{
 			const char* codechar = code_in;
 			char* plainchar = plaintext_out;
 			char fragment;
 	
-			*plainchar = state_in->plainchar;
+			*plainchar = state->plainchar;
 	
-			switch (state_in->step)
-			{
+			switch (state->step) {
 				while (1)
 				{
 			case step_a:
 					do {
 						if (codechar == code_in+length_in)
 						{
-							state_in->step = step_a;
-							state_in->plainchar = *plainchar;
+							state->step = step_a;
+							state->plainchar = *plainchar;
 							return plainchar - plaintext_out;
 						}
-						fragment = (char)base64_decode_value(*codechar++);
+						fragment = (char)base64_decode_value_ (*codechar++);
 					} while (fragment < 0);
 					*plainchar    = (fragment & 0x03f) << 2;
 			case step_b:
 					do {
 						if (codechar == code_in+length_in)
 						{
-							state_in->step = step_b;
-							state_in->plainchar = *plainchar;
+							state->step = step_b;
+							state->plainchar = *plainchar;
 							return plainchar - plaintext_out;
 						}
-						fragment = (char)base64_decode_value(*codechar++);
+						fragment = (char)base64_decode_value_ (*codechar++);
 					} while (fragment < 0);
 					*plainchar++ |= (fragment & 0x030) >> 4;
 					*plainchar    = (fragment & 0x00f) << 4;
@@ -103,11 +106,11 @@ namespace	{
 					do {
 						if (codechar == code_in+length_in)
 						{
-							state_in->step = step_c;
-							state_in->plainchar = *plainchar;
+							state->step = step_c;
+							state->plainchar = *plainchar;
 							return plainchar - plaintext_out;
 						}
-						fragment = (char)base64_decode_value(*codechar++);
+						fragment = (char)base64_decode_value_ (*codechar++);
 					} while (fragment < 0);
 					*plainchar++ |= (fragment & 0x03c) >> 2;
 					*plainchar    = (fragment & 0x003) << 6;
@@ -115,11 +118,11 @@ namespace	{
 					do {
 						if (codechar == code_in+length_in)
 						{
-							state_in->step = step_d;
-							state_in->plainchar = *plainchar;
+							state->step = step_d;
+							state->plainchar = *plainchar;
 							return plainchar - plaintext_out;
 						}
-						fragment = (char)base64_decode_value(*codechar++);
+						fragment = (char)base64_decode_value_ (*codechar++);
 					} while (fragment < 0);
 					*plainchar++   |= (fragment & 0x03f);
 				}
@@ -133,9 +136,8 @@ vector<Byte>	Cryptography::DecodeBase64 (const string& s)
 {
 	size_t dataSize1 = s.length ();
 	SmallStackBuffer<Byte>	buf1 (dataSize1);	// MUCH more than big enuf
-	base64_decodestate _state;
-	base64_init_decodestate(&_state);
-	int r = base64_decode_block(s.c_str (), s.length (), (char*)buf1.begin (), &_state);
+	base64_decodestate_ state;
+	size_t r = base64_decode_block_ (s.c_str (), s.length (), (char*)buf1.begin (), &state);
 	Assert (r <= dataSize1);
 	return vector<Byte> (buf1.begin (), buf1.begin () + r);
 }
@@ -159,123 +161,117 @@ namespace	{
 		step_A, step_B, step_C
 	};
 	struct base64_encodestate {
-		base64_encodestep step;
-		char result;
-		int stepcount;
-		LineBreak lb;
+		base64_encodestep	step;
+		char				result;
+		int					stepcount;
+		LineBreak			fLineBreak;
+
+		base64_encodestate  (LineBreak lb)
+			: step (step_A)
+			, result (0)
+			, stepcount (0)
+			, fLineBreak (lb)
+			{
+			}
 	};
 
-	void base64_init_encodestate(base64_encodestate* state_in)
+	char base64_encode_value_ (char value_in)
 		{
-			state_in->step = step_A;
-			state_in->result = 0;
-			state_in->stepcount = 0;
+			const char	BASE64_CHARS_[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+			Assert  (NEltsOf (BASE64_CHARS_) == (2*26 + 10 + 2 + 1));
+			if (value_in > 63) {
+				return '=';
+			}
+			return BASE64_CHARS_[(int)value_in];
 		}
 
-	char base64_encode_value(char value_in)
-		{
-			const char	BASE64_CHARS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-			if (value_in > 63) return '=';
-			return BASE64_CHARS[(int)value_in];
-		}
-
-	int base64_encode_block(const char* plaintext_in, int length_in, char* code_out, base64_encodestate* state_in)
+	size_t base64_encode_block_ (const char* plaintext_in, size_t length_in, char* code_out, base64_encodestate* state)
 		{
 			const int CHARS_PER_LINE = 76;
-			const char* plainchar = plaintext_in;
-			const char* const plaintextend = plaintext_in + length_in;
-			char* codechar = code_out;
-			char result;
+			
+			const char*			plainchar		= plaintext_in;
+			const char* const	plaintextend	= plaintext_in + length_in;
+			char*				codechar		=	code_out;
+			char				result			=	state->result;
+	
 			char fragment;
-	
-			result = state_in->result;
-	
-			switch (state_in->step)
-			{
-				while (1)
-				{
+			switch (state->step) {
+				while (1) {
 			case step_A:
-					if (plainchar == plaintextend)
-					{
-						state_in->result = result;
-						state_in->step = step_A;
+					if (plainchar == plaintextend) {
+						state->result = result;
+						state->step = step_A;
 						return codechar - code_out;
 					}
 					fragment = *plainchar++;
 					result = (fragment & 0x0fc) >> 2;
-					*codechar++ = base64_encode_value(result);
+					*codechar++ = base64_encode_value_ (result);
 					result = (fragment & 0x003) << 4;
 			case step_B:
-					if (plainchar == plaintextend)
-					{
-						state_in->result = result;
-						state_in->step = step_B;
+					if (plainchar == plaintextend) {
+						state->result = result;
+						state->step = step_B;
 						return codechar - code_out;
 					}
 					fragment = *plainchar++;
 					result |= (fragment & 0x0f0) >> 4;
-					*codechar++ = base64_encode_value(result);
+					*codechar++ = base64_encode_value_ (result);
 					result = (fragment & 0x00f) << 2;
 			case step_C:
 					if (plainchar == plaintextend)
 					{
-						state_in->result = result;
-						state_in->step = step_C;
+						state->result = result;
+						state->step = step_C;
 						return codechar - code_out;
 					}
 					fragment = *plainchar++;
 					result |= (fragment & 0x0c0) >> 6;
-					*codechar++ = base64_encode_value(result);
+					*codechar++ = base64_encode_value_ (result);
 					result  = (fragment & 0x03f) >> 0;
-					*codechar++ = base64_encode_value(result);
+					*codechar++ = base64_encode_value_ (result);
 			
-					++(state_in->stepcount);
-					if (state_in->stepcount == CHARS_PER_LINE/4)
-					{
-						switch (state_in->lb) {
-							case	eLF_LB: *codechar++ = '\n'; break;
-							case	eCRLF_LB: *codechar++ = '\r'; *codechar++ = '\n'; break;
+					++(state->stepcount);
+					if (state->stepcount == CHARS_PER_LINE/4) {
+						switch (state->fLineBreak) {
+							case	eLF_LB:		*codechar++ = '\n';		break;
+							case	eCRLF_LB:	*codechar++ = '\r';		*codechar++ = '\n'; break;
 						}
-						state_in->stepcount = 0;
+						state->stepcount = 0;
 					}
 				}
 			}
 			return codechar - code_out;
 		}
-	int base64_encode_blockend(char* code_out, base64_encodestate* state_in)
+	inline	size_t	base64_encode_blockend_ (char* code_out, base64_encodestate* state)
 		{
 			char* codechar = code_out;
-	
-			switch (state_in->step)
-			{
-			case step_B:
-				*codechar++ = base64_encode_value(state_in->result);
-				*codechar++ = '=';
-				*codechar++ = '=';
-				break;
-			case step_C:
-				*codechar++ = base64_encode_value(state_in->result);
-				*codechar++ = '=';
-				break;
-			case step_A:
-				break;
+			switch (state->step) {
+				case step_B:
+					*codechar++ = base64_encode_value_ (state->result);
+					*codechar++ = '=';
+					*codechar++ = '=';
+					break;
+				case step_C:
+					*codechar++ = base64_encode_value_ (state->result);
+					*codechar++ = '=';
+					break;
+				case step_A:
+					break;
 			}
-	
-			return codechar - code_out;
+			return static_cast<size_t> (codechar - code_out);
 		}
 }
 
 string	Cryptography::EncodeBase64 (const Byte* start, const Byte* end, LineBreak lb)
 {
-	base64_encodestate _state;
-	base64_init_encodestate(&_state);
-	_state.lb = lb;
-	int srcLen = end - start;
-	int bufSize = 4 * srcLen;
+	base64_encodestate state (lb);
+	size_t srcLen = end - start;
+	size_t bufSize = 4 * srcLen;
 	SmallStackBuffer<char>	data (bufSize);
-	int r = 	 base64_encode_block((char*)start, (end-start), data.begin (), &_state);
-	int extraBytes = base64_encode_blockend(data.begin () + r, &_state);
-	int totalBytes = r + extraBytes;
+	size_t r = 	 base64_encode_block_ ((char*)start, (end-start), data.begin (), &state);
+	size_t extraBytes = base64_encode_blockend_ (data.begin () + r, &state);
+	size_t totalBytes = r + extraBytes;
+	Assert (totalBytes <= bufSize);
 	return string (data.begin (), data.begin () + totalBytes);
 }
 
@@ -283,4 +279,3 @@ string	Cryptography::EncodeBase64 (const vector<Byte>& b, LineBreak lb)
 {
 	return EncodeBase64 (Containers::Start (b), Containers::End (b), lb);
 }
-
