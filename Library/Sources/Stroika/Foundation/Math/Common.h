be22dbae8746f762280c127751f042fcc7eb84ad
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-07-13 18:38:55 -0400

    Added kE and kPi math constants

--- Library/Sources/Stroika/Foundation/Math/Common.h
+++ Library/Sources/Stroika/Foundation/Math/Common.h
@@ -6,16 +6,16 @@
 
 #include	"../StroikaPreComp.h"
 
-
-
 namespace	Stroika {	
 	namespace	Foundation {
 		namespace	Math {
 
 			double	nan ();
 
-		}
+			const	double	kE	=	2.71828182845904523536;
+			const	double	kPi	=	3.14159265358979323846;
 
+		}
 	}
 }
 #endif	/*_Stroika_Foundation_Math_Basic_h_*/
