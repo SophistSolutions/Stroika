a53936f97a307231f68ca752c3b0f28f8177b27b
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-07-29 19:19:02 -0400

    fixed indentaiton one place and no need for Stroika::Foundation::Characters:xxxx - just use Characters::xxx.... (assume Stroika::Foundation - find stuff from there inside foundation)

--- Library/Sources/Stroika/Foundation/Containers/TimedCache.h
+++ Library/Sources/Stroika/Foundation/Containers/TimedCache.h
@@ -29,56 +29,56 @@ namespace	Stroika {
 #endif
 
 
-	// Keeps track of all items - indexed by Key - but throws away items which are any more
-	// stale than given by the TIMEOUT
-	//
-	// The only constraint on KEY is that it must have an operator== and operator<, and the only constraint on
-	// both KEY and RESULT is that they must be copyable objects.
-	//
-	// Note - this class doesn't employ a thread to throw away old items, so if you count on that
-	// happening (e.g. because the RESULT object DTOR has a side-effect like closing a file), then
-	// you may call DoBookkeeping () peridocially.
-	template	<typename	KEY, typename RESULT>
-		class	TimedCache {
-			public:
-				TimedCache (bool accessFreshensDate, Stroika::Foundation::Time::DurationSecondsType timeoutInSeconds);
+			// Keeps track of all items - indexed by Key - but throws away items which are any more
+			// stale than given by the TIMEOUT
+			//
+			// The only constraint on KEY is that it must have an operator== and operator<, and the only constraint on
+			// both KEY and RESULT is that they must be copyable objects.
+			//
+			// Note - this class doesn't employ a thread to throw away old items, so if you count on that
+			// happening (e.g. because the RESULT object DTOR has a side-effect like closing a file), then
+			// you may call DoBookkeeping () peridocially.
+			template	<typename	KEY, typename RESULT>
+				class	TimedCache {
+					public:
+						TimedCache (bool accessFreshensDate, Time::DurationSecondsType timeoutInSeconds);
 
-				nonvirtual	void	SetTimeout (Stroika::Foundation::Time::DurationSecondsType timeoutInSeconds);
+						nonvirtual	void	SetTimeout (Time::DurationSecondsType timeoutInSeconds);
 
-				nonvirtual	bool	AccessElement (const KEY& key, RESULT* result);
-				nonvirtual	void	AddElement (const KEY& key, const RESULT& result);
+						nonvirtual	bool	AccessElement (const KEY& key, RESULT* result);
+						nonvirtual	void	AddElement (const KEY& key, const RESULT& result);
 
-				nonvirtual	void	DoBookkeeping ();	// optional - need not be called
+						nonvirtual	void	DoBookkeeping ();	// optional - need not be called
 
-			private:
-				bool	fAccessFreshensDate;
-				Stroika::Foundation::Time::DurationSecondsType	fTimeout;
-				Stroika::Foundation::Time::DurationSecondsType	fNextAutoClearAt;
+					private:
+						bool						fAccessFreshensDate;
+						Time::DurationSecondsType	fTimeout;
+						Time::DurationSecondsType	fNextAutoClearAt;
 			
-			private:
-				nonvirtual	void	ClearIfNeeded_ ();
-				nonvirtual	void	ClearOld_ ();
+					private:
+						nonvirtual	void	ClearIfNeeded_ ();
+						nonvirtual	void	ClearOld_ ();
 
-			private:
-				struct	MyResult {
-					MyResult (const RESULT& r):
-						fResult (r),
-						fLastAccessedAt (Time::GetTickCount ())
-						{
-						}
-					RESULT	fResult;
-					Stroika::Foundation::Time::DurationSecondsType	fLastAccessedAt;
+					private:
+						struct	MyResult {
+							MyResult (const RESULT& r):
+								fResult (r),
+								fLastAccessedAt (Time::GetTickCount ())
+								{
+								}
+							RESULT	fResult;
+							Time::DurationSecondsType	fLastAccessedAt;
+						};
+						map<KEY,MyResult>	fMap;
+
+					#if		qKeepTimedCacheStats
+					public:
+						void	DbgTraceStats (const Characters::TChar* label) const;
+					public:
+						size_t		fCachedCollected_Hits;
+						size_t		fCachedCollected_Misses;
+					#endif
 				};
-				map<KEY,MyResult>	fMap;
-
-			#if		qKeepTimedCacheStats
-			public:
-				void	DbgTraceStats (const Stroika::Foundation::Characters::TChar* label) const;
-			public:
-				size_t		fCachedCollected_Hits;
-				size_t		fCachedCollected_Misses;
-			#endif
-		};
 
 		}
 	}
