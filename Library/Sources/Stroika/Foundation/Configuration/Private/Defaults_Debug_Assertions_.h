c15ebfe8668f7899c642073576392dd8ae9ac4ae
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-08-11 08:34:06 -0400

    get rid of warnings when _DEBUG and NDEBUG (_DEBUG windows-specific) are not defined; just warn (used to error) when they are define inconsistently with qDebug)

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_Debug_Assertions_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_Debug_Assertions_.h
@@ -21,6 +21,7 @@
 		#elif	defined (NDEBUG)
 			#define	qDebug	0
 		#else
+			// NB #warning is a non-standard extension - maybe we shouldnt use?
 			// DEFAULT if we have no idea?
 			#warning Neither qDebug nor _DEBUG no NDEBUG defined, defaulting to no debug (qDebug=0)
 			#define	qDebug	0
@@ -31,22 +32,14 @@
 
 	// Check for consistent defines
 	#if		qDebug
-		#if		!defined(_DEBUG)
-			// NB #warning is a non-standard extension - maybe we shouldnt use?
-			// Maybe get rid of this?
-			#warning 	Perhaps _DEBUG should also have been defined?
-		#endif
 		#if		defined (NDEBUG)
-			#error	INCONSISTENT DEFINES (NDEBUG and qDebug=1)
+			// NB #warning is a non-standard extension - maybe we shouldnt use?
+			#warning	INCONSISTENT DEFINES (NDEBUG and qDebug=1)
 		#endif
 	#else
 		#if		defined(_DEBUG)
-			#error	INCONSISTENT DEFINES (_DEBUG and qDebug=0)
-		#endif
-		#if		!defined (NDEBUG)
 			// NB #warning is a non-standard extension - maybe we shouldnt use?
-			// Maybe get rid of this?
-			#warning	Perhaps NDEBUG should aslo have been defined
+			#warning	INCONSISTENT DEFINES (_DEBUG and qDebug=0)
 		#endif
 	#endif
 
