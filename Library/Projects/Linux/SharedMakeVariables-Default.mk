46407e248851b4d229394a6b32ac23690acabc70
Lewis G. Pringle, Jr <lewis@sophists.com>
2011-09-04 11:12:09 -0700

    more factoring of the Linux makefiles

---
+++ Library/Projects/Linux/SharedMakeVariables-Default.mk
@@ -0,0 +1,16 @@
+# This file should generally not be edited. Mostly - one edits/configures the Configuration.mk, and then this generates the right
+# values in make variables to be used in teh SharedBuildRules.mk file.
+#
+# One would mostly want to edit this file if there was some flag one wanted to add to CFLAGS for example, that wasn't arleady controlled
+# by a value in Configuration.mk
+#
+# NB: Invididual makefiles will OFTEN override these values - adding INCLUDES ot the list, or (all of them should) override RelPathToDevRoot)
+
+RelPathToDevRoot		=	_NO_DEFAULT_
+ObjDir                          =       ./
+LibDir				=	$(RelPathToDevRoot)Builds/Platform_Linux/
+Includes                        =       -I$(RelPathToDevRoot)/Library/Sources/
+
+StroikaFoundationLib		=	$(LibDir)Stroika-Foundation.a
+
+CFLAGS= -c -std=c++0x $(DEBUGFLAG)
