8215aba2ca7fe8a92bc70bbd4966ca2eeb46f396
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-05 22:03:17 -0400

    comments and partly lose backward compat WideStringToACP/... etc routines

--- Library/Sources/Stroika/Foundation/Characters/TString.h
+++ Library/Sources/Stroika/Foundation/Characters/TString.h
@@ -19,17 +19,31 @@ namespace	Stroika {
 
 			typedef	std::basic_string<TChar>	TString;
 
+			/*
+			 * Even when the platform sdk uses wide strings (but more especailly when it does not) it will typically have
+			 * a prefered code-page for mapping narrow characters to/from wide ones (part of locale?). WideStringToNarrowSDKString will
+			 * perform that mapping.
+			 */
 			string	WideStringToNarrowSDKString (const wstring& ws);
+
+			/*
+			 * Even when the platform sdk uses wide strings (but more especailly when it does not) it will typically have
+			 * a prefered code-page for mapping narrow characters to/from wide ones (part of locale?). NarrowSDKStringToWide will
+			 * perform that mapping.
+			 */
 			wstring	NarrowSDKStringToWide (const string& s);
 
 			string	TString2NarrowSDK (const TString& s);
 			TString	NarrowSDK2TString (const string& s);
 
+
+#if 0
 //BACKWARD COMPAT NAMES - TIL WE FIX HEALTHFRAME -- LGP 2011-09-05
 inline string	WideStringToACP (const wstring& ws) { return WideStringToNarrowSDKString (ws); }
 inline wstring	ACPStringToWide (const string& s) { return NarrowSDKStringToWide (s); }
 inline string	TString2ANSI (const TString& s) { return TString2NarrowSDK (s); }
 inline TString	ANSI2TString (const string& s) { return NarrowSDK2TString (s); }
+#endif
 
 
 			wstring	TString2Wide (const TString& s);
