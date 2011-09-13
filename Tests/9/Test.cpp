e47819bb26ba13f64508692382197820806c0237
Sterling Wight <stroika@sterlingwight.net>
2011-09-13 17:21:31 -0500

    moved SimpleClass to separate file, as needed in multiple tests suites

--- Tests/9/Test.cpp
+++ Tests/9/Test.cpp
@@ -11,119 +11,13 @@
 #include	"Stroika/Foundation/Debug/Assertions.h"
 #include	"Stroika/Foundation/Debug/Trace.h"
 
+#include	"../TestHarness/SimpleClass.h"
 #include	"../TestHarness/TestHarness.h"
 
 
-
+using   namespace   Stroika;
 using	namespace	Stroika::Foundation;
 using	namespace	Stroika::Foundation::Containers;
-
-
-
-
-namespace	{
-
-
-class	SimpleClass {
-	public:
-	#if qIteratorsRequireNoArgContructorForT
-            SimpleClass ();
-    #endif
-        SimpleClass (size_t v);
-		SimpleClass (const SimpleClass& f);
-		~SimpleClass ();
-
-		nonvirtual	size_t	GetValue () const;
-		static		size_t	GetTotalLiveCount ();
-
-	private:
-		size_t	fValue;
-		int		fConstructed;
-		static	size_t	sTotalLiveObjects;
-
-	friend	bool	operator== (const SimpleClass& lhs, const SimpleClass& rhs);
-	friend	bool	operator< (const SimpleClass& lhs, const SimpleClass& rhs);
-};
-bool	operator== (const SimpleClass& lhs, const SimpleClass& rhs);
-bool	operator< (const SimpleClass& lhs, const SimpleClass& rhs);
-
-
-/*
- ********************************************************************************
- ********************************** SimpleClass *********************************
- ********************************************************************************
- */
-static	const	int kFunnyValue = 1234;
-
-size_t	SimpleClass::sTotalLiveObjects	=	0;
-
-#if qIteratorsRequireNoArgContructorForT
-     SimpleClass::SimpleClass () :
-        fValue (3),
-        fConstructed (kFunnyValue)
-    {
-        sTotalLiveObjects++;
-    }
-#endif
-
-SimpleClass::SimpleClass (size_t v) :
-	fValue (v),
-	fConstructed (kFunnyValue)
-{
-	sTotalLiveObjects++;
-}
-
-SimpleClass::SimpleClass (const SimpleClass& f) :
-	fValue (f.fValue),
-	fConstructed (kFunnyValue)
-{
-	sTotalLiveObjects++;
-	VerifyTestResult (f.fConstructed == kFunnyValue);
-}
-
-SimpleClass::~SimpleClass ()
-{
-	VerifyTestResult (fConstructed == kFunnyValue);
-	VerifyTestResult (sTotalLiveObjects != 0);
-	sTotalLiveObjects--;
-	fConstructed = 0;
-	VerifyTestResult (fConstructed != kFunnyValue);
-}
-
-size_t	SimpleClass::GetValue () const
-{
-	VerifyTestResult (fConstructed == kFunnyValue);
-	return (fValue);
-}
-
-size_t	SimpleClass::GetTotalLiveCount ()
-{
-	return (sTotalLiveObjects);
-}
-
-
-/*
- ********************************************************************************
- ******************************* comparison operators  **************************
- ********************************************************************************
- */
-bool	operator== (const SimpleClass& lhs, const SimpleClass& rhs)
-{
-	VerifyTestResult (lhs.fConstructed == kFunnyValue);
-	VerifyTestResult (rhs.fConstructed == kFunnyValue);
-	return (bool (lhs.fValue == rhs.fValue));
-}
-
-bool	operator< (const SimpleClass& lhs, const SimpleClass& rhs)
-{
-	VerifyTestResult (lhs.fConstructed == kFunnyValue);
-	VerifyTestResult (rhs.fConstructed == kFunnyValue);
-	return (bool (lhs.fValue < rhs.fValue));
-}
-
-
-
-}
 
 
 
