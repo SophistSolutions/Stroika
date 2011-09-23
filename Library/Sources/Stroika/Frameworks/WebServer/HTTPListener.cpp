c850b3e29463a7cb10a79efa2ad9a4dcb48cf0e4
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-22 23:16:01 -0400

    added placeholder Frameworks/WebServer/HTTPListener module

---
+++ Library/Sources/Stroika/Frameworks/WebServer/HTTPListener.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#include	"../StroikaPreComp.h"
+
+#include	<algorithm>
+#include	<cstdlib>
+
+#include	"../../Foundation/Containers/Common.h"
+#include	"../../Foundation/DataExchangeFormat/BadFormatException.h"
+#include	"../../Foundation/Debug/Assertions.h"
+#include	"../../Foundation/Execution/Exceptions.h"
+#include	"../../Foundation/Memory/SmallStackBuffer.h"
+
+#include	"HTTPListener.h"
+
+
+using	namespace	Stroika::Foundation;
+using	namespace	Stroika::Foundation::Containers;
+using	namespace	Stroika::Foundation::Memory;
+
+
+
+
+
+
+
+
+/*
+ ********************************************************************************
+ ************************************ Service::Main *****************************
+ ********************************************************************************
+ */
---
+++ Library/Sources/Stroika/Frameworks/WebServer/HTTPListener.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#ifndef	_Stroika_Framework_WebServer_HTTPListener_h_
+#define	_Stroika_Framework_WebServer_HTTPListener_h_	1
+
+#include	"../StroikaPreComp.h"
+
+#include	<string>
+#include	<vector>
+
+#include	"../../Foundation/Characters/String.h"
+#include	"../../Foundation/Configuration/Common.h"
+#include	"../../Foundation/Memory/SharedPtr.h"
+
+
+/*
+ * TODO:
+ *		Very early stage draft.
+ *
+ *		(o)	
+ */
+
+namespace	Stroika {	
+	namespace	Framework {
+		namespace	Service {
+
+			using	namespace	Stroika::Foundation;
+			using	Characters::String;
+
+
+		}
+	}
+}
+#endif	/*_Stroika_Framework_WebServer_HTTPListener_h_*/
+
+
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include	"HTTPListener.inl"
