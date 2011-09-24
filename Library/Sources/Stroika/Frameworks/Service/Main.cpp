04910e0419dbd7c361f60f2564d2f09b3f140310
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-23 23:10:04 -0400

    more implememntation of POSIX version of ServiceMain Framework

--- Library/Sources/Stroika/Frameworks/Service/Main.cpp
+++ Library/Sources/Stroika/Frameworks/Service/Main.cpp
@@ -6,10 +6,19 @@
 #include	<algorithm>
 #include	<cstdlib>
 
+#if		qPlatform_POSIX
+	#include	<sys/types.h>
+	#include	<unistd.h>
+#endif
+
 #include	"../../Foundation/Containers/Common.h"
 #include	"../../Foundation/DataExchangeFormat/BadFormatException.h"
 #include	"../../Foundation/Debug/Assertions.h"
+#include	"../../Foundation/Debug/Trace.h"
+#include	"../../Foundation/Execution/CommandLine.h"
 #include	"../../Foundation/Execution/Exceptions.h"
+#include	"../../Foundation/Execution/Module.h"
+#include	"../../Foundation/Execution/ThreadAbortException.h"
 #include	"../../Foundation/Memory/SmallStackBuffer.h"
 
 #include	"Main.h"
@@ -18,6 +27,43 @@
 using	namespace	Stroika::Foundation;
 using	namespace	Stroika::Foundation::Containers;
 using	namespace	Stroika::Foundation::Memory;
+
+using	namespace	Stroika::Frameworks;
+using	namespace	Stroika::Frameworks::Service;
+
+
+
+
+
+
+
+
+/*
+ ********************************************************************************
+ ****************************** Service::Main::IRep *****************************
+ ********************************************************************************
+ */
+Main::IRep::~IRep ()
+{
+}
+
+void	Main::IRep::OnStartRequest ()
+{
+	// This procedure ends when the entire service process ends...
+	Debug::TraceContextBumper traceCtx (TSTR ("Stroika::Frameworks::Service::Main::IRep::OnStartRequest"));
+	MainLoop ();
+}
+
+void	Main::IRep::OnStopRequest ()
+{
+	// default to using thread stuff to send us a signal to abort...
+}
+
+void	Main::IRep::OnReReadConfigurationRequest ()
+{
+}
+
+
 
 
 
@@ -31,3 +77,117 @@ using	namespace	Stroika::Foundation::Memory;
  ************************************ Service::Main *****************************
  ********************************************************************************
  */
+
+const	wchar_t	Service::Main::CommandNames::kRunAsService[]		=	L"Run-As-Service";
+const	wchar_t	Service::Main::CommandNames::kStart[]				=	L"Start";
+const	wchar_t	Service::Main::CommandNames::kStop[]				=	L"Stop";
+const	wchar_t	Service::Main::CommandNames::kKill[]				=	L"Kill";
+const	wchar_t	Service::Main::CommandNames::kRestart[]				=	L"Restart";
+const	wchar_t	Service::Main::CommandNames::kReloadConfiguration[]	=	L"Reload-Configuration";
+const	wchar_t	Service::Main::CommandNames::kPause[]				=	L"Pause";
+const	wchar_t	Service::Main::CommandNames::kContinue[]			=	L"Continue";
+
+Main::Main (Memory::SharedPtr<IRep> rep)
+	: _fRep (rep)
+{
+}
+
+void	Main::RunAsService ()
+{
+	try {
+		_fRep->OnStartRequest ();
+	}
+	catch (const Execution::ThreadAbortException& /*threadAbort*/) {
+		// ignore this - just means service ended normally
+	}
+	catch (...) {
+		DbgTrace (TSTR ("Unexpected exception ended running service"));
+		throw;
+	}
+}
+
+void	Main::Start ()
+{
+	// Check not already runnig, (someday) and then for and exec the 
+
+	Characters::TString	thisEXEPath	=	Execution::GetEXEPath ();
+#if		qPlatform_POSIX
+	pid_t	pid	=	fork ();
+	if (pid == 0) {
+		// Child - exec
+		const char*	v[]	=	{ CommandNames::kRunAsService., NULL };
+		int	r	=	execl (thisEXEPath.c_str (), thisEXEPath.c_str (), CommandNames::kRunAsService.AsTString ().c_str (), nullptr);
+		exit (-1);
+	}
+	else if (pid < 0) {
+		// failed to fork - serious error
+		ThrowIfError_errno_t ();
+		Assert (false);	// errno SB set!!!
+	}
+	else {
+		// parent - in this case - no reason to wait - our work is done... Future versions might wait to
+		// see if the 'pidfile' got created....
+		//		--LGP 2011-09-23
+	}
+#endif
+}
+
+void	Main::Stop ()
+{
+	// Send signal to server to stop
+}
+
+void	Main::Restart ()
+{
+	IgnoreExceptionsForCall (Stop ());
+	Start ();
+}
+
+void	Main::ReReadConfiguration ()
+{
+	// SEND APPROPRIATE SIGNAL
+}
+
+Main::ServiceDescription	Main::GetServiceDescription () const
+{
+	return _fRep->GetServiceDescription ();
+}
+
+bool	Main::_HandleStandardCommandLineArgument (const String& arg)
+{
+	if (Execution::MatchesCommandLineArgument (arg, CommandNames::kRunAsService)) {
+		RunAsService ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kStart)) {
+		Start ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kStop)) {
+		Stop ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kKill)) {
+		AssertNotImplemented ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kRestart)) {
+		AssertNotImplemented ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kReloadConfiguration)) {
+		AssertNotImplemented ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kPause)) {
+		AssertNotImplemented ();
+		return true;
+	}
+	else if (Execution::MatchesCommandLineArgument (arg, CommandNames::kContinue)) {
+		AssertNotImplemented ();
+		return true;
+	}
+	/// MANY more neeeded, and fix to use named constants...
+	return false;
+}
+
--- Library/Sources/Stroika/Frameworks/Service/Main.h
+++ Library/Sources/Stroika/Frameworks/Service/Main.h
@@ -1,8 +1,8 @@
 /*
  * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
  */
-#ifndef	_Stroika_Framework_Service_Main_h_
-#define	_Stroika_Framework_Service_Main_h_	1
+#ifndef	_Stroika_Frameworks_Service_Main_h_
+#define	_Stroika_Frameworks_Service_Main_h_	1
 
 #include	"../StroikaPreComp.h"
 
@@ -36,7 +36,7 @@
  */
 
 namespace	Stroika {	
-	namespace	Framework {
+	namespace	Frameworks {
 		namespace	Service {
 
 			using	namespace	Stroika::Foundation;
@@ -58,28 +58,99 @@ namespace	Stroika {
 					class	IRep {
 						public:
 							virtual ~IRep ();
+
+							// This should be overridden by each service, and should  never return until the service is done (stop request).
 							virtual	void				MainLoop () = 0;
 
 							virtual	void				OnStartRequest ();
 							virtual	void				OnStopRequest ();
 							virtual	void				OnReReadConfigurationRequest ();
-							virtual	ServiceDescription	GetServiceDescription () const;
+							virtual	ServiceDescription	GetServiceDescription () const = 0;
 					};
 				public:
 					Main (Memory::SharedPtr<IRep> rep);
 
+				protected:
+					Memory::SharedPtr<IRep>	_fRep;
+
 				public:
+					/*
+					 *	Checks the state of the given service. NOTE - this works ACROSS PROCESSES. It can be called to ask in a controller exe
+					 *	if the serviceMain EXE is running. It also - COULD give the wrong answer - given races, so use with care.
+					 */
+					enum	State { 
+						eStopped,
+						eRunning,
+						ePaused,		// STOPPED in unix
+					};
+					nonvirtual	State				GetState () const;
+
+				public:
+					/*
+					 *	RunAsService () will not return - until the service has terminated. It runs the service 'MainLoop'.
+					 */
+					nonvirtual	void				RunAsService ();
+					/*
+					 */
 					nonvirtual	void				Start ();
+					/*
+					 */
 					nonvirtual	void				Stop ();
+					/*
+					 */
 					nonvirtual	void				Restart ();
+					/*
+					 */
 					nonvirtual	void				ReReadConfiguration ();
+					/*
+					 */
 					nonvirtual	ServiceDescription	GetServiceDescription () const;
+
+				public:
+					struct	CommandNames {
+						/*
+						 *  The kRunAsService command is about the only command that tends to NOT be called by users on the command line.
+						 *	it tells the code to run indefinitely, (until told to stop) - running the service loop.
+						 *
+						 *	This is typically called INDRECTLY via a specail fork/exec as a result of a kStart command, or its called from
+						 *	init as part of system startup.
+						 */
+						static	const	wchar_t	kRunAsService[];
+						/*
+						 *  The kStart command tells the service to start running. It returns an error
+						 *  if the service is already started.
+						 */
+						static	const	wchar_t	kStart[];
+						/*
+						 *  The kStop command tells the service to start terminate
+						 */
+						static	const	wchar_t	kStop[];
+						//DOCUMENT EACH
+						//NEATLY
+						// KILL termiantes (kill-9)
+						//
+						static	const	wchar_t	kKill[];
+						// restart synonmy for stop (no error if not already running), and then start
+						static	const	wchar_t	kRestart[];
+						// If service knows how to find its own config files - recheck them
+						static	const	wchar_t	kReloadConfiguration[];
+						// SIGSTOP
+						static	const	wchar_t	kPause[];
+						// SIGCONT
+						static	const	wchar_t	kContinue[];
+					};
+
+				protected:
+					/*
+					 * Will handle the given command line argument, and return true if it recognized it, and handled it. It will return false otherwise.
+					 */
+					nonvirtual	bool	_HandleStandardCommandLineArgument (const String& arg);
 			};
 
 		}
 	}
 }
-#endif	/*_Stroika_Framework_Service_Main_h_*/
+#endif	/*_Stroika_Frameworks_Service_Main_h_*/
 
 
 
