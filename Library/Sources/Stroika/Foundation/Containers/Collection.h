ec47acf332852485e4232feef9df82c162ac10bc
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-23 23:09:16 -0400

    a few small draft Collection changes

--- Library/Sources/Stroika/Foundation/Containers/Collection.h
+++ Library/Sources/Stroika/Foundation/Containers/Collection.h
@@ -37,129 +37,88 @@ namespace	Stroika {
 	namespace	Foundation {
 		namespace	Containers {
 
-            template	<typename T>	class	Collection;
-            template	<typename T>	class	CollectionRep;
-            template	<typename T>	class	CollectionIteratorRep;
-            template	<typename T>	class	CollectionMutatorRep;
-
-            template	<typename T>	bool	operator== (const Collection<T>& lhs, const Collection<T>& rhs);
-            template	<typename T>	bool	operator!= (const Collection<T>& lhs, const Collection<T>& rhs);
 
 
-            template	<typename T>	class	CollectionIterator : public Iterator<T> {
-                public:
-                    CollectionIterator (CollectionIteratorRep<T>* it);
-            };
+			/*
+			 * TODO:
+			 *		START by create a 'base class' with all the common stuff fro all contaers. EVENTUALLY also merge
+			 *		the ITERATOR stuff here too...
+			 */
+			template	<typename T>
+				class	Collection {
+					public:
+						class	IRep;
 
-            template	<typename T>	class	CollectionMutator : public Iterator<T> {
-                public:
-                    CollectionMutator (BagMutatorRep<T>* it);
+					protected:
+						Collection (const Memory::SharedByValue<IRep<T>>& rep);
 
-                public:
-                    void	RemoveCurrent ();
-                    void	UpdateCurrent (T newValue);
+					public:
+						nonvirtual	size_t	GetLength () const;
+						nonvirtual	bool	IsEmpty () const;
+						nonvirtual	bool	Contains (T item) const;
+						nonvirtual	void	RemoveAll ();
+						nonvirtual	void	Compact ();
 
-                protected:
-                    nonvirtual  CollectionMutatorRep<T>*	GetMutatorRep ();
-            };
+					// STL-style wrapper names
+					public:
+						nonvirtual	bool	empty () const;
+						nonvirtual	size_t	length () const;
+						nonvirtual	void	clear ();
 
-            template	<typename T>	class	Collection {
-                protected:
-                    Collection (const Collection<T>& collection);
+					public:
+						nonvirtual	void	Add (T item);
+						nonvirtual	void	Add (const Collection<T>& items);
 
-                protected:
-                    Collection (CollectionRep<T>* rep);
+					/*
+					 * It is legal to remove something that is not there (but not necessarily legal for all subtypes)
+					 */
+					public:
+						nonvirtual	void	Remove (T item);
+						nonvirtual	void	Remove (const Collection<T>& items);
 
-                public:
-                    nonvirtual	Collection<T>& operator= (const Collection<T>& collection);
+					/*
+					 *		+=/-= are equivilent Add() and Remove(). They
+					 *	are just syntactic sugar.
+					 */
+					public:
+						nonvirtual	Collection<T>&	operator+= (T item);
+						nonvirtual	Collection<T>&	operator+= (const Collection<T>& items);
+						nonvirtual	Collection<T>&	operator-= (T item);
+						nonvirtual	Collection<T>&	operator-= (const Collection<T>& items);
 
-                    nonvirtual	size_t	GetLength () const;
-                    nonvirtual	bool	IsEmpty () const;
-                    nonvirtual	bool	Contains (T item) const;
-                    nonvirtual	void	RemoveAll ();
-                    nonvirtual	void	Compact ();
-                    nonvirtual	operator Iterator<T> () const;
-
-                public:
-                    nonvirtual	void	Add (T item);
-                    nonvirtual	void	Add (const Collection<T>& items);
-
-
-                /*
-                 * It is legal to remove something that is not there.
-                 */
-                public:
-                    nonvirtual	void	Remove (T item);
-                    nonvirtual	void	Remove (const Collection<T>& items);
-
- 
-                /*
-                 *		+=/-= are equivilent Add() and Remove(). They
-                 *	are just syntactic sugar.
-                 */
-                public:
-                    nonvirtual	Collection<T>&	operator+= (T item);
-                    nonvirtual	Collection<T>&	operator+= (const Bag<T>& items);
-                    nonvirtual	Collection<T>&	operator-= (T item);
-                    nonvirtual	Collection<T>&	operator-= (const Bag<T>& items);
+					private:
+						Memory::SharedByValue<IRep<T> >	fRep;
+				};
 
 
-                /*
-                 *	Build BagIterators or Mutators.
-                 */
-                public:
-                    nonvirtual	operator CollectionIterator<T> () const;
-                    nonvirtual	operator CollectionMutator<T> ();
-
-                    // Support for ranged for, and stl syntax in general
-                    nonvirtual  Iterator<T>    begin () const;
-                    nonvirtual  IterationState end () const;
-
-                private:
-                    Memory::SharedByValue<CollectionRep<T> >	fRep;
-            };
+            template	<typename T>
+				bool	operator== (const Collection<T>& lhs, const Collection<T>& rhs);
+            template	<typename T>	
+				bool	operator!= (const Collection<T>& lhs, const Collection<T>& rhs);
+			
+            template	<typename T>	
+				Collection<T>	operator+ (const Collection<T>& lhs, const Collection<T>& rhs);
+            template	<typename T>	
+				Collection<T>	operator- (const Collection<T>& lhs, const Collection<T>& rhs);
 
 
-            template	<typename T>	Collection<T>	operator+ (const Collection<T>& lhs, const Collection<T>& rhs);
-            template	<typename T>	Collection<T>	operator- (const Collection<T>& lhs, const Collection<T>& rhs);
-
-
-
-            template	<typename T>	class	CollectionIteratorRep : public IteratorRep<T> {
-                protected:
-                    BagIteratorRep ();
-            };
-
-            template	<typename T>	class	CollectionMutatorRep : public BagIteratorRep<T> {
-                protected:
-                    CollectionMutatorRep ();
+            template	<typename T>	
+				class	Collection::IRep {
+					protected:
+						IRep ();
+	                    virtual ~IRep ();
 
                 public:
-                    virtual	void	RemoveCurrent () 			=	0;
-                    virtual	void	UpdateCurrent (T newValue) 	=	0;
-            };
-
-            template	<typename T>	class	CollectionRep {
-                protected:
-                    CollectionRep ();
-
-                public:
-                    virtual ~CollectionRep ();
-
-                public:
-                    virtual	BagRep<T>*	Clone () const					=	0;
-                    virtual	bool		Contains (T item) const			=	0;
                     virtual	size_t		GetLength () const 				=	0;
-                    virtual	void		Compact ()						=	0;
+                    virtual	bool		Contains (T item) const			=	0;
                     virtual	void		RemoveAll () 					=	0;
+                    virtual	void		Compact ()						=	0;
 
+                public:
                     virtual	void		Add (T item)					=	0;
                     virtual	void		Remove (T item)					=	0;
-
-                    virtual	IteratorRep<T>*		MakeIterator () 		=	0;
-                    virtual	CollectionIteratorRep<T>*	MakeCollectionIterator () 		=	0;
-                    virtual	CollectionMutatorRep<T>*	MakeCollectionMutator () 		=	0;
             };
+
 
 		}
     }
