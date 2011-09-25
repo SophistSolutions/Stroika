61cd9030b0923406d055745430fbf3ba71e3fb2f
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-24 21:55:02 -0400

    socket stream support POSIX

--- Library/Sources/Stroika/Foundation/IO/Network/SocketStream.cpp
+++ Library/Sources/Stroika/Foundation/IO/Network/SocketStream.cpp
@@ -28,18 +28,36 @@ using	namespace	Stroika::Foundation::IO::Network;
 
 SocketStream::~SocketStream ()
 {
+#if		qPlatform_Windows
 	::_close (fSD_);
+#elif	qPlatform_POSIX
+	::close (fSD_);
+#else
+	AssertNotImplemented ();
+#endif
 }
 
 size_t	SocketStream::_Read (Byte* buffer, size_t bufSize) override
 {
 	// Must do erorr checking and throw exceptions!!!
+#if		qPlatform_Windows
 	return ::_read (fSD_, buffer, bufSize);
+#elif	qPlatform_POSIX
+	return ::read (fSD_, buffer, bufSize);
+#else
+	AssertNotImplemented ();
+#endif
 }
 
 void	SocketStream::_Write (const Byte* buffer, size_t bufSize) override
 {
 	// Must do erorr checking and throw exceptions!!!
+#if		qPlatform_Windows
 	int		n	=	::_write (fSD_, buffer, bufSize);
+#elif	qPlatform_POSIX
+	int		n	=	::write (fSD_, buffer, bufSize);
+#else
+	AssertNotImplemented ();
+#endif
 }
 
