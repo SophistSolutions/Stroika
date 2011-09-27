6d994a839feb341ffb4b90eee51ebb4743fd90ee
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-09-26 23:32:33 -0400

    Draft/outline of new FileSYstem/Directory class

---
+++ Library/Sources/Stroika/Foundation/IO/FileSystem/Directory.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#include	"../../StroikaPreComp.h"
+
+#include	<sys/stat.h>
+#include	<ctime>
+#include	<limits>
+#include	<fstream>
+#include	<fcntl.h>
+#include	<cstdio>
+
+#if		qPlatform_Windows
+	#include	<aclapi.h>
+	#include	<io.h>
+	#include	<shlobj.h>
+	#include	<windows.h>
+#endif
+
+#include	"../../Characters/Format.h"
+#include	"../../Characters/StringUtils.h"
+#include	"../../Execution/Exceptions.h"
+#include	"../../Containers/Common.h"
+#include	"../../Containers/SetUtils.h"
+#include	"../../Debug/Trace.h"
+#include	"../../IO/FileAccessException.h"
+#include	"../../Memory/SmallStackBuffer.h"
+#include	"WellKnownLocations.h"
+
+#include	"Directory.h"
---
+++ Library/Sources/Stroika/Foundation/IO/FileSystem/Directory.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#ifndef	_Stroika_Foundation_IO_FileSystem_Directory_h_
+#define	_Stroika_Foundation_IO_FileSystem_Directory_h_	1
+
+#include	"../../StroikaPreComp.h"
+
+#if			qPlatform_Windows
+	#include	<Windows.h>
+#endif
+
+#include	"../../Characters/TChar.h"
+#include	"../../Configuration/Common.h"
+#include	"../../Debug/Assertions.h"
+#include	"../../Execution/Exceptions.h"
+#include	"../../Execution/Thread.h"
+#include	"../../Time/DateTime.h"
+
+#include	"../FileAccessMode.h"
+
+
+
+/**
+ * TODO:
+ *
+ *			o	
+ */
+
+
+
+
+namespace	Stroika {	
+	namespace	Foundation {
+		namespace	IO {
+			namespace	FileSystem {
+
+				using	Characters::TChar;
+				using	Characters::TString;
+				using	Time::DateTime;
+
+
+				/*
+				 * TODO:
+				 *		(o)		Add access /creation time etc calls
+				 *		(o)		TOTALLY un-implemtend - miggrade code from FileUtils to here...
+				 */
+
+
+				/* 
+				 * A 'Directory' here refers to a "Directory Name". The methods of Directory operate on the actual filesystem (like Create), but
+				 * this objects lifetime is not connected to the lifetime of any objects with similar names on the filesystem.
+				 */
+				class	Directory {
+					public:
+
+						// If the argument name is not already '/'-terminated, this CTOR will adjust it
+						Directory (const TString& fileFullPath);
+
+						void	Create () const;	// fails if exists
+						void	AssureExists (bool createParentComponentsIfNeeded = true) const;	// fails if doesnt exist at the end, or is not a directory (unclear if its a directry but not accessible)?
+						bool	Exists () const;	// returns true iff exsits AND is directory (what about slink?)
+
+						void	Delete () const;	// fails if exists after teh operaotion - Fails if doest exist
+						void	AssureDeleted (bool autoDeleteContentsAsNeeded) const;
+
+
+
+						// only works (For now) with type wstring
+						template	<typename T>
+							wstring	As () const;
+
+						TString	AsTString () const;
+				};
+
+			}
+		}
+	}
+}
+#endif	/*_Stroika_Foundation_IO_FileSystem_Directory_h_*/
+
+
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include	"Directory.inl"
