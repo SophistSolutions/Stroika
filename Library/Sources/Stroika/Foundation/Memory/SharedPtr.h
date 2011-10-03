96cf74264ec58b2a07163de66da09fd57dd79ffe
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-10-02 22:15:07 -0400

    comments on alternatives for fixing up SharedPtr<T>

--- Library/Sources/Stroika/Foundation/Memory/SharedPtr.h
+++ Library/Sources/Stroika/Foundation/Memory/SharedPtr.h
@@ -11,6 +11,81 @@
 #include	"../Execution/Exceptions.h"
 
 
+/*
+ * GETTING RID OF SharedPtrBase/UsesSharedPtrBase
+ *
+ *		(o)	I'm not happy with the design of SharedPtrBase/UsesSharedPtrBase. In practice - its awkward to use.
+ *
+ *			But there is one fairly import use of smart pointers it seems hard to avoid.
+ *
+ *			Consider a letter/envelope pair DB (envelope) and DBRep. Consider that there are LOTS of subclasses of DBRep, and the API
+ *			is rich. Consider that there are many modules - which use the class DB (such as Import/Export).
+ *
+ *			DB is a simple envelope that uses SharedPtr<DBrep>.
+ *
+ *			How - in a subclass of DBRep - I want to call some API - such as Export - with DB (this). That would be BAD - as it would
+ *			create a shared pointer out of this, and delete it when DB goes out of scope.
+ *
+ *			It is principally to solve THIS problem - that SharedPtrBase/UsesSharedPtrBase still exists for. There is as common base class
+ *			(so you can always find the shared reference count).
+ *
+ *			For example:
+ *
+				void	PHRDBBaseRep::DoSave (ProgressStatusCallback* progressCallback)
+				{
+					wstring	soapBody;
+					{
+						BLOBs::BLOB	phrBLOB;
+						{
+							stringstream	tmpOut;
+							ImportExport::Export (PHRDB::DB (SharedPtr<IDBRep> (SharedPtr<IDBRep>::eUsesSharedPtrBase, this)), MimeTypes::Predefined::HealthFramePHR2_CT (), ImportExport::ExportOptions (), tmpOut, ProgressSubTask (progressCallback, 0.0f, .5f));
+							phrBLOB = StreamUtils::ReadStreamAsBLOB (tmpOut);
+						}
+						CreateSOAPEnvelope_PUTPHR (&soapBody, NarrowSDKStringToWide (Cryptography::EncodeBase64 (phrBLOB)), fUserName, fPassword, fOverridePHRName);
+					}
+					const wstring	kSoapAction	=	 wstring (kHealthFrameWorksURLNamespaceURI_V10) + L"UploadPHR";
+					(void)HTTPSupport::SimpleSOAPSendReceive (L"POST", fUploadURL, kSoapAction, soapBody, CalcSendBestTimeout2Use (soapBody), sOurUserAgent, ProgressSubTask (progressCallback, 0.5f, 1.0f));
+				}
+
+ *
+ *			ALTERNATIVES:
+ *
+ *			(A)		DELETER argument to SharedPtr<>. Basically - we would have a TEMPLATD ARG to SharedPtr - the default of
+ *					which would take up zero size (calling regular operaotr delete on T arg to SharedPtr).
+ *
+ *					That has the advantage that if someone delcared SharedPtr<T,T::SPECIALDELETER> and tried to assign something of
+ *					type SharedPtr<T> - that would not compile (in contrast with existing practice where SharedPtr<IDBRep> (this) would compile
+ *					and just crash badly).
+ *
+ *			(B)		DELETER function pointer - in EVERY SharedPtr<> - passed in for orignal CTOR call. Stores extra pointer with every underlying SharedPtrREP.
+ *					But then have specail one that does NOTHING. And then 
+ *
+ *			(C)		ScopedNODELETER
+ *					This would be a specail class (or parameter to SharedPtr<>) which initializes the sharedptr with a specail count
+ *					with - perhaps - the value 1, and then asserts at the end - when the envoleope is desroyed, that hte value is still 1,
+ *					and THEN delete the COUNTER (cuz 1 was magically like zero - means all refernces went away).
+ *
+ *					It would be a BUG if the originating ScopeNoDeleter/SharedPtr didn't left behind any references. All the cases I have currently
+ *					that would be fine for - and it works pretty well - but could be create buggy code if called code with this special pointer
+ *					stored those poitners. Really just doesnt work for those cases. Maybe not too bad.
+ *
+ *			(D)		Partial specialization with SharedPtrBase.
+ *					If I had faith in this always working - it would probably be the best choice. I guess I should try it...
+ *					Basically - if you have a class you want to pull this trick on - then inheirt from a specail base class, and the rest
+ *					happens fully automatically.
+ *					[attractive enuf - IF I can make the partial-specailation stuff work well enuf - then its perhaps the best way to go]
+ *
+ *					This MAYBE as simple as template specialize of
+ *									template	<typename T>
+										inline	SharedPtr<T>::~SharedPtr ()
+							and
+								op=
+							(or bette ryet - but the delete part in its own template and template partial specialize that
+
+ *
+ */
+
+
 
 /*
  * Description:
@@ -23,36 +98,9 @@
  *		+	CAREFULLY writeup differences between this class and shared_ptr<>
  *			+	I DONT BELIEVE weak_ptr<T> makes sense, and seems likely to generate bugs in multithreaded
  *				applications. Maybe I'm missing something. Ask around a bit...
+ *			+	SEE GETTING RID OF SharedPtrBase/UsesSharedPtrBase ABOVE...
  *
- *			+	Mechanism to pass SharedPtr<T> through non-C++ APIS. I NOW do that through a funky base class. But i JUST
- *				thought of a way - which BTW works for shared_ptr<T>. 
- *					Implement a new class
- *						struct	SharedPtrHandOff<T, SharedPtr<T>> {
-							CTOR takes ADDRESS of SharedPtr<> to be passed to another place (e.g throug win32 API).
-							
-							void DoHandoff (SharedPtr<T>* into)
-								{
-									magically copies the value in.
-									*into = fCopy;
-								}
-
-								private:
-									SharedPtr<T> fCopy;
-						};
-						Then use is
-								RefCntPtr<WINDOWOBJ> w (new WINDOWOBJ);
-								SharedPtrHandOff<WINDOWOBJ)> handoff(&w);
-								::CreateWindow (......, &handoff);
-
-						In WM_CREATE method
-							SharedPtrHandoff<T>* handoff = reintpret_cast<SharedPtrHandoff<T>*> (voidStarUserdataArg);
-
-							RefCntPtr<WINDOWOBJ> myCopy	=	handoff->DoHandoff();
-
-
-				That basic process (varios syntax cleanups needed) - should work for either shared_ptr or SharedPtr, and allow me
-				to get rid of the funky shared base class used for SharedPtrRep...
-
+ *
 
 				Then - we COULD get rid of SharedPtr<> altoegher and just not use the weak_ptr stuff if its a bad idea. I'm undecided on that
 				later point. More thought required
@@ -60,6 +108,8 @@
  *
  *
  */
+
+
 /*
  * Description:
  *		<<<<OBSOLETE/WRONG - FROM OLD STROIKA SHARED CLASS - JUSTPLACE HOLDER DOC TIL WE CAN WRITE GOOD STUFF>>>>>
