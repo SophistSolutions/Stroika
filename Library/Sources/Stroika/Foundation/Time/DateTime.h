6db7864a31a55aa9adbebb02308fc56f4743c42a
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-10-12 22:45:40 -0400

    more datetime comments

--- Library/Sources/Stroika/Foundation/Time/DateTime.h
+++ Library/Sources/Stroika/Foundation/Time/DateTime.h
@@ -54,6 +54,19 @@
  *		o	Only SetDate (const Date& d);/SetTimeOfDay are non-const methods. Consider making this const and just return a new date. Would matter from
  *			an API standpoint if the internl rep was BIGGER (and we did some kind of cache/ptr think like with GC'd langauges). BUt then I can always use
  *			SharedByValue<> template. Maybe a boondoggle?
+ *
+ *		o	Future direction – consider representing as big struct
+ *			o	Like with STRUCT DATETIME or struct tm
+ *				o	Int year
+ *				o	Int month
+ *				o	….
+ *			o	And maybe store cached string reps for common cases – as optimization – and 
+ *				store cached second-offset (mutable) for quick compares
+ *			o	Note in docs – for future versions – the min/max date COULD be expanded
+ *			o	Note for timeofday – it COULD be enhanced in the future to store TOD as 
+ *				fractional number of seconds. COULD use LINUX style struct with number of seconds and fixed
+ *				point like number of nanoseconds (or some such)
+ *
  */
 
 
