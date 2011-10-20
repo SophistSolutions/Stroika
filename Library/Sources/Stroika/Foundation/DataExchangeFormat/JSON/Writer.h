80a810bd5bd70c6014c5c6f94c53e19ceedc6de1
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-10-19 21:29:52 -0400

    Comments

--- Library/Sources/Stroika/Foundation/DataExchangeFormat/JSON/Writer.h
+++ Library/Sources/Stroika/Foundation/DataExchangeFormat/JSON/Writer.h
@@ -12,14 +12,26 @@
 #include	"../../Memory/VariantValue.h"
 
 
+/*
+ * TODO:
+ *		o	MUST RE-IMPLEMENT USING STROIKA TEXT_STREAM OUTPUT!!! BUT THATS NOT IMPLEMENTED/SHAKEN OUT ENUF!!! and needs tie-in to 
+ *			stdio streams so can work effectively...
+ *
+ *		o	When we have BINPARYOUTPUTSTREAM_IOSTREAM - then we can use that to impem,ent the ostrean& overload and just use
+ *			regular BINARYOUTPUTSTREAM version for basic impl.
+ */
+
+
+
 namespace	Stroika {	
 	namespace	Foundation {
 		namespace	DataExchangeFormat {
 			namespace	JSON {
 
-// MUST RE-IMPLEMENT USING STROIKA TEXT_STREAM OUTPUT!!! BUT THATS NOT IMPLEMENTED/SHAKEN OUT ENUF!!! and needs tie-in to 
-// stdio streams so can work effectively...
 
+				/*
+				 * Note that PrettyPrint () writes in UTF-8 format to the output stream.
+				 */
 				void	PrettyPrint (const Memory::VariantValue& v, ostream& out);
 
 			}
