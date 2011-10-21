8b357c76712ca5480825d1ad11480ab665d79862
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-10-20 20:31:59 -0400

    first draft of signal handling module

---
+++ Library/Sources/Stroika/Foundation/Execution/Signals.cpp
@@ -0,0 +1,173 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#include	"../StroikaPreComp.h"
+
+#include	<map>
+
+#include	"CriticalSection.h"
+
+#include	"Signals.h"
+
+
+
+using	namespace	Stroika::Foundation;
+using	namespace	Stroika::Foundation::Execution;
+
+
+
+namespace	{
+	typedef	SignalHandlerRegistry::SignalIDType			SignalIDType;
+	typedef	SignalHandlerRegistry::SignalHandlerType	SignalHandlerType;
+
+	CriticalSection	sCritSection_;
+
+	bool										sInstalled_		=	false;
+	map<SignalIDType,set<SignalHandlerType>>	sHandlers_;
+
+	bool	IsSigIgnore_ (const set<SignalHandlerType>& sigSet)
+		{
+			return sigSet.size () == 1 and *sigSet.begin () == SignalHandlerRegistry::kIGNORED;
+		}
+
+	void	MyHandler_ (int signal)
+		{
+			set<SignalHandlerType>	handlers;
+			{
+				AutoCriticalSection critSec (sCritSection_);
+				map<SignalIDType,set<SignalHandlerType>>::const_iterator i = sHandlers_.find (signal);
+				Assert (i != sHandlers_.end ());
+				handlers = i->second;
+			}
+			for (set<SignalHandlerType>::const_iterator i = handlers.begin (); i != handlers.end (); ++i) {
+				(*i) (signal);
+			}
+		}
+}
+
+
+
+
+
+/*
+ ********************************************************************************
+ ******************** Execution::SignalHandlerRegistry **************************
+ ********************************************************************************
+ */
+
+const	SignalHandlerRegistry::SignalHandlerType	SignalHandlerRegistry::kIGNORED	=	SIG_IGN;
+
+SignalHandlerRegistry&	SignalHandlerRegistry::Get ()
+{
+	static	SignalHandlerRegistry	sThe_;
+	return sThe_;
+}
+
+SignalHandlerRegistry::SignalHandlerRegistry ()
+{
+}
+
+void	SignalHandlerRegistry::Install ()
+{
+	AutoCriticalSection critSec (sCritSection_);
+	Require (not sInstalled_);
+	sInstalled_ = true;
+	for (map<SignalIDType,set<SignalHandlerType>>::const_iterator i = sHandlers_.begin (); i != sHandlers_.end (); ++i) {
+		if (IsSigIgnore_ (i->second)) {
+			signal (i->first, SIG_IGN);
+		}
+		else {
+			signal (i->first, MyHandler_);
+		}
+	}
+}
+
+void	SignalHandlerRegistry::Uninstall ()
+{
+	AutoCriticalSection critSec (sCritSection_);
+	Require (sInstalled_);
+	sInstalled_ = false;
+	for (map<SignalIDType,set<SignalHandlerType>>::const_iterator i = sHandlers_.begin (); i != sHandlers_.end (); ++i) {
+		signal (i->first, SIG_DFL);
+	}
+}
+
+bool	SignalHandlerRegistry::Installed () const
+{
+	return sInstalled_;
+}
+
+set<SignalHandlerRegistry::SignalIDType>	SignalHandlerRegistry::GetHandledSignals () const
+{
+	set<SignalHandlerRegistry::SignalIDType>	result;
+	{
+		AutoCriticalSection critSec (sCritSection_);
+		for (map<SignalIDType,set<SignalHandlerType>>::const_iterator i = sHandlers_.begin (); i != sHandlers_.end (); ++i) {
+			result.insert (i->first);
+		}
+	}
+	return result;
+}
+
+set<SignalHandlerType>	SignalHandlerRegistry::GetSignalHandlers (SignalIDType signal) const
+{
+	AutoCriticalSection critSec (sCritSection_);
+	map<SignalIDType,set<SignalHandlerType>>::const_iterator i = sHandlers_.find (signal);
+	if (i == sHandlers_.end ()) {
+		return set<SignalHandlerType> ();
+	}
+	else {
+		return i->second;
+	}
+}
+
+void	SignalHandlerRegistry::SetSignalHandlers (SignalIDType signal)
+{
+	SetSignalHandlers (signal, set<SignalHandlerType> ());
+}
+
+void	SignalHandlerRegistry::SetSignalHandlers (SignalIDType signal, SignalHandlerType handler)
+{
+	SetSignalHandlers (signal, set<SignalHandlerType> (&handler, &handler + 1));
+}
+
+void	SignalHandlerRegistry::SetSignalHandlers (SignalIDType signal, const set<SignalHandlerType>& handlers)
+{
+	AutoCriticalSection critSec (sCritSection_);
+	map<SignalIDType,set<SignalHandlerType>>::iterator i = sHandlers_.find (signal);
+	if (i == sHandlers_.end ()) {
+		if (not handlers.empty ()) {
+			sHandlers_.insert (map<SignalIDType,set<SignalHandlerType>>::value_type (signal, handlers));
+		}
+	}
+	else {
+		i->second = handlers;
+	}
+	if (sInstalled_) {
+		if (handlers.empty ()) {
+			// nothing todo - empty list treated as not in sHandlers_ list
+			::signal (signal, SIG_DFL);
+		}
+		else if (IsSigIgnore_ (handlers)) {
+			::signal (signal, SIG_IGN);
+		}
+		else {
+			::signal (signal, MyHandler_);
+		}
+	}
+}
+
+void	SignalHandlerRegistry::AddSignalHandler (SignalIDType signal, SignalHandlerType handler)
+{
+	set<SignalHandlerType>	s	=	GetSignalHandlers (signal);
+	s.insert (handler);
+	SetSignalHandlers (signal, s);
+}
+
+void	SignalHandlerRegistry::RemoveSignalHandler (SignalIDType signal, SignalHandlerType handler)
+{
+	set<SignalHandlerType>	s	=	GetSignalHandlers (signal);
+	Require (s.find (handler) != s.end ());
+	s.erase (handler);
+	SetSignalHandlers (signal, s);
+}
---
+++ Library/Sources/Stroika/Foundation/Execution/Signals.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
+ */
+#ifndef	_Stroika_Foundation_Execution_Signals_h_
+#define	_Stroika_Foundation_Execution_Signals_h_	1
+
+#include	"../StroikaPreComp.h"
+
+#include	<csignal>
+#include	<set>
+
+#include	"../Configuration/Common.h"
+
+/*
+ * Description:
+ *
+ *		This module defines support for POSIX (and std c++ defined) Signals (not to be confused with the 'Signals and slots'
+ *	design pattern which is largely unrelated).
+ *	
+ *
+ */
+
+
+/*
+ * TODO:
+ *		THINK OUT AND DESCRIBE IN DETAIL HOW WE HANDLE THREADS
+ */
+
+namespace	Stroika {	
+	namespace	Foundation {
+		namespace	Execution {
+
+
+			/*
+			 *	Description:
+			 *		SignalHandlerRegistry is a singleton object. If used - it itself registers signal handlers for each supported signal.
+			 *
+			 *		The user can then add in their own 'handlers' for those signals, and they are ALL called - one after the other (TDB how threads
+			 *		work with this).
+			 *
+			 *		When an platform signal-handler is installed (via 'sigaction' for example) - and then later UNINSTALLED (due to Uninstall or
+			 *		changes in GetHandledSignals - this code resets the signal handler to SIG_DFL (not the previous value).
+			 *
+			 *		This object is thread-safe.
+			 */
+			class	SignalHandlerRegistry {
+				public:
+					typedef	int		SignalIDType;
+					typedef	void	(*SignalHandlerType) (SignalIDType);
+
+					/*
+					 * If this handler is set to the the ONLY handler for a given signal, then that signal handler is effectively ignored.
+					 *
+					 * To get the signal to be handled the DEFAULT way - remove all signal handlers.
+					 */
+					static	const	SignalHandlerType	kIGNORED;
+				
+				public:
+					// Access singleton implementation. None exists until this is called.
+					static	SignalHandlerRegistry&	Get ();
+
+				private:
+					SignalHandlerRegistry ();
+					NO_COPY_CONSTRUCTOR (SignalHandlerRegistry);
+					NO_ASSIGNMENT_OPERATOR (SignalHandlerRegistry);
+
+				public:
+					/*
+					 * Because this mechanism can 'interfere' with other application of the POSIX signal API, use requires you
+					 * explicitly call Install (or uninstall).
+					 *
+					 * This ONLY installs handlers for the signals returned from GetHandledSignals ().
+					 *
+					 * Illegal to call if already Installed ().
+					 */
+					nonvirtual	void	Install ();
+					/*
+					 * See Install (). Illegal to call if not Installed ();
+					 */
+					nonvirtual	void	Uninstall ();
+
+					/*
+					 * Returns true if Installed has been called more times than Uninstall ();
+					 */
+					nonvirtual	bool	Installed () const;
+
+				public:
+					/*
+					 * Returns the set of signals trapped by the SignalHandlerRegistry registry. Note - if not 'Installed ()' - these 
+					 * are tracked internally by Stroika but not actually installed in the OS.
+					 */
+					nonvirtual	set<SignalIDType>	GetHandledSignals () const;
+
+				public:
+					/*
+					 * Returns the set of signals trapped by the SignalHandlerRegistry registry. This doesn't imply there is a handler.
+					 * NB: A signal handler must be registered for a given signal number AND the signal number must be in GetHandledSignals () AND
+					 * the SignalHandlerRegistry must be Installed () - to get the signal called.
+					 *
+					 * It is NOT an error to have a signal handler registered for a signal not in the set of GetHandledSignals () - or vice versa.
+					 * Signals in the list of GetHandledSignals() with no handlers are effectively ignored.
+					 */
+					nonvirtual	set<SignalHandlerType>	GetSignalHandlers (SignalIDType signal) const;
+					/*
+					 * See GetSignalHandlers().
+					 *
+					 * SetSignalHandlers () with NO arguments uninstalls all Stroika signal handlers for this signal.
+					 * SetSignalHandlers () with ONE argument makes Stroika take-over the signal handling - and sets the set of hanlders to be
+					 * exactly the one given.
+					 * SetSignalHandlers () with ONE a set of handlers registers all the givne handlers.
+					 *
+					 * Note - if through ANY combination of set/add/remvoe - you have NO signal handler - this reverts to SIG_DFL, and if you have
+					 * exactly ONE signal handler - and its kIGNORED- the signal will be ignored.
+					 */
+					nonvirtual	void					SetSignalHandlers (SignalIDType signal);
+					nonvirtual	void					SetSignalHandlers (SignalIDType signal, SignalHandlerType handler);
+					nonvirtual	void					SetSignalHandlers (SignalIDType signal, const set<SignalHandlerType>& handlers);
+					/*
+					 * See GetSignalHandlers()
+					 */
+					nonvirtual	void					AddSignalHandler (SignalIDType signal, SignalHandlerType handler);
+					/*
+					 * See GetSignalHandlers()
+					 */
+					nonvirtual	void					RemoveSignalHandler (SignalIDType signal, SignalHandlerType handler);
+			};
+
+		}
+	}
+}
+#endif	/*_Stroika_Foundation_Execution_Signals_h_*/
+
+
