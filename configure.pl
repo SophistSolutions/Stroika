1462d1b8a4d8488f95acd4da30d0cb91ec826d9a
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-11-02 21:15:44 -0400

    Execution::User module for Windows/POSIX and part portable

--- configure.pl
+++ configure.pl
@@ -183,6 +183,7 @@ sub MakeUnixDirs {
 		mkDirWithLinks2("Foundation/DataExchangeFormat/XML", "Makefile-Foundation-DataExchangeFormat-XML");
 		mkDirWithLinks("Foundation/Debug", "Makefile-Foundation-Debug");
 		mkDirWithLinks("Foundation/Execution", "Makefile-Foundation-Execution");
+		mkdir "$intermediateFiles/$platform/$target/Library/Foundation/Execution/Platform";
 		mkDirWithLinks3("Foundation/Execution/Platform/POSIX", "Makefile-Foundation-Execution-Platform-POSIX");
 		mkDirWithLinks("Foundation/IO", "Makefile-Foundation-IO");
 		mkDirWithLinks2("Foundation/IO/FileSystem", "Makefile-Foundation-IO-FileSystem");
