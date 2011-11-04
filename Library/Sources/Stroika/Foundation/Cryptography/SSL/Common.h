25a8216bcac3fbed9acf06af50656bdf83d3d526
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-11-03 23:31:44 -0400

    SSL comments

--- Library/Sources/Stroika/Foundation/Cryptography/SSL/Common.h
+++ Library/Sources/Stroika/Foundation/Cryptography/SSL/Common.h
@@ -11,6 +11,16 @@
 
 #include	"../../Configuration/Common.h"
 
+/*
+ * TODO:
+ *		o	MAYBE impleemnt SSLStream class- using 'memory-based' BIO. Supposedly that possible. Not totally clear.
+ *			(i think even with it - it maybe still best to use SSLSocket - cuz some stuff stuff seems to require using
+ *			socket layer directly).
+ *
+ *		o	http://www.rtfm.com/openssl-examples/
+ *
+ *
+ */
 
 namespace	Stroika {	
 	namespace	Foundation {
