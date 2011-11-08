3d0cfa54aa8e77d9d14a0b3aabc01ec9b1a1806a
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-11-08 17:45:22 -0500

    comments

--- Library/Sources/Stroika/Foundation/IO/Network/HTTP/Status.h
+++ Library/Sources/Stroika/Foundation/IO/Network/HTTP/Status.h
@@ -27,15 +27,29 @@ namespace	Stroika {
 
 
 					typedef	unsigned int	Status;
+					/*
+					 * See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html for details on these status codes.
+					 */
 					namespace	StatusCodes {
 						const	Status	kOK					=	200;	
 						const	Status	kMovedPermanently	=	301;
 						const	Status	kNotModified		=	304;
+						/*
+						 * Bad CLIENT request.
+						 * The request could not be understood by the server due to malformed syntax.
+						 * The client SHOULD NOT repeat the request without modifications.
+						 */
 						const	Status	kBadRequest			=	400;	
 						const	Status	kUnauthorized		=	401;	
 						const	Status	kNotFound			=	404;
+						/*
+						 * This is principally for internal server exceptions.
+						 */
 						const	Status	kInternalError		=	500;
-						const	Status	kServiceUnavailable	=	503;	//	indicates that the server is temporarily unavailable, usually due to high load or maintenance.
+						/*
+						 * Tndicates that the server is temporarily unavailable, usually due to high load or maintenance.
+						 */
+						const	Status	kServiceUnavailable	=	503;
 					}
 
 
