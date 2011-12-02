e1843ab602e15e1234468ccd2048fdda1ec529fc
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-12-01 19:12:59 -0500

    Comments about things to do (eventually) on Database side

--- Library/Sources/Stroika/Foundation/Database/ODBCClient.h
+++ Library/Sources/Stroika/Foundation/Database/ODBCClient.h
@@ -20,6 +20,39 @@ namespace	Stroika {
 		namespace	Database {
 
 
+
+
+			/*
+			 * TODO:
+			 *			ADO + ORM!!!
+			 *
+			 *		o	First - in the Stroika Foundation - add a layer - like ADO - which provides database connectivity, and simple variant-record objects
+			 *			to return values of rows from queries, and to provide safe arguments to queries (auto quoting etc).
+			 *			> This layer must ALSO define METACLASS inforamtion about a target database. It need not (at least initially) provide and modifciation
+			 *				functions, but must be able to read back table structure and types of fields etc.
+			 *
+			 *
+			 *		o	THEN - as a totally separate (layered on top) matter, provide a FRAMEWORK LAYER "ORM" - which provides mapping to C++ objects, in a
+			 *			maner vaguely similar to SQLAlchemy. 
+			 *			>	Define MAPPERS - which specify a correspondence between C++ struct and database 'variant objects' from above.
+			 *
+			 *			>	Use virtual class with SharedByValue<> rep popinters to actual C++ objects, so C++-side objects all GC'd, and can be constructed by
+			 *				mapping layer.
+			 *
+			 *			>	Avoid circular dependencies by having 'set' relationships managed by central object (or faked trough mtohod calls on smart object),
+			 *				but not throgh direct pointers (just ids).
+			 *
+			 *			>	SharedByValue<> important, for update semantics - cuz core mapping layer can cache/store orig objects whcih can be copied, and then 'updated'
+			 *				by save operation.
+			 *
+			 *			>	COULD autogenerate templates from SQL schema, but also can use user-provided ones to generate a schema, or 'hook up' and dynamcially gneerate
+			 *				runtime error of C++ template specification of related types doesn't match that read back from DB dynamically.
+			 *
+			 *
+			 */
+
+
+
 /*
 @CONFIGVAR:		qHasLibrary_ODBC
 @DESCRIPTION:	<p>Defines if Stroika is built supporting ODBC (only do if ODBC headers in -I path)/p>
