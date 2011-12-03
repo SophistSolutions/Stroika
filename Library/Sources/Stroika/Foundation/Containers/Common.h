d9bd9520f98953488108a77f80f2da9c287ad87b
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2011-12-03 12:45:10 -0500

    Comments

--- Library/Sources/Stroika/Foundation/Containers/Common.h
+++ Library/Sources/Stroika/Foundation/Containers/Common.h
@@ -26,7 +26,7 @@ namespace	Stroika {
 
 			/*
 			 * Take the given value and map it to -1, 0, 1 - without any compiler warnings. Handy for 32/64 bit etc codiing when you maybe comparing
-			 * differnt sized values and just returning an int, but don't wnat the warnings about overflow etc.
+			 * different sized values and just returning an int, but don't want the warnings about overflow etc.
 			 */
 			template	<typename FROM_INT_TYPE, typename TO_INT_TYPE>
 				TO_INT_TYPE	CompareResultNormalizeHelper (FROM_INT_TYPE f);
