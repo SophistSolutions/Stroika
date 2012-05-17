9cffcfcd54164e9f3a40814828b5770078d746e5
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-05-17 00:23:46 -0400

    first draft of RunAstyle.pl script - to help maintain common format of Stroika sources

---
+++ RunAstyle.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+my $runFile;
+
+my $aStyleArgs = "";
+
+
+#$aStyleArgs .= "--style=stroustrup ";
+#$aStyleArgs .= "--style=whitesmith ";
+$aStyleArgs .= "--style=kr ";
+
+$aStyleArgs .= "--convert-tabs ";
+$aStyleArgs .= "--indent-namespaces ";
+$aStyleArgs .= "--indent-cases ";
+$aStyleArgs .= "--indent-switches ";
+$aStyleArgs .= "--indent-preprocessor ";
+$aStyleArgs .= "--pad-oper ";
+$aStyleArgs .= "--break-closing-brackets ";
+$aStyleArgs .= "--keep-one-line-blocks ";
+$aStyleArgs .= "--indent=spaces ";
+
+$aStyleArgs .= "--align-pointer=type ";
+$aStyleArgs .= "--align-reference=type ";
+$aStyleArgs .= "--mode=c ";
+$aStyleArgs .= "--suffix=none ";
+
+
+
+
+foreach $i (@ARGV) {
+	$runFile = $i;
+}
+
+if ($runFile == "") {
+	system ("astyle $aStyleArgs --recursive Library/*.cpp Library/*.h Library/*.inl Tools/*.cpp Tools/*.h Tools/*.inl Tests/*.cpp Tests/*.h Tests/*.inl");
+}
+else {
+	system ("astyle $aStyleArgs < $runFile > $runFile.XXX");
+	system ("mv $runFile.XXX $runFile");
+}
+
+
+
+
+
