3f236763265230415f03585c1c983dd017941572
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-05-23 21:54:00 -0400

    Cleanup docs on PTHREAD impl (condition variable) for Event objects. I now have a decent udnerstanding - of why it doesn't deadlock

--- Library/Sources/Stroika/Foundation/Execution/Event.cpp
+++ Library/Sources/Stroika/Foundation/Execution/Event.cpp
@@ -10,11 +10,33 @@ using   namespace   Stroika::Foundation;
 using   namespace   Stroika::Foundation::Execution;
 
 
+
+/*
+ * Design notes:
+ *
+ *      o   The use of condition variables is non-obvious. I haven't found good documentation, but
+ *          the best I've found would be
+ *              https://computing.llnl.gov/tutorials/pthreads/#ConVarOverview
+ *
+ *          In particular, on the surface, it looks like the mutex locks in Wait() and signal should prevent things
+ *          form working (deadlock). But they apparently do not cause a deadlock because
+ *              "pthread_cond_wait() blocks the calling thread until the specified condition is signalled. 
+ *               This routine should be called while mutex is locked, and it will automatically release the
+ *               mutex while it waits. After signal is received and thread is awakened, mutex will be
+ *               automatically locked for use by the thread. The programmer is then responsible for
+ *               unlocking mutex when the thread is finished with it."
+ *
+ */
+
+
 /*
  * TODO:
- *          o   POSIX/C++ code below on wiat is a HORRIBLE KLUGE. Unclear if it was a red-herring or
+ *          o   POSIX/C++ code below on wait is a bit of a KLUGE. Unclear if it was a red-herring or
  *              osmething like that needed. Review once threading stuff stable.
  *                  -- LGP 2011-10-27
+ *              NB: its been working and stable for quite a while. It could use cleanup/docs (working on that).
+ *              and the timeout part maybe wrong (esp with signals). But it appears to mostly be
+ *              correct.
  */
 
 
