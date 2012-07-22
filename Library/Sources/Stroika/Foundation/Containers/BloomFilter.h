f654c4964c3462263001b6d2f27495a1e7735cff
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-07-21 20:20:46 -0400

    ideas/cleanups from KDJ

---
+++ Library/Sources/Stroika/Foundation/Containers/BloomFilter.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2012.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Containers_BloomFilter_h_
+#define _Stroika_Foundation_Containers_BloomFilter_h_   1
+
+
+/*
+ *
+ * Description:
+ *		http://en.wikipedia.org/wiki/Bloom_filter
+
+ *			KDJ suggested adding these - very useufl. Not totally clear it belongs under Containers.
+ *			MAYBE add new section "ALGORITHMS"  -or maybe have new section "CACHES" - and move LRUCache stuff
+ *			there along with this BloomFilter stuff...
+ *
+ * TODO:
+ *
+ * Notes:
+ *
+ *
+ *
+ */
+
+
+#include    "../StroikaPreComp.h"
+
+#include    "../Configuration/Common.h"
+#include    "../Memory/SharedByValue.h"
+
+#include    "Iterator.h"
+
+
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Containers {
+
+
+        }
+    }
+}
+
+
+#endif  /*_Stroika_Foundation_Containers_BloomFilter_h_ */
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+//#include    "Bag.inl"
+
