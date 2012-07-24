0259acd326998f9c57a1a5240de6acf967229eb0
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-07-23 20:05:49 -0400

    draft (placeholder) implemenation of wrappers for Stack, Mapping, Sortable, Queue, PriorityQueue, Deque, etc...

---
+++ Library/Sources/Stroika/Foundation/Containers/Deque.inl
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2012.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Containers_Deque_inl_
+#define _Stroika_Foundation_Containers_Deque_inl_
+
+#include    "../Debug/Assertions.h"
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Containers {
+
+        }
+    }
+}
+
+
+
+#endif /* _Stroika_Foundation_Containers_Deque_inl_ */
---
+++ Library/Sources/Stroika/Foundation/Containers/Mapping.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2012.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Containers_Mapping_h_
+#define _Stroika_Foundation_Containers_Mapping_h_  1
+
+/*
+ *
+ *
+ *
+ *  TODO:
+ *
+ *		(o)         Implement first draft of code based on
+ *					http://github.com/SophistSolutions/Stroika/blob/master/Archive/Stroika_FINAL_for_STERL_1992/Library/Foundation/Headers/Mapping.hh
+ *
+ *
+ */
+
+
+#include    "../StroikaPreComp.h"
+
+#include    "../Configuration/Common.h"
+#include    "../Memory/SharedByValue.h"
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Containers {
+
+
+        }
+    }
+}
+
+#endif  /*_Stroika_Foundation_Containers_Mapping_h_ */
+
+
+
+/*
+ ********************************************************************************
+ ******************************* Implementation Details *************************
+ ********************************************************************************
+ */
+
+#include	"Mapping.inl"
