4f6f8eebc0de28468719c9e4974fc8a0714a6c86
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-08-20 20:15:37 -0400

    Cleanups for Defaults_CompilerAndStdLib_.h

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -11,8 +11,8 @@
  */
 
 
-#if     defined(__cplusplus)
 
+#if     defined(__cplusplus)
 
 
 
@@ -299,7 +299,8 @@
 
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Supports_TemplateUsingEquals
-@DESCRIPTION:   <p>Unclear if my usage is right, and what compiler purports to support it. Definetely not msvc 2010</p>
+@DESCRIPTION:   <p>Unclear if my usage is right, and what compiler purports to support it.
+                Definetely not msvc 2010</p>
 */
 #ifndef qCompilerAndStdLib_Supports_TemplateUsingEquals
 #define qCompilerAndStdLib_Supports_TemplateUsingEquals 0
@@ -387,7 +388,8 @@
 
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Supports_errno_t
-@DESCRIPTION:   <p>Controls whether or not the compiler provides an implementation of errno_t (gcc for example just assumes its int)</p>
+@DESCRIPTION:   <p>Controls whether or not the compiler provides an implementation
+                of errno_t (gcc for example just assumes its int)</p>
 */
 #if     !defined (qCompilerAndStdLib_Supports_errno_t)
 
@@ -468,6 +470,7 @@
 
 
 
+
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Supports_stdatomic
 @DESCRIPTION:   <p>Controls whether or not the compiler the override function annotion (added in C++11).</p>
@@ -481,6 +484,11 @@
 #endif
 
 #endif
+
+
+
+
+
 
 
 
@@ -503,6 +511,13 @@
 
 
 
+
+
+
+
+
+
+
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Supports_lambda_default_argument
 @DESCRIPTION:   <p>Defined true if the compiler supports lamba functions as default arguments to other functions</p>
@@ -516,6 +531,7 @@
 #endif
 
 #endif
+
 
 
 
@@ -551,6 +567,8 @@
 
 
 
+
+
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Support__PRETTY_FUNCTION__
 @DESCRIPTION:   <p>FOR ASSERT</p>
@@ -566,6 +584,9 @@
 #endif
 
 
+
+
+
 /*
 @CONFIGVAR:     qCompilerAndStdLib_Support__func__
 @DESCRIPTION:   <p>FOR ASSERT</p>
@@ -579,6 +600,9 @@
 #endif
 
 #endif
+
+
+
 
 
 /*
@@ -624,52 +648,62 @@
 
 
 
-// If override is not supported, just stubbing it out but leaving it there for compilers that do support it, and/or documentation purposes
+
+// If override is not supported, just stubbing it out but leaving it there for
+// compilers that do support it, and/or documentation purposes
 #if     !qCompilerAndStdLib_Supports_override
 #define override
 #endif
 
 
+
 /*
-    *   The Standard C++ mechanism of commenting out unused parameters isn't good enuf
-    *   in the case where the parameters might be used conditionally. This hack is
-    *   to shutup compiler warnings in those cases.
-    */
-#ifndef Arg_Unused
+ *   The Standard C++ mechanism of commenting out unused parameters isn't good enuf
+ *   in the case where the parameters might be used conditionally. This hack is
+ *   to shutup compiler warnings in those cases.
+ */
+#if     !defined (Arg_Unused)
+
 #define Arg_Unused(x)   ((void) &x)
+
 #endif
 
 
 
 
+
 /*
-    *   Sometimes its handy to mark a function as not actually returning (because of throws or other reasons)
-    *   This can allow the compiler to occasionally better optimize, but mostly avoid spurrious warnings.
-    */
-#if defined(_MSC_VER)
+ *   Sometimes its handy to mark a function as not actually returning (because of throws or other reasons)
+ *   This can allow the compiler to occasionally better optimize, but mostly avoid spurrious warnings.
+ */
+#if     !defined (_NoReturn_)
+
+#if     defined(_MSC_VER)
 #define _NoReturn_  __declspec(noreturn)
-#elif defined (__GNUG__ )
+#elif   defined (__GNUG__ )
 #define _NoReturn_  __attribute__((noreturn))
 #else
 #define _NoReturn_
 #endif
 
+#endif
+
 
 
 
 /*
-    *   Sometimes its handy to mark a line of code as a no-op - so its arguments are not executed (as with
-    * trace macros).
-    */
+ *   Sometimes its handy to mark a line of code as a no-op - so its arguments are not executed (as with
+ * trace macros).
+ */
+#if     !defined (_NoOp_)
+
 #if defined(_MSC_VER)
 #define _NoOp_  __noop
 #else
 #define _NoOp_(...)
 #endif
 
-
-
-
+#endif
 
 
 
@@ -679,6 +713,7 @@
 
 
 #endif  /*defined(__cplusplus)*/
+
 
 
 
