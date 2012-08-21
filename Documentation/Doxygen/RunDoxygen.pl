f2034cb0d3a9e974b8fd0ea775258e9d53fadf25
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-08-20 23:19:30 -0400

    minor progress on doxygen makefiles

--- Documentation/Doxygen/RunDoxygen.pl
+++ Documentation/Doxygen/RunDoxygen.pl
@@ -3,11 +3,16 @@
 #print ("OS: $^O\n");
 
 my $doxygen = "";
+
+my $latexMake = "";
+
 if ("$^O" eq "linux") {
 	$doxygen = "doxygen";
+	$latexMake = "make";
 }
 if ("$^O" eq "cygwin") {
 	$doxygen = "\'c:/Program Files/doxygen/bin/doxygen.exe\'";
+	$latexMake = "make.bat";
 }
 
 my $doxyArgs = "Stroika-Library.cfg";
@@ -18,7 +23,9 @@ my $doxyArgs = "Stroika-Library.cfg";
 #	$runFile = $i;
 #}
 
+system ("rm -rf IntermediateFiles");
 system ("$doxygen $doxyArgs");
+system ("cd IntermediateFiles && cd latex && $latexMake");
 
 #### # INSTALL LATEX ...
 #### # USE IT TO GENERATE PDF
