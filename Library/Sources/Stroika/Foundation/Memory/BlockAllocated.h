153c569c130ada0899d1190d2d72521089a7a1c5
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-08-31 19:59:54 -0400

    A bit of progress on doxygen help for BlockAllocated<T>

--- Library/Sources/Stroika/Foundation/Memory/BlockAllocated.h
+++ Library/Sources/Stroika/Foundation/Memory/BlockAllocated.h
@@ -13,25 +13,36 @@
 #include    "../Execution/ModuleInit.h"
 
 
-/*
- * TODO:
- *      o   We should either add a VARIANT or template parameter to BlockAllocated<> saying whether we should
- *          force block allocation, or ONLY block-allocate if size is appropriate to one of our preformed pools.
- *          Reason for this option is for better use in templates like LinkedList<> - where we might wnat to blockallocate
- *          for small sizes of T, but not for ALL.
+/**
+ *  \file
+ *
+ *  @todo   We should either add a VARIANT or template parameter to BlockAllocated<> saying whether we
+ *          should force block allocation, or ONLY block-allocate if size is appropriate to one of our
+ *          preformed pools.
+ *
+ *          The reason for this option is for better use in templates like LinkedList<> - where we might
+ *          want to blockallocate for small sizes of T, but not for ALL.
  */
+
 
 namespace   Stroika {
     namespace   Foundation {
         namespace   Memory {
 
 
-            /*
-            @CONFIGVAR:     qAllowBlockAllocation
-            @DESCRIPTION:   <p>Allow use of block-allocation. The main reason to
-                disable it indescriminantly is for debugging purposes (looking for
-                memory leaks). But others may have other reasons.</p>
-                    <p>Defaults to true.</p>
+
+            /**
+             *  \def qAllowBlockAllocation
+             *
+             *  \brief  qAllowBlockAllocation
+             *
+             *  Allow use of block-allocation. The main reason to disable it indescriminantly
+             *  is for debugging purposes (looking for memory leaks). But others may have other
+             *  reasons.
+             *
+             *      Defaults to true
+             *
+             *  \hideinitializer
              */
 #if     !defined (qAllowBlockAllocation)
 #error "qAllowBlockAllocation should normally be defined indirectly by StroikaConfig.h"
@@ -40,19 +51,23 @@ namespace   Stroika {
 
 
 
-            /*
-            @CLASS:         BlockAllocated<T>
-            @DESCRIPTION:   <p><code>BlockAllocated&ltT&gt</code> is a templated class designed to allow easy use
-                of a block-allocated memory strategy. This means zero overhead malloc/memory allocation for fixed
-                size blocks (with the only problem being the storage is never - or almost never - returned to the
-                free store - it doesn't leak - but cannot be used for other things). This is often a useful
-                tradeoff for things you allocate a great number of.</p>
-                    <p>You shouldn't disable it lightly. But you may wish to temporarily disable block-allocation
-                while checking for memory leaks by shutting of the @'qAllowBlockAllocation' compile-time configuration variable.</p>
-                    <p>Note also - you can avoid some of the uglines of the overload declarations by using the
-                @'DECLARE_USE_BLOCK_ALLOCATION' macro.</p>
+            /**
+              * \brief  Utility class to implement special memory allocator pattern which can greatly improve performance - @see DECLARE_USE_BLOCK_ALLOCATION()
+              *
+              * BlockAllocated<T> is a templated class designed to allow easy use
+              * of a block-allocated memory strategy. This means zero overhead malloc/memory allocation for fixed
+              * size blocks (with the only problem being the storage is never - or almost never - returned to the
+              * free store - it doesn't leak - but cannot be used for other things). This is often a useful
+              * tradeoff for things you allocate a great number of.
+              *
+              * You shouldn't disable it lightly. But you may wish to temporarily disable block-allocation
+              * while checking for memory leaks by shutting of the @'qAllowBlockAllocation' compile-time configuration variable.
+              *
+              * Note also - you can avoid some of the uglines of the overload declarations by using the
+              * DECLARE_USE_BLOCK_ALLOCATION() macro.
             */
-            template    <typename   T>  class   BlockAllocated {
+            template    <typename   T>
+            class   BlockAllocated {
             public:
                 static  void*   operator new (size_t n);
                 static  void    operator delete (void* p);
@@ -70,22 +85,29 @@ namespace   Stroika {
             };
 
 
-            /*
-            @CLASS:         DECLARE_USE_BLOCK_ALLOCATION
-            @DESCRIPTION:   <p>This helper macro can be used to avoid some of the C++ gorp required in declaring that you are
-                using block-allocation with a given class.</p>
-                    <p>An example of use might be:
-                <code>
-                <pre>
-                    class   Foo {
-                        public:
-                            DECLARE_USE_BLOCK_ALLOCATION(Foo);
-                    }
-                </pre>
-                </code>
-                    </p>
-                    <p>See also - @'DECLARE_DONT_USE_BLOCK_ALLOCATION' and @'BlockAllocated<T>'.</p>
-            */
+            /**
+             * \def DECLARE_USE_BLOCK_ALLOCATION(THIS_CLASS)
+             *
+             *  \brief  Use this to enable block allocation for a particular class. *Beware* of subclassing.
+             *
+             *  This helper macro can be used to avoid some of the C++ gorp required in declaring that you are
+             *  using block-allocation with a given class.
+             *
+             *  An example of use might be:
+             *  <code>
+             *  <pre>
+             *      class   Foo {
+             *          public:
+             *              DECLARE_USE_BLOCK_ALLOCATION(Foo);
+             *      }
+             *  </pre>
+             *  </code>
+             *
+             *  @see DECLARE_DONT_USE_BLOCK_ALLOCATION()
+             *  @see BlockAllocated
+             *
+             *  \hideinitializer
+             */
 #if     qAllowBlockAllocation
 #define DECLARE_USE_BLOCK_ALLOCATION(THIS_CLASS)\
     static  void*   operator new (size_t n)                         {   return (Stroika::Foundation::Memory::BlockAllocated<THIS_CLASS>::operator new (n)); }\
@@ -96,12 +118,34 @@ namespace   Stroika {
 #define DECLARE_USE_BLOCK_ALLOCATION(THIS_CLASS)
 #endif
 
-            /*
-            @CLASS:         DECLARE_DONT_USE_BLOCK_ALLOCATION
-            @DESCRIPTION:   <p>If you subclass from a class which uses block-allication, you can use this to turn off block allocation
-                in your particular subclass.</p>
-                    <p>See also @'DECLARE_USE_BLOCK_ALLOCATION' and @'BlockAllocated<T>'.</p>
-            */
+
+            /**
+             *  \def    DECLARE_DONT_USE_BLOCK_ALLOCATION(THIS_CLASS)
+             *
+             *  \brief  Use this to disable block allocation for a subclass of a class that HAD been using block allocation.
+             *
+             *  If you subclass from a class which uses block-allocation, you can use this to turn off block allocation
+             *  in your particular subclass.</p>
+             *
+             *
+             *  An example of use might be:
+             *  <code>
+             *  <pre>
+             *      class   Foo {
+             *          public:
+             *              DECLARE_USE_BLOCK_ALLOCATION(Foo);
+             *      };
+             *      class   Bar : public Foo {
+             *          public:
+             *              DECLARE_DONT_USE_BLOCK_ALLOCATION(Bar);
+             *      };
+             *  </pre>
+             *  </code>
+			 *
+             *  @see BlockAllocated
+             *
+             *  \hideinitializer
+             */
 #if     qAllowBlockAllocation
 #define DECLARE_DONT_USE_BLOCK_ALLOCATION(THIS_CLASS)\
     static  void*   operator new (size_t n)     {   return ::operator new (n);  }\
@@ -109,6 +153,7 @@ namespace   Stroika {
 #else
 #define DECLARE_DONT_USE_BLOCK_ALLOCATION(THIS_CLASS)
 #endif
+
 
         }
     }
