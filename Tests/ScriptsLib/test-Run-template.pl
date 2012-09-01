69ea2680293575d34326ddc1d6d3fb19e32b4ed7
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-09-01 19:06:26 -0400

    silence some noise in test printing (wrapped in $progres arg but never passed in)

--- Tests/ScriptsLib/test-Run-template.pl
+++ Tests/ScriptsLib/test-Run-template.pl
@@ -6,10 +6,13 @@ require "../../../ScriptsLib/TestsList.pl";
 sub DoRun {
 	my $testNumber = $_[0];
 	my $testName = GetTestName ($testNumber);
+	my $progress = $_[1];
 
 	local $date = `sh -c date`;
 	chomp ($date);
-	print "Starting regression test #[$testNumber] $testName Test: $date ------------------------------\r\n";
+	if ($progress) {
+		print "Starting regression test #[$testNumber] $testName Test: $date ------------------------------\r\n";
+	}
 	
 	if ("$^O" eq "linux") {
 		DoRunSimpleTestArgv ($ARGV[0], "[$testNumber] $testName", "../../../../Builds/Platform_Linux/", "Test$testNumber");
@@ -18,7 +21,9 @@ sub DoRun {
 		DoRunSimpleTestArgv ($ARGV[0], "[$testNumber] $testName", "../../../../Builds/Windows/", "Test$testNumber/Test$testNumber.exe");
 	}
 	
-	local $date = `sh -c date`;
-	chomp ($date);
-	print "End of Regression Test #[$testNumber] $testName Test: $date -------------------------------\r\n";
+	if ($progress) {
+		local $date = `sh -c date`;
+		chomp ($date);
+		print "End of Regression Test #[$testNumber] $testName Test: $date -------------------------------\r\n";
+	}
 }
