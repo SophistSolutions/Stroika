2bc5a366bbb9bfa24963cf8d00520fb0a449b6d0
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-09-01 21:22:01 -0400

    Streams comments

--- Library/Sources/Stroika/Foundation/Streams/Seekable.h
+++ Library/Sources/Stroika/Foundation/Streams/Seekable.h
@@ -37,26 +37,36 @@ namespace   Stroika {
             /**
              * Design Overview:
              *
-             *      o   Designed to be mixed with BinaryInputStream, BinaryOutputStream, TextInputStream, or TextOutputStream.
+             *      o   Designed to be mixed with BinaryInputStream, BinaryOutputStream, TextInputStream,
+			 *			or TextOutputStream.
              *
-             *      o   It should be mixed in as a virtual base, so an IO stream shares a COMMON notion of current offset. Note - this choice differs from iostreams, but mirrors
+             *      o   It should be mixed in as a virtual base, so an IO stream shares a COMMON notion
+			 *			of current offset. Note - this choice differs from iostreams, but mirrors
              *          the choice in UNIX fileio ('read and write pointers the same).
              *
-             *      o   If called on an input stream that has already returned EOF, may cause subsequence Read() calls to NOT be at EOF.
+             *      o   If called on an input stream that has already returned EOF, may cause
+			 *			subsequence Read() calls to NOT be at EOF.
              *
-             *      o   Offsets are in whatever unit makes sense for the kind of stream this is mixed into, so for TextInputStreams, offsets are in CHARACTERS
-             *          and in Binary streams, offsets are in Bytes. This implies one CANNOT reasonably mix together Binary streams and Text streams (one combines them
-             *          by use of a special TextStream that refers to another BinaryStream for actual IO).
+             *      o   Offsets are in whatever unit makes sense for the kind of stream this is mixed into,
+			 *			so for TextInputStreams, offsets are in CHARACTERS and in Binary streams, offsets
+			 *			are in Bytes. This implies one CANNOT reasonably mix together Binary streams and Text
+			 *			streams (one combines them by use of a special TextStream that refers to another
+			 *			BinaryStream for actual IO).
              *
-             *      o   BinaryInputStream and BinaryOutputStream CAN be naturally mixed togehter to make an input/output stream. Similarly, they can both be
-             *          mixed together with Seekable. But NONE of the Binary*Stream classes may be mixed together with Text*Stream classes.
+             *      o   BinaryInputStream and BinaryOutputStream CAN be naturally mixed togehter to make
+			 *			an input/output stream. Similarly, they can both be mixed together with Seekable.
+			 *			But NONE of the Binary*Stream classes may be mixed together with Text*Stream classes.
              *
-             *      o   Considered doing something like .Net CanSeek (), CanRead(), etc, but I decided it was simpler and more elegant to just keep things separate and
-             *          use mixin classes like 'interfaces' - and use dynamic_cast<> to see what functionality is available on a stream.
+             *      o   Considered doing something like .Net CanSeek (), CanRead(), etc, but I decided it
+			 *			was simpler and more elegant to just keep things separate and use mixin classes
+			 *			like 'interfaces' - and use dynamic_cast<> to see what functionality is available
+			 *			on a stream.
              *
-             *      o   COULD have allowed for GetOffset() to work separately from Seek () - but decided this was a simpler API, and I could think of no
-             *          cases where it was particularly useful to track a 'seek offset' but not be able to Seek() - and if it is useful - its easy
-             *          enuf to do another way (e.g. wrap the stream you are using, and track read calls to it and increment your own offset).
+             *      o   COULD have allowed for GetOffset() to work separately from Seek () - but decided
+			 *			this was a simpler API, and I could think of no cases where it was particularly
+			 *			useful to track a 'seek offset' but not be able to Seek() - and if it is useful -
+			 *			its easy enuf to do another way (e.g. wrap the stream you are using, and track
+			 *			read calls to it and increment your own offset).
              */
             class   Seekable {
             public:
