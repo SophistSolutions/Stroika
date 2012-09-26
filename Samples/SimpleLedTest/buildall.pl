189dba32c209a55be66c2bb7d260718a88bc85c3
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-09-26 09:07:30 -0400

    fixed minor script typo

--- Samples/SimpleLedTest/buildall.pl
+++ Samples/SimpleLedTest/buildall.pl
@@ -11,7 +11,7 @@ my @kConfigurations = (
 
 
 print("Building Samples/SimpleLedTest...\n");
-if ("^0" eq "cygwin") {
+if ("$^O" eq "cygwin") {
 	foreach (@kConfigurations) {
 		my $curConfig	=	$_;
 		my $extraArgs = GetMSBuildArgs();
