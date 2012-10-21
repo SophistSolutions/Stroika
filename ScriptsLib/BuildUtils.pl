d6c209f825b4d4d73d992d2c47cdb7396e0822a2
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-10-21 18:45:46 -0400

    new helper script - /ScriptsLib/BuildUtils.pl - maybe some shared code

---
+++ ScriptsLib/BuildUtils.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl 
+
+sub	GetThisScriptDir {
+	use File::Basename;
+	use Cwd 'abs_path';
+	my $p = __FILE__;
+	my $A = abs_path ($p);
+	my $dirname = dirname($A);
+	return $dirname;
+}
+my	$thisScriptDir	=	GetThisScriptDir ();
+
+require "$thisScriptDir/ConfigurationReader.pl";
+
+
+my $useProjectDir= "$thisScriptDir/../Library/Projects/" . GetProjectPlatformSubdir ();
+if (-e "$useProjectDir/SetupBuildCommonVars.pl") {
+	require "$useProjectDir/SetupBuildCommonVars.pl";
+}
+
+
+
+
+
+
+sub	GetMSBuildArgs {
+	my $EXTRA_MSBUILD_ARGS = "/nologo /v:quiet /clp:Summary";
+	return $EXTRA_MSBUILD_ARGS;
+}
+
+
+
+sub	NormalizeBuildArg {
+	my $useBld =	$_[0];
+	if ($useBld eq '') {
+		$useBld = 'Build';
+	}
+	if (lc ($useBld) eq "clobber") {
+		$useBld = "Clean";
+	}
+	return $useBld;
+}
+
+
+sub RunAndPrint
+{
+	my $cmd2Run = $_[0];
+	print ("$cmd2Run...\n");
+	my $result = system ($cmd2Run);
+	if ($result != 0) {
+		print "Run result = $result\r\n";
+	}
+}
+
+1
