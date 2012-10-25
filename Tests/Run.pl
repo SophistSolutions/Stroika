a77d5f6ae6286f1ea4270ce6a3261b6115833955
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-10-25 08:54:45 -0400

    fixed another script to use GetProjectPlatformSubdir ()

--- Tests/Run.pl
+++ Tests/Run.pl
@@ -1,21 +1,16 @@
 #!/usr/bin/perl 
 
+require "../ScriptsLib/ConfigurationReader.pl";
 require ("ScriptsLib/TestsList.pl");
 
 print ("Running Tests...\n");
 
-my $checkallDir;
-if ("$^O" eq "linux") {
-    $checkallDir  = "Projects/Linux/";
-}
-else {
-    $checkallDir  = "Projects/VisualStudio.Net-2010/";
-}
+my $runAllDir	= "Projects/" . GetProjectPlatformSubdir ();
 
 use Cwd;
 use Cwd 'abs_path';
 my $savedDir = abs_path (getcwd ());
-chdir ($checkallDir);
+chdir ($runAllDir);
 	foreach $tst (GetAllTests ()) {
 		system ("cd $tst; perl Run.pl");
 	}
