2ce4b0f2e537fac318c5fbcf47cc529b3714c3dc
Lewis G. Pringle, Jr <lewis@sophists.com>
2012-10-25 23:11:22 -0400

    fix typo in configure script

--- configure.pl
+++ configure.pl
@@ -225,7 +225,7 @@ sub MakeUnixDirs {
 
 		mkDirWithLinks("Foundation/Cache", "Makefile-Foundation-Cache");
 		mkDirWithLinks("Foundation/Characters", "Makefile-Foundation-Characters");
-		mkDirWithLinks2("Foundation/Characters/CString", "Makefile-Foundation-Characters-CString);
+		mkDirWithLinks2("Foundation/Characters/CString", "Makefile-Foundation-Characters-CString");
 		mkDirWithLinks("Foundation/Configuration", "Makefile-Foundation-Configuration");
 		mkDirWithLinks("Foundation/Containers", "Makefile-Foundation-Containers");
 		mkDirWithLinks("Foundation/Cryptography", "Makefile-Foundation-Cryptography");
