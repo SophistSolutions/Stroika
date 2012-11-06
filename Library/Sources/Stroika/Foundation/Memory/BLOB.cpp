54fb04e8a3e31ff9e22b7cb1e403fc8bf05054a8
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2012-11-05 21:35:02 -0500

    COmments, and added As<BinaryInputStream>() support to BLOB class

--- Library/Sources/Stroika/Foundation/Memory/BLOB.cpp
+++ Library/Sources/Stroika/Foundation/Memory/BLOB.cpp
@@ -47,16 +47,53 @@ int  Memory::BLOB::compare (const BLOB& rhs) const
     pair<const Byte*, const Byte*>   l =   fRep_->GetBounds ();
     pair<const Byte*, const Byte*>   r =   rhs.fRep_->GetBounds ();
 
-	size_t	lSize = l.second-l.first;
-	size_t	rSize = r.second-r.first;
-	size_t	nCommonBytes = min (lSize, rSize);
-	int tmp = ::memcmp (l.first, r.first, nCommonBytes);
-	if (tmp != 0) {
-		return tmp;
-	}
-	// if tmp is zero, and same size - its really zero. But if lhs shorter than right, say lhs < right
-	if (lSize == rSize) {
-		return 0;
-	}
-	return (lSize < rSize)? -1 : 1;
+    size_t  lSize = l.second - l.first;
+    size_t  rSize = r.second - r.first;
+    size_t  nCommonBytes = min (lSize, rSize);
+    int tmp = ::memcmp (l.first, r.first, nCommonBytes);
+    if (tmp != 0) {
+        return tmp;
+    }
+    // if tmp is zero, and same size - its really zero. But if lhs shorter than right, say lhs < right
+    if (lSize == rSize) {
+        return 0;
+    }
+    return (lSize < rSize) ? -1 : 1;
+}
+
+
+namespace {
+    using namespace Streams;
+    // @todo - add seekability to this helper!!!
+    struct BLOBBINSTREAM_ : BinaryInputStream {
+        BLOBBINSTREAM_ (const Memory::BLOB& b)
+            : BinaryInputStream (_SharedIRep (DEBUG_NEW REP (b))) {
+        }
+        struct REP : BinaryInputStream::_IRep  {
+            REP (const Memory::BLOB& b)
+                : fCur (b.begin ())
+                , fEnd (b.end ()) {
+            }
+            virtual size_t  Read (Byte* intoStart, Byte* intoEnd)  override {
+                RequireNotNull (intoStart);
+                RequireNotNull (intoEnd);
+                Require (intoStart < intoEnd);
+                size_t bytesToRead  =   intoEnd - intoStart;
+                size_t bytesLeft    =   fEnd - fCur;
+                bytesToRead = min (bytesLeft, bytesToRead);
+                memcpy (intoStart, fCur, bytesToRead);
+                fCur += bytesToRead;
+                return bytesToRead;
+            }
+            const Byte* fCur;
+            const Byte* fEnd;
+        };
+    };
+
+}
+
+template    <>
+Streams::BinaryInputStream Memory::BLOB::As () const
+{
+    return BLOBBINSTREAM_ (*this);
 }
--- Library/Sources/Stroika/Foundation/Memory/BLOB.h
+++ Library/Sources/Stroika/Foundation/Memory/BLOB.h
@@ -10,11 +10,17 @@
 #include    <vector>
 
 #include    "../Configuration/Common.h"
+#include    "../Streams/BinaryInputStream.h"
 
 
 
 /**
  * TODO:
+ *
+ *
+ *      @todo   Redo API - so its all based on SeekOffsetType - not size_t. Document that if you exceed
+ *              available in-RAM storage, no biggie - you just throw bad_alloc. But at least you can
+ *              construct and operate on large BLOBs (as streams).
  *
  *      @todo   Add MemoryMappedFileBLOB to Foundation/File section – and have it subclass
  *              BLOB (object slicing) – and have different CTOR, and different virtual Rep
@@ -30,6 +36,12 @@
  *      @todo   Create overloads in the MD5/SHA1 hash code taking BLOBs as arguments.
  *
  *      @todo   Do CTOR that uses iterator start/end not just const Byte* start, const Byte* end.
+ *
+ *      @todo   BLOB::As<BinaryInputStream>() should be seekable
+ *
+ *      @todo   Fix qWierdPrivateBLOBBug
+ *
+ *
  *
  */
 
@@ -54,8 +66,8 @@ namespace   Stroika {
 
 
             /**
-			 *	A BLOB is a read-only binary region of memory. Once a BLOB is constructed, the data inside cannot
-			 *	change.
+             *  A BLOB is a read-only binary region of memory. Once a BLOB is constructed, the data inside cannot
+             *  change.
              */
             class   BLOB {
             public:
@@ -75,28 +87,32 @@ namespace   Stroika {
 
             public:
                 /**
-				 *	Returns true iff the size of the BLOB is zero.
+                 *  Returns true iff the size of the BLOB is zero.
                  */
                 nonvirtual  bool    empty () const;
 
             public:
                 /**
-                 *  Convert BLOB losslessly into a standard C++ type (right now just <vector<Byte>>
+                 *  Convert BLOB losslessly into a standard C++ type (right now just <vector<Byte>, or Streams::BinaryOutputStream>
                  *  supported)
                  */
                 template    <typename   T>
                 nonvirtual  T   As () const;
+                /**
+                 *  Convert BLOB losslessly into a standard C++ type (right now just <vector<Byte>>
+                 *  supported)
+                 */
                 template    <typename   T>
                 nonvirtual  void   As (T* into) const;
 
             public:
                 /**
-				 *
-				 *	Pointers returned by begin(), remain valid for the lifetime of the containing BLOB.
+                 *
+                 *  Pointers returned by begin(), remain valid for the lifetime of the containing BLOB.
                  */
                 nonvirtual  const Byte* begin () const;
                 /**
-				 *	Pointers returned by end(), remain valid for the lifetime of the containing BLOB.
+                 *  Pointers returned by end(), remain valid for the lifetime of the containing BLOB.
                  */
                 nonvirtual  const Byte* end () const;
 
@@ -113,9 +129,9 @@ namespace   Stroika {
 
             public:
                 /**
-				 *	This is like memcmp() - it returns == 0, if the RHS and LHS are the same, and it
-				 *	returns < 0 if the first byte where the two regions differ is less than the first byte
-				 *	of the RHS (where they differ).
+                 *  This is like memcmp() - it returns == 0, if the RHS and LHS are the same, and it
+                 *  returns < 0 if the first byte where the two regions differ is less than the first byte
+                 *  of the RHS (where they differ).
                  */
                 nonvirtual  int      compare (const BLOB& rhs) const;
 
@@ -148,6 +164,11 @@ namespace   Stroika {
             void    BLOB::As (vector<Byte>* into) const;
             template    <>
             vector<Byte> BLOB::As () const;
+
+            template    <>
+            Streams::BinaryInputStream BLOB::As () const;
+
+
 
         }
     }
