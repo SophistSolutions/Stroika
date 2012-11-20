5f996c5118b5eb6480feb73689e78aa55fd800d1
Lewis G. Pringle, Jr <lewis@sophists.com>
2012-11-19 20:59:43 -0500

    Ammended ScriptsLib/ConfigurationReader.pl to include support for arbitrary elements (new ones) in XML Configuration file.

--- ScriptsLib/ConfigurationReader.pl
+++ ScriptsLib/ConfigurationReader.pl
@@ -33,21 +33,80 @@ sub	GetAllOfStringToEndTag($) {
 
 my %configuration = ();
 
+
+sub	ReadValue_ {
+	my $line = shift;
+	my $tag = shift;
+	my $startOff = index($line, $tag);
+	if ($startOff != -1) {
+		my $tmp = substr ($line, $startOff + length($tag));
+		return GetAllOfStringToEndTag ($tmp);
+	}
+	return undef;
+}
+
 sub	ReadConfigFile_ {
 
 	my $myDirName	=	GetThisScriptDir ();
+
+	my @useExtraCDefines;
+	my @useExtraMakeDefines;
 
 	open (FILE, "$masterXMLConfigFile") or die("Unable to open $masterXMLConfigFile");
 	my @data = <FILE>;
 	close(FILE);
 	foreach $line (@data) {
-		my $startOff = index($line, "<ProjectPlatformSubdir>");
-		if ($startOff != -1) {
-			my $tmp = substr ($line, $startOff + length('<ProjectPlatformSubdir>'));
-			$tmp = GetAllOfStringToEndTag ($tmp);
-			$configuration {'ProjectPlatformSubdir'} = $tmp;
+		my $pps = ReadValue_($line, "<ProjectPlatformSubdir>");
+		if (defined $pps) {
+			$configuration {'ProjectPlatformSubdir'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<Platform>");
+		if (defined $pps) {
+			$configuration {'Platform'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<Target>");
+		if (defined $pps) {
+			$configuration {'Target'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<CompilerDriver>");
+		if (defined $pps) {
+			$configuration {'CompilerDriver'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<qHasLibrary_Xerces>");
+		if (defined $pps) {
+			$configuration {'qHasLibrary_Xerces'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<qHasFeature_libcurl>");
+		if (defined $pps) {
+			$configuration {'qHasFeature_libcurl'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<qHasFeature_WinHTTP>");
+		if (defined $pps) {
+			$configuration {'qHasFeature_WinHTTP'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<IncludeDebugSymbtolsInExecutables>");
+		if (defined $pps) {
+			$configuration {'IncludeDebugSymbtolsInExecutables'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<STATIC_LINK_GCCRUNTIME>");
+		if (defined $pps) {
+			$configuration {'STATIC_LINK_GCCRUNTIME'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<STATIC_LINK_GCCRUNTIME>");
+		if (defined $pps) {
+			$configuration {'STATIC_LINK_GCCRUNTIME'} = $pps;
+		}
+		my $pps = ReadValue_($line, "<CDefine>");
+		if (defined $pps) {
+			$useExtraCDefines[@useExtraCDefines] = $pps;
+		}
+		my $pps = ReadValue_($line, "<MakeDefine>");
+		if (defined $pps) {
+			$useExtraMakeDefines[@useExtraMakeDefines] = $var;
 		}
 	}
+	$configuration {'ExtraCDefines'} = $useExtraCDefines;
+	$configuration {'ExtraMakeDefines'} = $useExtraMakeDefines;
 }
 
 
@@ -59,6 +118,11 @@ sub	GetProjectPlatformSubdir {
 		die ("GetProjectPlatformSubdir () EMPTY\n");
 	}
 	return $configuration{'ProjectPlatformSubdir'};
+}
+
+sub	GetConfigurationParameter {
+	my $paramName = shift;
+	return $configuration{$paramName};
 }
 
 
