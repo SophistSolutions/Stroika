bb05bbec7ce10997de0ac97ca908345ccc116927
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-01-03 01:11:16 -0500

    cosmetic

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -689,12 +689,14 @@
 
 
 #ifndef qCompilerAndStdLib_Supports_lambda_default_argument_WITH_closureCvtToFunctionPtrSupported
+
 #if     defined (_MSC_VER)
 #define qCompilerAndStdLib_Supports_lambda_default_argument_WITH_closureCvtToFunctionPtrSupported   (_MSC_VER > _MS_VS_2k12_VER_)
 #else
 #define qCompilerAndStdLib_Supports_lambda_default_argument_WITH_closureCvtToFunctionPtrSupported	qCompilerAndStdLib_Supports_lambda_default_argument
 #endif
-#endif  qCompilerAndStdLib_Supports_lambda_default_argument_WITH_closureCvtToFunctionPtrSupported
+
+#endif
 
 
 
