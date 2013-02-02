1473ce86150e7f04721a8a7bec5c24d0ac68c357
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-02-02 13:06:51 -0500

    astyle

--- Library/Sources/Stroika/Frameworks/WebServer/HTTPConnection.cpp
+++ Library/Sources/Stroika/Frameworks/WebServer/HTTPConnection.cpp
@@ -49,8 +49,8 @@ HTTPConnection::~HTTPConnection ()
 
 void    HTTPConnection::ReadHeaders ()
 {
-	// @todo - DONT use TextStream::ReadLine - because that asserts SEEKABLE - which may not be true (and probably isn't here anymore)
-	// Instead - we need a special variant that looks for CRLF - which doesn't require backtracking...!!!
+    // @todo - DONT use TextStream::ReadLine - because that asserts SEEKABLE - which may not be true (and probably isn't here anymore)
+    // Instead - we need a special variant that looks for CRLF - which doesn't require backtracking...!!!
 
 
     {
