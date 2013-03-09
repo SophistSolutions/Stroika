94c631384b3af9a0f12aa8a9a9253b47e8c3f252
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-03-08 23:13:04 -0500

    get BasicBinaryInputStream working for gcc 4.7.2

--- Library/Sources/Stroika/Foundation/Streams/BasicBinaryInputStream.cpp
+++ Library/Sources/Stroika/Foundation/Streams/BasicBinaryInputStream.cpp
@@ -5,10 +5,7 @@
 
 #include    <algorithm>
 
-#if     !qCompilerAndStdLib_Supports_ConstructorDelegation
 #include    "../Containers/Common.h"
-#endif
-
 #include    "../Execution/OperationNotSupportedException.h"
 
 #include    "BasicBinaryInputStream.h"
