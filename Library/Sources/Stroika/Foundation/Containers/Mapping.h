09500b9daaf61c5037bd95ccf6253c4663a41221
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-05-05 09:54:01 -0400

    comments

--- Library/Sources/Stroika/Foundation/Containers/Mapping.h
+++ Library/Sources/Stroika/Foundation/Containers/Mapping.h
@@ -8,11 +8,11 @@
  *  TODO:
  *      @todo   Support thread safety
  *
- *		@todo	Support more backends
- *				Especially HashTable, RedBlackTree, and stlmap, and stlhashmap
- *				And of course change default here
+ *      @todo   Support more backends
+ *              Especially HashTable, RedBlackTree, and stlmap, and stlhashmap
+ *              And of course change default here
  *
- *		@todo	
+ *      @todo
  *
  *
  */
