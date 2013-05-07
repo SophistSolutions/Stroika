9af7b0a92e6727edd824f8608a3b1bafa9d5d09f
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-05-06 22:25:51 -0400

    Added draft Containers_Private_DataStructures_STLContainerWrapper

---
+++ Library/Sources/Stroika/Foundation/Containers/Private/DataStructures/STLContainerWrapper.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2013.  All rights reserved
+ */
+
+#ifndef _Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_h_
+#define _Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_h_
+
+
+/*
+ *  TODO:
+ *
+ */
+
+
+
+
+
+/*
+ *
+ *  Description:
+ *
+ *
+ */
+
+#include    "../../../StroikaPreComp.h"
+
+#include    "../../../Configuration/Common.h"
+
+#include    "../../Common.h"
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Containers {
+            namespace Private {
+                namespace   DataStructures {
+
+
+                }
+            }
+        }
+    }
+}
+
+
+#endif  /*_Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_h_ */
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include    "STLContainerWrapper.inl"
+
---
+++ Library/Sources/Stroika/Foundation/Containers/Private/DataStructures/STLContainerWrapper.inl
@@ -0,0 +1,26 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2013.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_inl_
+#define _Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_inl_   1
+
+
+#include    "../../../Debug/Assertions.h"
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Containers {
+            namespace Private {
+                namespace   DataStructures {
+
+
+
+
+                }
+            }
+        }
+    }
+}
+#endif /* _Stroika_Foundation_Containers_Private_DataStructures_STLContainerWrapper_inl_ */
