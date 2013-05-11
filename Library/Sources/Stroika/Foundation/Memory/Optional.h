68ad5d8926ac04f0b85d6c8563c34043ddef82ba
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-05-11 11:14:02 -0400

    major cleanups to Memory::Optional<> documentation - in preps to do likewise with docs for other modules

--- Library/Sources/Stroika/Foundation/Memory/Optional.h
+++ Library/Sources/Stroika/Foundation/Memory/Optional.h
@@ -15,23 +15,6 @@
  *  \file
  *
  *  TODO:
- *      @todo   Better document why we allow internal pointers to be returned from
- *              get (), operator-> (), and operator* ().
- *
- *              GIST is this class is intrinsically not thread safe anyhow. Main reason
- *              to not allow that stuff is to assure thread safety and controlled update.
- *
- *              Allowing returning const ptrs is just as unsafe and returning non-const pointers
- *              cuz another thread could still modify that data.
- *
- *              And returning non-const pointers very confenenti when doing optional of a struct,
- *              and allowing people to modify parts of the struct.
- *
- *              Optional<some_object> o;
- *              ...
- *              o->x = 1;   // assert fails if o not set first
- *              o->y = 2;
- *              return o->z;
  *
  */
 
@@ -59,47 +42,66 @@ namespace   Stroika {
              *  and instead of explicitly using pointers and checking for null all over.
              *
              *  When dereferencing an empty value, there are three plausible interpretions:
-             *      (o)     Return the default value T()
-             *      (o)     throw bad_alloc()
-             *      (o)     Assertion error
+             *      -   Return the default value T()
+             *      -   throw bad_alloc()
+             *      -   Assertion error
              *
              *  Because the 'default value' isn't always well defined, and because throwing bad_alloc
              *  runs the risk of producing surprising exceptions, we treat dereferencing an empty
              *  Optional<T> as an Assertion Erorr.
              *
-             *  However, see @Optional<T>::Value()
+             *  However, see @ref Value()
              *
              *  \note   \em Thread-Safety
-             *      Different instances of Optional<T> can be freely used  in different threads,
-             *      but a given instance can only be safely used (read + write, or write + write)
-             *      from a single thread at a time.
+             *      -   Different instances of Optional<T> can be freely used  in different threads,
+             *          but a given instance can only be safely used (read + write, or write + write)
+             *          from a single thread at a time.
              *
-             *  \note   \em Design-Note
-             *      We considered using the SharedByValue<T> template which would be more efficient when we copy
-             *      Optional objects of type T, where T is expensive to copy. But for T where T is cheap to copy,
-             *      just using BlockAllocated<T> is probably cheaper to copy. Which is more common? Who knows, so this
-             *      probably doesn't matter much. Also - consider perhaps if there is a way to use a TRAITS argument?
-             *      If we chose to use SharedByValue<T>, then non-const methods like 'operator* ()' would just
-             *      'BreakReferences'.
+             *  \note   \em Design-Note - why not SharedByValue<T>
+             *      -   We considered using the SharedByValue<T> template which would be more efficient
+             *          when we copy Optional objects of type T, where T is expensive to copy. But for
+             *          T where T is cheap to copy, just using BlockAllocated<T> is probably cheaper
+             *          to copy. Which is more common? Who knows, so this probably doesn't matter much.
+             *          Also - consider perhaps if there is a way to use a TRAITS argument?
+             *          If we chose to use SharedByValue<T>, then non-const methods like 'operator* ()'
+             *          would just 'BreakReferences'.
              *
-             *  \note   \em Design-Note
-             *      operator T();
+             *  \note   \em Design-Note - Internal pointers
+             *      -   Several APIs return internal pointers - both const and non-const. Because of this
+             *          it up to callers to assure thread-safety. The lifetime of the returned pointers
+             *          is gauranteed only until the start of the next call on a the particular
+             *          Optional<> instance.
+             *      -   Why we allow internal pointers to be returned from
+             *          get (), operator-> (), and operator* ().
+             *          -   GIST is this class is intrinsically not thread safe anyhow. Main reason
+             *              to not allow that stuff is to assure thread safety and controlled update.
+             *          -   Allowing returning const ptrs is just as unsafe and returning non-const pointers
+             *              cuz another thread could still modify that data.
+             *          -   And returning non-const pointers very confenenti when doing optional of a struct,
+             *              and allowing people to modify parts of the struct.
              *
-             *      We considered having an operator T () method. This has advantages, in that it makes more seemless
-             *      replacing use of a T with an Optional<T>. But it has the disadvantage that, when coupled with
-             *      the Optional<T> (T) CTOR, you can get overloading problems.
+             *              \code
+             *              Optional<some_object> o;
+             *              ...
+             *              o->x = 1;   // assert fails if o not set first
+             *              o->y = 2;
+             *              return o->z;
+             *              \endcode
              *
-             *      Plus, one must carefully check each use of variable o of type T, being converted to type
-             *      Optional<T>, so being forced to say "*" first isn't totally unreasonable.
+             *  \note   \em Design-Note - why no operator T()
+             *      -   We considered having an operator T () method. This has advantages, in that
+             *          it makes more seemless replacing use of a T with an Optional<T>. But it has
+             *          the disadvantage that, when coupled with the Optional<T> (T) CTOR, you can
+             *          get overloading problems.
+             *      -   Plus, one must carefully check each use of a variable of type T, being converted
+             *          to type Optional<T>, so being forced to say "*" first isn't totally unreasonable.
              *
-             *
-             *
-             *  @todo   DOCUMENT FACT THAT WE NEVER RETURN INTERNAL POINTER EXCEPT CONST, and THEN ONLY VALID TEXT NEXT METHOD
-             *          CALL ON OPTIONAL.
              */
             template    <typename T, typename TRAITS = Optional_DefaultTraits<T> >
             class   Optional {
             public:
+                /**
+                 */
                 Optional ();
                 Optional (const T& from);
                 Optional (T && from);
@@ -109,6 +111,8 @@ namespace   Stroika {
                 ~Optional ();
 
             public:
+                /**
+                 */
                 nonvirtual  Optional<T, TRAITS>& operator= (const T& from);
                 nonvirtual  Optional<T, TRAITS>& operator= (T && from);
                 nonvirtual  Optional<T, TRAITS>& operator= (const Optional<T, TRAITS>& from);
@@ -128,20 +132,17 @@ namespace   Stroika {
                  */
                 nonvirtual  bool    empty () const; // means no value (it is optional!)
 
-
             public:
                 /**
-                 *  Always safe to call. If empty, returns argument 'default' or sentinal value.
+                 *  Always safe to call. If empty, returns argument 'default' or 'sentinal' value.
                  */
                 nonvirtual  T   Value (T defaultValue = T ()) const;
 
-
             public:
-                /*
-                 * Unclear if we want a non-const version too?
-                 * Returns nullptr if value is missing
+                /**
+                 *  Returns nullptr if value is missing
                  *
-                 *  \note   \em Warning
+                 *  \warning
                  *      This method returns a pointer internal to (owned by) Optional<T>, and its lifetime
                  *      is only guaranteed until the next method call on this Optional<T> instance.
                  */
@@ -152,12 +153,13 @@ namespace   Stroika {
                 /**
                  *  \pre (not empty ())
                  *
-                 *  \note   \em Warning
+                 *  \warning
                  *      This method returns a pointer internal to (owned by) Optional<T>, and its lifetime
                  *      is only guaranteed until the next method call on this Optional<T> instance.
                  *
-                 *      This is really just syntactic sugar equivilent to get () - but more convenient since
-                 *      it allows the use of an optional to syntactically mirror dereferencing a pointer.
+                 *  This is really just syntactic sugar equivilent to get () - except that it requires
+                 *  not-null - but more convenient since it allows the use of an optional to
+                 *  syntactically mirror dereferencing a pointer.
                  */
                 nonvirtual  T* operator-> ();
                 nonvirtual  const T* operator-> () const;
@@ -166,7 +168,7 @@ namespace   Stroika {
                 /**
                  *  \pre (not empty ())
                  *
-                 *  \note   \em Warning
+                 *  \warning
                  *      This method returns a pointer internal to (owned by) Optional<T>, and its lifetime
                  *      is only guaranteed until the next method call on this Optional<T> instance.
                  */
@@ -190,9 +192,10 @@ namespace   Stroika {
 
             private:
                 /*
-                 *  Carefully manage the storage ourselves - not doing shared copy. No shared copy (copyonwrite) because these
-                 *  are rarely copied, and this is cheaper (since can use block-allocation - even if type T not
-                 *  block allocated) - and no extra count infrastructure, or threadafe locking.
+                 *  Carefully manage the storage ourselves - not doing shared copy. No shared
+                 *  copy (copyonwrite) because these are rarely copied, and this is cheaper
+                 *  (since can use block-allocation - even if type T not block allocated) -
+                 *  and no extra count infrastructure, or threadafe locking.
                  */
                 BlockAllocated<T>*  fValue_;
             };
