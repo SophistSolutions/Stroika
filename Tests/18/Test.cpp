8181cea52b589abc26dcbec722fcd561c707751f
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-05-15 14:03:34 -0400

    draft of Tests for sortedtally - though no sorted tally class yet

--- Tests/18/Test.cpp
+++ Tests/18/Test.cpp
@@ -5,15 +5,22 @@
 //      STATUS  very minimal/incomplete
 #include    "Stroika/Foundation/StroikaPreComp.h"
 
-#include    "Stroika/Foundation/Containers/SortedMapping.h"
-#include    "Stroika/Foundation/Containers/Concrete/SortedMapping_stdmap.h"
+
+/// WRONG - we dont have SortedTally Yet - but when we do!!!
+#include    "Stroika/Foundation/Containers/Tally.h"
+#include    "Stroika/Foundation/Containers/Concrete/Tally_Array.h"
+#include    "Stroika/Foundation/Containers/Concrete/Tally_LinkedList.h"
 #include    "Stroika/Foundation/Debug/Assertions.h"
 #include    "Stroika/Foundation/Debug/Trace.h"
 
-
+#include    "../TestCommon/CommonTests_Tally.h"
 #include    "../TestHarness/SimpleClass.h"
 #include    "../TestHarness/TestHarness.h"
 
+
+#include    "../TestCommon/CommonTests_Tally.h"
+#include    "../TestHarness/SimpleClass.h"
+#include    "../TestHarness/TestHarness.h"
 
 
 using   namespace   Stroika;
@@ -21,105 +28,28 @@ using   namespace   Stroika::Foundation;
 using   namespace   Stroika::Foundation::Containers;
 
 
-using   Concrete::SortedMapping_stdmap;
-
-
-namespace {
-    template <typename MappingOfKeyT>
-    void    SimpleTest_1_ (MappingOfKeyT m)
-    {
-        MappingOfKeyT s;
-    }
-}
-
-
-namespace {
-    template <typename MappingOfKeyT>
-    void    SimpleTest_2_ (MappingOfKeyT m)
-    {
-        m.Add (1, 2);
-        VerifyTestResult (m.size () == 1);
-        Verify (m.Lookup (1, nullptr));
-        Verify (not m.Lookup (2, nullptr));
-        m.Add (1, 2);
-        VerifyTestResult (m.size () == 1);
-        m.Remove (1);
-        VerifyTestResult (m.size () == 0);
-        m.RemoveAll ();
-        VerifyTestResult (m.size () == 0);
-    }
-}
-
-
-namespace {
-    template <typename MappingOfKeyT>
-    void    SimpleTest_3_Iteration_ (MappingOfKeyT m)
-    {
-        m.Add (1, 2);
-        VerifyTestResult (m.size () == 1);
-        for (auto i : m) {
-            VerifyTestResult (i.first == 1);
-            VerifyTestResult (i.second == 2);
-        }
-        m.Add (1, 2);
-        VerifyTestResult (m.size () == 1);
-        for (auto i : m) {
-            VerifyTestResult (i.first == 1);
-            VerifyTestResult (i.second == 2);
-        }
-        m.Remove (1);
-        VerifyTestResult (m.size () == 0);
-        for (auto i : m) {
-            VerifyTestResult (false);
-        }
-        m.Add (2, 3);
-        m.Add (1, 2);
-        m.Add (3, 4);
-        unsigned int cnt = 0;
-        for (auto i : m) {
-            cnt++;
-            if (cnt == 1) {
-                VerifyTestResult (i.first == 1);
-                VerifyTestResult (i.second == 2);
-            }
-            if (cnt == 2) {
-                VerifyTestResult (i.first == 2);
-                VerifyTestResult (i.second == 3);
-            }
-            if (cnt == 3) {
-                VerifyTestResult (i.first == 3);
-                VerifyTestResult (i.second == 4);
-            }
-        }
-        VerifyTestResult (cnt == 3);
-        m.RemoveAll ();
-        VerifyTestResult (m.size () == 0);
-    }
-}
-
-
-namespace {
-    template <typename MappingOfKeyT>
-    void    SimpleMappingTest_All_For_Type ()
-    {
-        MappingOfKeyT m;
-        SimpleTest_1_ (m);
-        SimpleTest_2_ (m);
-        SimpleTest_3_Iteration_ (m);
-    }
-}
+using   Concrete::Tally_Array;
+using   Concrete::Tally_LinkedList;
 
 
 namespace   {
+
     void    DoRegressionTests_ ()
     {
-        SimpleMappingTest_All_For_Type<SortedMapping<size_t, size_t>> ();
-        SimpleMappingTest_All_For_Type<SortedMapping<SimpleClass, SimpleClass>> ();
+        using namespace CommonTests::TallyTests;
 
-        SimpleMappingTest_All_For_Type<SortedMapping_stdmap<size_t, size_t>> ();
-        SimpleMappingTest_All_For_Type<SortedMapping_stdmap<SimpleClass, SimpleClass>> ();
+        SimpleTallyTest_All_For_Type<size_t, Tally_LinkedList<size_t>> ();
+        SimpleTallyTest_All_For_Type<SimpleClass, Tally_LinkedList<SimpleClass>> ();
+
+        SimpleTallyTest_All_For_Type<size_t, Tally_Array<size_t>> ();
+        SimpleTallyTest_All_For_Type<SimpleClass, Tally_Array<SimpleClass>> ();
+
+        SimpleTallyTest_All_For_Type<size_t, Tally<size_t>> ();
+        SimpleTallyTest_All_For_Type<SimpleClass, Tally<SimpleClass>> ();
     }
+
 }
+
 
 
 int     main (int argc, const char* argv[])
