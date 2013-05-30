c7aac2903452b789d7153b722eeb889c43bb9f26
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-05-29 22:00:37 -0400

    Minor cleanup to Cryptography/Hash/Common.h for llvm

--- Library/Sources/Stroika/Foundation/Cryptography/Hash/Common.h
+++ Library/Sources/Stroika/Foundation/Cryptography/Hash/Common.h
@@ -36,17 +36,10 @@ namespace   Stroika {
 
                 template    <typename RETURN_TYPE, typename ALGORITHM>
                 struct Hasher {
-                    typedef typename    RETURN_TYPE ReturnType;
+                    typedef RETURN_TYPE ReturnType;
 
                     static  ReturnType  Hash (const Streams::BinaryInputStream& from);
                     static  ReturnType  Hash (const Byte* from, const Byte* to);
-
-#if 0
-                    // maybe can get rid of this - but maybe handy to specialize??? For common case of
-                    template    <typename   PODTYPE>
-                    static  ReturnType  Hash (const PODTYPE& pod);
-#endif
-
                 };
 
 
