3fe23acae9d211674dd05ddcd6b824223fcb93ce
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-06-06 19:47:24 -0400

    minor fix to IgnoreExceptionsExceptThreadAbortForCall() - namespaces

--- Library/Sources/Stroika/Foundation/Execution/Exceptions.h
+++ Library/Sources/Stroika/Foundation/Execution/Exceptions.h
@@ -81,7 +81,7 @@ namespace   Stroika {
             class   ThreadAbortException;
             /**
              */
-#define IgnoreExceptionsExceptThreadAbortForCall(theCode)        try {theCode;} catch (Execution::ThreadAbortException& e) { DoReThrow (); } catch (...) {}
+#define IgnoreExceptionsExceptThreadAbortForCall(theCode)        try {theCode;} catch (Execution::ThreadAbortException&) { Execution::DoReThrow (); } catch (...) {}
 
 
         }
