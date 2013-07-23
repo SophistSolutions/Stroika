80f636112a7246e9b7dfcbf892bb93e3fb3e4756
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-07-23 10:34:41 -0400

    Cleanup Set<> regressiont ests - so now test Set_stdset<>

--- Tests/14/Test.cpp
+++ Tests/14/Test.cpp
@@ -30,35 +30,57 @@ using   Concrete::Set_stdset;
 
 
 
+namespace {
+    template    <typename   CONCRETE_CONTAINER>
+    void    DoTestForConcreteContainer_ ()
+    {
+        typedef typename CONCRETE_CONTAINER::ElementType    ElementType;
+        typedef typename CONCRETE_CONTAINER::TraitsType     TraitsType;
+        auto extraChecksFunction = [] (const Set<ElementType, TraitsType>& s) {
+            // only work todo on sorted sets
+        };
+        CommonTests::SetTests::Test_All_For_Type<CONCRETE_CONTAINER, Set<ElementType, TraitsType>> (extraChecksFunction);
+    }
+}
+
+
+
 namespace   {
     void    DoRegressionTests_ ()
     {
         using namespace CommonTests::SetTests;
 
-        struct  MySimpleClassWithoutComparisonOperators_Comparer_ {
+        struct  MySimpleClassWithoutComparisonOperators_CompareEquals_ {
             typedef SimpleClassWithoutComparisonOperators ElementType;
             static  bool    Equals (ElementType v1, ElementType v2) {
                 return v1.GetValue () == v2.GetValue ();
             }
         };
-        typedef Set_DefaultTraits<SimpleClassWithoutComparisonOperators, MySimpleClassWithoutComparisonOperators_Comparer_>   SimpleClassWithoutComparisonOperators_SETRAITS;
+        typedef Set_DefaultTraits<SimpleClassWithoutComparisonOperators, MySimpleClassWithoutComparisonOperators_CompareEquals_>   SimpleClassWithoutComparisonOperators_SETRAITS;
 
-        auto testFunc1 = [] (const Set<size_t>& s) {
-        };
-        auto testFunc2 = [] (const Set<SimpleClass>& s) {
-        };
-        auto testFunc3 = [] (const Set<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>& s) {
-        };
+        DoTestForConcreteContainer_<Set<size_t>> ();
+        DoTestForConcreteContainer_<Set<SimpleClass>> ();
+        DoTestForConcreteContainer_<Set<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>> ();
 
-        Test_All_For_Type<Set<size_t>, Set<size_t>> (testFunc1);
-        Test_All_For_Type<Set<SimpleClass>, Set<SimpleClass>> (testFunc2);
-        Test_All_For_Type<Set<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>, Set<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>> (testFunc3);
+        DoTestForConcreteContainer_<Set_LinkedList<size_t>> ();
+        DoTestForConcreteContainer_<Set_LinkedList<SimpleClass>> ();
+        DoTestForConcreteContainer_<Set_LinkedList<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>> ();
 
-        Test_All_For_Type<Set_LinkedList<size_t>, Set<size_t>> (testFunc1);
-        Test_All_For_Type<Set_LinkedList<SimpleClass>, Set<SimpleClass>> (testFunc2);
-        Test_All_For_Type<Set_LinkedList<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>, Set<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperators_SETRAITS>> (testFunc3);
-
-        Set_stdset<size_t>  tmp;
+        DoTestForConcreteContainer_<Set_stdset<size_t>> ();
+        DoTestForConcreteContainer_<Set_stdset<SimpleClass>> ();
+        {
+            struct  MySimpleClassWithoutComparisonOperators_ComparerWithCompare_ : MySimpleClassWithoutComparisonOperators_CompareEquals_ {
+                typedef SimpleClassWithoutComparisonOperators ElementType;
+                static  int    Compare (ElementType v1, ElementType v2) {
+                    return v1.GetValue () - v2.GetValue ();
+                }
+            };
+            typedef Concrete::Set_stdset_DefaultTraits <
+            SimpleClassWithoutComparisonOperators,
+            MySimpleClassWithoutComparisonOperators_ComparerWithCompare_
+            >   SimpleClassWithoutComparisonOperatorsSet_stdset_TRAITS;
+            DoTestForConcreteContainer_<Set_stdset<SimpleClassWithoutComparisonOperators, SimpleClassWithoutComparisonOperatorsSet_stdset_TRAITS>> ();
+        }
 
     }
 }
