c70c13e7a2c2dee4b95d2c5588e37850993119ad
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-08-16 17:44:13 -0400

    cosmetic

--- Library/Sources/Stroika/Foundation/Containers/Concrete/Mapping_LinkedList.h
+++ Library/Sources/Stroika/Foundation/Containers/Concrete/Mapping_LinkedList.h
@@ -48,10 +48,8 @@ namespace   Stroika {
                     template    <typename COPY_FROM_ITERATOR_KEY_T>
                     explicit Mapping_LinkedList (COPY_FROM_ITERATOR_KEY_T start, COPY_FROM_ITERATOR_KEY_T end);
 
-
                 public:
                     nonvirtual  Mapping_LinkedList<KEY_TYPE, VALUE_TYPE, TRAITS>& operator= (const Mapping_LinkedList<KEY_TYPE, VALUE_TYPE, TRAITS>& rhs);
-
 
                 private:
 #if     !qCompilerAndStdLib_Supports_SharedPtrOfPrivateTypes
