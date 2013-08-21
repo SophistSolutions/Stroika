d98d3d1082da7bc3cce3960b7e454db93a0818b0
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-08-21 14:28:54 -0400

    lose ThirdPartyProducts/astyle: the published/distributed one is fine now (updates for C++11 now in published build)

--- ThirdPartyLibs/astyle/FetchLatestAStyle.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-my $date = "2013-02-25";
-system ("svn export https://astyle.svn.sourceforge.net/svnroot/astyle/trunk astyle-trunk");
-system ("tar cvf astyle-trunk-$date.tar astyle-trunk");
-system ("gzip --best astyle-trunk-$date.tar");
-system ("mv astyle-trunk-$date.tar.gz Origs/");
-system ("rm -rf astyle-trunk");
