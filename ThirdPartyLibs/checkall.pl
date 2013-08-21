9458ff3c5f1374750119cae34dabd409280f0733
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-08-21 14:30:13 -0400

    lose ThirdPartyProducts/astyle: the published/distributed one is fine now (updates for C++11 now in published build)

--- ThirdPartyLibs/checkall.pl
+++ ThirdPartyLibs/checkall.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
 
-system ("cd astyle; perl checkall.pl");
 system ("cd doxygen; perl checkall.pl");
 system ("cd openssl; perl buildall.pl $BLD_TRG");
 system ("cd Xerces; perl checkall.pl");
