ef948822427bfa4d570e2be5006c18c54710f103
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-09-06 18:21:45 -0400

    minor namespace fix for Framework(s) in Frameworks/StrokaPreComp.h

--- Library/Sources/Stroika/Frameworks/StroikaPreComp.h
+++ Library/Sources/Stroika/Frameworks/StroikaPreComp.h
@@ -1,18 +1,17 @@
 /*
  * Copyright(c) Sophist Solutions, Inc. 1990-2011.  All rights reserved
  */
-#ifndef _Stroika_Framework_StroikaPreComp_h_
-#define _Stroika_Framework_StroikaPreComp_h_    1
+#ifndef _Stroika_Frameworks_StroikaPreComp_h_
+#define _Stroika_Frameworks_StroikaPreComp_h_    1
 
 #include    "../Foundation/StroikaPreComp.h"
-//#include  "Configuration/StroikaConfig.h"
 
 #if     defined (__cplusplus)
 // Declare the namespaces so code early on can freely say stuff like "uses namespace Stroika"
 namespace   Stroika {
-    namespace   Framework {
+    namespace   Frameworks {
     }
 }
 #endif
 
-#endif  /*_Stroika_Framework_StroikaPreComp_h_*/
+#endif  /*_Stroika_Frameworks_StroikaPreComp_h_*/
