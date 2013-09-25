121e80008512a5a14a9248fe9b16982b38dbb2db
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-09-25 14:03:16 -0400

    fixed small bug with Characters::Private_::String2Int_ long long int result overload

--- Library/Sources/Stroika/Foundation/Characters/Format.cpp
+++ Library/Sources/Stroika/Foundation/Characters/Format.cpp
@@ -227,7 +227,7 @@ long long int Characters::Private_::String2Int_ (const String& s)
 {
     using   std::numeric_limits;
 #if     qCompilerAndStdLib_Supports_strtoll
-    unsigned    long long int    l   =   wcstoul (s.c_str (), nullptr, 10);
+    unsigned    long long int    l   =   wcstoll (s.c_str (), nullptr, 10);
 #else
     unsigned    long long int    l   =   _wcstoi64 (s.c_str (), nullptr, 10);
 #endif
