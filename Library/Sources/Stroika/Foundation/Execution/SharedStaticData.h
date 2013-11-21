d714bafc49e7a3b6d0537844ff70f8ed115a42f7
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-11-21 09:36:13 -0500

    Code comments

--- Library/Sources/Stroika/Foundation/Execution/SharedStaticData.h
+++ Library/Sources/Stroika/Foundation/Execution/SharedStaticData.h
@@ -28,18 +28,46 @@ namespace   Stroika {
 
 
             /**
-             *   SharedStaticData<T> is used to safely create a copy of static data shared among various users
-             *   in a thread safe way, and where the shared data goes away when the last reference does.
+             *  SharedStaticData<T> is used to safely create a copy of static data shared among various users
+             *  in a thread safe way, and where the shared data goes away when the last reference does.
              *
-             *   This is very similar to have a single static variable of type T, except that instead of
-             *   having T constructed at global execution time, and destroyed at global object destruction time,
-             *   it happens when the first owner comes into existence and when teh last owner goes out of existance.
+             *  This is very similar to have a single static variable of type T, except that instead of
+             *  having T constructed at global execution time, and destroyed at global object destruction time,
+             *  it happens when the first owner comes into existence and when teh last owner goes out of existance.
              *
-             *   For example - if the shared data object creates threads, it can be a problem having this destroyed in
-             *   a static (file scope) lifetime.
+             *  For example - if the shared data object creates threads, it can be a problem having this destroyed in
+             *  a static (file scope) lifetime.
              *
-             *   This is also similar to the @see ModuleInit<> template, except that this is intended to give the
-             *   user tighter control over lifetime of the shared data.
+             *  This is also similar to the @see ModuleInit<> template, except that this is intended to give the
+             *  user tighter control over lifetime of the shared data.
+             *
+             *  Example usage (from HealthFrameWorksServer):
+             *
+             *              class   AuditLogSink {
+             *                  ...
+             *                  private:
+             *                      struct SharedData_;
+             *                      Execution::SharedStaticData<SharedData_>    fSharedData_;
+             *              };
+             *              struct  AuditLogSink::SharedData_ {
+             *                  Execution::Thread                   fCleanupThread;
+             *
+             *                  SharedData_ ()
+             *                      : fCleanupThread (Thread (&AuditLogSink::SimpleCleanupThread_))
+             *                  {
+             *                      fCleanupThread.SetThreadPriority (Thread::Priority::eLowest);
+             *                      fCleanupThread.SetThreadName (L"AuditTrailCleanupThread");
+             *                      fCleanupThread.Start ();
+             *                  }
+             *                  ~SharedData_ ()
+             *                  {
+             *                      fCleanupThread.AbortAndWaitForDone ();
+             *                  }
+             *              };
+             *
+             *  So then on the first AuditLogSink construction - we construct the cleanup thread, and on the last the
+             *  thread is shutdown. If these objects are all created after main, this assures the thread startup/cleanup
+             *  is all done after the start of main and before it completes.
              *
              */
             template <typename T>
