afe5d779d25a4c5db2487fe4877101a5fc6c154c
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-11-30 20:43:06 -0500

    qCompilerAndStdLib_constexpr_Buggy fixed ifdef(recent regressions)

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -175,7 +175,7 @@
 #if     defined (_MSC_VER)
 #define qCompilerAndStdLib_constexpr_Buggy   (_MSC_VER == _MS_VS_2k13_VER_)
 #else
-#define qCompilerAndStdLib_constexpr_Buggy   1
+#define qCompilerAndStdLib_constexpr_Buggy   0
 #endif
 
 #endif
