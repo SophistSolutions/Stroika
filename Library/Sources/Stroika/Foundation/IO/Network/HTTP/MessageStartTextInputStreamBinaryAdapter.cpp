b9197dd9ca80ea54deb93cbb84107a8d7cd838b5
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-12-06 17:45:14 -0500

    Runastyle

--- Library/Sources/Stroika/Foundation/IO/Network/HTTP/MessageStartTextInputStreamBinaryAdapter.cpp
+++ Library/Sources/Stroika/Foundation/IO/Network/HTTP/MessageStartTextInputStreamBinaryAdapter.cpp
@@ -3,7 +3,7 @@
  */
 #include    "../../../StroikaPreComp.h"
 
-#include	<mutex>
+#include    <mutex>
 
 #include    "../../../Execution/OperationNotSupportedException.h"
 #include    "../../../Execution/OperationNotSupportedException.h"
@@ -25,7 +25,7 @@ using   namespace   Stroika::Foundation::Streams;
 
 
 namespace {
-	const size_t	kDefaultBufSize_ = 1024;	// guess enough for http headers?
+    const size_t    kDefaultBufSize_ = 1024;    // guess enough for http headers?
 }
 
 
@@ -40,48 +40,48 @@ public:
     IRep_ (const BinaryInputStream& src)
         : fCriticalSection_ ()
         , fSource_ (src)
-		, fReadDataBuf_ (kDefaultBufSize_)
+        , fReadDataBuf_ (kDefaultBufSize_)
         , fOffset_ (0)
-		, fBufferFilledUpValidBytes_ (0)
+        , fBufferFilledUpValidBytes_ (0)
     {
     }
 
 protected:
-	virtual size_t    _Read (Character* intoStart, Character* intoEnd) override
-	{
-		Require ((intoStart == intoEnd) or (intoStart != nullptr));
-		Require ((intoStart == intoEnd) or (intoEnd != nullptr));
+    virtual size_t    _Read (Character* intoStart, Character* intoEnd) override
+    {
+        Require ((intoStart == intoEnd) or (intoStart != nullptr));
+        Require ((intoStart == intoEnd) or (intoEnd != nullptr));
 
-		if (intoStart == nullptr) {
-			return 0;
-		}
-		AssertNotNull (intoStart);
-		lock_guard<recursive_mutex>  critSec (fCriticalSection_);
-		Assert (fBufferFilledUpValidBytes_ >= fOffset_);				// limitation/feature of current implemetnation
-		if (fBufferFilledUpValidBytes_ == fOffset_) {
-			size_t roomLeftInBuf = fReadDataBuf_.GetSize () - fBufferFilledUpValidBytes_;
-			if (roomLeftInBuf == 0) {
-				// should be quite rare
-				fReadDataBuf_.GrowToSize (fBufferFilledUpValidBytes_ + kDefaultBufSize_);
-				roomLeftInBuf = fReadDataBuf_.GetSize () - fBufferFilledUpValidBytes_;
-			}
-			Assert (roomLeftInBuf > 0);
-			Byte* startReadAt = fReadDataBuf_.begin () + fBufferFilledUpValidBytes_;
-			size_t  n = fSource_.Read (startReadAt, startReadAt + roomLeftInBuf);
-			Assert (n <= roomLeftInBuf);
-			// if n == 0, OK, just means EOF
-			fBufferFilledUpValidBytes_ += n;
-		}
+        if (intoStart == nullptr) {
+            return 0;
+        }
+        AssertNotNull (intoStart);
+        lock_guard<recursive_mutex>  critSec (fCriticalSection_);
+        Assert (fBufferFilledUpValidBytes_ >= fOffset_);                // limitation/feature of current implemetnation
+        if (fBufferFilledUpValidBytes_ == fOffset_) {
+            size_t roomLeftInBuf = fReadDataBuf_.GetSize () - fBufferFilledUpValidBytes_;
+            if (roomLeftInBuf == 0) {
+                // should be quite rare
+                fReadDataBuf_.GrowToSize (fBufferFilledUpValidBytes_ + kDefaultBufSize_);
+                roomLeftInBuf = fReadDataBuf_.GetSize () - fBufferFilledUpValidBytes_;
+            }
+            Assert (roomLeftInBuf > 0);
+            Byte* startReadAt = fReadDataBuf_.begin () + fBufferFilledUpValidBytes_;
+            size_t  n = fSource_.Read (startReadAt, startReadAt + roomLeftInBuf);
+            Assert (n <= roomLeftInBuf);
+            // if n == 0, OK, just means EOF
+            fBufferFilledUpValidBytes_ += n;
+        }
 
-		// At this point - see if we can fullfill the request. If not - its cuz we got EOF
-		size_t  outN = 0;
-		for (Character* outChar = intoStart; outChar != intoEnd; ++outChar) {
-			if (fOffset_ < fBufferFilledUpValidBytes_) {
-				*outChar = Characters::Character ((char)* (fReadDataBuf_.begin () + fOffset_));
-				fOffset_++;
-				outN++;
-			}
-		}
+        // At this point - see if we can fullfill the request. If not - its cuz we got EOF
+        size_t  outN = 0;
+        for (Character* outChar = intoStart; outChar != intoEnd; ++outChar) {
+            if (fOffset_ < fBufferFilledUpValidBytes_) {
+                *outChar = Characters::Character ((char) * (fReadDataBuf_.begin () + fOffset_));
+                fOffset_++;
+                outN++;
+            }
+        }
         Ensure (outN <= static_cast<size_t> (intoEnd - intoStart));
         return outN;
     }
@@ -100,7 +100,7 @@ protected:
                     if (offset < 0) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
-					if (offset > fBufferFilledUpValidBytes_) {
+                    if (offset > fBufferFilledUpValidBytes_) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
                     // Note - warning here  legit - our caching strategy wtih string is bogus and wont work with large streams
@@ -113,7 +113,7 @@ protected:
                     if (newOffset < 0) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
-					if (newOffset > fBufferFilledUpValidBytes_) {
+                    if (newOffset > fBufferFilledUpValidBytes_) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
                     // Note - warning here  legit - our caching strategy wtih string is bogus and wont work wtih large streams
@@ -122,11 +122,11 @@ protected:
                 break;
             case    Whence::eFromEnd: {
                     Streams::SeekOffsetType         curOffset   =   fOffset_;
-					Streams::SignedSeekOffsetType   newOffset	=	fBufferFilledUpValidBytes_ + offset;
+                    Streams::SignedSeekOffsetType   newOffset   =   fBufferFilledUpValidBytes_ + offset;
                     if (newOffset < 0) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
-					if (newOffset > fBufferFilledUpValidBytes_) {
+                    if (newOffset > fBufferFilledUpValidBytes_) {
                         Execution::DoThrow (std::range_error ("seek"));
                     }
                     // Note - warning here  legit - our caching strategy wtih string is bogus and wont work wtih large streams
@@ -134,17 +134,17 @@ protected:
                 }
                 break;
         }
-		Ensure ((0 <= fOffset_) and (fOffset_ <= fBufferFilledUpValidBytes_));
+        Ensure ((0 <= fOffset_) and (fOffset_ <= fBufferFilledUpValidBytes_));
         return GetOffset ();
     }
 
 private:
-    mutable recursive_mutex			fCriticalSection_;
-    BinaryInputStream				fSource_;
-	Memory::SmallStackBuffer<Byte>	fReadDataBuf_;					// OK cuz typically this will be very small (1k) and not really grow...
-																	// but it can if we must
-	size_t							fOffset_;						// text stream offset
-	size_t							fBufferFilledUpValidBytes_;		// nbytes of valid text in fReadDataBuf_
+    mutable recursive_mutex         fCriticalSection_;
+    BinaryInputStream               fSource_;
+    Memory::SmallStackBuffer<Byte>  fReadDataBuf_;                  // OK cuz typically this will be very small (1k) and not really grow...
+    // but it can if we must
+    size_t                          fOffset_;                       // text stream offset
+    size_t                          fBufferFilledUpValidBytes_;     // nbytes of valid text in fReadDataBuf_
 };
 
 
@@ -156,6 +156,6 @@ private:
  ********************************************************************************
  */
 MessageStartTextInputStreamBinaryAdapter::MessageStartTextInputStreamBinaryAdapter (const BinaryInputStream& src)
-	: TextInputStream (shared_ptr<_IRep> (new IRep_ (src)))
+    : TextInputStream (shared_ptr<_IRep> (new IRep_ (src)))
 {
 }
--- Library/Sources/Stroika/Foundation/IO/Network/HTTP/MessageStartTextInputStreamBinaryAdapter.h
+++ Library/Sources/Stroika/Foundation/IO/Network/HTTP/MessageStartTextInputStreamBinaryAdapter.h
@@ -21,38 +21,38 @@
 
 namespace   Stroika {
     namespace   Foundation {
-		namespace   IO {
-			namespace   Network {
-				namespace   HTTP {
+        namespace   IO {
+            namespace   Network {
+                namespace   HTTP {
 
 
-					/**
-					 * This can be used to create a readable stream of strings (readline) - which 
-					 * consume a BinaryStream (pre-seeked to the right place) - as a series of characters.
-					 *
-					 * This code respects the format defined in http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
-					 * and doesnt read more than needed (no pre-buffering).
-					 * This lack of pre-buffering (peeking) is critical so that the underlying binary stream
-					 * only reads the minimal amount needed and will be placed at the right position when handed
-					 * to the next process to interpret.
-					 *
-					 *	Though this TextInputStream is seekable, it only supports seeking backwards over materials
-					 *	already read. It never allows seeking past its last read point (throws not supported).
-					 *
-					 *	This adpater defines 0 seek offset as the point at which its constructed. And then you can seek to any locaiton
-					 *	0 .. up to the max point ever read (with Read).
-					 */
-					class   MessageStartTextInputStreamBinaryAdapter : public Streams::TextInputStream {
-					public:
-						MessageStartTextInputStreamBinaryAdapter (const Streams::BinaryInputStream& src);
-					private:
-						class   IRep_;
-					};
+                    /**
+                     * This can be used to create a readable stream of strings (readline) - which
+                     * consume a BinaryStream (pre-seeked to the right place) - as a series of characters.
+                     *
+                     * This code respects the format defined in http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+                     * and doesnt read more than needed (no pre-buffering).
+                     * This lack of pre-buffering (peeking) is critical so that the underlying binary stream
+                     * only reads the minimal amount needed and will be placed at the right position when handed
+                     * to the next process to interpret.
+                     *
+                     *  Though this TextInputStream is seekable, it only supports seeking backwards over materials
+                     *  already read. It never allows seeking past its last read point (throws not supported).
+                     *
+                     *  This adpater defines 0 seek offset as the point at which its constructed. And then you can seek to any locaiton
+                     *  0 .. up to the max point ever read (with Read).
+                     */
+                    class   MessageStartTextInputStreamBinaryAdapter : public Streams::TextInputStream {
+                    public:
+                        MessageStartTextInputStreamBinaryAdapter (const Streams::BinaryInputStream& src);
+                    private:
+                        class   IRep_;
+                    };
 
 
-				}
-			}
-		}
-	}
+                }
+            }
+        }
+    }
 }
 #endif  /*_Stroika_Foundation_IO_Network_HTTP_MessageStartTextInputStreamBinaryAdapter_h_*/
