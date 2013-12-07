46d1d034a9a3fbb53051e7bddcedfd5cf5d2943f
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-12-07 01:55:20 -0500

    More tweaks to UPnP/SSDP/Server/PeriodicNotifier but still not being found by windows

--- Library/Sources/Stroika/Frameworks/UPnP/SSDP/Server/PeriodicNotifier.cpp
+++ Library/Sources/Stroika/Frameworks/UPnP/SSDP/Server/PeriodicNotifier.cpp
@@ -44,17 +44,17 @@ namespace {
             Streams::TextOutputStreamBinaryAdapter  textOut (out);
             //// SUPER ROUGH FIRST DRAFT
             textOut.Write (Format (L"NOTIFY * HTTP/1.1\r\n"));
-            textOut.Write (Format (L"Host: %s:%d\r\n", SSDP::V4::kSocketAddress.GetInternetAddress ().As<String> ().AsUTF8 ().c_str (), SSDP::V4::kSocketAddress.GetPort ()));
-            textOut.Write (Format (L"NT: %s\r\n", deviceAnnouncement.fST.c_str ()));
-            textOut.Write (Format (L"NTS: ssdp:alive\r\n"));
-            textOut.Write (Format (L"USN: %s\r\n", deviceAnnouncement.fUSN.c_str ()));
+            textOut.Write (Format (L"Host: %s:%d\r\n", SSDP::V4::kSocketAddress.GetInternetAddress ().As<String> ().c_str (), SSDP::V4::kSocketAddress.GetPort ()));
+            textOut.Write (Format (L"Cache-Control: max-age=60\r\n"));    // @todo fix
             if (not deviceAnnouncement.fLocation.empty ()) {
                 textOut.Write (Format (L"Location: %s\r\n", deviceAnnouncement.fLocation.c_str ()));
             }
-            textOut.Write (Format (L"Cache-Control: max-age = 7393\r\n"));
+            textOut.Write (Format (L"NTS: ssdp:alive\r\n"));
             if (not deviceAnnouncement.fServer.empty ()) {
                 textOut.Write (Format (L"Server: %s\r\n", deviceAnnouncement.fServer.c_str ()));
             }
+            textOut.Write (Format (L"NT: %s\r\n", deviceAnnouncement.fST.c_str ()));
+            textOut.Write (Format (L"USN: %s\r\n", deviceAnnouncement.fUSN.c_str ()));
             ///need fluush API on  OUTSTREAM
             data = out.As<Memory::BLOB> ();
         }
@@ -66,13 +66,16 @@ void    PeriodicNotifier::Run (const Device& d, const FrequencyInfo& fi)
 {
     Execution::Thread t ([d, fi]() {
         Socket s (Socket::SocketKind::DGRAM);
+        Socket::BindFlags   bindFlags = Socket::BindFlags ();
+        bindFlags.fReUseAddr = true;
+        s.Bind (SocketAddress (Network::V4::kAddrAny, UPnP::SSDP::V4::kSocketAddress.GetPort ()), bindFlags);
         while (true) {
             DeviceAnnouncement  dan;
             dan.fLocation = d.fLocation;
             dan.fServer = d.fServer;
             {
                 dan.fST = L"upnp:rootdevice";
-                dan.fUSN = Format (L"uuid:device-%s::upnp:rootdevice", d.fDeviceID.c_str ());
+                dan.fUSN = Format (L"uuid:%s::upnp:rootdevice", d.fDeviceID.c_str ());
                 DoSend_ (dan, s);
             }
             {
