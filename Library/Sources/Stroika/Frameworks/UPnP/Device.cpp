7fd9712063be2bfc73bef56c04c0573ece46f300
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-12-07 22:08:33 -0500

    runastyle

--- Library/Sources/Stroika/Frameworks/UPnP/Device.cpp
+++ Library/Sources/Stroika/Frameworks/UPnP/Device.cpp
@@ -27,17 +27,17 @@ using   namespace   Stroika::Frameworks::UPnP;
 */
 String  UPnP::MungePrimaryMacAddrIntoBaseDeviceID (String baseDeviceID)
 {
-	Require (baseDeviceID.length () == 36); // also require layout normal as in 315CAAE0-1335-57BF-A178-24C9EE756627 but check later
-	String  result = baseDeviceID;
-	String  macAddr = IO::Network::GetPrimaryNetworkDeviceMacAddress ();
-	macAddr = macAddr.ReplaceAll (L"-", L"");
-	if (macAddr.length () > 12) {
-		macAddr = macAddr.SubString (0, 12);
-	}
-	while (macAddr.length () < 12) {
-		macAddr += L"0";
-	}
-	result = baseDeviceID.SubString (0, 36 - 12) + macAddr;
-	Ensure (result.length () == 36);
-	return result.ToLowerCase ();       // just normalize
+    Require (baseDeviceID.length () == 36); // also require layout normal as in 315CAAE0-1335-57BF-A178-24C9EE756627 but check later
+    String  result = baseDeviceID;
+    String  macAddr = IO::Network::GetPrimaryNetworkDeviceMacAddress ();
+    macAddr = macAddr.ReplaceAll (L"-", L"");
+    if (macAddr.length () > 12) {
+        macAddr = macAddr.SubString (0, 12);
+    }
+    while (macAddr.length () < 12) {
+        macAddr += L"0";
+    }
+    result = baseDeviceID.SubString (0, 36 - 12) + macAddr;
+    Ensure (result.length () == 36);
+    return result.ToLowerCase ();       // just normalize
 }
