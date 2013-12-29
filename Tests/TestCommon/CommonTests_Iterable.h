f1a799f354ae4444ff541b9ead9720b9b1755c8f
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2013-12-28 22:49:51 -0500

    minor cleanups of template test iterable code

--- Tests/TestCommon/CommonTests_Iterable.h
+++ Tests/TestCommon/CommonTests_Iterable.h
@@ -53,7 +53,7 @@ namespace CommonTests {
         void    Test4_TallyEquals_ (const Iterable<typename USING_ITERABLE_CONTAINER::ElementType>& container, EQUALS_COMPARER equalsComparer)
         {
             auto iterableCopy = container;
-            VerifyTestResult ((iterableCopy.tempalte TallyEquals<USING_ITERABLE_CONTAINER, EQUALS_COMPARER> (container)));
+            VerifyTestResult ((iterableCopy.template TallyEquals<USING_ITERABLE_CONTAINER, EQUALS_COMPARER> (container)));
         }
 
 
