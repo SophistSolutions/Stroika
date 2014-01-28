d73bf66d816d78133a9a67df9c9f46ea17bcfa53
Lewis G. Pringle, Jr <lewis@sophists.com>
2014-01-27 22:25:56 -0500

    progress getting Tests makefile to implement run-tests (in prep for haivng it pay attention to params like run-remotely)

---
+++ Tests/ScriptsLib/PrintTestsList.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl 
+
+sub	GetThisScriptDir {
+	use File::Basename;
+	use Cwd 'abs_path';
+	my $p = __FILE__;
+	my $A = abs_path ($p);
+	my $dirname = dirname($A);
+	return $dirname;
+}
+my	$thisScriptDir	=	GetThisScriptDir ();
+
+require "$thisScriptDir/../../ScriptsLib/ConfigurationReader.pl";
+require "$thisScriptDir/TestsList.pl";
+my @allTests   = GetAllTests ();
+
+print "$_ " for @allTests;
