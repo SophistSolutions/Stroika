ca72237b5f22d1218d85880811b20dbe3ffa8222
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-01-29 08:08:59 -0500

    Test19_ConstCharStar_ () added test cases cuz I saw warnings from valgrind - found no bugs but added extra tests

--- Tests/02/Test.cpp
+++ Tests/02/Test.cpp
@@ -757,6 +757,10 @@ namespace   {
     void    Test19_ConstCharStar_ ()
     {
         VerifyTestResult (wcscmp (String (L"fred").c_str (), L"fred") == 0);
+        VerifyTestResult (wcscmp (String (L"0123456789abcde").c_str (), L"0123456789abcde") == 0);                                      // 15 chars
+        VerifyTestResult (wcscmp (String (L"0123456789abcdef").c_str (), L"0123456789abcdef") == 0);                                    // 16 chars
+        VerifyTestResult (wcscmp (String (L"0123456789abcdef0123456789abcde").c_str (), L"0123456789abcdef0123456789abcde") == 0);      // 31 chars
+        VerifyTestResult (wcscmp (String (L"0123456789abcdef0123456789abcdef").c_str (), L"0123456789abcdef0123456789abcdef") == 0);    // 32 chars
         {
             String  tmp =   L"333";
             VerifyTestResult (wcscmp (tmp.c_str (), L"333") == 0);
