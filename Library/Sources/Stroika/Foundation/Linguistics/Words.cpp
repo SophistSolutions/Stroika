e4f97acfc9290c68fbdf5e5358c9c1192a12e1e0
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-02-11 11:16:00 -0500

    Minor cleanup of Lingustics code (lose old std::string code)

--- Library/Sources/Stroika/Foundation/Linguistics/Words.cpp
+++ Library/Sources/Stroika/Foundation/Linguistics/Words.cpp
@@ -29,203 +29,28 @@ using   namespace   Stroika::Foundation::Linguistics;
 
 
 
-namespace  {
-    ////////// LEGACY CODE FROM OL STRINGUTILS MOVED HERE - PRBABLY SHOULD ALL BE REMOVED - BUT LEAVE FOR NOW ///////////////
-    ///////// -- LGP 2013-10-17
-    inline  char    TOLOWER (char c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if ('A' <= c and c <= 'Z') {
-            Ensure (std::tolower (c) == ((c - 'A') + 'a'));
-            return (c - 'A') + 'a';
-        }
-        else if (c <= 127) {
-            Ensure (std::tolower (c) == c);
-            return c;
-        }
-        else {
-            return std::tolower (c);
-        }
-#else
-        return std::tolower (c);
-#endif
-    }
-    inline  wchar_t TOLOWER (wchar_t c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if ('A' <= c and c <= 'Z') {
-            Ensure (towlower (c) == ((c - 'A') + 'a'));
-            return (c - 'A') + 'a';
-        }
-        else if (c <= 127) {
-            Ensure (wchar_t (towlower (c)) == c);
-            return c;
-        }
-        else {
-            return towlower (c);
-        }
-#else
-        return towlower (c);
-#endif
-    }
-    inline  bool    ISSPACE (char c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if (0 < c and c <= 127) {
-            //  space, tab, carriage return, newline, vertical tab or form feed
-            bool    r   =   (c == ' ' or c == '\t' or c == '\r' or c == '\n' or c == '\v' or c == '\f');
-            Ensure (!!isspace (c) == r);
-            return r;
-        }
-        else {
-            DISABLE_COMPILER_MSC_WARNING_START(4800)
-            return isspace (c);
-            DISABLE_COMPILER_MSC_WARNING_END(4800)
-        }
-#else
-        return !!isspace (c);
-#endif
-    }
-    inline  bool    ISSPACE (wchar_t c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if (0 < c and c <= 127) {
-            //  space, tab, carriage return, newline, vertical tab or form feed
-            bool    r   =   (c == ' ' or c == '\t' or c == '\r' or c == '\n' or c == '\v' or c == '\f');
-            Ensure (!!iswspace (c) == r);
-            return r;
-        }
-        else {
-            DISABLE_COMPILER_MSC_WARNING_START(4800)
-            return iswspace (c);
-            DISABLE_COMPILER_MSC_WARNING_END(4800)
-        }
-#else
-        return !!iswspace (c);
-#endif
-    }
-    inline  char    TOUPPER (char c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if ('a' <= c and c <= 'z') {
-            Ensure (std::toupper (c) == ((c - 'a') + 'A'));
-            return (c - 'a') + 'A';
-        }
-        else if (c <= 127) {
-            Ensure (std::toupper (c) == c);
-            return c;
-        }
-        else {
-            return std::toupper (c);
-        }
-#else
-        return std::toupper (c);
-#endif
-    }
-    inline  wchar_t TOUPPER (wchar_t c)
-    {
-#if     qUseASCIIRangeSpeedHack
-        if ('a' <= c and c <= 'z') {
-            Ensure (towupper (c) == ((c - 'a') + 'A'));
-            return (c - 'a') + 'A';
-        }
-        else if (c <= 127) {
-            Ensure (wchar_t (towupper (c)) == c);
-            return c;
-        }
-        else {
-            return towupper (c);
-        }
-#else
-        return towupper (c);
-#endif
-    }
-    void    toupper (wstring* s)
-    {
-        RequireNotNull (s);
-        wstring::iterator   end =   s->end ();
-        for (wstring::iterator i = s->begin (); i != end; ++i) {
-            *i = TOUPPER (*i);
-        }
-    }
-
-    void    toupper (string* s)
-    {
-        RequireNotNull (s);
-        string::iterator    end =   s->end ();
-        for (string::iterator i = s->begin (); i != end; ++i) {
-            *i = TOUPPER (*i);
-        }
-    }
-    inline  wstring toupper (const wstring& s)
-    {
-        wstring r   =   s;
-        toupper (&r);
-        return r;
-    }
-    inline  string  toupper (const string& s)
-    {
-        string  r   =   s;
-        toupper (&r);
-        return r;
-    }
-}
-
-
-
-
 /*
  ********************************************************************************
  ****************************** PluralizeNoun ***********************************
  ********************************************************************************
  */
-namespace   {
-    template    <typename STRING>
-    inline  STRING  PluralizeNoun_HLPR (const STRING& str, int count)
-    {
-        // Implement VERY WEAK ENGLISH rules for now... (ignores y ->ies, and other cases too)
-        if (count == 1) {
-            return str;
-        }
-        else {
-            STRING  tmp =   str;
-            tmp.push_back ('s');
-            return tmp;
-        }
-    }
-}
-#if 1
 String Linguistics::PluralizeNoun (const String& s, int count)
 {
-    return PluralizeNoun_HLPR (s, count);
+    // Implement VERY WEAK ENGLISH rules for now... (ignores y ->ies, and other cases too)
+    if (count == 1) {
+        return s;
+    }
+    else {
+        String  tmp =   s;
+        tmp.push_back ('s');
+        return tmp;
+    }
 }
 
 String  Linguistics::PluralizeNoun (const String& s, const String& sPlural, int count)
 {
     return count == 1 ? s : sPlural;
 }
-
-#else
-string  Linguistics::PluralizeNoun (const string& s, int count)
-{
-    return PluralizeNoun_HLPR (s, count);
-}
-
-wstring Linguistics::PluralizeNoun (const wstring& s, int count)
-{
-    return PluralizeNoun_HLPR (s, count);
-}
-
-string  Linguistics::PluralizeNoun (const string& s, const string& sPlural, int count)
-{
-    return count == 1 ? s : sPlural;
-}
-
-wstring Linguistics::PluralizeNoun (const wstring& s, const wstring& sPlural, int count)
-{
-    return count == 1 ? s : sPlural;
-}
-#endif
 
 
 
@@ -237,62 +62,43 @@ wstring Linguistics::PluralizeNoun (const wstring& s, const wstring& sPlural, in
  **************************** MungeStringSoSingular *****************************
  ********************************************************************************
  */
-namespace   {
-    template    <typename STR>
-    inline  STR MungeStringSoSingular_ (const STR& s)
-    {
-        STR r   =   s;
-        // take an ENGLISH string (assume English) - and munge it so its singular (if it happened to have been plural)
-
-        // hande special case of 'wives' -> 'wife' as in 'midwives'
-        if (r.length () >= 5) {
-            size_t  l   =   r.length ();
-            if (s[l - 5] == 'w' and s[l - 4] == 'i' and s[l - 3] == 'v' and s[l - 2] == 'e' and s[l - 1] == 's') {
-                r = r.substr (0, l - 3);
-                r.push_back ('f');
-                r.push_back ('e');
-                return r;
-            }
-        }
-
-        // trim trailing s from the name (if prev letter is a non-s consonant)
-        // or change 'ies' to 'y' at end
-        if (s.length () > 3) {
-            if (s[s.length () - 3] == 'i' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
-                //r = s.substr (0, s.length () - 3) + "y";
-                r = s.substr (0, s.length () - 3);
-                r.push_back ('y');
-            }
-            else if (s.length () > 4 and s[s.length () - 4] == 's' and s[s.length () - 3] == 's' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
-                r = s.substr (0, s.length () - 2);
-            }
-            else if (s[s.length () - 3] == 's' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
-                r = s.substr (0, s.length () - 1);
-            }
-            // because of diabets mellitus - (and others???? - don't map trailing 'us' to 'u'
-            else if (s[s.length () - 1] == 's' and s[s.length () - 2].IsAscii () and s[s.length () - 2].IsAlphabetic () and (s[s.length () - 2] != 's' and s[s.length () - 2] != 'u')) {
-                r = s.substr (0, s.length () - 1);
-            }
-        }
-        return r;
-    }
-}
-#if 1
 String  Linguistics::MungeStringSoSingular (const String& s)
 {
-    return MungeStringSoSingular_ (s);
-}
-#else
-wstring Linguistics::MungeStringSoSingular (const wstring& s)
-{
-    return MungeStringSoSingular_ (s);
-}
+    String r   =   s;
+    // take an ENGLISH string (assume English) - and munge it so its singular (if it happened to have been plural)
 
-string  Linguistics::MungeStringSoSingular (const string& s)
-{
-    return MungeStringSoSingular_ (s);
+    // hande special case of 'wives' -> 'wife' as in 'midwives'
+    if (r.length () >= 5) {
+        size_t  l   =   r.length ();
+        if (s[l - 5] == 'w' and s[l - 4] == 'i' and s[l - 3] == 'v' and s[l - 2] == 'e' and s[l - 1] == 's') {
+            r = r.substr (0, l - 3);
+            r.push_back ('f');
+            r.push_back ('e');
+            return r;
+        }
+    }
+
+    // trim trailing s from the name (if prev letter is a non-s consonant)
+    // or change 'ies' to 'y' at end
+    if (s.length () > 3) {
+        if (s[s.length () - 3] == 'i' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
+            //r = s.substr (0, s.length () - 3) + "y";
+            r = s.substr (0, s.length () - 3);
+            r.push_back ('y');
+        }
+        else if (s.length () > 4 and s[s.length () - 4] == 's' and s[s.length () - 3] == 's' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
+            r = s.substr (0, s.length () - 2);
+        }
+        else if (s[s.length () - 3] == 's' and s[s.length () - 2] == 'e' and s[s.length () - 1] == 's') {
+            r = s.substr (0, s.length () - 1);
+        }
+        // because of diabets mellitus - (and others???? - don't map trailing 'us' to 'u'
+        else if (s[s.length () - 1] == 's' and s[s.length () - 2].IsAscii () and s[s.length () - 2].IsAlphabetic () and (s[s.length () - 2] != 's' and s[s.length () - 2] != 'u')) {
+            r = s.substr (0, s.length () - 1);
+        }
+    }
+    return r;
 }
-#endif
 
 
 
@@ -305,28 +111,6 @@ string  Linguistics::MungeStringSoSingular (const string& s)
  ************************************ CapitalizeEachWord ************************
  ********************************************************************************
  */
-#if 0
-namespace   {
-    template    <typename STR>
-    inline  STR CapitalizeEachWord_ (const STR& s)
-    {
-        STR r;
-        // take an ENGLISH string (assume English)
-
-        // toupper each lower-case character preceeded by a space
-        bool    prevCharSpace   =   true;   // so we upper first char
-        for (Character i : r)
-            if (prevCharSpace) {
-                i = i.ToUpperCase ();
-            }
-        r += i;
-        prevCharSpace = i.IsWhitespace ();
-    }
-    return r;
-}
-}
-#endif
-#if 1
 String Linguistics::CapitalizeEachWord (const String& s)
 {
     String r;
@@ -343,17 +127,6 @@ String Linguistics::CapitalizeEachWord (const String& s)
     }
     return r;
 }
-#else
-wstring Linguistics::CapitalizeEachWord (const wstring& s)
-{
-    return CapitalizeEachWord_ (s);
-}
-
-string  Linguistics::CapitalizeEachWord (const string& s)
-{
-    return CapitalizeEachWord_ (s);
-}
-#endif
 
 
 
@@ -366,31 +139,6 @@ string  Linguistics::CapitalizeEachWord (const string& s)
  ******************************* CapitalizeEachSentence *************************
  ********************************************************************************
  */
-namespace   {
-    template    <typename STR>
-    inline  STR CapitalizeEachSentence_ (const STR& s)
-    {
-        // WAY too kludgly - but hopefully adequate for primitive message cleanups...
-        //      -- LGP 2008-09-20
-        STR r   =   s;
-        // take an ENGLISH string (assume English)
-
-        // toupper each lower-case character preceeded by a ENDOFSENTECE PUNCT
-        bool    nextCharStartsSentence  =   true;   // so we upper first char
-        for (auto i = r.begin (); i != r.end (); ++i) {
-            if (nextCharStartsSentence and !ISSPACE (*i)) {
-                *i = TOUPPER (*i);
-                nextCharStartsSentence = false;
-            }
-            else {
-                nextCharStartsSentence |= (*i == '.' or * i == '!' or * i == '?');
-            }
-        }
-        return r;
-    }
-}
-
-#if 1
 String  Linguistics::CapitalizeEachSentence (const String& s)
 {
     // WAY too kludgly - but hopefully adequate for primitive message cleanups...
@@ -412,17 +160,6 @@ String  Linguistics::CapitalizeEachSentence (const String& s)
     }
     return r;
 }
-#else
-wstring Linguistics::CapitalizeEachSentence (const wstring& s)
-{
-    return CapitalizeEachSentence_ (s);
-}
-
-string  Linguistics::CapitalizeEachSentence (const string& s)
-{
-    return CapitalizeEachSentence_ (s);
-}
-#endif
 
 
 
@@ -436,22 +173,6 @@ string  Linguistics::CapitalizeEachSentence (const string& s)
  ******************************** UnCapitalizeFirstWord *************************
  ********************************************************************************
  */
-namespace   {
-    template    <typename STR>
-    inline  STR UnCapitalizeFirstWord_ (const STR& s)
-    {
-        // WAY too kludgly - but hopefully adequate for primitive message cleanups...
-        //      -- LGP 2008-09-20
-        STR r   =   s;
-        if (r.length () > 2) {
-            if (TOUPPER (r[0]) == r[0] and TOUPPER (r[1]) != r[1]) {
-                r[0] = TOLOWER (r[0]);
-            }
-        }
-        return r;
-    }
-}
-#if 1
 String Linguistics::UnCapitalizeFirstWord (const String& s)
 {
     // WAY too kludgly - but hopefully adequate for primitive message cleanups...
@@ -464,17 +185,6 @@ String Linguistics::UnCapitalizeFirstWord (const String& s)
     }
     return r;
 }
-#else
-wstring Linguistics::UnCapitalizeFirstWord (const wstring& s)
-{
-    return UnCapitalizeFirstWord_ (s);
-}
-
-string  Linguistics::UnCapitalizeFirstWord (const string& s)
-{
-    return UnCapitalizeFirstWord_ (s);
-}
-#endif
 
 
 
@@ -486,22 +196,10 @@ string  Linguistics::UnCapitalizeFirstWord (const string& s)
  ********************************** IsAllCaps ***********************************
  ********************************************************************************
  */
-#if 1
 bool    Linguistics::IsAllCaps (const String& s)
 {
     return not s.empty () and s == s.ToUpperCase ();
 }
-#else
-bool    Linguistics::IsAllCaps (const string& s)
-{
-    return not s.empty () and s == toupper (s);
-}
-
-bool    Linguistics::IsAllCaps (const wstring& s)
-{
-    return not s.empty () and s == toupper (s);
-}
-#endif
 
 
 
--- Library/Sources/Stroika/Foundation/Linguistics/Words.h
+++ Library/Sources/Stroika/Foundation/Linguistics/Words.h
@@ -6,10 +6,8 @@
 
 #include    "../StroikaPreComp.h"
 
-#include    <string>
-
-#include    "../Configuration/Common.h"
 #include    "../Characters/String.h"
+#include    "../Configuration/Common.h"
 
 
 
@@ -18,10 +16,10 @@
  *  \version    <a href="code_status.html#Early-Alpha">Early-Alpha</a>
  *
  * TODO:
- *      (o)     Very VERY primitive linguistic support, but this could easily evolve over time. Led has a bunch of
+ *      @todo   Very VERY primitive linguistic support, but this could easily evolve over time. Led has a bunch of
  *              lingusitic code I could move here, and I'm sure I could dig up more...
  *
- *      @todo   Lose CString variants - do purely with String class.
+ *      @todo   Perhaps these functions should take as arugment a locale() object. This is currently totally English-biased/based
  */
 
 
@@ -30,14 +28,11 @@ namespace   Stroika {
     namespace   Foundation {
         namespace   Linguistics {
 
-#if 0
-            using   std::string;
-            using   std::wstring;
-#endif
+
             using   Characters::String;
 
 
-            /*
+            /**
              *  Implement current-ui-language-specific noun-pluralization logic for the given noun string (assuming the count of that noun
              *  is given (english rules - if count != 1 - append s, but we dont wnat that logic to proliferate through the app, so
              *  its easier to localize.
@@ -46,65 +41,34 @@ namespace   Stroika {
              */
             String PluralizeNoun (const String& s, int count = 1000);
             String  PluralizeNoun (const String& s, const String& sPlural, int count = 1000);
-#if 0
-            string  PluralizeNoun (const string& s, int count = 1000);
-            wstring PluralizeNoun (const wstring& s, int count = 1000);
-            string  PluralizeNoun (const string& s, const string& sPlural, int count = 1000);
-            wstring PluralizeNoun (const wstring& s, const wstring& sPlural, int count = 1000);
-#endif
 
 
-#if 1
             /**
              */
             String MungeStringSoSingular (const String& s);
-#else
-            wstring MungeStringSoSingular (const wstring& s);
-            string  MungeStringSoSingular (const string& s);
-#endif
 
 
-#if 1
             /**
              * e.g. 'joe smith' becomes 'Joe Smith'
              */
             String  CapitalizeEachWord (const String& s);
-#else
-            string  CapitalizeEachWord (const string& s);   // e.g. 'joe smith' becomes 'Joe Smith'
-            wstring CapitalizeEachWord (const wstring& s);
-#endif
 
 
-#if 1
             /**
              * e.g. 'joe smith' becomes 'Joe smith'
              */
             String CapitalizeEachSentence (const String& s);
-#else
-            string  CapitalizeEachSentence (const string& s);   // e.g. 'joe smith' becomes 'Joe smith'
-            wstring CapitalizeEachSentence (const wstring& s);
-#endif
 
 
-#if 1
             /**
              * e.g.  'Joe Smith' becomes 'joe Smith', but 'IBM eats' stays 'IBM eats'
              */
             String UnCapitalizeFirstWord (const String& s);
-#else
-            string  UnCapitalizeFirstWord (const string& s);    // e.g. 'Joe Smith' becomes 'joe Smith', but 'IBM eats' stays 'IBM eats'
-            wstring UnCapitalizeFirstWord (const wstring& s);
-#endif
 
 
-#if 1
             /**
              */
             bool    IsAllCaps (const String& s);
-#else
-            bool    IsAllCaps (const string& s);
-            bool    IsAllCaps (const wstring& s);
-#endif
 
 
         }
