ea6e6f04c36477dbf92962eaffa84c93842cfb8a
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-02-10 22:24:40 -0500

    defined bug define qCompilerAndStdLib_UsingInheritedConstructor_Buggy cuz I was going to use this feature, but found a simpler way. Leave define in since I will likely use this feature again

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -238,6 +238,49 @@
 
 
 
+
+
+
+
+/*
+@CONFIGVAR:     qCompilerAndStdLib_UsingInheritedConstructor_Buggy
+@DESCRIPTION:
+EXAMPLE:
+        struct UnAssignedableCharacter : Character {
+#if     qCompilerAndStdLib_UsingInheritedConstructor_Buggy
+            UnAssignedableCharacter (const Character&c) : Character (c) {}
+#else
+            using Character::Character;
+#endif
+
+            operator Character () const
+            {
+                return *this;
+            }
+
+            const UnAssignedableCharacter& operator= (const UnAssignedableCharacter&) = delete;
+        };
+*/
+#ifndef qCompilerAndStdLib_UsingInheritedConstructor_Buggy
+
+#if     defined (__GNUC__) && !defined (__clang__)
+#define qCompilerAndStdLib_UsingInheritedConstructor_Buggy       (__GNUC__ < 4 || (__GNUC__ == 4 && (__GNUC_MINOR__ <= 7)))
+#elif   defined (_MSC_VER)
+#define qCompilerAndStdLib_UsingInheritedConstructor_Buggy      (_MSC_VER == _MS_VS_2k13_VER_)
+#else
+#define qCompilerAndStdLib_UsingInheritedConstructor_Buggy       0
+#endif
+
+#endif
+
+
+
+
+
+
+
+
+
 /*
 @CONFIGVAR:     qCompilerAndStdLib_regex_Buggy
 @DESCRIPTION:   <p>Defined true if the compiler supports regex_replace</p>
