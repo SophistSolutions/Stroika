42873a1a7524ea3abb295b01f7361a89e24b0d5b
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-03-13 20:48:46 -0400

    lose deprecated CopyableMutex and WaitAbandonedException

--- Library/Sources/Stroika/Foundation/Execution/CopyableMutex.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright(c) Sophist Solutions, Inc. 1990-2014.  All rights reserved
- */
-#include    "../StroikaPreComp.h"
-
-#include    "../Configuration/StroikaConfig.h"
-
-//#include    "CopyableMutex.h"
