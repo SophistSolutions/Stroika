008967437199ce87e03fe7cbb07d9280e3eff2d4
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-04-19 13:47:10 -0400

    Added Platform/Windows/CriticalSectionRecursiveMutex

---
+++ Library/Sources/Stroika/Foundation/Execution/Platform/Windows/CriticalSectionRecursiveMutex.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2014.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_h_
+#define _Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_h_   1
+
+#include    "../../../StroikaPreComp.h"
+
+#if     qPlatform_Windows
+#include    <Windows.h>
+#else
+#error "WINDOWS REQUIRED FOR THIS MODULE"
+#endif
+
+#include    "../../../Configuration/Common.h"
+
+
+/**
+ *
+ *  \version    <a href="code_status.html#Alpha-Early">Alpha-Early</a>
+ *
+ */
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Execution {
+            namespace   Platform {
+                namespace   Windows {
+
+
+                    /**
+                     *  (reason is to test if perofrmance better than msvc mutex)
+                     */
+                    class   CriticalSectionRecursiveMutex {
+                    public:
+                        CriticalSectionRecursiveMutex ();
+                        CriticalSectionRecursiveMutex (const CriticalSectionRecursiveMutex&) = delete;
+
+                    public:
+                        ~CriticalSectionRecursiveMutex ();
+
+                    public:
+                        CriticalSectionRecursiveMutex& operator= (const CriticalSectionRecursiveMutex&) = delete;
+
+                    public:
+                        nonvirtual  void    lock ();
+                        nonvirtual  void    unlock ();
+
+                    private:
+                        CRITICAL_SECTION    fCritSec_;
+                    };
+
+
+                }
+            }
+        }
+    }
+}
+
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include    "CriticalSectionRecursiveMutex.inl"
+
+#endif  /*_Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_h_*/
---
+++ Library/Sources/Stroika/Foundation/Execution/Platform/Windows/CriticalSectionRecursiveMutex.inl
@@ -0,0 +1,50 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2014.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_inl_
+#define _Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_inl_ 1
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Execution {
+            namespace   Platform {
+                namespace   Windows {
+
+
+                    /*
+                     ********************************************************************************
+                     ****************************** CriticalSectionRecursiveMutex ****************************
+                     ********************************************************************************
+                     */
+                    inline  CriticalSectionRecursiveMutex::CriticalSectionRecursiveMutex ()
+                        : fCritSec_ ()
+                    {
+                        ::InitializeCriticalSection (&fCritSec_);
+                    }
+                    inline  CriticalSectionRecursiveMutex::~CriticalSectionRecursiveMutex ()
+                    {
+                        ::DeleteCriticalSection (&fCritSec_);
+                    }
+                    inline  void    CriticalSectionRecursiveMutex::lock ()
+                    {
+                        ::EnterCriticalSection (&fCritSec_);
+                    }
+                    inline  void    CriticalSectionRecursiveMutex::unlock ()
+                    {
+                        ::LeaveCriticalSection (&fCritSec_);
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
+#endif  /*_Stroika_Foundation_Execution_Platform_Windows_CriticalSectionRecursiveMutex_inl_*/
