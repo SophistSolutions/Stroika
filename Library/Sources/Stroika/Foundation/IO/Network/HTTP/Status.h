f2bcd10e94b0f89bcf203498cfaa2e67f9d68c6e
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-10-04 13:09:26 -0400

    use constexpr

--- Library/Sources/Stroika/Foundation/IO/Network/HTTP/Status.h
+++ Library/Sources/Stroika/Foundation/IO/Network/HTTP/Status.h
@@ -34,25 +34,25 @@ namespace   Stroika {
                      * See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html for details on these status codes.
                      */
                     namespace   StatusCodes {
-                        const   Status  kOK                 =   200;
-                        const   Status  kMovedPermanently   =   301;
-                        const   Status  kNotModified        =   304;
+                        constexpr   Status  kOK                 =   200;
+                        constexpr   Status  kMovedPermanently   =   301;
+                        constexpr   Status  kNotModified        =   304;
                         /*
                          * Bad CLIENT request.
                          * The request could not be understood by the server due to malformed syntax.
                          * The client SHOULD NOT repeat the request without modifications.
                          */
-                        const   Status  kBadRequest         =   400;
-                        const   Status  kUnauthorized       =   401;
-                        const   Status  kNotFound           =   404;
+                        constexpr   Status  kBadRequest         =   400;
+                        constexpr   Status  kUnauthorized       =   401;
+                        constexpr   Status  kNotFound           =   404;
                         /*
                          * This is principally for internal server exceptions.
                          */
-                        const   Status  kInternalError      =   500;
+                        constexpr   Status  kInternalError      =   500;
                         /*
                          * Tndicates that the server is temporarily unavailable, usually due to high load or maintenance.
                          */
-                        const   Status  kServiceUnavailable =   503;
+                        constexpr   Status  kServiceUnavailable =   503;
                     }
 
 
