9a1c2a05ca9c8ee96e76fbcb292d4c91e165c287
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-10-04 13:37:56 -0400

    fixed typo inClient_libcurl

--- Library/Sources/Stroika/Foundation/IO/Network/Transfer/Client_libcurl.cpp
+++ Library/Sources/Stroika/Foundation/IO/Network/Transfer/Client_libcurl.cpp
@@ -263,7 +263,7 @@ Response    Connection_LibCurl::Rep_::Send (const Request& request)
 
     Mapping<String, String>  overrideHeaders = request.fOverrideHeaders;
     if (fOptions.fAssumeLowestCommonDenominatorHTTPServer) {
-        static  const   Syncrhonized<Mapping<String, String>>    kSilenceTheseHeaders_ = initializer_list<pair<String, String>> {
+        static  const   Synchronized<Mapping<String, String>>    kSilenceTheseHeaders_ = initializer_list<pair<String, String>> {
             { String_Constant (L"Expect"), String ()},
             { String_Constant (L"Transfer-Encoding"), String ()}
         };
