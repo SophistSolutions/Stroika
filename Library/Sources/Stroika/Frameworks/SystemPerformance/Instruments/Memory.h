a83bde732391c7f553e84a8c2c9ee94cfd91f722
Lewis G. Pringle, Jr <lewis@RecordsForLiving.com>
2014-10-29 17:32:18 -0400

    Worked out docs, sources, and definitions for first draft of Frameworks/SystemPerformance/Instruments/Memory

--- Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Memory.h
+++ Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Memory.h
@@ -7,6 +7,7 @@
 #include    "../../StroikaPreComp.h"
 
 #include    "../../../Foundation/DataExchange/ObjectVariantMapper.h"
+#include    "../../../Foundation/Memory/Optional.h"
 
 #include    "../Instrument.h"
 
@@ -31,11 +32,77 @@ namespace   Stroika {
 
 
                     using   DataExchange::ObjectVariantMapper;
+                    using   Foundation::Memory::Optional;
+
 
                     /**
-                     *
+                     *      @see https://www.centos.org/docs/5/html/5.1/Deployment_Guide/s2-proc-meminfo.html
                      */
                     struct  Info {
+                        /**
+                         *  The amount of physical RAM, left unused by the system (in bytes).
+                         *
+                         *  /proc/meminfo::MemFree
+                         */
+                        Optional<uint64_t>  fFreePhysicalMemory {};
+
+                        /**
+                         *      The total amount of allocated virtual address space (in bytes).
+                         *
+                         *      /proc/meminfo::VMallocTotal
+                         */
+                        Optional<uint64_t>  fTotalVirtualMemory {};
+
+                        /**
+                         *      The total amount of used virtual address space (in bytes).
+                         *
+                         *      /proc/meminfo::VMallocUsed
+                         */
+                        Optional<uint64_t>  fUsedVirtualMemory {};
+
+                        /**
+                         *      The largest contiguous block of available virtual memory (in bytes).
+                         *
+                         *      /proc/meminfo::VMallocChunk
+                         */
+                        Optional<uint64_t>  fLargestAvailableVirtualChunk {};
+
+                        /**
+                         *  @see http://en.wikipedia.org/wiki/Page_fault
+                         *  @see http://www.linuxinsight.com/proc_vmstat.html
+                         *
+                         *  fMajorPageFaultsSinceBoot   is the number of major (requiring a disk read)
+                         *  faults since the system booted.
+                         *
+                         *  @see /proc/vmstat::pgmajfault
+                         *  @see /proc/vmstat::pgfault
+                         */
+                        Optional<double>    fMajorPageFaultsSinceBoot {};
+
+                        /**
+                         *  @see http://en.wikipedia.org/wiki/Page_fault
+                         *  @see http://www.linuxinsight.com/proc_vmstat.html
+                         *
+                         *  fMinorPageFaultsSinceBoot   is the number of minor (not requiring a disk read)
+                         *  faults since the system booted.
+                         *
+                         *  @see /proc/vmstat::pgfault and proc/vmstat::pgmajfault (this is pgfault-pgmajfault)
+                         */
+                        Optional<double>    fMinorPageFaultsSinceBoot {};
+
+                        /**
+                         *  @see fMajorPageFaultsSinceBoot.
+                         *
+                         *  This is not computed in the first call to the intstrument, but based on successive calls
+                         */
+                        Optional<double>    fMajorPageFaultsPerSecond {};
+
+                        /**
+                         *  @see fMinorPageFaultsSinceBoot.
+                         *
+                         *  This is not computed in the first call to the intstrument, but based on successive calls
+                         */
+                        Optional<double>    fMinorPageFaultsPerSecond {};
                     };
 
 
