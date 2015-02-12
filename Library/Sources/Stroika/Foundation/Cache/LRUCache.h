882488cbecb38a71f971c764d6abaa495c6d17c1
Lewis Pringle <lewis@sophists.com>
2015-02-11 21:15:56 -0500

    LRUCache cleanups

--- Library/Sources/Stroika/Foundation/Cache/LRUCache.h
+++ Library/Sources/Stroika/Foundation/Cache/LRUCache.h
@@ -22,36 +22,14 @@
  *
  * TODO:
  *
- *  @todo   Make sure the Mapping<> is constructed with TRAITS that allow for the proper "EQUALS" method.
+ *		@todo   Make sure the Mapping<> is constructed with TRAITS that allow for the proper "EQUALS" method.
  *
- *  @todo   Hash_SFINAE_<> IS HORRIBLE HACK!!!! CLEANUP!!! Tricky... SFINAE -- SEE SerializeForHash1_
+ *		@todo	Find some reasonable/simple way to get 
+ *				LRUCache<PHRShortcutSpec, PHRShortcutSpec, PHRShortcutSpecNoAuthCacheTraits_>   sRecentlyUsedCache (kMaxEltsInReceltlyUsedCache_);
+ *				Working with ONE T argument
+ *				Add(elt2cache).
  *
- *
- *          >>>> BIG PICTURE TODO - REVIEW THIS TODO LIST CUZ MOST ALREADY DONE NOW, BUT INCOMPLETE SO LEAVE TIL CONVERSION COMPLETE <<<<
- *
- *      @todo   Look at nu_LRUCache<> and consider possability of migrating to it, or something like that
- *              Much slower, but threadsafe, and simpler to use.
- *
- *              Probably migrate older code to class Legacy_LRUCache<>
- *
- *      @todo   TERRIBLE API!
- *                  LRUCache<string> tmp (3);
- *                  *tmp.AddNew ("x") = "x";
- *                  *tmp.AddNew ("y") = "y";
- *                  *tmp.AddNew ("z") = "z";
- *                  *tmp.AddNew ("a") = "a";
- *              So awkward to use!!! - find better - more map like - API
- *
- *      @todo   One major source of UNSAFETY (especially thread unsafety) is
- *              AddNew/Lookup() API returning internal pointers. We really want this to do copy by
- *              value. But look where its used, and see the costs are not too high of doing copy my value.
- *              Or maybe use concext update object (maybe taking a lambda) for the update).
- *
- *      @todo   NOTE - I'm NOT sure public API for this is stable yet - there are many problems (though
- *              I've used this class quite a lot - its not buggy - just not flexibly designed or good
- *              thread safety properties).
- *
- *      @todo   Test and make sure ITERATOR stuff works properly when using Hashing mode (HASTABLE SIZE > 1)
+ *		@todo   Hash_SFINAE_<> IS HORRIBLE HACK!!!! CLEANUP!!! Tricky... SFINAE -- SEE SerializeForHash1_
  *
  *      @todo   Currently we have redundant storage - _Buf, and _First, and _Last (really just need _Buf cuz
  *              has first/last, or do our own storage managemnet with secondary array? - we do the mallocs/frees.
@@ -62,32 +40,12 @@
  *              in impl, consider using _fEnd? Or if it is (I think last maybe better then document clearly why
  *              its better.
  *
- *      @todo   Need way to produce ITERABLE (copy??? of cache data - so callers can LOOK at what is in teh cache)
- *
- *      @todo   Consider restructuring the API more like STL-MAP
- *              KEY,VALUE, intead of LRUCache<ELEMENT> and the ability to extract an element.
- *              Doing this MIGHT even allow us to make this class fit more neatly within the Stroika container
- *              pantheon. BUt it will be a bit of a PITA for all/some of the existing LRUCache<> uses...
- *
- *              Problem of doing this is that in some cases - we may want to create a cache of objects that
- *              already pre-exist, and have the 'key part' as a subobject of themselves. Not a killer as we
- *              COULD just call the KEY/ELEMENT the same type and just use a simple test traits function
- *              that just pays attention to the logical key parts?
- *
- *      @todo   Cleanup docs to reflect new TRAITS style, and document each public method (still todo) and docs for
- *              main class needs cleanup).
- *
- *              AND docs for main class need a simple example of usage (without a traits override).
- *
  *      @todo   Verify the size of  TRAITS::StatsType   fStats; is zero, or go back to old qKeepLRUCacheStats
  *              macro stuff to avoid wasted space.
  *                  o  Trouble with subobject approach is C++ seems to force all objects to be at least one byte,
  *                      so there WOULD be cost. Could avoid that by having the TRAITS
  *                      OBJECT ITSELF be what owns the counters - basically global vars. Since just
  *                      used for testing, could still be usable that way...
- *
- *      @todo   LRUCache CONSTRUCTS all its elements, even if they arent in the cache, That might be bad. Redo if not
- *              too hard so when items removed from cache (use optional) they arent really there. Cached objects COULD be big.
  *
  *      @todo   Come up with easy way to persist cache. I suppose this counts on fact that you can load/store
  *              cached elements – so persist mechanism must parameterize that).
@@ -181,6 +139,10 @@ namespace   Stroika {
             /**
              *  LRUCache is NOT threadsafe (checks usage with Debug::AssertExternallySynchronizedLock), so typical uses would use Execution::Synchonized.
              *
+			 *	\note	LRUCache destroys objects when they are cleared from the cache. This guarantee is
+			 *			relevant only in case where the objects use significant resources, or where their lifetime has
+			 *			externally visible (e.g. lockfiles) impact.
+			 *
              *  EXAMPLE USAGE:
              *      Execution::Synchronized<LRUCache<DetailsID, Details_>>      sDetailsCache_; // caches often helpful in multithreaded situations
              */
@@ -325,46 +287,25 @@ namespace   Stroika {
                 class   LRUCache_ {
                 public:
                     using ELEMENT = LEGACYLRUCACHEOBJ_;
-                public:
                     using   ElementType     =   typename LEGACYLRUCACHEOBJ_TRAITS_::ElementType;
                     using   KeyType         =   typename LEGACYLRUCACHEOBJ_TRAITS_::KeyType;
 
-                public:
                     LRUCache_ (size_t maxCacheSize);
                     LRUCache_ () = delete;
                     LRUCache_ (const LRUCache_&) = delete;
-
-                public:
                     nonvirtual  LRUCache_& operator= (const LRUCache_&) = delete;
 
-                public:
                     nonvirtual  size_t  GetMaxCacheSize () const;
                     nonvirtual  void    SetMaxCacheSize (size_t maxCacheSize);
 
-                public:
                     struct  CacheIterator;
-                public:
                     nonvirtual  CacheIterator   begin ();
                     nonvirtual  CacheIterator   end ();
 
-                public:
-                    nonvirtual  void    ClearCache ();
-
-                public:
-                    /**
-                     *  NOTE - though you can CHANGE the value of ELEMENT, it is illegal to change its KEY part/key
-                     *  value if you specified HASH_TABLE_SIZE != 1 in TRAITS object.
-                     */
+                    nonvirtual  void		ClearCache ();
                     nonvirtual  ELEMENT*    AddNew (const KeyType& item);
-
-                public:
-                    /*
-                     *  NOTE - though you can CHANGE the value of ELEMENT, it is illegal to change its KEY part/key
-                     *  value if you specified HASH_TABLE_SIZE != 1 in TRAITS object.
-                     */
                     nonvirtual  ELEMENT*    LookupElement (const KeyType& item);
 
-                public:
                     typename LEGACYLRUCACHEOBJ_TRAITS_::StatsType  fStats;
 
                 private:
