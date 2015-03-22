0bea84b922a24b0fb190d05b05d53c25a9e2f46a
Lewis Pringle <lewis@sophists.com>
2015-03-22 10:18:44 -0400

    fixed #include of DataExcahnge::XML::Reader

--- Library/Sources/Stroika/Foundation/DataExchange/XML/Reader.h
+++ Library/Sources/Stroika/Foundation/DataExchange/XML/Reader.h
@@ -1,8 +1,8 @@
 /*
  * Copyright(c) Sophist Solutions, Inc. 1990-2015.  All rights reserved
  */
-#ifndef _Stroika_Foundation_DataExchange_JSON_Reader_h_
-#define _Stroika_Foundation_DataExchange_JSON_Reader_h_   1
+#ifndef _Stroika_Foundation_DataExchange_XML_Reader_h_
+#define _Stroika_Foundation_DataExchange_XML_Reader_h_   1
 
 #include    "../../StroikaPreComp.h"
 
@@ -78,4 +78,4 @@ namespace   Stroika {
  ********************************************************************************
  */
 
-#endif  /*_Stroika_Foundation_DataExchange_JSON_Reader_h_*/
+#endif  /*_Stroika_Foundation_DataExchange_XML_Reader_h_*/
