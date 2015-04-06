2f28d38f47d1dd712bfd83148026a6a396ec903e
Lewis Pringle <lewis@sophists.com>
2015-04-06 09:50:29 -0400

    fixed missing namespace in definition

--- Library/Sources/Stroika/Foundation/Configuration/SystemConfiguration.cpp
+++ Library/Sources/Stroika/Foundation/Configuration/SystemConfiguration.cpp
@@ -344,7 +344,7 @@ SystemConfiguration::ComputerNames Configuration::GetSystemConfiguration_Compute
  ****************** SystemConfiguration GetSystemConfiguration ******************
  ********************************************************************************
  */
-inline  SystemConfiguration GetSystemConfiguration ()
+inline  SystemConfiguration Configuration::GetSystemConfiguration ()
 {
     return SystemConfiguration {
         GetSystemConfiguration_CPU (),
