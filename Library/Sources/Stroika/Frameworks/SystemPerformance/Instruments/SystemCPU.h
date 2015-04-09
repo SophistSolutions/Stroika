6db1c168f80aa6cf0e7b83c7d16303b4c004b84f
Lewis Pringle <lewis@sophists.com>
2015-04-09 12:34:33 -0400

    New draft Frameworks/SystemPerformance/Instruments/SystemCPU module

---
+++ Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/SystemCPU.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2015.  All rights reserved
+ */
+#ifndef _Stroika_Framework_SystemPerformance_Instruments_SystemCPU_h_
+#define _Stroika_Framework_SystemPerformance_Instruments_SystemCPU_h_ 1
+
+#include    "../../StroikaPreComp.h"
+
+#include    "../../../Foundation/DataExchange/ObjectVariantMapper.h"
+
+#include    "../Instrument.h"
+
+
+
+/*
+ *  \file
+ *
+ *  \version    <a href="code_status.html#Late-Alpha">Late-Alpha</a>
+ *
+ */
+
+
+
+namespace   Stroika {
+    namespace   Frameworks {
+        namespace   SystemPerformance {
+            namespace   Instruments {
+
+//#define qSupport_SystemPerformance_Instruments_SystemCPU_LoadAverage 1
+#ifndef qSupport_SystemPerformance_Instruments_SystemCPU_LoadAverage
+#define qSupport_SystemPerformance_Instruments_SystemCPU_LoadAverage  qPlatform_POSIX
+#endif
+
+
+                namespace   SystemCPU {
+
+
+
+
+                    using   DataExchange::ObjectVariantMapper;
+                    using   Memory::Optional_Indirect_Storage;
+
+                    /**
+                     *
+                     */
+                    struct  Info {
+#if     qSupport_SystemPerformance_Instruments_SystemCPU_LoadAverage
+                        struct  LoadAverage;
+                        Optional_Indirect_Storage<LoadAverage>  fLoadAverage;
+#endif
+
+                        /**
+                         *  This is a number from 0..1, and is the weighted average across all CPU cores (so
+                         *  even on a 4 core machine, this can never execeed 1).
+                         */
+                        double  fTotalCPUUsage {};
+                    };
+
+
+#if     qSupport_SystemPerformance_Instruments_SystemCPU_LoadAverage
+                    struct  Info::LoadAverage {
+                        double  f1MinuteAve;
+                        double  f5MinuteAve;
+                        double  f15MinuteAve;
+                        LoadAverage (double oneMinuteAve, double fiveMinuteAve, double fifteenMinuteAve);
+                    };
+#endif
+
+                    /**
+                     *  For Info type.
+                     */
+                    ObjectVariantMapper GetObjectVariantMapper ();
+
+
+                    /**
+                     */
+                    Instrument          GetInstrument ();
+
+
+                }
+
+
+            }
+        }
+    }
+}
+
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+#endif  /*_Stroika_Framework_SystemPerformance_Instruments_SystemCPU_h_*/
