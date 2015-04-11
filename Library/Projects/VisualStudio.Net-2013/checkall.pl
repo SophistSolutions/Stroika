bd99981079bcb4ef400959c95e0416d0596cac98
Lewis Pringle <lewis@sophists.com>
2015-04-11 12:35:44 -0400

    Lose 'Windows' in path for IntermediateFiles and Builds in output - just use configuration name - as part of effort to UNIFY and SIMPLIFY how we build on windoze and UNIX - now just {Builds|IntermediateFiles}/{CONFIGNAME}/stuff

--- Library/Projects/VisualStudio.Net-2013/checkall.pl
+++ Library/Projects/VisualStudio.Net-2013/checkall.pl
@@ -1,12 +1,12 @@
-CheckFileExists ("../../../Builds/Windows/Debug-U-32/Library/Stroika-Foundation.lib");
-CheckFileExists ("../../../Builds/Windows/Debug-U-64/Library/Stroika-Foundation.lib");
-CheckFileExists ("../../../Builds/Windows/Release-U-32/Library/Stroika-Foundation.lib");
-CheckFileExists ("../../../Builds/Windows/Release-U-64/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Debug-U-32/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Debug-U-64/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Release-U-32/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Release-U-64/Library/Stroika-Foundation.lib");
 
-CheckFileExists ("../../../Builds/Windows/Release-Logging-U-32/Library/Stroika-Foundation.lib");
-CheckFileExists ("../../../Builds/Windows/Release-Logging-U-64/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Release-Logging-U-32/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Release-Logging-U-64/Library/Stroika-Foundation.lib");
 
-CheckFileExists ("../../../Builds/Windows/Release-DbgMemLeaks-U-32/Library/Stroika-Foundation.lib");
+CheckFileExists ("../../../Builds/Release-DbgMemLeaks-U-32/Library/Stroika-Foundation.lib");
 
 
 print "[SUCCEEDED]\r\n";
