2978655efd5b2c3799441411b1283a8638783997
Lewis Pringle <lewis@sophists.com>
2015-04-10 23:25:29 -0400

    Improved verison parsing and 2 formats of output for ScriptsLib/MakeVersionFile.sh

--- ScriptsLib/MakeVersionFile.sh
+++ ScriptsLib/MakeVersionFile.sh
@@ -1,40 +1,62 @@
 #!/bin/bash
 VERSION_IN_FILE=$1
 VERSION_OUT_FILE=$2
-DEFINE_PROTECTOR=$3
-CPPNAMESPACE=$4
-
-getawk() {
-    if command -v gawk &>/dev/null; then
-        echo -n "gawk"
-    else
-        echo -n "awk";
-    fi
-}
-
-AWK=$(getawk)
+OUT_FILE_TYPE=$3
 
 #echo "-----------"
+#echo VERSION_IN_FILE=$VERSION_IN_FILE
 #echo VERSION_OUT_FILE=$VERSION_OUT_FILE
-#echo DEFINE_PROTECTOR=$DEFINE_PROTECTOR
+#echo OUT_FILE_TYPE=$OUT_FILE_TYPE
 #echo "-----------"
 
-MAJOR=`cat $VERSION_IN_FILE | $AWK -F. '{ print $1 }'`
-MINOR=`cat $VERSION_IN_FILE | $AWK -F. '{ print $2 }'`
-VERSIONSTAGE=`cat $VERSION_IN_FILE | $AWK -F. '{ print $3 }'`
-VERSIONSUBSTAGE=`cat $VERSION_IN_FILE | $AWK -F. '{ print $4 }'`
-VERSIONFINAL=`cat $VERSION_IN_FILE | $AWK -F. '{ print $5 }'`
+FULLVERSIONSTRING=`sed 's/[ \t]*$//' STROIKA_VERSION | xargs echo -n`
+#FULLVERSIONSTRING=`cat $VERSION_IN_FILE | xargs echo -n`
+#echo "FULLVERSIONSTRING=$FULLVERSIONSTRING"
+MAJOR=`sed 's/\([0-9]*\).*/\1/' $VERSION_IN_FILE`
+MINOR=`sed 's/[0-9]*\.\([0-9]*\).*/\1/' $VERSION_IN_FILE`
+VERSIONSTAGE=`sed 's/[0-9]*\.[0-9]*\([abdr]\).*/\1/' $VERSION_IN_FILE`
+VERSIONSUBSTAGE=`sed 's/[0-9]*\.[0-9]*[abdr]\([0-9]*\).*/\1/' $VERSION_IN_FILE`
+VERSIONFINAL=`sed 's/[0-9]*\.[0-9]*[abdr][0-9]*\(.*\)/\1/' $VERSION_IN_FILE`
+
+if [ "$VERSIONSTAGE" == "a" ] ; then VERSIONSTAGE="Alpha"; fi
+if [ "$VERSIONSTAGE" == "b" ] ; then VERSIONSTAGE="Beta"; fi
+if [ "$VERSIONSTAGE" == "d" ] ; then VERSIONSTAGE="Dev"; fi
+if [ "$VERSIONSTAGE" == "r" ] ; then VERSIONSTAGE="ReleaseCandidate"; fi
+
 if [ "$VERSIONFINAL" == "x" ] ; then VERSIONFINAL="false"; fi
 if [ "$VERSIONFINAL" == "" ] ; then VERSIONFINAL="true"; fi
 if [ "$VERSIONFINAL" != "false" ] && [ "$VERSIONFINAL" != "true" ] ; then echo "BAD VERSIONFINAL"; fi
 
+VERSIONFINALBUILD='0'
+if [ "$VERSIONFINAL" == "true" ] ; then VERSIONFINALBUILD="1"; fi
+
 rm -f $VERSION_OUT_FILE
 touch $VERSION_OUT_FILE
-echo "#ifndef $DEFINE_PROTECTOR " >> $VERSION_OUT_FILE
-echo "#define $DEFINE_PROTECTOR 1" >> $VERSION_OUT_FILE
-echo "#include \"Stroika/Foundation/StroikaPreComp.h\"" >> $VERSION_OUT_FILE
-echo "#include \"Stroika/Foundation/Configuration/Version.h\"" >> $VERSION_OUT_FILE
-echo "namespace $CPPNAMESPACE {" >> $VERSION_OUT_FILE
-echo "constexpr Stroika::Foundation::Configuration::Version  kVersion = Stroika::Foundation::Configuration::Version ($MAJOR, $MINOR, Stroika::Foundation::Configuration::VersionStage::$VERSIONSTAGE, $VERSIONSUBSTAGE, $VERSIONFINAL);" >> $VERSION_OUT_FILE
-echo "}" >> $VERSION_OUT_FILE
-echo "#endif /*$DEFINE_PROTECTOR*/" >> $VERSION_OUT_FILE
+
+if [ "$OUT_FILE_TYPE" == "AppCPlusPlusVersion" ]
+  then
+	DEFINE_PROTECTOR=$4
+	CPPNAMESPACE=$5
+	echo "#ifndef $DEFINE_PROTECTOR " >> $VERSION_OUT_FILE
+	echo "#define $DEFINE_PROTECTOR 1" >> $VERSION_OUT_FILE
+	echo "#include \"Stroika/Foundation/StroikaPreComp.h\"" >> $VERSION_OUT_FILE
+	echo "#include \"Stroika/Foundation/Configuration/Version.h\"" >> $VERSION_OUT_FILE
+	echo "namespace $CPPNAMESPACE {" >> $VERSION_OUT_FILE
+	echo "constexpr Stroika::Foundation::Configuration::Version  kVersion = Stroika::Foundation::Configuration::Version ($MAJOR, $MINOR, Stroika::Foundation::Configuration::VersionStage::$VERSIONSTAGE, $VERSIONSUBSTAGE, $VERSIONFINAL);" >> $VERSION_OUT_FILE
+	echo "}" >> $VERSION_OUT_FILE
+	echo "#endif /*$DEFINE_PROTECTOR*/" >> $VERSION_OUT_FILE
+fi
+
+if [ "$OUT_FILE_TYPE" == "StroikaLibVersion" ]
+  then
+	echo "#define kStroika_Version_Major                  $MAJOR" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_Minor                  $MINOR" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_MajorMinor             0x$MAJOR$MINOR" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_Stage                  kStroika_Version_Stage_$VERSIONSTAGE" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_SubStage               $VERSIONSUBSTAGE" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_FinalBuild             $VERSIONFINALBUILD" >> $VERSION_OUT_FILE
+		
+	echo "#define kStroika_Version_MajorMinorVersionString        \"$MAJOR.$MINOR\"" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_ShortVersionString             \"$FULLVERSIONSTRING\"" >> $VERSION_OUT_FILE
+	echo "#define kStroika_Version_FullVersion                    Stroika_Make_FULL_VERSION (kStroika_Version_Major, kStroika_Version_Minor, kStroika_Version_Stage, kStroika_Version_SubStage, kStroika_Version_FinalBuild)" >> $VERSION_OUT_FILE
+fi
