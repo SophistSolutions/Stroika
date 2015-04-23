1f2790f8ec0c651f5dc20350dacc84fd865b406e
Lewis Pringle <lewis@sophists.com>
2015-04-23 13:16:50 -0400

    fixed Configuration.mk genreation - missing a few flags

--- ScriptsLib/ApplyConfiguration.pl
+++ ScriptsLib/ApplyConfiguration.pl
@@ -484,7 +484,10 @@ sub WriteStroikaConfigMakeHeader
 
 
 	print (OUT "qFeatureFlag_LibCurl='$FEATUREFLAG_LIBCURL'\n");
+	print (OUT "qFeatureFlag_LibOpenSSL='$FEATUREFLAG_OpenSSL'\n");
+	print (OUT "qFeatureFlag_WinHTTP='$FEATUREFLAG_WinHTTP'\n");
 	print (OUT "qFeatureFlag_Xerces='$FEATUREFLAG_XERCES'\n");
+	print (OUT "qFeatureFlag_LibZLib='$FEATUREFLAG_ZLib'\n");
 
 
 	print (OUT "#Third Party Product Libs to Build:\n");
