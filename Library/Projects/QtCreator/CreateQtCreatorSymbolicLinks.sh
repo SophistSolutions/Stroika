7ada30e4b974e8b971cf418cdee95eff13887c1d
Lewis G. Pringle, Jr <lewis.sophists.com>
2015-04-23 14:54:24 -0400

    lose Projects/QtCreator/CreateQtCreatorSymbolicLinks.sh since we now directly copy from top-level makefile

--- Library/Projects/QtCreator/CreateQtCreatorSymbolicLinks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ln -s Library/Projects/QtCreator/StroikaDevRoot.config
-ln -s Library/Projects/QtCreator/StroikaDevRoot.creator
-ln -s Library/Projects/QtCreator/StroikaDevRoot.files
-ln -s Library/Projects/QtCreator/StroikaDevRoot.includes
