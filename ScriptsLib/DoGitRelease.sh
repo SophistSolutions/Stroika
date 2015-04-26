8797212e70d63b9e925c0895f3ed9f5a268c53d8
Lewis Pringle <lewis@sophists.com>
2015-04-26 16:21:21 -0400

    tweaks to ScriptsLib/DoGitRelease.sh

--- ScriptsLib/DoGitRelease.sh
+++ ScriptsLib/DoGitRelease.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 VER=`dos2unix < STROIKA_VERSION | xargs echo -n`
 echo "STROIKA_VERSION=$VER"
+echo "Checking out V2-Release"
 git checkout V2-Release
+echo "Pull/Rebasing..."
 git pull --rebase
+echo "Merge in V2-Dev..."
 git merge V2-Dev
-echo "OK to commit/push?"
+echo "OK to commit/push?: CTRLC to stop or RETURN to continue"
 read isOK
 #git commit
 git push
 git tag v$VER
 git push origin v$VER
-git checkout Dev-v1.1
-
+git checkout V2-Dev
