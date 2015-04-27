b5d7b08744b7bd0c46971b249c7d47f312a84b3a
Lewis Pringle <lewis@sophists.com>
2015-04-27 08:30:52 -0400

    cosmetic

--- Library/Sources/Stroika/Foundation/Configuration/StroikaConfig.h
+++ Library/Sources/Stroika/Foundation/Configuration/StroikaConfig.h
@@ -1,38 +1,38 @@
 /*
  * Copyright(c) Sophist Solutions, Inc. 1990-2015.  All rights reserved
  */
-#ifndef	_Stroika_Foundation_Configuration_StroikaConfig_h_
-#define	_Stroika_Foundation_Configuration_StroikaConfig_h_	1
+#ifndef _Stroika_Foundation_Configuration_StroikaConfig_h_
+#define _Stroika_Foundation_Configuration_StroikaConfig_h_  1
 
 
 /*
- *	For the dyanmically generated (from the configuraiton system) defines
+ *  For the dyanmically generated (from the configuraiton system) defines
  */
-#include	"Stroika-Current-Configuration.h"
+#include    "Stroika-Current-Configuration.h"
 
 
 
 /*
- *	For the defaults based on the above, and compiler defines etc.
+ *  For the defaults based on the above, and compiler defines etc.
  */
 
-#include	"Private/Defaults_Configuration_Common_.h"
+#include    "Private/Defaults_Configuration_Common_.h"
 
 //Out of alphabetic order because other defaults depend on qDebug
-#include	"Private/Defaults_Debug_Assertions_.h"
+#include    "Private/Defaults_Debug_Assertions_.h"
 
-#include	"Private/Defaults_Characters_Character_.h"
-#include	"Private/Defaults_Characters_StringUtils_.h"
-#include	"Private/Defaults_Characters_TChar_.h"
-#include	"Private/Defaults_CompilerAndStdLib_.h"
-#include	"Private/Defaults_Cryptography_SSL_.h"
-#include	"Private/Defaults_Database_ODBCClient_.h"
-#include	"Private/Defaults_DataExchange_XML_Common_.h"
-#include	"Private/Defaults_Debug_Trace_.h"
-#include	"Private/Defaults_Execution_Logging_.h"
-#include	"Private/Defaults_Execution_Module_.h"
-#include	"Private/Defaults_Execution_Threads_.h"
-#include	"Private/Defaults_Memory_Common_.h"
-#include	"Private/Defaults_Memory_BlockAllocated_.h"
+#include    "Private/Defaults_Characters_Character_.h"
+#include    "Private/Defaults_Characters_StringUtils_.h"
+#include    "Private/Defaults_Characters_TChar_.h"
+#include    "Private/Defaults_CompilerAndStdLib_.h"
+#include    "Private/Defaults_Cryptography_SSL_.h"
+#include    "Private/Defaults_Database_ODBCClient_.h"
+#include    "Private/Defaults_DataExchange_XML_Common_.h"
+#include    "Private/Defaults_Debug_Trace_.h"
+#include    "Private/Defaults_Execution_Logging_.h"
+#include    "Private/Defaults_Execution_Module_.h"
+#include    "Private/Defaults_Execution_Threads_.h"
+#include    "Private/Defaults_Memory_Common_.h"
+#include    "Private/Defaults_Memory_BlockAllocated_.h"
 
-#endif	/*_Stroika_Foundation_Configuration_StroikaConfig_h_*/
+#endif  /*_Stroika_Foundation_Configuration_StroikaConfig_h_*/
