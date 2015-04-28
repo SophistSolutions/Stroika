fc4573916dd88b879127faef335da068f47385ad
Lewis Pringle <lewis@sophists.com>
2015-04-28 09:57:01 -0400

    do regular pull - not pull-rebase in DoGitRelease.sh - for now

--- ScriptsLib/DoGitRelease.sh
+++ ScriptsLib/DoGitRelease.sh
@@ -3,8 +3,8 @@ VER=`dos2unix < STROIKA_VERSION | xargs echo -n`
 echo "STROIKA_VERSION=$VER"
 echo "Checking out V2-Release"
 git checkout V2-Release
-echo "Pull/Rebasing..."
-git pull --rebase
+echo "Pull..."
+git pull
 echo "Merge in V2-Dev..."
 git merge V2-Dev
 echo "OK to commit/push?: CTRLC to stop or RETURN to continue"
