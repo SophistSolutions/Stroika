51e941a18f68cb4f2a0d8e58c87045dd4240d588
Lewis Pringle <lewis@sophists.com>
2015-07-06 19:08:49 -0400

    Comments

--- Library/Sources/Stroika/Foundation/Streams/OutputStream.h
+++ Library/Sources/Stroika/Foundation/Streams/OutputStream.h
@@ -48,6 +48,11 @@
  *      @todo   Consider/document approaches to timeouts. We COULD have a stream class where
  *              it was a PROPERTY OF THE CLASS (or alternate API) where writes timeout after
  *              a certain point.
+ *
+ *      @todo   Add abiiliy to SetEOF (); You can SEEK, but if you seek backwards, and start writing - that doesnt change EOF. EOF
+ *              remains fixed as max written to. DODUCMNET THIS (for text and binary) - and provide a SetEOF() method
+ *              (maybe just for seekable streams)? Maybe add rule that SetEOF () can only go backwards (shorten). Then call
+ *              PullBackEOF() or RestrictEOF() or RemovePast(); OR ResetEOFToCurrentPosiiton(). Later maybe best API.
  */
 
 
