868e4152e56d5746b6799c7f7fde5ead02f51bc1
Lewis Pringle <lewis@sophists.com>
2015-07-16 16:00:21 -0400

    Comments

--- Library/Sources/Stroika/Foundation/Characters/RegularExpression.h
+++ Library/Sources/Stroika/Foundation/Characters/RegularExpression.h
@@ -58,6 +58,9 @@ namespace   Stroika {
                 };
 
             public:
+                /**
+                 *  \note this throws std::regex_error () if provided an invalid regular expression.
+                 */
                 explicit RegularExpression (const String& re, SyntaxType syntaxType, CompareOptions co = CompareOptions::eWithCase);
                 RegularExpression (const wregex& regEx);
 
