5d7024f48297b7458d6d142a0c83794c0b908098
Lewis Pringle <lewis@sophists.com>
2015-07-20 09:16:55 -0400

    hack ArgByValueType so builds on older gcc

--- Library/Sources/Stroika/Foundation/Configuration/TypeHints.h
+++ Library/Sources/Stroika/Foundation/Configuration/TypeHints.h
@@ -18,9 +18,12 @@ namespace   Stroika {
              *
              *  This is NOT intended to be used very widely, but can be used to decide systematically (by type)
              *  if you should pass a given argument by value or by const reference.
+             *
+             *  @todo SHOULD use is_trivially_copyable<> but didnt work on gcc47 (and maybe more - look into which and iether
+             *      add bug define or?? not worries - not much differnt)
              */
             template    <typename   T>
-            using   ArgByValueType = typename std::conditional < (sizeof(T) <= sizeof(void*)) and std::is_trivially_copyable<T>::value, T, const T& >::type;
+            using   ArgByValueType = typename std::conditional < (sizeof(T) <= sizeof(void*)) and std::is_trivial<T>::value, T, const T& >::type;
 
 
         }
