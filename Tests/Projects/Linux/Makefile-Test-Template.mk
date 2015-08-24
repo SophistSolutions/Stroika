23104208c9d2b6ced6d412a12d5bfc7bd79c32c7
Lewis Pringle <lewis@sophists.com>
2015-08-24 16:54:31 -0400

    makefile typos

--- Tests/Projects/Linux/Makefile-Test-Template.mk
+++ Tests/Projects/Linux/Makefile-Test-Template.mk
@@ -30,6 +30,6 @@ all:	$(TARGETEXE)
 $(TARGETEXE):	$(Objs)
 	@echo "   Linking $(TARGETEXE)..."
 	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
-	    echo "      $(Linker) $(LinkerPrefixArgs) -o $(TARGETEXE) $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs);\
+	    echo "      $(Linker) $(LinkerPrefixArgs) -o $(TARGETEXE) $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs)";\
 	fi
 	@$(Linker) $(LinkerPrefixArgs) -o $(TARGETEXE) $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs)
