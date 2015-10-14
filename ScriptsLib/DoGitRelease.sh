08aaf59f8bf22ee3f7c689d84edcd41284cfef94
Lewis Pringle <lewis@sophists.com>
2015-10-13 21:21:58 -0400

    ScriptsLib/DoGitRelease git pull --rebase

--- ScriptsLib/DoGitRelease.sh
+++ ScriptsLib/DoGitRelease.sh
@@ -4,7 +4,7 @@ echo "STROIKA_VERSION=$VER"
 echo "Checking out V2-Release"
 git checkout V2-Release
 echo "Pull..."
-git pull
+git pull --rebase
 echo "Merge in V2-Dev..."
 git merge V2-Dev
 echo "OK to commit/push?: CTRLC to stop or RETURN to continue"
