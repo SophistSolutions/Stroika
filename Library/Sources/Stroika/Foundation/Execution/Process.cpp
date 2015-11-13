5b18dda1a2de107f1d40e87e605faeb8ed555635
Lewis Pringle <lewis@sophists.com>
2015-11-13 18:32:57 -0500

    Workaround buggy Execution::IsProcessRunning / ::getpgid on AIX (means cannot tell if service running)

--- Library/Sources/Stroika/Foundation/Execution/Process.cpp
+++ Library/Sources/Stroika/Foundation/Execution/Process.cpp
@@ -3,6 +3,10 @@
  */
 #include    "../StroikaPreComp.h"
 
+#if     qPlatform_AIX
+#include    <sys/stat.h>
+#endif
+
 #include    "Process.h"
 
 
@@ -27,11 +31,32 @@ using   namespace   Stroika::Foundation::Execution;
 
 bool    Execution::IsProcessRunning (pid_t pid)
 {
-#if     qPlatform_POSIX
+#if     USE_NOISY_TRACE_IN_THIS_MODULE_
+    Debug::TraceContextBumper traceCtx ("Stroika::Foundation::Execution::IsProcessRunning");
+    DbgTrace (L"(pid=%d)", pid);
+#endif
+#if     qPlatform_AIX
+    // sadly getpgid doesnt appear to work on AIX --LGP 2015-11-13
+    // From http://stackoverflow.com/questions/9152979/check-if-process-exists-given-its-pid
+    struct stat sts;
+    char    buf[1024];
+    snprintf (buf, NEltsOf(buf), "/proc/%d", pid);
+    if (::stat (buf, &sts) == -1 && errno == ENOENT) {
+        // process doesn't exist
+        return true;
+    }
+    else {
+        return true;
+    }
+#elif     qPlatform_POSIX
     // http://stackoverflow.com/questions/9152979/check-if-process-exists-given-its-pid
     // http://linux.die.net/man/2/getpgid
     // if not owner, trick of kill (pid, 0) returns error EPERM
-    return ::getpgid (pid) > 0;
+    pid_t   tmp { ::getpgid (pid) };
+#if     USE_NOISY_TRACE_IN_THIS_MODULE_
+    DbgTrace (L"getpgid (pid=%d) -> %d, with ernno=%d", pid, tmp, errno);
+#endif
+    return tmp > 0;
 #else
     AssertNotImplemented ();
     return false;
