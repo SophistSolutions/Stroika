88713dba7850e71cd2b2bb74c6877a3a7b9a0bce
Lewis G. Pringle, Jr <lewis.sophists.com>
2015-11-21 17:28:51 -0500

    Port ScriptsLib/GetConfigurations.sh

--- ScriptsLib/GetConfigurations.sh
+++ ScriptsLib/GetConfigurations.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 MY_PATH="`dirname \"$0\"`"
 MY_PATH=`realpath $MY_PATH`
-/bin/find "$MY_PATH/../ConfigurationFiles" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} basename "{}" .xml | tr '\n' ' '
+#NB: need full path top find cuz of winDoze
+/usr/bin/find "$MY_PATH/../ConfigurationFiles" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} basename "{}" .xml | tr '\n' ' '
