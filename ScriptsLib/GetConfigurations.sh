ab5328d75eec3b05db6f86f3ed89e57ca1a4612f
Lewis Pringle <lewis@sophists.com>
2015-11-21 17:52:56 -0500

    handle no configfiles dir in ScriptsLib/GetConfigurations.sh

--- ScriptsLib/GetConfigurations.sh
+++ ScriptsLib/GetConfigurations.sh
@@ -2,4 +2,6 @@
 MY_PATH="`dirname \"$0\"`"
 MY_PATH=`realpath $MY_PATH`
 #NB: need full path top find cuz of winDoze
-/usr/bin/find "$MY_PATH/../ConfigurationFiles" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} basename "{}" .xml | tr '\n' ' '
+if [ -e "$MY_PATH/../ConfigurationFiles" ] ; then
+	/usr/bin/find "$MY_PATH/../ConfigurationFiles" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} basename "{}" .xml | tr '\n' ' '
+fi
