e05179ec168187cbb5839107671c7dc5534323a0
Lewis Pringle <lewis@sophists.com>
2015-11-29 13:50:07 -0500

    fixed Samples/ArchiveUtility/buildall script for Release-DbgMemLeaks-U-32

--- Samples/ArchiveUtility/buildall.pl
+++ Samples/ArchiveUtility/buildall.pl
@@ -29,7 +29,7 @@ if (index($projectPlatformSubdir, "VisualStudio") == -1) {
 	chdir ($savedDir);
 }
 else {
-	if ($activeConfig eq "Debug-U-32" || $activeConfig eq "Release-U-32" || $activeConfig eq "Debug-U-64" || $activeConfig eq "Release-U-64" || $activeConfig eq "Release-Logging-U-64") {
+	if ($activeConfig eq "Debug-U-32" || $activeConfig eq "Release-U-32" || $activeConfig eq "Debug-U-64" || $activeConfig eq "Release-U-64" || $activeConfig eq "Release-Logging-U-64" || $activeConfig eq "Release-DbgMemLeaks-U-32") {
 		my $curConfig	=	`../../ScriptsLib/GetVisualStudioConfigLine.pl $activeConfig`;
 		my $extraArgs = GetMSBuildArgs();
 		RunAndPrint ("cd $useProjectDir; msbuild.exe $extraArgs ArchiveUtility.vcxproj /p:$curConfig /target:$useBld");
