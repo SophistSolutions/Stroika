67836594b40ca20bb8468b506803820a49432198
Lewis Pringle <lewis@sophists.com>
2015-12-03 13:03:54 -0500

    added ANOTHER backup for openssl urls, and fixed WGet script to support even more mirrors

--- ScriptsLib/WebGet.sh
+++ ScriptsLib/WebGet.sh
@@ -3,12 +3,20 @@ OUTPUTFILE=$1
 SRCURL1=$2
 SRCURL2=$3
 SRCURL3=$3
+SRCURL4=$4
+SRCURL5=$5
 
 if [ "$SRCURL2" == "" ] ; then
 	SRCURL2=$SRCURL1
 fi
 if [ "$SRCURL3" == "" ] ; then
-	SRCURL3=$SRCURL1
+	SRCURL3=$SRCURL2
+fi
+if [ "$SRCURL4" == "" ] ; then
+	SRCURL4=$SRCURL1
+fi
+if [ "$SRCURL5" == "" ] ; then
+	SRCURL4=$SRCURL3
 fi
 
 RETRIES=2
@@ -33,6 +41,24 @@ echo failed
 echo "WARNING: second mirror failed, so retrying:"
 echo -n "wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL3 ... "
 wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL3
+if [ $? -eq 0 ] ; then
+	echo "done"
+    	exit 0;
+fi
+
+echo failed
+echo "WARNING: second mirror failed, so retrying:"
+echo -n "wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL4 ... "
+wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL4
+if [ $? -eq 0 ] ; then
+	echo "done"
+    	exit 0;
+fi
+
+echo failed
+echo "WARNING: second mirror failed, so retrying:"
+echo -n "wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL5 ... "
+wget --quiet --tries=$RETRIES -O $OUTPUTFILE $SRCURL5
 if [ $? -eq 0 ] ; then
 	echo "done"
     	exit 0;
