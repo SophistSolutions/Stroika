9b5eef6dd54858f8f7622f5748bcc053a1f0fe4c
Lewis Pringle <lewis@sophists.com>
2015-12-28 22:42:57 -0500

    minor regression test tweaks

--- ScriptsLib/RegressionTests-Unix.sh
+++ ScriptsLib/RegressionTests-Unix.sh
@@ -61,5 +61,5 @@ echo "Run-Tests VALGRIND PURIFY/BLOCK_ALLOC..."
 VALGRIND_SUPPRESSIONS="Common-Valgrind.supp"  make CONFIGURATION=DefaultConfig_With_VALGRIND_PURIFY_NO_BLOCK_ALLOC VALGRIND=1 run-tests 2>&1 >> REGRESSION-TESTS.OUT
 
 #slow, and largely useless test...
-#VALGRIND_SUPPRESSIONS="OpenSSL.supp Common-Valgrind.supp BlockAllocation-Valgrind.supp" make CONFIGURATION=DEFAULT_CONFIG_WITH_VALGRIND VALGRIND=1 run-tests
+#VALGRIND_SUPPRESSIONS="OpenSSL.supp Common-Valgrind.supp BlockAllocation-Valgrind.supp" make CONFIGURATION=DEFAULT_CONFIG VALGRIND=1 run-tests
 echo "Finished at `date`"
