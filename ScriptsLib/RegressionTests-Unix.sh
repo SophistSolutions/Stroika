b42e3621f5c6754baf68181a4fc22fe119474d75
Lewis Pringle <lewis@sophists.com>
2016-01-14 13:37:37 -0500

    regression test cleanups - printing time taken - and minor fixes - and output name includes stroika version

--- ScriptsLib/RegressionTests-Unix.sh
+++ ScriptsLib/RegressionTests-Unix.sh
@@ -3,11 +3,15 @@
 set -E
 trap '[ "$?" -ne 77 ] || exit 77' ERR
 
-TEST_OUT_FILE=REGRESSION-TESTS.OUT
+
+PRETTY_II_VERSION=
+
+TEST_OUT_FILE=REGRESSION-TESTS-`cat STROIKA_VERSION | tr -d '[[:space:]]`.OUT
 PARALELLMAKEFLAG=-j3
 
 PREFIX_OUT_LABEL=")))-"
 
+STARTAT_INT=$(date +%s)
 
 
 STARTAT=`date`;
@@ -25,15 +29,21 @@ echo "$PREFIX_OUT_LABEL" "Building configurations ($NUM_CONFIGURATIONS): `sh Scr
 
 
 make clobber >>$TEST_OUT_FILE 2>&1
+STAGE_STARTAT_INT=$(date +%s)
 echo -n "Make all..."
 echo "$PREFIX_OUT_LABEL" "Make all..."  >>$TEST_OUT_FILE 2>&1
 make all $PARALELLMAKEFLAG >>$TEST_OUT_FILE 2>&1
-echo "done"
+STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 
 echo -n "Run-Tests ALL..."
 echo "$PREFIX_OUT_LABEL" "Run-Tests ALL..." >>$TEST_OUT_FILE 2>&1
+STAGE_STARTAT_INT=$(date +%s)
 make run-tests >>$TEST_OUT_FILE 2>&1
-echo "done"
+STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 
 
 RASPBERRYPI_REMOTE_MACHINE=raspberrypi
@@ -42,8 +52,11 @@ echo -n "Run-Tests raspberrypi remote..."
 echo "$PREFIX_OUT_LABEL" "Run-Tests raspberrypi remote..." >>$TEST_OUT_FILE 2>&1
 ARMTESTMACHINEAVAIL=`(ping raspberrypi -c 4 2>/dev/null 1>/dev/null); echo $?`
 if [ $ARMTESTMACHINEAVAIL -eq 0 ]; then
+	STAGE_STARTAT_INT=$(date +%s)
 	make run-tests CONFIGURATION=raspberrypi-gcc-4.9 REMOTE=$RASPBERRYPI_REMOTE_WITH_LOGIN >>$TEST_OUT_FILE 2>&1
-	echo "done"
+	STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 else
 	echo "...skipped raspberrypi tests cuz machine not available"
 	echo "...skipped raspberrypi tests cuz machine not available">>$TEST_OUT_FILE 2>&1
@@ -56,21 +69,29 @@ fi
 #test with usual set of valgrind suppressions
 echo -n "Run-Tests VALGRIND PURIFY/BLOCK_ALLOC..."
 echo "$PREFIX_OUT_LABEL" "Run-Tests VALGRIND PURIFY/BLOCK_ALLOC..." >>$TEST_OUT_FILE 2>&1
+STAGE_STARTAT_INT=$(date +%s)
 VALGRIND_SUPPRESSIONS="Common-Valgrind.supp"  make CONFIGURATION=ReleaseConfig_With_VALGRIND_PURIFY_NO_BLOCK_ALLOC VALGRIND=1 run-tests >>$TEST_OUT_FILE 2>&1 
-echo "done"
+STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 
 #more suppressions, but run on debug build...
 echo -n "Run-Tests VALGRIND gcc-5.2.0-debug-c++17..."
 echo "$PREFIX_OUT_LABEL" "Run-Tests VALGRIND gcc-5.2.0-debug-c++17..." >>$TEST_OUT_FILE 2>&1
+STAGE_STARTAT_INT=$(date +%s)
 VALGRIND_SUPPRESSIONS="OpenSSL.supp Common-Valgrind.supp BlockAllocation-Valgrind.supp" make CONFIGURATION=gcc-5.2.0-debug-c++17 VALGRIND=1 run-tests >>$TEST_OUT_FILE 2>&1
-echo "done"
+STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
+
+
 
 
 echo "Regression Tests Summary:"
 echo "$PREFIX_OUT_LABEL" "Regression Tests Summary:" >>$TEST_OUT_FILE 2>&1
 X1=`cat $TEST_OUT_FILE | grep seconds | grep -i -F [Succeeded] | wc -l`
 TOTAL_REGTESTS_EXPECTED_TO_PASS=$(($NUM_PASSES_OF_REGTESTS_RUN * $NUM_REGTESTS))
-if [ $X1 -lt TOTAL_REGTESTS_EXPECTED_TO_PASS ]; then
+if [ $X1 -lt $TOTAL_REGTESTS_EXPECTED_TO_PASS ]; then
 	echo "   ***   $X1 tests succeeded and expected $TOTAL_REGTESTS_EXPECTED_TO_PASS";
 fi
 
@@ -84,5 +105,6 @@ echo "   $XF items failed">>$TEST_OUT_FILE 2>&1
 echo "   $XC core dumps"
 echo "   $XC core dumps">>$TEST_OUT_FILE 2>&1
 
-echo "Finished at `date`"
-echo "$PREFIX_OUT_LABEL" "Finished at `date`">>$TEST_OUT_FILE 2>&1
+TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STARTAT_INT )) / 60))
+echo "Finished at `date` ($TOTAL_MINUTES_SPENT minutes)"
+echo "$PREFIX_OUT_LABEL" "Finished at `date`  ($TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
