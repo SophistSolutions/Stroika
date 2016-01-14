fa7b2c7ef27c7db0801538f21deb3f9f52dfbe66
Lewis Pringle <lewis@sophists.com>
2016-01-13 22:05:21 -0500

    regression test cleanups

--- ScriptsLib/RegressionTests-Unix.sh
+++ ScriptsLib/RegressionTests-Unix.sh
@@ -44,29 +44,36 @@ rm -f $TEST_OUT_FILE
 make regression-test-configurations >>$TEST_OUT_FILE 2>&1
 
 make clobber >>$TEST_OUT_FILE 2>&1
-echo "Make all..."
+echo -n "Make all..."
 make all $PARALELLMAKEFLAG >>$TEST_OUT_FILE 2>&1
+echo "done"
 
-echo "Run-Tests ALL..."
+echo -n "Run-Tests ALL..."
 make run-tests >>$TEST_OUT_FILE 2>&1
+echo "done"
 
-echo "Run-Tests raspberrypi remote..."
+echo -n "Run-Tests raspberrypi remote..."
 ARMTESTMACHINEAVAIL=`(ping www.sophists.com 2>/dev/null 1>/dev/null); echo $?`
 if [ $ARMTESTMACHINEAVAIL -eq 0 ]; then
 	make run-tests CONFIGURATION=raspberrypi-gcc-4.9 REMOTE=lewis@raspberrypi >>$TEST_OUT_FILE 2>&1
+	echo "done"
 else
 	echo "...skipped raspberrypi tests cuz machine not available"
 fi
 
 
 #test with usual set of valgrind suppressions
-echo "Run-Tests VALGRIND PURIFY/BLOCK_ALLOC..."
+echo -n "Run-Tests VALGRIND PURIFY/BLOCK_ALLOC..."
 VALGRIND_SUPPRESSIONS="Common-Valgrind.supp"  make CONFIGURATION=ReleaseConfig_With_VALGRIND_PURIFY_NO_BLOCK_ALLOC VALGRIND=1 run-tests >>$TEST_OUT_FILE 2>&1 
+echo "done"
 
 #more suppressions, but run on debug build...
+echo -n "Run-Tests VALGRIND gcc-5.2.0-debug-c++17..."
 VALGRIND_SUPPRESSIONS="OpenSSL.supp Common-Valgrind.supp BlockAllocation-Valgrind.supp" make CONFIGURATION=gcc-5.2.0-debug-c++17 VALGRIND=1 run-tests >>$TEST_OUT_FILE 2>&1
+echo "done"
 
 
+echo "Regression Tests Summary:"
 X1=`cat $TEST_OUT_FILE | grep seconds | grep -i -F [Succeeded] | wc -l`
 if [ $ARMTESTMACHINEAVAIL -eq 0 ]; then
 	if [ $X1 -lt 611 ]; then
@@ -80,9 +87,9 @@ fi
 XF=`cat $TEST_OUT_FILE | grep -i -F FAILED | wc -l`
 XC=`cat $TEST_OUT_FILE | grep -i -F "core dump" | wc -l`
 
-echo "$X1 items succeeded"
-echo "$XF items failed"
-echo "$XC core dumps"
+echo "   $X1 items succeeded"
+echo "   $XF items failed"
+echo "   $XC core dumps"
 
 echo "Finished at `date`"
 
