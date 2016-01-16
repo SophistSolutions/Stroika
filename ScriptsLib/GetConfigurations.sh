5189b78b0bd7edede878436c8142736d9619f047
Lewis Pringle <lewis@sophists.com>
2016-01-16 10:55:51 -0500

    auto-sort (instead seems pretty random) list of configs from ScriptsLib/GetConfigurations.sh

--- ScriptsLib/GetConfigurations.sh
+++ ScriptsLib/GetConfigurations.sh
@@ -4,5 +4,5 @@ MY_PATH=`realpath $MY_PATH`
 #NB: need full path top find cuz of winDoze
 if [ -e "$MY_PATH/../ConfigurationFiles/" ] ; then
 	#/usr/bin/find "$MY_PATH/../ConfigurationFiles/" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} basename "{}" .xml | tr '\n' ' '
-	/usr/bin/find "$MY_PATH/../ConfigurationFiles/" -type f -exec basename \{} .xml \; | tr '\n' ' '
+	/usr/bin/find "$MY_PATH/../ConfigurationFiles/" -type f -exec basename \{} .xml \; | sort | tr '\n' ' '
 fi
