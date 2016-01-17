9343660545ce58305c50a4a76d8f9504e711d42a
Lewis Pringle <lewis@sophists.com>
2016-01-17 12:05:39 -0500

    comments

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -1125,7 +1125,8 @@ Compiling regtests for Median/OrderBy...
  *
  * clang uses the g++ stdlib, and since the bug is in both places, I suspect the issue is with the library, and
  * not the compiler, but its hard to unravel, and I need to get back to other things, so just disable. Now it only
- * affects Bijection<> and when you use it with types that have no operator<
+ * affects Bijection<> and when you use it with types that have no operator< and operator== (type needs both to workaround
+ * this bug).
  *
  EXAMPLE OUTPUT:
  In file included from /media/Sandbox/lewis-Sandbox/Stroika-DevRoot/Tests/11/Test.cpp:7:
