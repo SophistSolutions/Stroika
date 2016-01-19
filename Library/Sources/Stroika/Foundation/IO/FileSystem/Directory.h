f0b4cb09ca658ab94d8d211e638f4079bdff4696
Lewis Pringle <lewis@sophists.com>
2016-01-19 18:26:34 -0500

    docs/comments

--- Library/Sources/Stroika/Foundation/IO/FileSystem/Directory.h
+++ Library/Sources/Stroika/Foundation/IO/FileSystem/Directory.h
@@ -62,7 +62,13 @@ namespace   Stroika {
                     nonvirtual  void    Create () const;    // fails if exists
 
                 public:
-                    nonvirtual  void    AssureExists (bool createParentComponentsIfNeeded = true) const;    // fails if doesnt exist at the end, or is not a directory (unclear if its a directry but not accessible)?
+                    /**
+                     *  Checks if the directory named in the object CTOR exists. If yes, this has no effect.
+                     *  if no, the directory is created.
+                     *
+                     *  AssureExists fails if doesnt exist at the end, or is not a directory (unclear if its a directry but not accessible)
+                     */
+                    nonvirtual  void    AssureExists (bool createParentComponentsIfNeeded = true) const;
 
                 public:
                     // @todo cleanup / clarify docs (first is from old code second is my guess at new code at one point)
