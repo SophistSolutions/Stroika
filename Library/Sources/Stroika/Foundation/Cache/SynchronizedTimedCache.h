3ca1f8b1ce6b104f5dfce25d3fba02d058dbcd73
Lewis Pringle <lewis@sophists.com>
2016-02-26 13:03:51 -0500

    Super Rough draft (to test) of Cache/SynchronizedTimedCache

---
+++ Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.h
@@ -0,0 +1,101 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2016.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Cache_SynchronizedTimedCache_h_
+#define _Stroika_Foundation_Cache_SynchronizedTimedCache_h_    1
+
+#include    "../StroikaPreComp.h"
+
+#include    <mutex>
+#if     !qCompilerAndStdLib_shared_mutex_module_Buggy
+#include    <shared_mutex>
+#endif
+
+#include    "TimedCache.h"
+
+
+
+/**
+ *      \file
+ *
+ *  \version    <a href="code_status.html#Alpha">Alpha</a>
+ *
+ * TODO:
+ *
+ *      @todo   HIGHLY EXPERIEMNTAL FIRST DRAFT OF IDEA
+ */
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Cache {
+
+
+            /**
+             */
+            template    <typename   KEY, typename VALUE, typename TRAITS = TimedCacheSupport::DefaultTraits<KEY, VALUE>>
+            class   SynchonizedTimedCache : public TimedCache<KEY, VALUE, TRAITS> {
+                using inherited = TimedCache<KEY, VALUE, TRAITS>;
+            public:
+                SynchonizedTimedCache (Time::DurationSecondsType timeoutInSeconds)
+                    : inherited (timeoutInSeconds)
+                {
+                }
+                // support eventually, but not trivial
+                SynchonizedTimedCache (const SynchonizedTimedCache&) = delete;
+
+                // @todo need much more but this is atart
+            public:
+                nonvirtual  VALUE                   Lookup (typename Configuration::ArgByValueType<KEY> key, const std::function<VALUE(typename Configuration::ArgByValueType<KEY>)>& cacheFiller)
+                {
+#if     qCompilerAndStdLib_shared_mutex_module_Buggy
+                    lock_guard<mutex>               lock (this->fMutex_);
+#else
+                    shared_lock<shared_timed_mutex> lock (this->fMutex_);
+#endif
+                    if (Memory::Optional<VALUE> o = inherited::Lookup (key)) {
+                        return *o;
+                    }
+                    else {
+                        VALUE   v   =   cacheFiller (key);
+                        {
+#if     !qCompilerAndStdLib_shared_mutex_module_Buggy
+                            lock.unlock ();
+                            unique_lock<shared_timed_mutex>   newRWLock (this->fMutex_);
+#endif
+                            this->Add (key, v);
+                        }
+                        return move (v);
+                    }
+                }
+
+            public:
+                // support eventually, but not trivial
+                nonvirtual  SynchonizedTimedCache& operator= (const SynchonizedTimedCache&) = delete;
+
+            private:
+#if     qCompilerAndStdLib_shared_mutex_module_Buggy
+                mutable mutex               fMutex_;
+#else
+                mutable shared_timed_mutex  fMutex_;
+#endif
+
+            };
+
+
+        }
+    }
+}
+
+
+
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include    "SynchronizedTimedCache.inl"
+
+#endif  /*_Stroika_Foundation_Cache_SynchronizedTimedCache_h_*/
---
+++ Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.inl
@@ -0,0 +1,30 @@
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#ifndef _Stroika_Foundation_Cache_SynchronizedTimedCache_inl_
+#define _Stroika_Foundation_Cache_SynchronizedTimedCache_inl_  1
+
+
+
+namespace   Stroika {
+    namespace   Foundation {
+        namespace   Cache {
+
+
+            /*
+             ********************************************************************************
+             ************************ TimedCacheSupport::Stats_Basic ************************
+             ********************************************************************************
+             */
+
+
+
+        }
+    }
+}
+
+
+#endif  /*_Stroika_Foundation_Cache_SynchronizedTimedCache_inl_*/
