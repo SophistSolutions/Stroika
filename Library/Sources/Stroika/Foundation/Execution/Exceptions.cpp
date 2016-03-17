ac971f1a1097fa5c1bf06816c95edafad25c2f2a
Lewis Pringle <lewis@sophists.com>
2016-03-16 23:59:29 -0400

    make sure Exception::Private_::GetBT_s/GetBT_ws dont throw thread abort exception (if they did we wouldnt get DbgTrace of throwing thread abort excetpion

--- Library/Sources/Stroika/Foundation/Execution/Exceptions.cpp
+++ Library/Sources/Stroika/Foundation/Execution/Exceptions.cpp
@@ -8,6 +8,7 @@
 #if     qStroika_Foundation_Exection_Exceptions_TraceThrowpointBacktrace
 #include    "../Characters/String.h"
 #include    "../Debug/BackTrace.h"
+#include    "../Execution/Thread.h"
 #endif
 
 
@@ -26,10 +27,12 @@ using   namespace   Stroika::Foundation::Execution;
 #if     qStroika_Foundation_Exection_Exceptions_TraceThrowpointBacktrace
 string  Private_::GetBT_s ()
 {
+    Thread::SuppressInterruptionInContext   suppressCtx;    // avoid throwing here even if we allocate memory (because messes up DoThrow() logging)
     return Debug::BackTrace ().AsNarrowSDKString ();
 }
 wstring Private_::GetBT_ws ()
 {
+    Thread::SuppressInterruptionInContext   suppressCtx;    // avoid throwing here even if we allocate memory (because messes up DoThrow() logging)
     return Debug::BackTrace ().As<wstring> ();
 }
 #endif
