780628c161a7071939c0542c8b853f499f2170f2
Lewis Pringle <lewis@sophists.com>
2016-03-28 20:42:34 -0400

    simplified InputStream<Byte>::ReadAll () - so maybe easier to later optimize (probably slightly worse as is performance wise)

--- Library/Sources/Stroika/Foundation/Streams/InputStream.cpp
+++ Library/Sources/Stroika/Foundation/Streams/InputStream.cpp
@@ -144,93 +144,51 @@ Memory::BLOB InputStream<Byte>::ReadAll (size_t upTo) const
     Debug::TraceContextBumper ctx (L"InputStream<Byte>::ReadAll");
     DbgTrace (L"(upTo: %u)", upTo);
 #endif
+    vector<Byte>    r;      // @todo Consider using SmallStackBuffer<>
     if (IsSeekable ()) {
-#if     USE_NOISY_TRACE_IN_THIS_MODULE_
-        DbgTrace ("Seekable case");
-#endif
+        /*
+         * Avoid realloc's if not hard.
+         */
         SeekOffsetType  size = GetOffsetToEndOfStream ();
         if (size >= numeric_limits<size_t>::max ()) {
             Execution::Throw (bad_alloc ());
         }
         size_t sb = static_cast<size_t> (size);
         sb = min (sb, upTo);
-        if (sb == 0) {
-            return BLOB ();
-        }
 #if     USE_NOISY_TRACE_IN_THIS_MODULE_
-        DbgTrace ("(should be able to read)sb=%u", sb);
+        DbgTrace ("Seekable case: expectedSize = %d, reserving %d", size, sb);
 #endif
-        // @todo this isn't crazy worse than SmallStackBuffer, because if sb is the size read and wouldn't
-        // fit in a small stack buffer (stack part) - we avoid a second allocation.
-        // But - on balance - thats a lot of iffs. And we probably should use SmallStackBuffer and just
-        // do another alloc if needed. Maybe use an artificially large stack allocaiton here so we are likely to
-        // actually use the stack, or new stl 'get_temporeary_buffer' or wahtever the new routine is for stack allocs
-        //
-        Byte* b = new Byte[sb];   // if this fails, we had no way to create the BLOB
-        try {
-
-            // Even though we've allocated a large enough buffer, the guarnatee of Read () is that it will return
-            // > 0, or less or equal than args buf size, but only 0 means EOF, so must re-read if more out there.
-#if 1
-            size_t  bytesLeft =  sb;
-            Byte*   pi = b;
-            while (bytesLeft > 0) {
-                size_t n = Read (pi, pi + bytesLeft);
-                Assert (n <= bytesLeft);
-                bytesLeft -= n;
-                pi += n;
-                if (n == 0) {
-                    AssertNotReached ();        // @todo decide how to handle this - means stream changed size - doesnt work with this
-                    // algorithm, but not necesarily SB disallowed.
-                    // for now - hack
-                    sb -= bytesLeft;
-                }
-            }
-#else
-            // WRONG
-            size_t n = Read (b, b + sb);
-            Assert (n <= sb);
-#endif
+        r.reserve (sb);
+    }
+    for (size_t nEltsLeft = upTo; nEltsLeft != 0; ) {
 #if     USE_NOISY_TRACE_IN_THIS_MODULE_
-            DbgTrace ("returning %u bytes", sb);
+        DbgTrace ("nEltsLeft=%u", nEltsLeft);
 #endif
-            return BLOB::Attach (b, b + sb);
+        Byte            buf[64 * 1024];
+        Byte*           s           =   std::begin (buf);
+        Byte*           e           =   std::end (buf);
+        if (nEltsLeft < NEltsOf (buf)) {
+            e = s + nEltsLeft;
         }
-        catch (...) {
-            delete[] (b);
-            Execution::ReThrow ();
+        Assert (s < e);
+        size_t          n           =   Read (s, e);
+        Assert (0 <= n and n <= nEltsLeft);
+        Assert (0 <= n and n < NEltsOf (buf));
+        if (n == 0) {
+            break;
+        }
+        else {
+            // @todo???
+            //      could also maintain linked list - std::list<> - of BLOBs, and then construct BLOB from
+            //      list of BLOBs - that would be quite efficeint too - maybe more
+            Containers::ReserveSpeedTweekAddNCapacity (r, n, 32 * 1024); // grow exponentially, so not too many reallocs
+            Assert (n <= nEltsLeft);
+            nEltsLeft -= n;
+            r.insert (r.end (), s, s + n);
         }
     }
-    else {
-        // Less efficient implementation
-        vector<Byte>    r;
-        for (size_t nEltsLeft = upTo; nEltsLeft != 0; ) {
 #if     USE_NOISY_TRACE_IN_THIS_MODULE_
-            DbgTrace ("Unseekable case: nEltsLeft=%u", nEltsLeft);
+    DbgTrace ("returning %d bytes", r.size ());
 #endif
-            Byte            buf[64 * 1024];
-            Byte*           s           =   std::begin (buf);
-            Byte*           e           =   std::end (buf);
-            if (nEltsLeft < NEltsOf (buf)) {
-                e = s + nEltsLeft;
-            }
-            Assert (s < e);
-            size_t          n           =   Read (s, e);
-            Assert (0 <= n and n <= nEltsLeft);
-            Assert (0 <= n and n < NEltsOf (buf));
-            if (n == 0) {
-                break;
-            }
-            else {
-                // @todo???
-                //      could also maintain linked list - std::list<> - of BLOBs, and then construct BLOB from
-                //      list of BLOBs - that would be quite efficeint too - maybe more
-                Containers::ReserveSpeedTweekAddNCapacity (r, n, 32 * 1024); // grow exponentially, so not too many reallocs
-                Assert (n <= nEltsLeft);
-                nEltsLeft -= n;
-                r.insert (r.end (), s, s + n);
-            }
-        }
-        return BLOB (r);
-    }
+    return BLOB (r);
 }
