5787c6e9954a4cdbbe4bd27b84353d5af8d29f30
Lewis Pringle <lewis@sophists.com>
2016-05-27 09:59:00 -0400

    added Modbus::SplitSrcAcrossOutputs

--- Library/Sources/Stroika/Frameworks/Modbus/Server.h
+++ Library/Sources/Stroika/Frameworks/Modbus/Server.h
@@ -75,6 +75,21 @@ namespace   Stroika {
              */
             Execution::Thread   MakeModbusTCPServerThread (const shared_ptr<IModbusService>& serviceHandler, const ServerOptions& options = ServerOptions {});
 
+            namespace {
+                template    <typename MODBUS_REGISTER_DESCRIPTOR, typename SRC_TYPE>
+                void    SplitSrcAcrossOutputs (const SRC_TYPE& s, typename MODBUS_REGISTER_DESCRIPTOR::NameType baseRegister, Containers::Mapping<typename MODBUS_REGISTER_DESCRIPTOR::NameType, typename MODBUS_REGISTER_DESCRIPTOR::ValueType>* update)
+                {
+                    using   VT = typename MODBUS_REGISTER_DESCRIPTOR::ValueType;
+                    constexpr   size_t  kNElts_  =   sizeof (SRC_TYPE) / sizeof (VT);
+                    static_assert (kNElts_ * sizeof (VT) == sizeof (SRC_TYPE), ""); // only works if divides evenly
+                    RequireNotNull (update);
+                    const VT* arr = reinterpret_cast<const VT*> (&s);
+                    for (size_t i = 0; i < kNElts_; ++i) {
+                        // high order addressed elements first - big endian
+                        update->Add (baseRegister + i - 1, arr[kNElts_ -  i - 1]);
+                    }
+                }
+            }
 
         }
     }
