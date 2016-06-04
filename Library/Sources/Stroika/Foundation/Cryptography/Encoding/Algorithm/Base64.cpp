52fdd1d4341b5e6423901481c619ec5e75e7a30a
Lewis Pringle <lewis@sophists.com>
2016-06-03 22:31:32 -0400

    silence a warning in Encoding/Algorithm/Base64

--- Library/Sources/Stroika/Foundation/Cryptography/Encoding/Algorithm/Base64.cpp
+++ Library/Sources/Stroika/Foundation/Cryptography/Encoding/Algorithm/Base64.cpp
@@ -71,10 +71,10 @@ namespace   {
     {
         static const signed char kDecoding[] = {62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -2, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
         value_in -= 43;
-        if (value_in < 0 || value_in >= NEltsOf (kDecoding)) {
+        if (value_in < 0 || static_cast<unsigned char> (value_in) >= NEltsOf (kDecoding)) {
             return -1;
         }
-        Assert (0 <= value_in and value_in < NEltsOf (kDecoding));
+        Assert (0 <= value_in and static_cast<unsigned char> (value_in) < NEltsOf (kDecoding));
         return kDecoding[(int)value_in];
     }
     size_t base64_decode_block_ (const signed char* code_in, size_t length_in, Byte* plaintext_out, base64_decodestate_* state)
