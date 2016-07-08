cbd6ab16c592627cd5e99a16b7b8c8697f76b35b
Lewis Pringle <lewis@sophists.com>
2016-07-08 09:32:34 -0400

    fixed typo

--- Library/Sources/Stroika/Foundation/Configuration/Version.cpp
+++ Library/Sources/Stroika/Foundation/Configuration/Version.cpp
@@ -35,7 +35,7 @@ Version Version::FromWin32Version4DotString (const Characters::String& win32Vers
     int nMatchingItems = ::swscanf (win32Version4DotString.c_str (), L"%d.%d.%d.%d", &major, &minor, &verStageOctet, &verSubStageOctet);
     DISABLE_COMPILER_MSC_WARNING_END(4996)
     int     verStage        =   static_cast<uint16_t> (verStageOctet) >> 5;
-    Assert (verStage == Memory::BitSubstring (5, 8));   // really only true cuz verStageOctet SB 8-bits - so if this fails, this answer probably better --LGP 2016-07-08
+    Assert (verStage == Memory::BitSubstring (verStageOctet, 5, 8));   // really only true cuz verStageOctet SB 8-bits - so if this fails, this answer probably better --LGP 2016-07-08
     int     verSubStage     =   (Memory::BitSubstring (verStageOctet, 0, 5) << 7) + Memory::BitSubstring (verSubStageOctet, 1, 8);
     bool    verFinal        =   verSubStageOctet & 0x1;
     if (nMatchingItems != 4 or not (ToInt (VersionStage::eSTART) <= verStage and verStage <= ToInt (VersionStage::eLAST))) {
