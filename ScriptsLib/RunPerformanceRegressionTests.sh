f5c8bcb1576588d8a66341cc6e78a7135955e899
Lewis G. Pringle, Jr <lewis@sophists.com>
2016-07-31 21:32:27 -0400

    tweak ScriptsLib/RunPerformanceRegressionTests.sh

--- ScriptsLib/RunPerformanceRegressionTests.sh
+++ ScriptsLib/RunPerformanceRegressionTests.sh
@@ -12,23 +12,23 @@
 VER=`ScriptsLib/ExtractVersionInformation.sh STROIKA_VERSION FullVersionString`
 if   [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
     # Do something under GNU/Linux platform
-    CONFIGURATION=${CONFIGURATION-"gcc-6.1.0-release-c++17"}
+    EXE=${EXE-"Builds/gcc-6.1.0-release-c++17/Test47"}
     CONFIG_LABEL_IN_OUTPUT="linux-gcc-6.1.0-x64"
     CMDRUN_PREFIX="nice -10 taskset -c 0 "
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ; then
     # Do something under Windows NT platform
-    CONFIGURATION=${CONFIGURATION-"Release-U-32"}
+    EXE=${EXE-"Builds/Release-U-32/Test47/Test47"}
     CONFIG_LABEL_IN_OUTPUT="x86-vs2k15"
     CMDRUN_PREFIX=""
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ] ; then
     # Do something under Windows NT platform
-    CONFIGURATION=${CONFIGURATION-"Release-U-32"}
+    EXE=${EXE-"Builds/Release-U-32/Test47/Test47"}
     CONFIG_LABEL_IN_OUTPUT="x86-vs2k15"
     CMDRUN_PREFIX=""
 fi
 
 MULTIPLIER=15
 
-echo -n "$CMDRUN_PREFIX" "Builds/$CONFIGURATION/Test47/Test47 --x $MULTIPLIER --show > Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-$CONFIG_LABEL_IN_OUTPUT.txt..."
-$CMDRUN_PREFIX Builds/$CONFIGURATION/Test47/Test47 --x $MULTIPLIER --show > Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-$CONFIG_LABEL_IN_OUTPUT.txt
+echo -n "$CMDRUN_PREFIX" "$EXE --x $MULTIPLIER --show > Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-$CONFIG_LABEL_IN_OUTPUT.txt..."
+$CMDRUN_PREFIX $EXE --x $MULTIPLIER --show > Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-$CONFIG_LABEL_IN_OUTPUT.txt
 echo "done"
