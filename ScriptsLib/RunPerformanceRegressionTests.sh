42975db62837b569c125bd36467d1dce16a502ad
Lewis G. Pringle, Jr <lewis@sophists.com>
2016-08-18 19:28:44 -0400

    changed regression test # to 48

--- ScriptsLib/RunPerformanceRegressionTests.sh
+++ ScriptsLib/RunPerformanceRegressionTests.sh
@@ -5,25 +5,25 @@
 #
 #	Examples:
 #		ScriptsLib/RunPerformanceRegressionTests.sh
-#		EXE=Builds/gcc-6.1.0-release-c++17/Test47 ScriptsLib/RunPerformanceRegressionTests.sh
-#		EXE=Builds/Release/Test47 CONFIG_LABEL_IN_OUTPUT=FRED ScriptsLib/RunPerformanceRegressionTests.sh
+#		EXE=Builds/gcc-6.1.0-release-c++17/Test48 ScriptsLib/RunPerformanceRegressionTests.sh
+#		EXE=Builds/Release/Test48 CONFIG_LABEL_IN_OUTPUT=FRED ScriptsLib/RunPerformanceRegressionTests.sh
 #		MULTIPLIER=1 ScriptsLib/RunPerformanceRegressionTests.sh
 #
 VER=`ScriptsLib/ExtractVersionInformation.sh STROIKA_VERSION FullVersionString`
 MULTIPLIER=${MULTIPLIER-15}
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
     # Do something under GNU/Linux platform
-    EXE=${EXE-"Builds/gcc-6.1.0-release-c++17/Test47"}
+    EXE=${EXE-"Builds/gcc-6.1.0-release-c++17/Test48"}
     CONFIG_LABEL_IN_OUTPUT="linux-gcc-6.1.0-x64"
     CMDRUN_PREFIX="nice -10 taskset -c 0 "
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ; then
     # Do something under Windows NT platform
-    EXE=${EXE-"Builds/Release-U-32/Test47/Test47"}
+    EXE=${EXE-"Builds/Release-U-32/Test48/Test48"}
     CONFIG_LABEL_IN_OUTPUT="x86-vs2k15"
     CMDRUN_PREFIX=""
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ] ; then
     # Do something under Windows NT platform
-    EXE=${EXE-"Builds/Release-U-32/Test47/Test47"}
+    EXE=${EXE-"Builds/Release-U-32/Test48/Test48"}
     CONFIG_LABEL_IN_OUTPUT="x86-vs2k15"
     CMDRUN_PREFIX=""
 else
