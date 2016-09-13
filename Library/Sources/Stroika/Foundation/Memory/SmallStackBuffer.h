472f0e4d2f411b69713c400e0aac4f852a31462f
Lewis Pringle <lewis@sophists.com>
2016-09-13 09:38:19 -0400

    Fix alignas usage in SmallStackBuffer - fBuffer sometimes used to hold T, and sometimes used to hold size_t

--- Library/Sources/Stroika/Foundation/Memory/SmallStackBuffer.h
+++ Library/Sources/Stroika/Foundation/Memory/SmallStackBuffer.h
@@ -141,7 +141,7 @@ namespace   Stroika {
 #if     qDebug
             private:
                 static  constexpr   Byte    kGuard1_[8]  =   { 0x45, 0x23, 0x12, 0x56, 0x99, 0x76, 0x12, 0x55, };
-                static  constexpr   Byte    kGuard2_[8]  =   { 0x15, 0x32, 0xa5, 0x16, 0x15, 0x7a, 0x90, 0x10, };
+                static  constexpr   Byte    kGuard2_[8]  =   { 0x15, 0x32, 0xa5, 0x16, 0x11, 0x7a, 0x90, 0x10, };
 #endif
 
             private:
@@ -149,7 +149,7 @@ namespace   Stroika {
 #if     qDebug
                 Byte    fGuard1_[sizeof(kGuard1_)];
 #endif
-                T       fBuffer_[BUF_SIZE];
+                alignas (T) alignas (size_t)	T       fBuffer_[BUF_SIZE];	// alignas both since sometimes accessed as array of T, and sometimes as size_t
 #if     qDebug
                 Byte    fGuard2_[sizeof(kGuard2_)];
 #endif
