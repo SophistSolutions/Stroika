c1be87f0d7add11431bd278542460d639a4cfed3
Lewis Pringle <lewis@sophists.com>
2016-09-15 09:24:46 -0400

    fixed NUM_REGTESTS in RegressionTests script

--- ScriptsLib/RegressionTests.sh
+++ ScriptsLib/RegressionTests.sh
@@ -41,7 +41,7 @@ echo "$PREFIX_OUT_LABEL" "Resetting all configurations to standard regression te
 make regression-test-configurations >>$TEST_OUT_FILE 2>&1
 
 NUM_CONFIGURATIONS=`sh ScriptsLib/GetConfigurations.sh | wc -w`
-NUM_REGTESTS=47
+NUM_REGTESTS=48
 NUM_PASSES_OF_REGTESTS_RUN=$NUM_CONFIGURATIONS
 
 echo "Building configurations ($NUM_CONFIGURATIONS):"
