a704f0c3e429a138e1e0b8a32445303df50427d5
Lewis <Lewis@ANTIPHON>
2016-09-30 16:39:12 -0400

    fixed serious bug in Request::GetBody - dont call ReadAll (0-size) - but just directly return empty blob

--- Library/Sources/Stroika/Frameworks/WebServer/Request.cpp
+++ Library/Sources/Stroika/Frameworks/WebServer/Request.cpp
@@ -13,6 +13,7 @@
 #include    "../../Foundation/Containers/Common.h"
 #include    "../../Foundation/DataExchange/BadFormatException.h"
 #include    "../../Foundation/Debug/Assertions.h"
+#include    "../../Foundation/Debug/Trace.h"
 #include    "../../Foundation/Execution/Exceptions.h"
 #include    "../../Foundation/Memory/SmallStackBuffer.h"
 #include    "../../Foundation/IO/Network/HTTP/Headers.h"
@@ -27,6 +28,15 @@ using   namespace   Stroika::Foundation::Memory;
 
 using   namespace   Stroika::Frameworks;
 using   namespace   Stroika::Frameworks::WebServer;
+
+
+
+
+
+
+
+// Comment this in to turn on aggressive noisy DbgTrace in this module
+//#define   USE_NOISY_TRACE_IN_THIS_MODULE_       1
 
 
 
@@ -47,16 +57,21 @@ Request::Request (const Streams::InputStream<Byte>& inStream)
 
 Memory::BLOB    Request::GetBody ()
 {
+#if     USE_NOISY_TRACE_IN_THIS_MODULE_
+    Debug::TraceContextBumper ctx (L"Request::GetBody");
+#endif
     lock_guard<const AssertExternallySynchronizedLock> critSec { *this };
     if (fBody_.IsMissing ()) {
         // if we have a content-length, read that many bytes. otherwise, read til EOF
         if (auto ci = fHeaders_.Lookup (IO::Network::HTTP::HeaderName::kContentLength)) {
             size_t contentLength = Characters::String2Int<size_t> (*ci);
             Memory::SmallStackBuffer<Memory::Byte>  buf (contentLength);
-            size_t  n   =   fInputStream_.ReadAll (begin (buf), end (buf));
-            Assert (n <= contentLength);
-            if (n != contentLength) {
-                Execution::Throw (Execution::StringException (Characters::Format (L"Unexpected wrong number of bytes returned in HTTP body (found %d, but content-length %d)", n, contentLength)));
+            if (contentLength != 0) {
+                size_t  n   =   fInputStream_.ReadAll (begin (buf), end (buf));
+                Assert (n <= contentLength);
+                if (n != contentLength) {
+                    Execution::Throw (Execution::StringException (Characters::Format (L"Unexpected wrong number of bytes returned in HTTP body (found %d, but content-length %d)", n, contentLength)));
+                }
             }
             fBody_ = Memory::BLOB (begin (buf), end (buf));
         }
