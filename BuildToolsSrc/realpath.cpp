bd4d64adc7ab90d6fdb66451fc63bb12ba03722a
Lewis <Lewis@ANTIPHON>
2016-12-30 20:20:14 -0500

    draft realpath impl - to maybe use to workaround missing realpath on macosx

---
+++ BuildToolsSrc/realpath.cpp
@@ -0,0 +1,23 @@
+// HACK - lifted from https://github.com/harto/realpath-osx
+#include <cstdlib>
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: realpath <path>\n");
+        return 1;
+    }
+
+    char* path = argv[1];
+    char result[PATH_MAX];
+
+    if (realpath(path, result)) {
+        printf("%s\n", result);
+        return 0;
+    }
+    else {
+        fprintf(stderr, "Bad path: %s\n", result);
+        return 2;
+    }
+}
