13a55ca453a8844585002690e7066e590796f31a
Lewis <Lewis@ANTIPHON>
2016-12-31 08:52:14 -0500

    fixed typo

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_Execution_Threads_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_Execution_Threads_.h
@@ -31,7 +31,10 @@
 #define qHas_pthread_setschedprio       qPlatform_POSIX
 #endif
 
-#endif
+#endif	/*qHas_pthread_setschedprio*/
+
+
+#endif	/*defined(__cplusplus)*/
 
 
 #endif  /*_Stroika_Foundation_Configuration_Private_Defaults_Execution_Threads_h_*/
