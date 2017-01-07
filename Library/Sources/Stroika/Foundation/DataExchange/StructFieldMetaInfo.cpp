0a3025d1008fa7d61a605a4b6860afd474f3bb56
Lewis <Lewis@ANTIPHON>
2017-01-07 11:55:06 -0500

    add missing file b/Library/Sources/Stroika/Foundation/DataExchange/StructFieldMetaInfo.cpp

---
+++ Library/Sources/Stroika/Foundation/DataExchange/StructFieldMetaInfo.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2016.  All rights reserved
+ */
+#include    "../StroikaPreComp.h"
+
+#include    "../Characters/ToString.h"
+
+#include    "StructFieldMetaInfo.h"
+
+
+
+using   namespace   Stroika::Foundation;
+using   namespace   Stroika::Foundation::Characters;
+using   namespace   Stroika::Foundation::DataExchange;
+
+
+
+
+
+
+/*
+ ********************************************************************************
+ ********************* DataExchange::StructFieldMetaInfo ************************
+ ********************************************************************************
+ */
+Characters::String  StructFieldMetaInfo::ToString () const
+{
+    return
+        String { L"{" } +
+        L"fOffset: " + Characters::ToString (fOffset) +  L", "  +
+        L"fTypeInfo: " + Characters::ToString (fTypeInfo) +
+        L"}"
+        ;
+}
+
