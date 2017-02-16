b15a3f4c514da759ecc2f1cabccafdec2898ac53
Lewis Pringle <lewis@sophists.com>
2017-02-16 09:00:37 -0500

    had to tweak Execution/ErrNoException for macos

--- Library/Sources/Stroika/Foundation/Execution/ErrNoException.cpp
+++ Library/Sources/Stroika/Foundation/Execution/ErrNoException.cpp
@@ -47,22 +47,27 @@ SDKString errno_ErrorException::LookupMessage (Execution::errno_t e)
     if (::_tcserror_s (buf, e) == 0) {
         return buf + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
     }
-#elif qPlatform_POSIX
-/*
-     * A bit quirky - GNU-specific and POSIX handle this API fairly differently.
-     * https://linux.die.net/man/3/strerror_r - in one case returns int and 0 means worked, 
-     * and other case always returns string to use (not in buf)
-     */
+#elif qPlatform_Linux
+	/*
+	 * A bit quirky - GNU-specific and POSIX handle this API fairly differently.
+	 * https://linux.die.net/man/3/strerror_r - in one case returns int and 0 means worked, 
+	 * and other case always returns string to use (not in buf)
+	 */
 #if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE
-    // The XSI-compliant strerror_r() function returns 0 on success
-    if (::strerror_r (e, buf, NEltsOf (buf)) == 0) {
-        return buf + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
-    }
+	// The XSI-compliant strerror_r() function returns 0 on success
+	if (::strerror_r (e, buf, NEltsOf (buf)) == 0) {
+		return buf + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
+	}
 #else
-    // the GNU-specific strerror_r() functions return the appropriate error description string
-    // NOTE - this version MAY or MAY NOT modify buf - it sometimes returns static strings!
-    return ::strerror_r (e, buf, NEltsOf (buf)) + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
+	// the GNU-specific strerror_r() functions return the appropriate error description string
+	// NOTE - this version MAY or MAY NOT modify buf - it sometimes returns static strings!
+	return ::strerror_r (e, buf, NEltsOf (buf)) + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
 #endif
+#elif qPlatform_POSIX
+	// The XSI-compliant strerror_r() function returns 0 on success
+	if (::strerror_r (e, buf, NEltsOf (buf)) == 0) {
+		return buf + SDKString (SDKSTR (" (") + justErrnoNumberMessage + SDKSTR (")"));
+	}
 #else
     AssertNotImplemented ();
 #endif
