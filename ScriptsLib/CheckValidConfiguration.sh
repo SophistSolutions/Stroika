93ce0455920738881c0e9dad2fe75525ec5e72ad
Lewis Pringle <lewis@sophists.com>
2017-02-27 12:52:12 -0500

    Improved reporting from ScriptsLib/CheckValidConfiguration.sh

--- ScriptsLib/CheckValidConfiguration.sh
+++ ScriptsLib/CheckValidConfiguration.sh
@@ -3,6 +3,6 @@ MY_PATH="`dirname \"$0\"`"
 MY_PATH=`realpath $MY_PATH`
 CONFIGURATION=$1
 if [ ! -e "$MY_PATH/../ConfigurationFiles/$CONFIGURATION.xml" ] ; then
-	echo "$CONFIGURATION is not a valid configuration"
+	echo -n "\"$CONFIGURATION\" is not a valid configuration; valid configurations include: [ " && $MY_PATH/GetConfigurations.sh && echo "]";
 	exit 1;
 fi
