3b816d7615028db28f732fb3919d1232833c44c9
Lewis Pringle <lewis@sophists.com>
2017-03-31 21:30:28 -0400

    silence needless info warning from mklink call

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -4,7 +4,7 @@ TO=$2
 if [[ `uname` =~ "CYGWIN" ]] ; then
 	#Sigh...
 	#cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $FROM $TO" || (echo "mklink failed- Allow UAC or run as administrator"  && exit 1)
-	cmd /C "mklink /D $FROM $TO"
+	cmd /C "mklink /D $FROM $TO" > /dev/null
 else
 	ln -s $TO $FROM
 fi
