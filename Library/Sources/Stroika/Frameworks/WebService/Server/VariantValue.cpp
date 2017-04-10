1e7f2caa191079242760f89d8e4deb44dece2e41
Lewis Pringle <lewis@sophists.com>
2017-04-10 12:14:01 -0400

    forgot missing checkins

---
+++ Library/Sources/Stroika/Frameworks/WebService/Server/VariantValue.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2017.  All rights reserved
+ */
+#include "../../StroikaPreComp.h"
+
+#include "../../../Foundation/DataExchange/Variant/JSON/Reader.h"
+#include "../../../Foundation/DataExchange/Variant/JSON/Writer.h"
+
+#include "Basic.h"
+
+#include "VariantValue.h"
+
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Containers;
+using namespace Stroika::Foundation::DataExchange;
+
+using namespace Stroika::Frameworks;
+using namespace Stroika::Frameworks::WebService;
+using namespace Stroika::Frameworks::WebService::Server;
+using namespace Stroika::Frameworks::WebService::Server;
+
+using Characters::StringBuilder;
+using Characters::String_Constant;
+using DataExchange::VariantValue;
+
+/*
+ ********************************************************************************
+ **** WebService::Server::VariantValue::GetWebServiceArgsAsVariantValue *********
+ ********************************************************************************
+ */
+DataExchange::VariantValue Server::VariantValue::GetWebServiceArgsAsVariantValue (Request* request, const Optional<String>& fromInMessage)
+{
+    String method{request->GetHTTPMethod ()};
+    if (method == L"POST") {
+        // Allow missing (content-size: 0) for args to method - and dont fail it as invalid json
+        // @also - @todo - check ContentType ebfore reading as JSON!!!
+        Memory::BLOB inData = request->GetBody ();
+        return inData.empty () ? VariantValue{} : Variant::JSON::Reader ().Read (inData);
+    }
+    else if (method == L"GET") {
+        IO::Network::URL url = request->GetURL ();
+        // get query args
+        // For now - only support String values of query-string args
+        Mapping<String, VariantValue> result;
+        url.GetQuery ().GetMap ().Apply ([&result](const KeyValuePair<String, String>& kvp) { result.Add (kvp.fKey, kvp.fValue); });
+        return VariantValue{result};
+    }
+    else {
+        Execution::Throw (Execution::StringException (
+            String_Constant{L"Expected GET with query-string arguments or POST"} +
+            (fromInMessage ? (L" from " + *fromInMessage) : L"")));
+    }
+}
+
+/*
+ ********************************************************************************
+ ***************** WebService::Server::VariantValue::WriteResponse **************
+ ********************************************************************************
+ */
+void Server::VariantValue::WriteResponse (Response* response, const WebServiceMethodDescription& webServiceDescription, const VariantValue& responseValue)
+{
+    Require (webServiceDescription.fResponseType == DataExchange::PredefinedInternetMediaType::JSON_CT ()); // all we support for now
+    response->write (Variant::JSON::Writer ().WriteAsBLOB (responseValue));
+    if (webServiceDescription.fResponseType) {
+        response->SetContentType (*webServiceDescription.fResponseType);
+    }
+}
+
+/*
+ ********************************************************************************
+ **************** WebService::Server::VariantValue::mkRequestHandler ************
+ ********************************************************************************
+ */
+WebServer::RequestHandler Server::VariantValue::mkRequestHandler (const WebServiceMethodDescription& webServiceDescription, const DataExchange::ObjectVariantMapper& objVarMapper, const function<Memory::BLOB (WebServer::Message* m)>& f)
+{
+    return [=](WebServer::Message* m) {
+        ExpectedMethod (m->PeekRequest (), webServiceDescription);
+        if (webServiceDescription.fResponseType) {
+            m->PeekResponse ()->SetContentType (*webServiceDescription.fResponseType);
+        }
+        m->PeekResponse ()->write (f (m));
+    };
+}
---
+++ Library/Sources/Stroika/Frameworks/WebService/Server/VariantValue.inl
@@ -0,0 +1,45 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2017.  All rights reserved
+ */
+#ifndef _Stroika_Framework_WebService_Server_ObjectVariantMapper_inl_
+#define _Stroika_Framework_WebService_Server_ObjectVariantMapper_inl_ 1
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+namespace Stroika {
+    namespace Frameworks {
+        namespace WebService {
+            namespace Server {
+                namespace VariantValue {
+
+                    /*
+                     ********************************************************************************
+                     **************** WebService::Server::VariantValue::mkRequestHandler ************
+                     ********************************************************************************
+                     */
+                    template <typename OUT_ARGS, typename IN_ARGS>
+                    WebServer::RequestHandler mkRequestHandler (const WebServiceMethodDescription& webServiceDescription, const DataExchange::ObjectVariantMapper& objVarMapper, const function<OUT_ARGS (IN_ARGS)>& f)
+                    {
+                        return [=](WebServer::Message* m) {
+                            ExpectedMethod (m->PeekRequest (), webServiceDescription);
+                            WriteResponse (m->PeekResponse (), webServiceDescription, objVarMapper.FromObject (f (objVarMapper.ToObject<IN_ARGS> (GetWebServiceArgsAsVariantValue (m->PeekRequest (), {})))));
+                        };
+                    }
+                    template <typename OUT_ARGS>
+                    WebServer::RequestHandler mkRequestHandler (const WebServiceMethodDescription& webServiceDescription, const DataExchange::ObjectVariantMapper& objVarMapper, const function<OUT_ARGS (void)>& f)
+                    {
+                        return [=](WebServer::Message* m) {
+                            ExpectedMethod (m->PeekRequest (), webServiceDescription);
+                            WriteResponse (m->PeekResponse (), webServiceDescription, objVarMapper.FromObject (f ()));
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
+#endif /*_Stroika_Framework_WebService_Server_ObjectVariantMapper_inl_*/
