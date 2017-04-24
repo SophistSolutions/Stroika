681cac1db5d4670e1ae9f90478c956808863a842
Lewis Pringle <lewis@sophists.com>
2017-04-24 12:33:52 -0400

    typo

--- Library/Sources/Stroika/Foundation/DataExchange/InternetMediaTypeRegistry.cpp
+++ Library/Sources/Stroika/Foundation/DataExchange/InternetMediaTypeRegistry.cpp
@@ -40,7 +40,7 @@ namespace {
         Debug::TraceContextBumper ctx{L"ReadGlobsFile_"};
 #endif
         Bijection<InternetMediaType, FileSuffixType> results{};
-        for (Sequence<String> line : DataExchange::Variant::CharacterDelimitedLines::Reader{L":"}) {
+        for (Sequence<String> line : DataExchange::Variant::CharacterDelimitedLines::Reader{ { ':' } }) {
             if (line.length () == 2) {
                 String glob = line[1];
                 if (glob.StartsWith ('*')) {
