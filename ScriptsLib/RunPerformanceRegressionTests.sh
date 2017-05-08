26519e73593920e7217ca198b2d6ca3010900edb
Lewis G. Pringle, Jr <lewis@sophists.com>
2017-05-08 10:12:13 -0400

    fixed ScriptsLib/RunPerformanceRegressionTests.sh for new test numbering

--- ScriptsLib/RunPerformanceRegressionTests.sh
+++ ScriptsLib/RunPerformanceRegressionTests.sh
@@ -5,29 +5,29 @@
 #
 #	Examples:
 #		ScriptsLib/RunPerformanceRegressionTests.sh
-#		EXE=Builds/my-gcc-6.1.0-release-c++17/Test48 ScriptsLib/RunPerformanceRegressionTests.sh
-#		EXE=Builds/Release/Test48 CONFIG_LABEL_IN_OUTPUT=FRED ScriptsLib/RunPerformanceRegressionTests.sh
+#		EXE=Builds/my-gcc-6.1.0-release-c++17/Test49 ScriptsLib/RunPerformanceRegressionTests.sh
+#		EXE=Builds/Release/Test49 CONFIG_LABEL_IN_OUTPUT=FRED ScriptsLib/RunPerformanceRegressionTests.sh
 #		MULTIPLIER=1 ScriptsLib/RunPerformanceRegressionTests.sh
 #
 VER=`ScriptsLib/ExtractVersionInformation.sh STROIKA_VERSION FullVersionString`
 MULTIPLIER=${MULTIPLIER-15}
 if [ "$(uname -s)" == "Darwin" ] ; then
-    EXE=${EXE-"Builds/Release/Test48"}
+    EXE=${EXE-"Builds/Release/Test49"}
     CONFIG_LABEL_IN_OUTPUT="MacOS-x86-XCode8"
     CMDRUN_PREFIX="nice -10 "
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
     # Do something under GNU/Linux platform
-    EXE=${EXE-"Builds/my-gcc-6.3.0-release-c++17/Test48"}
+    EXE=${EXE-"Builds/my-gcc-6.3.0-release-c++17/Test49"}
     CONFIG_LABEL_IN_OUTPUT="linux-gcc-6.3.0-x64"
     CMDRUN_PREFIX="nice -10 taskset -c 0 "
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ; then
     # Do something under Windows NT platform
-    EXE=${EXE-"Builds/Release-U-32/Test48/Test48"}
+    EXE=${EXE-"Builds/Release-U-32/Test49/Test49"}
     CONFIG_LABEL_IN_OUTPUT="Windows-x86-vs2k17"
     CMDRUN_PREFIX=""
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ] ; then
     # Do something under Windows NT platform
-    EXE=${EXE-"Builds/Release-U-32/Test48/Test48"}
+    EXE=${EXE-"Builds/Release-U-32/Test49/Test49"}
     CONFIG_LABEL_IN_OUTPUT="Windows-x86-vs2k17"
     CMDRUN_PREFIX=""
 else
