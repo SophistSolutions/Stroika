ecc82278d40b146051819638ee96cb0df6df553a
Lewis Pringle <lewis@sophists.com>
2017-05-14 22:30:57 -0400

    more tweaks to FileAccessMode change

--- Library/Sources/Stroika/Foundation/Execution/ProcessRunner.cpp
+++ Library/Sources/Stroika/Foundation/Execution/ProcessRunner.cpp
@@ -532,7 +532,7 @@ function<void()> ProcessRunner::CreateRunnable_ (Memory::Optional<ProcessResultT
                     try {
                         errno_ErrorException::Throw (errno);
                     }
-                    Stroika_Foundation_IO_FileAccessException_CATCH_REBIND_FILENAME_ACCCESS_HELPER (commandLine[0], FileAccessMode::eRead);
+                    Stroika_Foundation_IO_FileAccessException_CATCH_REBIND_FILENAME_ACCCESS_HELPER (commandLine[0], IO::FileSystem::FileAccessMode::eRead);
                 }
             }
         }
