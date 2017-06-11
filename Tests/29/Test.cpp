3d3acd362f03c2b1f0263183c7b28f1a0b71baef
Lewis Pringle <lewis@sophists.com>
2017-06-10 21:21:44 -0400

    remove pointless test

--- Tests/29/Test.cpp
+++ Tests/29/Test.cpp
@@ -359,7 +359,7 @@ namespace Test_04_CheckStringQuoting_ {
         CheckRoundtrip_encode_decode_unchanged (VariantValue (L"c:\\"));
         CheckRoundtrip_encode_decode_unchanged (VariantValue (L"'"));
         CheckRoundtrip_encode_decode_unchanged (VariantValue (L"\""));
-        CheckRoundtrip_encode_decode_unchanged (VariantValue (L"\\u20a9")); //  ₩
+        //this test is OK, but makes no sense, and isn't testing what it appears to--- CheckRoundtrip_encode_decode_unchanged (VariantValue (L"\\u20a9")); //  ₩
         CheckRoundtrip_encode_decode_unchanged (VariantValue (L"\u20a9"));  //  ₩
         CheckRoundtrip_encode_decode_unchanged (VariantValue (L"\"apple\""));
         CheckRoundtrip_encode_decode_unchanged (VariantValue (numeric_limits<int>::min ()));
