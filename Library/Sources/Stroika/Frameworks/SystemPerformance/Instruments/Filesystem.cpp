bd7286c34a0f1f4bfbe9dd33acc6d44f34d3a2fd
Lewis Pringle <lewis@sophists.com>
2017-07-13 23:22:26 -0400

    make format

--- Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Filesystem.cpp
+++ Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Filesystem.cpp
@@ -464,7 +464,7 @@ namespace {
         Mapping<MountedFilesystemNameType, MountedFilesystemInfoType> RunDF_POSIX_ ()
         {
             Mapping<MountedFilesystemNameType, MountedFilesystemInfoType> result;
-            ProcessRunner               pr{L"/bin/df -k -P"};
+            ProcessRunner                    pr{L"/bin/df -k -P"};
             Streams::MemoryStream<Byte>::Ptr useStdOut = Streams::MemoryStream<Byte>::New ();
             pr.SetStdOut (useStdOut);
             std::exception_ptr runException;
