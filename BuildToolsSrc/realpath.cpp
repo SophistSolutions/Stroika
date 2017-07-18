f9a873d6de790d0a0d889231f2f894956426492d
Lewis Pringle <lewis@sophists.com>
2017-07-18 11:19:52 -0400

    fixed regrressions in realpath.cpp

--- BuildToolsSrc/realpath.cpp
+++ BuildToolsSrc/realpath.cpp
@@ -1,12 +1,13 @@
 // HACK - lifted from https://github.com/harto/realpath-osx
 #include <climits>
+#include <cstring>
 #include <cstdio>
 #include <cstdlib>
 
 int main (int argc, char** argv)
 {
     size_t pathIdx = 1;
-    if (argc == 3, &&strcmp (argv[1], "--canonicalize-missing") == 0) {
+    if (argc == 3 && strcmp (argv[1], "--canonicalize-missing") == 0) {
         pathIdx += 1;
     }
     else if (argc != 2) {
