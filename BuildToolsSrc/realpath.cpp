62eadae9a86b338a05615705535e5cad1c4483bf
Lewis Pringle <lewis@sophists.com>
2017-07-20 09:47:17 -0400

    fixed macos-specific realpath override implementation so works with --canonicalize-missing - enough

--- BuildToolsSrc/realpath.cpp
+++ BuildToolsSrc/realpath.cpp
@@ -3,12 +3,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 
 int main (int argc, char** argv)
 {
-    size_t pathIdx = 1;
+    using namespace std;
+    size_t pathIdx             = 1;
+    bool   canonicalizeMissing = false;
     if (argc == 3 && strcmp (argv[1], "--canonicalize-missing") == 0) {
         pathIdx += 1;
+        canonicalizeMissing = true;
     }
     else if (argc != 2) {
         fprintf (stderr, "Usage: realpath <path>\n");
@@ -18,7 +22,19 @@ int main (int argc, char** argv)
     char* path = argv[pathIdx];
     char  result[PATH_MAX];
 
-    if (realpath (path, result)) {
+    if (canonicalizeMissing) {
+        // super-primitive impl - wont work with funny (quote) chars in pathnames
+        string cmd;
+        cmd += "echo `dirname " + string (path) + "`'/'`basename " + string (path) + "`";
+        FILE* in = nullptr;
+        if ((in = popen (cmd.c_str (), "r")) != NULL) {
+            fgets (result, sizeof (result) / sizeof (result[0]), in);
+            printf ("%s\n", result);
+            pclose (in);
+            return 0;
+        }
+    }
+    else if (realpath (path, result)) {
         printf ("%s\n", result);
         return 0;
     }
