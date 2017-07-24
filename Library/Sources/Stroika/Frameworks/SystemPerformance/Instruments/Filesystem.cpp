cad85b64371d94e909e2885e385ac89ced70a356
Lewis G. Pringle, Jr <lewis@sophists.com>
2017-07-24 17:11:44 -0400

    react to TestReader changes

--- Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Filesystem.cpp
+++ Library/Sources/Stroika/Frameworks/SystemPerformance/Instruments/Filesystem.cpp
@@ -475,7 +475,7 @@ namespace {
                 runException = current_exception ();
             }
             String              out;
-            Streams::TextReader stdOut{useStdOut};
+            Streams::TextReader::Ptr  stdOut = Streams::TextReader::New (useStdOut);
             bool                skippedHeader = false;
             for (String i = stdOut.ReadLine (); not i.empty (); i = stdOut.ReadLine ()) {
                 if (not skippedHeader) {
@@ -537,7 +537,7 @@ namespace {
                 runException = current_exception ();
             }
             String              out;
-            Streams::TextReader stdOut{useStdOut};
+            Streams::TextReader::Ptr stdOut = Streams::TextReader::New (useStdOut);
             bool                skippedHeader = false;
             for (String i = stdOut.ReadLine (); not i.empty (); i = stdOut.ReadLine ()) {
                 if (not skippedHeader) {
