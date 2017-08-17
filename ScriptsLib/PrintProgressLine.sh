af60da27e45ce636f1025c198bb0f965a1b6849e
Lewis Pringle <lewis@sophists.com>
2017-08-16 20:02:30 -0400

    first draft ScriptsLib/{PrintProgressLine,SubstituteBackVariables}

---
+++ ScriptsLib/PrintProgressLine.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+THIS_SCRIPT_DIR="`dirname \"$0\"`"
+THIS_SCRIPT_DIR=`realpath $THIS_SCRIPT_DIR`
+THIS_SCRIPT_DIR="$THIS_SCRIPT_DIR/"
+
+LEVEL=""
+NFLAG=""
+PRINT_STUFF_EXTRAS=""
+for arg in "$@"
+do
+	if [ "$LEVEL" = "" ] ; then
+		LEVEL=$arg
+	else
+		case "$arg" in
+		-n)    NFLAG=$arg
+				;;
+		*)      PRINT_STUFF_EXTRAS="$PRINT_STUFF_EXTRAS $arg"
+				;;
+		esac
+	fi
+done
+ECHO=${ECHO:-echo}
+
+STR=`$THIS_SCRIPT_DIR/PrintLevelLeader.sh $LEVEL`
+$ECHO $NFLAG "$STR$PRINT_STUFF_EXTRAS"
---
+++ ScriptsLib/SubstituteBackVariables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+THIS_SCRIPT_DIR="`dirname \"$0\"`"
+THIS_SCRIPT_DIR=`realpath $THIS_SCRIPT_DIR`
+THIS_SCRIPT_DIR="$THIS_SCRIPT_DIR/"
+
+TEXT="$1"
+
+
+if [ ! -z $StroikaRoot ] ; then
+	TEXT=`echo $TEXT | sed s/$StroikaRoot/\\$StroikaRoot/g`
+fi
+if [ ! -z $TOP_ROOT ] ; then
+	TEXT=`echo $TEXT | sed s/$TOP_ROOT/\\$TOP_ROOT/g`
+fi
+echo -n $TEXT
