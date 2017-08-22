b62ee59b7b07d3be008d87221e65ffc6ef273d9f
Lewis Pringle <lewis@sophists.com>
2017-08-22 10:23:29 -0400

    makefile tweak

--- Tests/Projects/Unix/Makefile-Test-Template.mk
+++ Tests/Projects/Unix/Makefile-Test-Template.mk
@@ -38,6 +38,6 @@ all:	$(ObjDir) $(TARGETEXE)
 $(TARGETEXE):	$(Objs)
 	@$(StroikaRoot)/ScriptsLib/PrintLevelLeader.sh $(MAKE_INDENT_LEVEL) && $(ECHO) "Linking `$(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh $@`..."
 	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
-	    $(StroikaRoot)/ScriptsLib/PrintLevelLeader.sh $$(($(MAKE_INDENT_LEVEL)+1)) && $(ECHO) "$(Linker) $(LinkerPrefixArgs) -o $(TARGETEXE) $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs)";\
+	    $(StroikaRoot)ScriptsLib/PrintLevelLeader.sh $$(($(MAKE_INDENT_LEVEL)+1)) && $(ECHO) "$(Linker) $(LinkerPrefixArgs) -o $(TARGETEXE) $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs)";\
 	fi
 	@$(Linker) $(LinkerPrefixArgs) -o $@ $(Objs) $(StroikaLinkerArgs) $(StroikaLibsWithSupportLibs)
