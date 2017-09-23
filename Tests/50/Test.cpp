d0e63702a21f272a95d0016966fea5e0074abef7
Lewis Pringle <lewis@sophists.com>
2017-09-23 10:07:56 -0400

    new regression test for Containers::STL

---
+++ Tests/50/Test.cpp
@@ -0,0 +1,41 @@
+/*
+* Copyright(c) Sophist Solutions, Inc. 1990-2017.  All rights reserved
+*/
+//  TEST    Foundation::Containers::STL
+#include "Stroika/Foundation/StroikaPreComp.h"
+
+#include "Stroika/Foundation/Containers/STL/Utilities.h"
+#include "Stroika/Foundation/Debug/Assertions.h"
+#include "Stroika/Foundation/Debug/Trace.h"
+
+#include "../TestHarness/SimpleClass.h"
+#include "../TestHarness/TestHarness.h"
+
+using namespace Stroika;
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Containers;
+
+namespace {
+    namespace Test01_Concatenate_ {
+        void DoTest ()
+        {
+            vector<int> v1 = {1, 2, 3};
+            VerifyTestResult ((STL::Concatenate (v1, v1) == vector<int>{1, 2, 3, 1, 2, 3}));
+            VerifyTestResult ((STL::Concatenate (v1, v1, v1) == vector<int>{1, 2, 3, 1, 2, 3, 1, 2, 3}));
+        }
+    }
+}
+
+namespace {
+
+    void DoRegressionTests_ ()
+    {
+        Test01_Concatenate_::DoTest ();
+    }
+}
+
+int main (int argc, const char* argv[])
+{
+    Stroika::TestHarness::Setup ();
+    return Stroika::TestHarness::PrintPassOrFail (DoRegressionTests_);
+}
