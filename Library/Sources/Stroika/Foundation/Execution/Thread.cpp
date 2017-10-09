0972c974d12b989818e96540cefe98185f840397
Lewis Pringle <lewis@sophists.com>
2017-10-09 14:49:48 -0400

    cosmetic cleanup to Thread::Ptr::AbortAndWaitForDoneUntil ()

--- Library/Sources/Stroika/Foundation/Execution/Thread.cpp
+++ Library/Sources/Stroika/Foundation/Execution/Thread.cpp
@@ -922,20 +922,11 @@ void Thread::Ptr::AbortAndWaitForDoneUntil (Time::DurationSecondsType timeoutAt)
             return;
         }
         else {
-// If timeLeft BIG - ignore timeout exception and go through loop again
-#if 1
+            // If timeLeft BIG - ignore timeout exception and go through loop again
             if (WaitForDoneUntilQuietly (Time::GetTickCount () + kAbortAndWaitForDoneUntil_TimeBetweenAborts_) == WaitableEvent::kWaitQuietlySetResult) {
                 return;
             }
-// timeout just continue to loop
-#else
-            try {
-                WaitForDone (kAbortAndWaitForDoneUntil_TimeBetweenAborts_);
-                return;
-            }
-            catch (const TimeOutException&) {
-            }
-#endif
+            // timeout just continue to loop
         }
         if (tries <= 1) {
             // this COULD happen due to a lucky race - OR - the code could just be BUSY for a while (not calling CheckForAborted). But even then - it COULD make
