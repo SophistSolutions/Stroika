aa69d49460bb67bb4454060f9a83969a0c453b61
Lewis Pringle <lewis@sophists.com>
2017-10-12 10:43:03 -0400

    updated filenames in ScriptsLib/FetchLinuxTestResults.sh

--- ScriptsLib/FetchLinuxTestResults.sh
+++ ScriptsLib/FetchLinuxTestResults.sh
@@ -1,11 +1,12 @@
 MACHINE=${MACHINE:-LewisDevVM5}
 LOGIN=lewis
 SSH_TARGET=$LOGIN@$MACHINE
-BUILD_DIR=Sandbox/Stroika-Reg-Tests-Dev
+#BUILD_DIR=Sandbox/Stroika-Reg-Tests-Dev
+BUILD_DIR=Sandbox/Stroika-RegressionTests
 
 VER=`ssh $SSH_TARGET cd $BUILD_DIR && ScriptsLib/ExtractVersionInformation.sh STROIKA_VERSION FullVersionString`
 echo scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalRegressionTestResults/REGRESSION-TESTS-Linux-$VER-OUT.txt Tests/HistoricalRegressionTestResults/
 scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalRegressionTestResults/REGRESSION-TESTS-Linux-$VER-OUT.txt Tests/HistoricalRegressionTestResults/
 
-echo scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-linux-gcc-6.3.0-x64.txt Tests/HistoricalPerformanceRegressionTestResults/
-scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-linux-gcc-6.3.0-x64.txt Tests/HistoricalPerformanceRegressionTestResults/ || true
+echo scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-linux-gcc-7.2.0-x64.txt Tests/HistoricalPerformanceRegressionTestResults/
+scp $SSH_TARGET:$BUILD_DIR/Tests/HistoricalPerformanceRegressionTestResults/PerformanceDump-$VER-linux-gcc-7.2.0-x64.txt Tests/HistoricalPerformanceRegressionTestResults/ || true
