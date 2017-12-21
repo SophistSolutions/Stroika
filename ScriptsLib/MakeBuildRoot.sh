57e012558f24916ec53fd28b3081eb56828d1130
Lewis Pringle <lewis@sophists.com>
2017-12-21 17:29:19 -0500

    script typo

--- ScriptsLib/MakeBuildRoot.sh
+++ ScriptsLib/MakeBuildRoot.sh
@@ -11,9 +11,9 @@ fi
 ROOT=`realpath $ROOT`
 
 echo "$PREFIX""Linking Stroika output and configuration directories from `pwd` to $ROOT"
-REM if [[ `uname` =~ "CYGWIN" ]] ; then
-REM 	echo "$PREFIX***NOTE - this may need to invoke UAC to create symbolic links***"
-REM fi
+#if [[ `uname` =~ "CYGWIN" ]] ; then
+# 	echo "$PREFIX***NOTE - this may need to invoke UAC to create symbolic links***"
+#fi
 
 rm -rf Builds ConfigurationFiles IntermediateFiles
 
