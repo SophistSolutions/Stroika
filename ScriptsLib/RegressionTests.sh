df63820dcd12abd8178bc8bc6ebd88eca086bc75
Lewis Pringle <lewis@sophists.com>
2017-12-25 07:25:29 -0500

    conditionalize running valgrind in regressionTest script

--- ScriptsLib/RegressionTests.sh
+++ ScriptsLib/RegressionTests.sh
@@ -5,7 +5,10 @@ trap '[ "$?" -ne 77 ] || exit 77' ERR
 
 
 : ${CONTINUE:=0}
-: ${INCLUDE_HELGRIND_TESTS:=1}
+if [ -z ${INCLUDE_VALGRIND_MEMCHECK_TESTS+x} ] ; then hash valgrind 2> /dev/null; if [ ? -eq 0 ]; then  INCLUDE_VALGRIND_MEMCHECK_TESTS=1; else  INCLUDE_VALGRIND_MEMCHECK_TESTS=0; fi; fi
+if [ -z ${INCLUDE_VALGRIND_HELGRIND_TESTS+x} ] ; then hash valgrind 2> /dev/null; if [ ? -eq 0 ]; then  INCLUDE_VALGRIND_HELGRIND_TESTS=1; else  INCLUDE_VALGRIND_HELGRIND_TESTS=0; fi; fi
+#: ${INCLUDE_VALGRIND_MEMCHECK_TESTS:=1}
+#: ${INCLUDE_VALGRIND_HELGRIND_TESTS:=1}
 : ${INCLUDE_PERFORMANCE_TESTS:=1}
 if [ -z ${CLOBBER_FIRST+x} ] ; then if [ $CONTINUE -eq 1 ]; then  CLOBBER_FIRST=0; else  CLOBBER_FIRST=1; fi; fi
 : ${PARALELLMAKEFLAG:=-j10}
@@ -135,37 +138,44 @@ fi
 
 ############## VALGRIND TESTS ################
 #MEMCHECK: release, no block allocation
-echo -n "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..."
-echo "$PREFIX_OUT_LABEL" "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..." >>$TEST_OUT_FILE 2>&1
-STAGE_STARTAT_INT=$(date +%s)
-make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1 
-STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
-NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
+if [ "INCLUDE_VALGRIND_MEMCHECK_TESTS" -ne 0 ] ; then
+	echo -n "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..."
+	echo "$PREFIX_OUT_LABEL" "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..." >>$TEST_OUT_FILE 2>&1
+	STAGE_STARTAT_INT=$(date +%s)
+	make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1 
+	STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
+	NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
 
-#MEMCHECK: debug (with block alloc)
-echo -n 'VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests ...'
-echo "$PREFIX_OUT_LABEL" 'VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests ...' >>$TEST_OUT_FILE 2>&1
-STAGE_STARTAT_INT=$(date +%s)
-VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1
-STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
-NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
+	#MEMCHECK: debug (with block alloc)
+	echo -n 'VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests ...'
+	echo "$PREFIX_OUT_LABEL" 'VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests ...' >>$TEST_OUT_FILE 2>&1
+	STAGE_STARTAT_INT=$(date +%s)
+	VALGRIND_SUPPRESSIONS="Valgrind-MemCheck-Common.supp Valgrind-MemCheck-BlockAllocation.supp" make CONFIGURATION=VALGRIND_LatestGCC_Dbg_SSLPurify VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1
+	STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
+	NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
 
-#MEMCHECK: debug (without block alloc)
-echo -n "make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..."
-echo "$PREFIX_OUT_LABEL" "make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..." >>$TEST_OUT_FILE 2>&1
-STAGE_STARTAT_INT=$(date +%s)
-make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1
-STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
-echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
-NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
+	#MEMCHECK: debug (without block alloc)
+	echo -n "make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..."
+	echo "$PREFIX_OUT_LABEL" "make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests ..." >>$TEST_OUT_FILE 2>&1
+	STAGE_STARTAT_INT=$(date +%s)
+	make CONFIGURATION=VALGRIND_LatestGCC_Debug_SSLPurify_NoBlockAlloc VALGRIND=memcheck run-tests >>$TEST_OUT_FILE 2>&1
+	STAGE_TOTAL_MINUTES_SPENT=$(($(( $(date +%s) - $STAGE_STARTAT_INT )) / 60))
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
+	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
+	NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
+else
+	echo "Skipping valgrind MemCheck test because INCLUDE_VALGRIND_MEMCHECK_TESTS=$INCLUDE_VALGRIND_MEMCHECK_TESTS"
+	echo "$PREFIX_OUT_LABEL" "Skipping valgrind memcheck test because INCLUDE_VALGRIND_MEMCHECK_TESTS=$INCLUDE_VALGRIND_MEMCHECK_TESTS" >>$TEST_OUT_FILE 2>&1
+fi
+
+
 
 #HELGRIND
-if [ "$INCLUDE_HELGRIND_TESTS" -ne 0 ] ; then
+if [ "$INCLUDE_VALGRIND_HELGRIND_TESTS" -ne 0 ] ; then
 	#HELGRIND: release, no block allocation
 	echo -n "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=helgrind run-tests ..."
 	echo "$PREFIX_OUT_LABEL" "make CONFIGURATION=VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc VALGRIND=helgrind run-tests ..." >>$TEST_OUT_FILE 2>&1
@@ -186,14 +196,14 @@ if [ "$INCLUDE_HELGRIND_TESTS" -ne 0 ] ; then
 	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 	NUM_PASSES_OF_REGTESTS_RUN=$(($NUM_PASSES_OF_REGTESTS_RUN + 1))
 else
-	echo "Skipping helgrind test because INCLUDE_HELGRIND_TESTS=$INCLUDE_HELGRIND_TESTS"
-	echo "$PREFIX_OUT_LABEL" "Skipping helgrind test because INCLUDE_HELGRIND_TESTS=$INCLUDE_HELGRIND_TESTS" >>$TEST_OUT_FILE 2>&1
+	echo "Skipping helgrind test because INCLUDE_VALGRIND_HELGRIND_TESTS=$INCLUDE_VALGRIND_HELGRIND_TESTS"
+	echo "$PREFIX_OUT_LABEL" "Skipping helgrind test because INCLUDE_VALGRIND_HELGRIND_TESTS=$INCLUDE_VALGRIND_HELGRIND_TESTS" >>$TEST_OUT_FILE 2>&1
 fi
 
 
 
 #HELGRIND ON SYSPERFORM (experimental - must find better way)
-if [ "$INCLUDE_HELGRIND_TESTS" -ne 0 ] ; then
+if [ "$INCLUDE_VALGRIND_HELGRIND_TESTS" -ne 0 ] ; then
 	echo -n "valgrind -q --tool=helgrind --suppressions=Tests/Valgrind-Helgrind-Common.supp --log-file=valgrind-log.tmp Builds/VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc/Samples_SystemPerformanceClient ..."
 	echo "$PREFIX_OUT_LABEL" "valgrind -q --tool=helgrind --suppressions=Tests/Valgrind-Helgrind-Common.supp --log-file=valgrind-log.tmp Builds/VALGRIND_LatestGCC_Release_SSLPurify_NoBlockAlloc/Samples_SystemPerformanceClient..." >>$TEST_OUT_FILE 2>&1
 	STAGE_STARTAT_INT=$(date +%s)
@@ -204,8 +214,8 @@ if [ "$INCLUDE_HELGRIND_TESTS" -ne 0 ] ; then
 	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)"
 	echo "done (in $STAGE_TOTAL_MINUTES_SPENT minutes)">>$TEST_OUT_FILE 2>&1
 else
-	echo "Skipping helgrind Samples_SystemPerformanceClient because INCLUDE_HELGRIND_TESTS=$INCLUDE_HELGRIND_TESTS"
-	echo "$PREFIX_OUT_LABEL" "Skipping helgrind Samples_SystemPerformanceClient because INCLUDE_HELGRIND_TESTS=$INCLUDE_HELGRIND_TESTS" >>$TEST_OUT_FILE 2>&1
+	echo "Skipping helgrind Samples_SystemPerformanceClient because INCLUDE_VALGRIND_HELGRIND_TESTS=$INCLUDE_VALGRIND_HELGRIND_TESTS"
+	echo "$PREFIX_OUT_LABEL" "Skipping helgrind Samples_SystemPerformanceClient because INCLUDE_VALGRIND_HELGRIND_TESTS=$INCLUDE_VALGRIND_HELGRIND_TESTS" >>$TEST_OUT_FILE 2>&1
 fi
 
 
