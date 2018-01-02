c19e7fe358b99334941a50ae1e511a6ba6523945
Lewis Pringle <lewis@sophists.com>
2018-01-02 17:41:40 -0500

    cleanup docs/coimments on MakeDirectorySymbolicLink.sh for windows UAC workaround

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -4,7 +4,8 @@ TO=$2
 if [[ `uname` =~ "CYGWIN" ]] ; then
 	#Sigh...
 	# Try it the quiet way, and if that fails, try with elevation
-	cmd /C "mklink /D $FROM $TO" > /dev/null || cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $FROM $TO" || (echo "mklink failed- Allow UAC or run as administrator"  && exit 1)
+	# Not sure how to enable/disable this make more/less noisy. I have one windows 10 laptop that always needs RunMKLinkViaUAC and one that doesnt. Not sure what (relevant) is different
+	cmd /C "mklink /D $FROM $TO" > /dev/null  2> /dev/null || echo "mklink failed- Trying RunMKLinkViaUAC.bat" && cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $FROM $TO" || (echo "mklink failed- Allow UAC or run as administrator"  && exit 1)
 else
 	ln -s $TO $FROM
 fi
