f38b7ae3ef1b22e4d2bd08cc52a48ae28ac8e843
Lewis Pringle <lewis@sophists.com>
2018-01-03 16:08:41 -0500

    cosmetic makefile tweaks

--- Library/Projects/Unix/SharedBuildRules-Default.mk
+++ Library/Projects/Unix/SharedBuildRules-Default.mk
@@ -15,7 +15,7 @@ $(ObjDir):
 
 ### not sure we need this one anymore...
 %.o : %.cpp
-	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`)..."
+	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`) ... "
 	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
 	    $(StroikaRoot)ScriptsLib/PrintProgressLine.sh $$(($(MAKE_INDENT_LEVEL)+1)) "$(CPlusPlus) $(CFLAGS) -c $< -o $@";\
 	fi
@@ -23,7 +23,7 @@ $(ObjDir):
 	@$(CPlusPlus) $(CFLAGS) -c $< -o $@
 
 $(ObjDir)%.o : %.cpp
-	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`)..."
+	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`) ... "
 	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
 	    $(StroikaRoot)ScriptsLib/PrintProgressLine.sh $$(($(MAKE_INDENT_LEVEL)+1)) "$(CPlusPlus) $(CFLAGS) -c $< -o $@";\
 	fi
@@ -32,7 +32,7 @@ $(ObjDir)%.o : %.cpp
 
 
 %.i : %.swsp
-	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`)..."
+	@$(StroikaRoot)ScriptsLib/PrintProgressLine.sh $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables.sh `realpath $<`) ... "
 	@$(HTMLViewCompiler) $< $@
 
 
