c22012b22030abb6ba621fc9f2b21696ca5c3b4b
Lewis Pringle <lewis@sophists.com>
2018-01-02 21:56:42 -0500

    tweak messages for ScriptsLib/MakeDirectorySymbolicLink.sh

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -5,7 +5,7 @@ if [[ `uname` =~ "CYGWIN" ]] ; then
 	#Sigh...
 	# Try it the quiet way, and if that fails, try with elevation
 	# Not sure how to enable/disable this make more/less noisy. I have one windows 10 laptop that always needs RunMKLinkViaUAC and one that doesnt. Not sure what (relevant) is different
-	(cmd /C "mklink /D $FROM $TO" > /dev/null  2> /dev/null) || (echo "mklink failed- Trying RunMKLinkViaUAC.bat" && cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $FROM $TO" ; if [ $? -eq 0 ] ; then echo "mklink failed- Allow UAC or run as administrator" && exit 1; else echo 'OK -RunMKLinkViaUAC worked'; fi)
+	(cmd /C "mklink /D $FROM $TO" > /dev/null  2> /dev/null) || (echo -n "mklink failed- Trying RunMKLinkViaUAC.bat ... " && cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $FROM $TO" ; if [ $? -eq 0 ] ; then echo "OK -RunMKLinkViaUAC worked"; else echo 'mklink failed- Allow UAC or run as administrator' && exit 1; fi)
 else
 	ln -s $TO $FROM
 fi
