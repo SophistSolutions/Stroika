5fbc50045b94e5a33f399eb89af7bf46479bd04d
Lewis Pringle <lewis@sophists.com>
2018-01-15 18:23:36 -0500

    fixed ctor call for DateTime(time_t)

--- Library/Sources/Stroika/Foundation/Time/DateTime.cpp
+++ Library/Sources/Stroika/Foundation/Time/DateTime.cpp
@@ -376,7 +376,7 @@ DateTime DateTime::Now () noexcept
 #elif qPlatform_POSIX
     // time() returns the time since the Epoch (00:00:00 UTC, January 1, 1970), measured in seconds.
     // Convert to LocalTime - just for symetry with the windows version (and cuz our API spec say so)
-    return DateTime (::time (nullptr), Timezone::kUTC).AsLocalTime ();
+    return DateTime (::time (nullptr)).AsLocalTime ();
 #else
     AssertNotImplemented ();
     return DateTime ();
