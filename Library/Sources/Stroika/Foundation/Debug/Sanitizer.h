7c0d0f21ced6c0254c90baf1d34ef72c824dcc17
Lewis Pringle <lewis@sophists.com>
2018-02-03 17:26:14 -0500

    new Foundation/Debug/Sanitizer module

---
+++ Library/Sources/Stroika/Foundation/Debug/Sanitizer.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2018.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Debug_Sanitizer_h_
+#define _Stroika_Foundation_Debug_Sanitizer_h_ 1
+
+#include "../StroikaPreComp.h"
+
+/**
+ *  \file
+ *
+ *  \version    <a href="Code-Status.md#Alpha-Late">Alpha-Late</a>
+ *
+ */
+
+#if defined(__cplusplus)
+namespace Stroika {
+    namespace Foundation {
+        namespace Debug {
+
+#if defined(__clang__) || defined(__GNUC__)
+#define Stroika_Foundation_Debug_ATTRIBUTE_NO_SANITIZE_ADDRESS __attribute__ ((no_sanitize_address))
+#else
+#define Stroika_Foundation_Debug_ATTRIBUTE_NO_SANITIZE_ADDRESS
+#endif
+        }
+    }
+}
+#endif
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include "Sanitizer.inl"
+
+#endif /*_Stroika_Foundation_Debug_Sanitizer_h_*/
---
+++ Library/Sources/Stroika/Foundation/Debug/Sanitizer.inl
@@ -0,0 +1,19 @@
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#ifndef _Stroika_Foundation_Debug_Sanitizer_inl_
+#define _Stroika_Foundation_Debug_Sanitizer_inl_ 1
+
+#if defined(__cplusplus)
+namespace Stroika {
+    namespace Foundation {
+        namespace Debug {
+
+        }
+    }
+}
+#endif
+
+#endif /*_Stroika_Foundation_Debug_Sanitizer_inl_*/
