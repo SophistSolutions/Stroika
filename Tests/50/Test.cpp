8c09eeb6cf80d7e9c79349868b5fdbfb9ed23c21
Lewis Pringle <lewis@sophists.com>
2018-02-17 10:47:06 -0500

    revert - I must have accdientally brokne Test_String_Format_<wstring> - so I was just restoring it to the way it was before

--- Tests/50/Test.cpp
+++ Tests/50/Test.cpp
@@ -749,6 +749,9 @@ namespace {
             wchar_t buf[1024];
             VerifyTestResult (swprintf (buf, NEltsOf (buf), L"a, %ls, %d", L"xxx", 33) == 10);
             VerifyTestResult (wstring (buf) == L"a, xxx, 33");
+        }
+        {
+            wchar_t buf[1024];
             VerifyTestResult (swprintf (buf, NEltsOf (buf), L"0x%x", 0x20) == 4);
             VerifyTestResult (wstring (buf) == L"0x20");
         }
