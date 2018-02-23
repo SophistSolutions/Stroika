08d814d8d2cdb6a2e5e04138a1031b78847894cc
Lewis Pringle <lewis@sophists.com>
2018-02-23 16:03:26 -0500

    SharedStaticData DEPRECATED

--- Library/Sources/Stroika/Foundation/Execution/SharedStaticData.h
+++ Library/Sources/Stroika/Foundation/Execution/SharedStaticData.h
@@ -9,6 +9,7 @@
 #include <mutex>
 
 #include "../Configuration/Common.h"
+#include "../Memory/Common.h"
 #include "SpinLock.h"
 
 /**
@@ -72,7 +73,7 @@ namespace Stroika {
              *          @ModuleInit code.
              */
             template <typename T>
-            class SharedStaticData {
+            [[deprecated ("as of Stroika v2.0a230 - use static member function -inline - returning static object reference - like AssertExternallySynchronizedLock::GetSharedLockMutex_")]] class SharedStaticData {
             public:
                 /**
                  */
@@ -104,6 +105,129 @@ namespace Stroika {
                 static unsigned int sCountUses_;
                 static T*           sOnceObj_;
             };
+
+#if 0
+            // NO NEED - 
+
+            /**
+            SharedStaticData_NEW<T>::Object - 
+            thred safety - fully thread-safe. ONLY legal to declare as static (app lifetime) object. Its just reserves storage.
+
+            Then use USE it - with a single paired SharedStaticData_NEW<T>::Reference. You can have as many instances of this as you want
+            and they are fully threadsafe in their bookkeeping, but the underlying object they own - must be externally synchronized.
+
+            Access to the underlying object is via SharedStaticData_NEW<T>::Reference::Get () - and the threadsafty of that underling object exactly matches that of "T"
+            This class only manages doing 
+
+
+
+            mutable Execution::SharedStaticData<mutex> fSharedLockThreadsMutex_;
+
+            // EXACMPLY WHEN DO USE
+
+            >   Class where you want to just use a static member, but you worry about it being initialized just in time. Inside a method, you can 
+            >   use a static variable, and it will be initialized just in time. BUt if you want those just in time semantics for the object init, but want it
+            >   scoped like a static variable, use this.
+
+            >>  NOTE - why not implement this just using a function that returns the static object?
+            https://stackoverflow.com/questions/185624/static-variables-in-an-inlined-function
+
+            [..] An inline function with external linkage shall have the same address in all translation units. A static local variable in an extern inline function always refers to the same object. A string literal in an extern inline function is the same object in different translation units.
+
+             */
+
+            template <typename T>
+            class SharedStaticData_NEW2 {
+
+                class Reference;
+
+                class Object {
+                public:
+                    /**
+                    */
+                    Object ()              = default;
+                    Object (const Object&) = delete;
+
+                public:
+                    ~Object ()
+                    {
+                        Assert (fCountUses_ == 0);
+                    }
+
+                public:
+                    Object& operator= (const Object&) = delete;
+
+                private:
+                    friend class Reference;
+
+                private:
+                    // nb. use mutex instead of atomic<> because must lock sOnceObj_ at same time (block subsequent callers while constructing)
+#if qStroika_Foundation_Execution_SpinLock_IsFasterThan_mutex
+                    SpinLock fMutex_;
+#else
+                    mutex fMutex_;
+#endif
+                    unsigned int fCountUses_{0};
+                    alignas (alignof (T)) Memory::Byte fOnceObj_Storage_[sizeof (T)]; // avoid actual memory allocation call - since only one of these
+                };
+
+                class Reference {
+                public:
+                    /**
+                    */
+                    Reference (Object& sharedStaticDataObject)
+                        : fSharedStaticDataObject (sharedStaticDataObject)
+                    {
+#if qCompilerAndStdLib_make_unique_lock_IsSlow
+                        MACRO_LOCK_GUARD_CONTEXT (sharedStaticDataObject.fMutex_);
+#else
+                        auto critSec{make_unique_lock (sharedStaticDataObject.fMutex_)};
+#endif
+                        sharedStaticDataObject.fCountUses_++;
+                        if (sharedStaticDataObject.fCountUses_ == 1) {
+                            new (&sharedStaticDataObject.fOnceObj_Storage_) T ();
+                        }
+                    }
+                    Reference (const Reference&) = delete;
+
+                public:
+                    ~Reference ()
+                    {
+#if qCompilerAndStdLib_make_unique_lock_IsSlow
+                        MACRO_LOCK_GUARD_CONTEXT (fSharedStaticDataObject.fMutex_);
+#else
+                        auto critSec{make_unique_lock (fSharedStaticDataObject.fMutex_)};
+#endif
+                        if (--fSharedStaticDataObject.fCountUses_ == 0) {
+                            reinterpret_cast<T*> (&sharedStaticDataObject.fOnceObj_Storage_)->~T ();
+                        }
+                    }
+
+                public:
+                    Reference& operator= (const Reference&) = delete;
+
+                public:
+                    /**
+                    *  Return value guaranteed lifetime at least as long as 'this' object.
+                    *
+                    *  Note - though THIS is fully threadsafe, use of the reference T& is only as threadsafe as T itself.
+                    */
+                    nonvirtual T& Get ()
+                    {
+                        Assert (fSharedStaticDataObject.fCountUses_ > 0);
+                        return *(reinterpret_cast<T*> (&fSharedStaticDataObject.fOnceObj_Storage_));
+                    }
+                    nonvirtual const T& Get () const
+                    {
+                        Assert (fSharedStaticDataObject.fCountUses_ > 0);
+                        return *(reinterpret_cast<T*> (&fSharedStaticDataObject.fOnceObj_Storage_));
+                    }
+
+                private:
+                    Object& fSharedStaticDataObject;
+                };
+            };
+#endif
         }
     }
 }
