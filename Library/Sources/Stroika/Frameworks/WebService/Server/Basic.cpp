91233c3764f1e1437c13d9be003806bac224fffc
Lewis Pringle <lewis@sophists.com>
2018-03-07 17:44:11 -0500

    more tweaks to WebServer code

--- Library/Sources/Stroika/Frameworks/WebService/Server/Basic.cpp
+++ Library/Sources/Stroika/Frameworks/WebService/Server/Basic.cpp
@@ -26,10 +26,8 @@ using Characters::String_Constant;
 using Characters::StringBuilder;
 using WebServer::ClientErrorException;
 
-
 // Comment this in to turn on aggressive noisy DbgTrace in this module
 //#define   USE_NOISY_TRACE_IN_THIS_MODULE_       1
-
 
 /*
  ********************************************************************************
@@ -39,17 +37,15 @@ using WebServer::ClientErrorException;
 void WebService::Server::ExpectedMethod (const Request& request, const Iterable<String>& methods, const Optional<String>& fromInMessage)
 {
 #if USE_NOISY_TRACE_IN_THIS_MODULE_
-	Debug::TraceContextBumper ctx{ Stroika_Foundation_Debug_OptionalizeTraceArgs (L"WebService::Server::ExpectedMethod", L"request=%s, methods=%s, fromInMessage=%s", Characters::ToString (request).c_str (), Characters::ToString (methods).c_str (), Characters::ToString (fromInMessage).c_str ()) };
+    Debug::TraceContextBumper ctx{Stroika_Foundation_Debug_OptionalizeTraceArgs (L"WebService::Server::ExpectedMethod", L"request=%s, methods=%s, fromInMessage=%s", Characters::ToString (request).c_str (), Characters::ToString (methods).c_str (), Characters::ToString (fromInMessage).c_str ())};
 #endif
-	String method{request.GetHTTPMethod ()};
+    String method{request.GetHTTPMethod ()};
     // @todo rewrite to do better case insensitive compare - fix with https://stroika.atlassian.net/browse/STK-642 - https://stroika.atlassian.net/browse/STK-642
     if (not methods.Select<String> ([](const String& s) { return s.ToLowerCase (); }).Contains (method.ToLowerCase ())) {
         Execution::Throw (
             ClientErrorException (
-				Characters::Format (L"Received HTTP method '%s'%s, but expected one from %s", Characters::ToString (method).c_str (), (fromInMessage ? (L" from '" + *fromInMessage + L"'").c_str () : L""), Characters::ToString (methods).c_str ())
-			)
-		);
-	}
+                Characters::Format (L"Received HTTP method '%s'%s, but expected one from %s", Characters::ToString (method).c_str (), (fromInMessage ? (L" from '" + *fromInMessage + L"'").c_str () : L""), Characters::ToString (methods).c_str ())));
+    }
 }
 
 void WebService::Server::ExpectedMethod (const Request& request, const WebServiceMethodDescription& wsMethodDescription)
