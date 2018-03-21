2c3bcfc19f291aa616283236626e7021fe8712e2
Lewis Pringle <lewis@sophists.com>
2018-03-21 09:47:38 -0400

    cosmetic set cleanup

--- Library/Sources/Stroika/Foundation/Containers/SortedSet.h
+++ Library/Sources/Stroika/Foundation/Containers/SortedSet.h
@@ -78,7 +78,7 @@ namespace Stroika {
                 SortedSet (const SortedSet& src) noexcept = default;
                 SortedSet (SortedSet&& src) noexcept      = default;
                 SortedSet (const initializer_list<T>& src);
-                template <typename CONTAINER_OF_T, typename ENABLE_IF = enable_if_t<Configuration::IsIterableOfT<CONTAINER_OF_T, T>::value and not std::is_convertible<const CONTAINER_OF_T*, const SortedSet<T>*>::value>>
+                template <typename CONTAINER_OF_T, typename ENABLE_IF = enable_if_t<Configuration::IsIterableOfT<CONTAINER_OF_T, T>::value and not std::is_convertible<const CONTAINER_OF_T*, const SortedSet*>::value>>
                 SortedSet (const CONTAINER_OF_T& src);
                 template <typename COPY_FROM_ITERATOR_OF_T, typename ENABLE_IF = enable_if_t<Configuration::is_iterator<COPY_FROM_ITERATOR_OF_T>::value>>
                 SortedSet (COPY_FROM_ITERATOR_OF_T start, COPY_FROM_ITERATOR_OF_T end);
@@ -90,8 +90,8 @@ namespace Stroika {
             public:
                 /**
                  */
-                nonvirtual SortedSet<T>& operator= (const SortedSet<T>& rhs) = default;
-                nonvirtual SortedSet<T>& operator= (SortedSet<T>&& rhs) = default;
+                nonvirtual SortedSet& operator= (const SortedSet& rhs) = default;
+                nonvirtual SortedSet& operator= (SortedSet&& rhs) = default;
 
             protected:
                 /**
