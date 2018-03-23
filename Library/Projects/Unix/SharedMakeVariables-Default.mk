3fd693d7d4a6b833c1118ebdc0ba0bbf966caaa7
Lewis Pringle <lewis@sophists.com>
2018-03-23 09:49:35 -0400

    fix makefile for more use of pkg-config

--- Library/Projects/Unix/SharedMakeVariables-Default.mk
+++ Library/Projects/Unix/SharedMakeVariables-Default.mk
@@ -146,20 +146,17 @@ ifndef StroikaFoundationSupportLibs
 	# Intentionally use '=' instead of ':=' so argument variables can get re-evaluated
 	StroikaFoundationSupportLibs	=
 
-	ifeq ($(qFeatureFlag_Xerces), 'use')
-		StroikaFoundationSupportLibs	+=  $(StroikaPlatformTargetBuildDir)ThirdPartyComponents/lib/libxerces-c.a
-	endif
-	ifeq ($(qFeatureFlag_Xerces), 'use-system')
-		StroikaFoundationSupportLibs	+=  -lxerces
+	StroikaFoundationSupportLibs	+= -L $(StroikaPlatformTargetBuildDir)/ThirdPartyComponents/lib/
+
+	ifneq ($(qFeatureFlag_Xerces), 'no')
+		StroikaFoundationSupportLibs	+=  -lxerces-c
 	endif
 
 	ifeq ($(qFeatureFlag_LibCurl), 'use')
-		# using curl-config better, but @todo - must fix ./configure script/args to point to right place to make that work
 		StroikaFoundationSupportLibs += $(shell PKG_CONFIG_PATH=$(TPP_PKG_CONFIG_PATH) pkg-config --static --libs libcurl)
-		#StroikaFoundationSupportLibs += $(StroikaPlatformTargetBuildDir)ThirdPartyComponents/lib/libcurl.a
 	endif
 	ifeq ($(qFeatureFlag_LibCurl), 'use-system')
-		StroikaFoundationSupportLibs	+=  -lcurl
+		StroikaFoundationSupportLibs += $(shell PKG_CONFIG_PATH=$(TPP_PKG_CONFIG_PATH) pkg-config --libs libcurl)
 	endif
 
 	ifeq ($(qFeatureFlag_LZMA), 'use')
@@ -175,10 +172,10 @@ ifndef StroikaFoundationSupportLibs
 	endif
 
 	ifeq ($(qFeatureFlag_OpenSSL), 'use')
-		StroikaFoundationSupportLibs	+=  $(StroikaPlatformTargetBuildDir)ThirdPartyComponents/lib/libssl.a $(StroikaPlatformTargetBuildDir)ThirdPartyComponents/lib/libcrypto.a -ldl
+		StroikaFoundationSupportLibs += $(shell PKG_CONFIG_PATH=$(TPP_PKG_CONFIG_PATH) pkg-config --static --libs openssl)
 	endif
 	ifeq ($(qFeatureFlag_OpenSSL), 'use-system')
-		StroikaFoundationSupportLibs	+=  -lssl -lcrypto
+		StroikaFoundationSupportLibs += $(shell PKG_CONFIG_PATH=$(TPP_PKG_CONFIG_PATH) pkg-config --libs openssl)
 	endif
 
 	StroikaFoundationSupportLibs	+=	  $(STDCPPLIBArgs)
