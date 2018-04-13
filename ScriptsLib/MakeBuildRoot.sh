fb88168c2bc6b360f882b559a312f3386200804b
Lewis Pringle <lewis@sophists.com>
2018-04-12 21:31:48 -0400

    Now that windows properly supports symbolic links (mixing with cygwin and WSL) - it appears at least - we can just directly call ln -s instead of all that horrible magic (at least for relative paths which are easy to use for this tool)

--- ScriptsLib/MakeBuildRoot.sh
+++ ScriptsLib/MakeBuildRoot.sh
@@ -8,7 +8,6 @@ if [ ! -e $ROOT ] ; then
 	echo "$PREFIX$ROOT doesn't exist, so you cannot link to it as a build root"
 	exit 1;
 fi
-ROOT=`realpath $ROOT`
 
 echo "$PREFIX""Linking Stroika output and configuration directories from `pwd` to $ROOT"
 
@@ -17,17 +16,12 @@ rm -rf Builds ConfigurationFiles IntermediateFiles
 TARGET_INTERMEDIATEFILES=$ROOT/IntermediateFiles
 TARGET_BUILDS=$ROOT/Builds
 TARGET_CONFIGURATIONFILES=$ROOT/ConfigurationFiles
-if [[ `uname` =~ "CYGWIN" ]] ; then
-	TARGET_INTERMEDIATEFILES=`cygpath -w $TARGET_INTERMEDIATEFILES`
-	TARGET_BUILDS=`cygpath -w $TARGET_BUILDS`
-	TARGET_CONFIGURATIONFILES=`cygpath -w $TARGET_CONFIGURATIONFILES`
-fi
 
 mkdir -p $TARGET_INTERMEDIATEFILES
-ScriptsLib/MakeDirectorySymbolicLink.sh IntermediateFiles $TARGET_INTERMEDIATEFILES
+ln -s $ROOT/IntermediateFiles IntermediateFiles
 
 mkdir -p $TARGET_BUILDS
-ScriptsLib/MakeDirectorySymbolicLink.sh Builds $TARGET_BUILDS
+ln -s $ROOT/Builds Builds
 
 mkdir -p $TARGET_CONFIGURATIONFILES
-ScriptsLib/MakeDirectorySymbolicLink.sh ConfigurationFiles $TARGET_CONFIGURATIONFILES
+ln -s $ROOT/ConfigurationFiles ConfigurationFiles
