38748bc95d862156fe9473150d31748bd529deb5
Lewis Pringle <lewis@sophists.com>
2018-04-18 09:38:38 -0400

    maybe fix assign of default value for USE_TEST_BASENAME in ScriptsLib/RunPerformanceRegressionTests.sh

--- ScriptsLib/RunPerformanceRegressionTests.sh
+++ ScriptsLib/RunPerformanceRegressionTests.sh
@@ -11,27 +11,27 @@
 #
 VER=`ScriptsLib/ExtractVersionInformation.sh STROIKA_VERSION FullVersionString`
 
-: "${USE_TEST_BASENAME:=}"
+: ${USE_TEST_BASENAME:=UNKNOWN}
 
 MULTIPLIER=${MULTIPLIER-15}
 if [ "$(uname -s)" == "Darwin" ] ; then
     EXE=${EXE-"Builds/Release/Test50"}
-	if [ "$USE_TEST_BASENAME" == "" ] ; then USE_TEST_BASENAME = "MacOS-x86-XCode9"; fi
+	if [ "$USE_TEST_BASENAME" == "UNKNOWN" ] ; then USE_TEST_BASENAME = "MacOS-x86-XCode9"; fi
     CMDRUN_PREFIX="nice -10 "
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] ; then
     # Do something under GNU/Linux platform
     EXE=${EXE-"Builds/my-gcc-7.2.0-release-c++17/Test50"}
-	if [ "$USE_TEST_BASENAME" == "" ] ; then USE_TEST_BASENAME = "linux-gcc-7.2.0-x64"; fi
+	if [ "$USE_TEST_BASENAME" == "UNKNOWN" ] ; then USE_TEST_BASENAME = "linux-gcc-7.2.0-x64"; fi
     CMDRUN_PREFIX="nice -10 taskset -c 0 "
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] ; then
     # Do something under Windows NT platform
     EXE=${EXE-"Builds/Release-U-32/Test50/Test50"}
-	if [ "$USE_TEST_BASENAME" == "" ] ; then USE_TEST_BASENAME = "Windows-x86-vs2k17"; fi
+	if [ "$USE_TEST_BASENAME" == "UNKNOWN" ] ; then USE_TEST_BASENAME = "Windows-x86-vs2k17"; fi
     CMDRUN_PREFIX=""
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ] ; then
     # Do something under Windows NT platform
     EXE=${EXE-"Builds/Release-U-32/Test50/Test50"}
-	if [ "$USE_TEST_BASENAME" == "" ] ; then USE_TEST_BASENAME = "Windows-x86-vs2k17"; fi
+	if [ "$USE_TEST_BASENAME" == "UNKNOWN" ] ; then USE_TEST_BASENAME = "Windows-x86-vs2k17"; fi
     CMDRUN_PREFIX=""
 else
     echo "WARNING - unrecognized OS"
