dac89b651057551c04dcbc2d3434c56c25baac3b
Lewis Pringle <lewis@sophists.com>
2018-04-21 13:21:19 -0400

    MakeDirectorySymbolicLink.sh only redirects output - not errors (so we see errors)

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -3,7 +3,7 @@ TARGET=$1
 ALIASPATH=$2
 if [[ `uname` =~ "CYGWIN" ]] ; then
 	### As of Windows 10 (version??) - this produces an interoperable symbolic link. But alas - the cygwin ln -s still does not -- LGP 2018-04-22
-	(cmd /C "mklink /D  $ALIASPATH $TARGET" > /dev/null  2> /dev/null) 
+	(cmd /C "mklink /D  $ALIASPATH $TARGET") > /dev/null
 else
 	ln -s $TARGET $ALIASPATH 
 fi
