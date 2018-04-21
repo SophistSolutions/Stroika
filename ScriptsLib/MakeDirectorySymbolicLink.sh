df79e0a33e43e0a0f8a12425e6bdc0918c519cd8
Lewis Pringle <lewis@sophists.com>
2018-04-21 13:56:59 -0400

    re-order slashes in ScriptsLib/MakeDirectorySymbolicLink.sh - so expects unix order slashes

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -3,7 +3,8 @@ TARGET=$1
 ALIASPATH=$2
 if [[ `uname` =~ "CYGWIN" ]] ; then
 	### As of Windows 10 (version??) - this produces an interoperable symbolic link. But alas - the cygwin ln -s still does not -- LGP 2018-04-22
-	(cmd /C "mklink /D  $ALIASPATH $TARGET") > /dev/null
+	UPDATED_TARGET=`echo $TARGET | tr '/' '\\' 2>/dev/null`
+	(cmd /C "mklink /D  $ALIASPATH $UPDATED_TARGET") > /dev/null
 else
 	ln -s $TARGET $ALIASPATH 
 fi
