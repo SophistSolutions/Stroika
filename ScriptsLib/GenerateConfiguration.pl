97bfa4e5cc491b3adf6f79cc4783db74d004dbb4
Lewis Pringle <lewis@sophists.com>
2018-04-28 11:00:44 -0400

    clang6 also needs separate extension library for filesystem code

--- ScriptsLib/GenerateConfiguration.pl
+++ ScriptsLib/GenerateConfiguration.pl
@@ -1195,7 +1195,7 @@ sub PostProcessOptions_ ()
 				}
 			}
 			else {
-				if (GetClangVersion_ ($COMPILER_DRIVER_CPlusPlus) < '6.0') {
+				if (GetClangVersion_ ($COMPILER_DRIVER_CPlusPlus) < '7.0') {
 					if (index ($EXTRA_LINKER_ARGS, "-stdlib=libc++") != -1) {
 						$EXTRA_LINKER_ARGS .= " -lc++experimental";
 					}
