8051e568ca633b905523bc83b5d5667605b0f744
Lewis Pringle <lewis@sophists.com>
2018-05-01 10:36:07 -0400

    re-instate ScriptsLib\RunMKLinkViaUAC.bat since needed still on some windows installs (not sure - I have two - one needed one not - unsure what the difference is)

--- ScriptsLib/MakeDirectorySymbolicLink.sh
+++ ScriptsLib/MakeDirectorySymbolicLink.sh
@@ -2,9 +2,16 @@
 TARGET=$1
 ALIASPATH=$2
 if [[ `uname` =~ "CYGWIN" ]] ; then
-	### As of Windows 10 (version??) - this produces an interoperable symbolic link. But alas - the cygwin ln -s still does not -- LGP 2018-04-22
+	### As of Windows 10 (version 1803) - this produces an interoperable symbolic link. But alas - the cygwin ln -s still does not -- LGP 2018-04-22
 	UPDATED_TARGET=`echo $TARGET | tr '/' '\\' 2>/dev/null`
-	(cmd /C "mklink /D  $ALIASPATH $UPDATED_TARGET") > /dev/null
+	#(cmd /C "mklink /D  $ALIASPATH $UPDATED_TARGET") > /dev/null
+	#Sigh...
+	# Try it the quiet way, and if that fails, try with elevation
+	# Not sure how to enable/disable this make more/less noisy. I have one windows 10 laptop that always needs RunMKLinkViaUAC and one that doesn't. Not sure what (relevant) is different
+	#
+	# NOTE - according to https://blogs.windows.com/buildingapps/2016/12/02/symlinks-windows-10/ this SHOULD worked
+	#
+	(cmd /C "mklink /D $ALIASPATH $UPDATED_TARGET" > /dev/null  2> /dev/null) || (echo -n "mklink failed- Trying RunMKLinkViaUAC.bat ... " && cmd /C "ScriptsLib\\RunMKLinkViaUAC.bat $ALIASPATH $UPDATED_TARGET" ; if [ $? -eq 0 ] ; then echo "OK -RunMKLinkViaUAC worked"; else echo 'mklink failed- Allow UAC or run as administrator' && exit 1; fi)
 else
-	ln -s $TARGET $ALIASPATH 
+	ln -s $TARGET $ALIASPATH
 fi
---
+++ ScriptsLib/RunMKLinkViaUAC.bat
@@ -0,0 +1,41 @@
+@echo off
+
+set _LINK=%1
+set _TARGET=%2
+set _DIR=%3
+
+::echo %_LINK%
+::echo %_TARGET%
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    ::echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "%_LINK% %_TARGET% %CD%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    cscript "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    ::pushd "%CD%"
+    ::CD /D "%~dp0"
+ 
+::put your main script here
+::set
+::echo %_LINK%
+::echo %_TARGET%
+::echo %_DIR%
+cd %_DIR%
+::echo mklink /D %_LINK% %_TARGET%
+::pause 
+mklink /D %_LINK% %_TARGET%
