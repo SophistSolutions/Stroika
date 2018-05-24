db5f85b4651704d7b74ec0d6b310db2cca9ef9b5
Lewis Pringle <lewis@sophists.com>
2018-05-24 11:02:24 -0400

    qCompiler_noSanitizeAttribute_Buggy is now false for gcc8, but instead qCompiler_noSanitizeAttributeMustUseOldStyleAttr_Buggy is true for gcc8

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -560,7 +560,7 @@ In file included from ./../../IO/Network/InternetAddress.h:392:
 #elif defined(__clang__) && !defined(__APPLE__)
 #define qCompiler_noSanitizeAttribute_Buggy CompilerAndStdLib_AssumeBuggyIfNewerCheck_ ((__clang_major__ <= 6))
 #elif defined(__GNUC__)
-#define qCompiler_noSanitizeAttribute_Buggy CompilerAndStdLib_AssumeBuggyIfNewerCheck_ (__GNUC__ <= 7))
+#define qCompiler_noSanitizeAttribute_Buggy CompilerAndStdLib_AssumeBuggyIfNewerCheck_ (__GNUC__ <= 8))
 #else
 #define qCompiler_noSanitizeAttribute_Buggy 1
 #endif
