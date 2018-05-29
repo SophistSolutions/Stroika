d7bc6224f8bba5ada3279fcd3ea02f99cec1fe1d
Lewis Pringle <lewis@sophists.com>
2018-05-29 09:27:53 -0400

    update DoGitRelease script

--- ScriptsLib/DoGitRelease.sh
+++ ScriptsLib/DoGitRelease.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VER=`dos2unix < STROIKA_VERSION | xargs echo -n`
 echo "STROIKA_VERSION=$VER"
-echo "Checking out V2-Release"
+echo "Checking out V2.0-Release"
 git checkout V2.0-Release
 echo "Pull..."
 git pull --rebase
