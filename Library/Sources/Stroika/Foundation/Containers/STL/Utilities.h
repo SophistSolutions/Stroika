a9cba4dda40d0334a839c3cc33289042fbeead3e
Lewis Pringle <lewis@sophists.com>
2018-05-29 12:33:02 -0400

    typo

--- Library/Sources/Stroika/Foundation/Containers/STL/Utilities.h
+++ Library/Sources/Stroika/Foundation/Containers/STL/Utilities.h
@@ -28,7 +28,7 @@ namespace Stroika {
                 /**
                  *  \note   similar to std::equal () - but takes iterables as arguments, not iterators
                  */
-                template <typename ITERABLE_OF_T, typename T = ITERABLE_OF_T::value_type, typename EQUALS_COMPARER = std::equal_to<T>, typename ENABLE_IF = enable_if_t<Common::IsPotentiallyComparerRelation<T, EQUALS_COMPARER> ()>>
+                template <typename ITERABLE_OF_T, typename T = typename ITERABLE_OF_T::value_type, typename EQUALS_COMPARER = std::equal_to<T>, typename ENABLE_IF = enable_if_t<Common::IsPotentiallyComparerRelation<T, EQUALS_COMPARER> ()>>
                 bool equal (const ITERABLE_OF_T& lhs, const ITERABLE_OF_T& rhs, EQUALS_COMPARER&& equalsComparer = {});
 
                 /*
