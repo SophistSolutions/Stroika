484246a4ed12c403476e591722ac0ece4b926f59
Lewis Pringle <lewis@sophists.com>
2018-06-04 21:50:29 -0400

    Cosmetic

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -591,7 +591,6 @@ In file included from ./../../IO/Network/InternetAddress.h:392:
 #endif
 #endif
 
-
 #ifndef qCompiler_noSanitizeAttributeMustUseOldStyleAttr_Buggy
 #if defined(__clang__) && defined(__APPLE__)
 #define qCompiler_noSanitizeAttributeMustUseOldStyleAttr_Buggy CompilerAndStdLib_AssumeBuggyIfNewerCheck_ ((__clang_major__ <= 9))
