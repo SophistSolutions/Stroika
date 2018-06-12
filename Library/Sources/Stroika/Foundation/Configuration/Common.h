66dae5a6ef522ec85d07ad974b11f6da4ae5f436
Lewis Pringle <lewis@sophists.com>
2018-06-12 16:37:08 -0400

    docs on #define nonvirtual

--- Library/Sources/Stroika/Foundation/Configuration/Common.h
+++ Library/Sources/Stroika/Foundation/Configuration/Common.h
@@ -21,6 +21,34 @@ namespace Stroika {
             /**
              *  \def nonvirtual
              *      Is for documentation purposes, to make clear a method is intended to be not 'virtual'
+             *
+             *  \par Example Usage
+             *      \code
+             *          // Conventional std c++
+             *          struct base {
+             *              int f1 ();
+             *              virtual int f2();
+             *          };
+             *          struct derived : base { 
+             *              // f2 is a virtual override here, but it sure isn't clear from the declaration
+             *              int f2 ();
+             *          };
+             *
+             *          // In Stroika, this will always be written as:
+             *          struct base {
+             *              nonvirtual int f1 ();
+             *              virtual int f2();
+             *          };
+             *          struct derived : base { 
+             *              // f2 is a virtual override here, but it sure isn't clear from the declaration
+             *              virtual int f2 () override;
+             *          };
+             *      \endcode
+             *
+             *  So its always clear at the point of declaration if a function is virtual or not, and if virtual, if its
+             *  a new virtual or override. 
+             *
+             *  \note   This is \em not really enforced by the compiler (the 'conventional std c++ code' produces no diagnostic).
              */
 #define nonvirtual
         }
