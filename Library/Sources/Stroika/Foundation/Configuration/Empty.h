7fe4113baacd3f16696960f88373819524946143
Lewis Pringle <lewis@sophists.com>
2018-06-12 22:14:30 -0400

    new helper class Configuration::Empty

---
+++ Library/Sources/Stroika/Foundation/Configuration/Empty.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2018.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Configuration_Empty_h_
+#define _Stroika_Foundation_Configuration_Empty_h_ 1
+
+#include "../StroikaPreComp.h"
+
+namespace Stroika {
+    namespace Foundation {
+        namespace Configuration {
+
+            /**
+             *  Believe it or not, this is often useful in template metaprogramming.
+             */
+            struct Empty {
+            };
+        }
+    }
+}
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+#endif /*_Stroika_Foundation_Configuration_Empty_h_*/
