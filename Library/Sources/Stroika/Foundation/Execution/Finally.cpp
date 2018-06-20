33f27f449e2f825bb61dec608b3aa2817896138e
Lewis Pringle <lewis@sophists.com>
2018-06-20 08:57:19 -0400

    lose empty Finally.cpp

--- Library/Sources/Stroika/Foundation/Execution/Finally.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright(c) Sophist Solutions, Inc. 1990-2018.  All rights reserved
- */
-#include "../StroikaPreComp.h"
-
-#include "Finally.h"
-
-using namespace Stroika::Foundation;
-using namespace Stroika::Foundation::Execution;
