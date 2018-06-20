e9f3d42e48e079ca3ce52a5403193b68bd20ec83
Lewis Pringle <lewis@sophists.com>
2018-06-20 10:35:38 -0400

    minor tweaks to FormatCode.sh script

--- ScriptsLib/FormatCode.sh
+++ ScriptsLib/FormatCode.sh
@@ -14,8 +14,10 @@
 
 
 EXPAND="${EXPAND:-/usr/bin/expand}"
-if [[ `uname` =~ "CYGWIN" ]] ; then
-	EXPAND=/bin/expand
+hash $EXPAND 2> /dev/null
+if [ $? -ne 0 ]; then
+   echo "Need missing tab expander: $EXPAND"
+   exit 1
 fi
 
 FIND=find
@@ -40,8 +42,7 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
-
 for filePattern in "${@:2}"
 do
-    $FIND $dirPattern -name $filePattern -exec sh -c "$EXPAND --tabs=4 {} | $FORMATTER --assume-filename={} > {}.tmp && if cmp -s {} {}.tmp ; then rm {}.tmp; else echo Updating {} && mv {}.tmp {} ; fi" \;
+    $FIND $dirPattern -name $filePattern -exec sh -c "$EXPAND --tabs=4 {} | $FORMATTER --assume-filename={} > {}.tmp && if cmp -s {} {}.tmp ; then rm -f {}.tmp; else echo Updating {} && mv {}.tmp {} ; fi" \;
 done
