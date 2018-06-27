867ea471f77ed42efca0c02e78aa77ee2a549f64
Lewis Pringle <lewis@sophists.com>
2018-06-27 17:43:06 -0400

    SynchronizedTimedCache<> cleanups, docs, options (performance)

--- Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.h
+++ Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.h
@@ -17,55 +17,57 @@
  *  \version    <a href="Code-Status.md#Alpha-Early">Alpha-Early</a>
  *
  * TODO:
- *
- *      @todo   HIGHLY EXPERIEMNTAL FIRST DRAFT OF IDEA
+ *      @todo   Add 'overrides' for Add/Remove methods - so those are safe too!
+ *              and add more overloads (from base class) of Lookup
  */
 
-namespace Stroika {
-    namespace Foundation {
-        namespace Cache {
+namespace Stroika::Foundation {
+    namespace Cache {
 
+        /**
+         */
+        template <typename KEY, typename VALUE, typename TRAITS = TimedCacheSupport::DefaultTraits<KEY, VALUE>>
+        class SynchronizedTimedCache : public TimedCache<KEY, VALUE, TRAITS> {
+            using inherited = TimedCache<KEY, VALUE, TRAITS>;
+
+        public:
             /**
+             *  @see TimedCache constructor for examples
              */
-            template <typename KEY, typename VALUE, typename TRAITS = TimedCacheSupport::DefaultTraits<KEY, VALUE>>
-            class SynchronizedTimedCache : public TimedCache<KEY, VALUE, TRAITS> {
-                using inherited = TimedCache<KEY, VALUE, TRAITS>;
+            SynchronizedTimedCache (Time::DurationSecondsType timeoutInSeconds);
+            // support eventually, but not trivial
+            SynchronizedTimedCache (const SynchronizedTimedCache&) = delete;
 
-            public:
-                SynchronizedTimedCache (Time::DurationSecondsType timeoutInSeconds)
-                    : inherited (timeoutInSeconds)
-                {
-                }
-                // support eventually, but not trivial
-                SynchronizedTimedCache (const SynchronizedTimedCache&) = delete;
+        public:
+            // support eventually, but not trivial
+            nonvirtual SynchronizedTimedCache& operator= (const SynchronizedTimedCache&) = delete;
 
-                // @todo need much more but this is atart
-            public:
-                nonvirtual VALUE Lookup (typename Configuration::ArgByValueType<KEY> key, const std::function<VALUE (typename Configuration::ArgByValueType<KEY>)>& cacheFiller)
-                {
-                    shared_lock<shared_timed_mutex> lock (this->fMutex_);
-                    if (Memory::Optional<VALUE> o = inherited::Lookup (key)) {
-                        return *o;
-                    }
-                    else {
-                        VALUE v = cacheFiller (key);
-                        {
-                            lock.unlock ();
-                            unique_lock<shared_timed_mutex> newRWLock (this->fMutex_);
-                            this->Add (key, v);
-                        }
-                        return move (v);
-                    }
-                }
+        public:
+            /*
+             *  Note:   We choose to not hold any lock while filling the cache (fHoldWriteLockDuringCacheFill false by default).
+             *  This is because typically, filling the cache
+             *  will be slow (otherwise you wouldbe us using the SynchronizedTimedCache).
+             *
+             *  But this has the downside, that you could try filling the cache multiple times with the same value.
+             *
+             *  That's perfectly safe, but not speedy.
+             *
+             *  Which is better depends on the likihood the caller will make multiple requests for the same non-existent value at
+             *  the same time. If yes, you should set fHoldWriteLockDuringCacheFill. If no (or if you care more about being able to
+             *  read the rest of the data and not having threads block needlessly for other values) set fHoldWriteLockDuringCacheFill false (default).
+             */
+            bool fHoldWriteLockDuringCacheFill{false};
 
-            public:
-                // support eventually, but not trivial
-                nonvirtual SynchronizedTimedCache& operator= (const SynchronizedTimedCache&) = delete;
+        public:
+            /**
+             *  Lookup the given value and return it. But if its not found, use argument 'cacheFiller' function to fetch the real value.
+             *  This supports (and propagates) exceptions, and is threadsafe (can be called multiple times from different threads safely.
+             */
+            nonvirtual VALUE Lookup (typename Configuration::ArgByValueType<KEY> key, const std::function<VALUE (typename Configuration::ArgByValueType<KEY>)>& cacheFiller);
 
-            private:
-                mutable shared_timed_mutex fMutex_;
-            };
-        }
+        private:
+            mutable shared_timed_mutex fMutex_;
+        };
     }
 }
 
--- Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.inl
+++ Library/Sources/Stroika/Foundation/Cache/SynchronizedTimedCache.inl
@@ -7,15 +7,43 @@
 #ifndef _Stroika_Foundation_Cache_SynchronizedTimedCache_inl_
 #define _Stroika_Foundation_Cache_SynchronizedTimedCache_inl_ 1
 
-namespace Stroika {
-    namespace Foundation {
-        namespace Cache {
+namespace Stroika::Foundation {
+    namespace Cache {
 
-            /*
-             ********************************************************************************
-             ************************ TimedCacheSupport::Stats_Basic ************************
-             ********************************************************************************
-             */
+        /*
+         ********************************************************************************
+         ************** SynchronizedTimedCache<KEY, VALUE, TRAITS> **********************
+         ********************************************************************************
+         */
+        template <typename KEY, typename VALUE, typename TRAITS>
+        SynchronizedTimedCache<KEY, VALUE, TRAITS>::SynchronizedTimedCache (Time::DurationSecondsType timeoutInSeconds)
+            : inherited (timeoutInSeconds)
+        {
+        }
+        template <typename KEY, typename VALUE, typename TRAITS>
+        VALUE SynchronizedTimedCache<KEY, VALUE, TRAITS>::Lookup (typename Configuration::ArgByValueType<KEY> key, const std::function<VALUE (typename Configuration::ArgByValueType<KEY>)>& cacheFiller)
+        {
+            shared_lock<shared_timed_mutex> lock{fMutex_};
+            if (optional<VALUE> o = inherited::Lookup (key)) {
+                return *o;
+            }
+            else {
+                lock.unlock ();
+                if (fHoldWriteLockDuringCacheFill) {
+                    // Avoid two threds calling cache for same key value at the same time
+                    lock_guard<shared_timed_mutex> newRWLock{fMutex_};
+                    VALUE                          v = cacheFiller (key);
+                    this->Add (key, v);
+                }
+                else {
+                    // Avoid needlessly blocking lookups (shared_lock above) until after we've filled the cache (typically slow)
+                    // and keep it to minimum logically required (inherited add).
+                    VALUE                          v = cacheFiller (key);
+                    lock_guard<shared_timed_mutex> newRWLock{fMutex_};
+                    this->Add (key, v);
+                }
+                return v;
+            }
         }
     }
 }
