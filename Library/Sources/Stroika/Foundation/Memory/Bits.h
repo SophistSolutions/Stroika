5eb62250b8eaff42cdbbdde25aa56999780f27b0
Lewis Pringle <lewis@sophists.com>
2018-07-14 00:03:36 -0400

    use namespace Stroika::Foundation::Memory { in Memory::Bits

--- Library/Sources/Stroika/Foundation/Memory/Bits.h
+++ Library/Sources/Stroika/Foundation/Memory/Bits.h
@@ -14,50 +14,49 @@
  *  \file
  */
 
-namespace Stroika::Foundation {
-    namespace Memory {
+namespace Stroika::Foundation::Memory {
 
-        /**
-         *  bitNumber's start with 0, not 1.
-         *
-         *  \par Example Usage
-         *      \code
-         *          VerifyTestResult (Bit (0) == 0x1);
-         *          VerifyTestResult (Bit (1) == 0x2);
-         *          VerifyTestResult (Bit (3) == 0x8);
-         *          VerifyTestResult (Bit (15) == 0x8000);
-         *          VerifyTestResult (Bit<int> (1, 2) == 0x6);
-         *          VerifyTestResult (Bit<int> (1, 2, 15) == 0x8006);
-         *      \endcode
-         */
-        template <typename INT_TYPE = unsigned int>
-        constexpr INT_TYPE Bit (unsigned int bitNumber);
-        template <typename INT_TYPE, typename... BIT_ARGS>
-        constexpr INT_TYPE Bit (unsigned int bitNumber, const BIT_ARGS&... args);
+    /**
+     *  bitNumber's start with 0, not 1.
+     *
+     *  \par Example Usage
+     *      \code
+     *          VerifyTestResult (Bit (0) == 0x1);
+     *          VerifyTestResult (Bit (1) == 0x2);
+     *          VerifyTestResult (Bit (3) == 0x8);
+     *          VerifyTestResult (Bit (15) == 0x8000);
+     *          VerifyTestResult (Bit<int> (1, 2) == 0x6);
+     *          VerifyTestResult (Bit<int> (1, 2, 15) == 0x8006);
+     *      \endcode
+     */
+    template <typename INT_TYPE = unsigned int>
+    constexpr INT_TYPE Bit (unsigned int bitNumber);
+    template <typename INT_TYPE, typename... BIT_ARGS>
+    constexpr INT_TYPE Bit (unsigned int bitNumber, const BIT_ARGS&... args);
 
-        /**
-         *  Capture the bits from 'bitField' - starting at bit 'startOffset' (zero-based),
-         *  extending to endOffset (also zero based - not inclusive). The number of bits captured
-         *  is endOffset-startOffset, so:
-         *       \req    startOffset < endOffset
-         *
-         *  \par Example Usage
-         *      \code
-         *          VerifyTestResult (BitSubstring (0x3, 0, 1) == 1);
-         *          VerifyTestResult (BitSubstring (0x3, 1, 2) == 1);
-         *          VerifyTestResult (BitSubstring (0x3, 2, 3) == 0);
-         *          VerifyTestResult (BitSubstring (0x3, 0, 3) == 0x3);
-         *          VerifyTestResult (BitSubstring (0xff, 0, 8) == 0xff);
-         *          VerifyTestResult (BitSubstring (0xff, 8, 16) == 0x0);
-         *      \endcode
-         *
-         *  \note   The startOffset/endOffset pattern matches that with STL iterators (not including the last item)
-         *
-         *  \note   This was previously named TakeNBitsFrom()
-         */
-        template <typename INT_TYPE>
-        constexpr INT_TYPE BitSubstring (INT_TYPE bitField, unsigned int startOffset, unsigned int endOffset);
-    }
+    /**
+     *  Capture the bits from 'bitField' - starting at bit 'startOffset' (zero-based),
+     *  extending to endOffset (also zero based - not inclusive). The number of bits captured
+     *  is endOffset-startOffset, so:
+     *       \req    startOffset < endOffset
+     *
+     *  \par Example Usage
+     *      \code
+     *          VerifyTestResult (BitSubstring (0x3, 0, 1) == 1);
+     *          VerifyTestResult (BitSubstring (0x3, 1, 2) == 1);
+     *          VerifyTestResult (BitSubstring (0x3, 2, 3) == 0);
+     *          VerifyTestResult (BitSubstring (0x3, 0, 3) == 0x3);
+     *          VerifyTestResult (BitSubstring (0xff, 0, 8) == 0xff);
+     *          VerifyTestResult (BitSubstring (0xff, 8, 16) == 0x0);
+     *      \endcode
+     *
+     *  \note   The startOffset/endOffset pattern matches that with STL iterators (not including the last item)
+     *
+     *  \note   This was previously named TakeNBitsFrom()
+     */
+    template <typename INT_TYPE>
+    constexpr INT_TYPE BitSubstring (INT_TYPE bitField, unsigned int startOffset, unsigned int endOffset);
+
 }
 
 /*
--- Library/Sources/Stroika/Foundation/Memory/Bits.inl
+++ Library/Sources/Stroika/Foundation/Memory/Bits.inl
@@ -13,39 +13,37 @@
 
 #include "../Debug/Assertions.h"
 
-namespace Stroika::Foundation {
-    namespace Memory {
+namespace Stroika::Foundation::Memory {
 
-        /*
-         ********************************************************************************
-         *************************************** Bit ************************************
-         ********************************************************************************
-         */
-        template <typename INT_TYPE>
-        inline constexpr INT_TYPE Bit (unsigned int bitNumber)
-        {
-            Require (bitNumber < CHAR_BIT * sizeof (INT_TYPE));
-            return (static_cast<INT_TYPE> (1) << bitNumber);
-        }
-        template <typename INT_TYPE, typename... BIT_ARGS>
-        inline constexpr INT_TYPE Bit (unsigned int bitNumber, const BIT_ARGS&... args)
-        {
-            return Bit<INT_TYPE> (bitNumber) | Bit<INT_TYPE> (args...);
-        }
+    /*
+     ********************************************************************************
+     *************************************** Bit ************************************
+     ********************************************************************************
+     */
+    template <typename INT_TYPE>
+    inline constexpr INT_TYPE Bit (unsigned int bitNumber)
+    {
+        Require (bitNumber < CHAR_BIT * sizeof (INT_TYPE));
+        return (static_cast<INT_TYPE> (1) << bitNumber);
+    }
+    template <typename INT_TYPE, typename... BIT_ARGS>
+    inline constexpr INT_TYPE Bit (unsigned int bitNumber, const BIT_ARGS&... args)
+    {
+        return Bit<INT_TYPE> (bitNumber) | Bit<INT_TYPE> (args...);
+    }
 
-        /*
-         ********************************************************************************
-         ********************************* BitSubstring *********************************
-         ********************************************************************************
-         */
-        template <typename INT_TYPE>
-        inline constexpr INT_TYPE BitSubstring (INT_TYPE bitField, unsigned int startOffset, unsigned int endOffset)
-        {
-            Require (startOffset < endOffset);
-            Require (startOffset < (CHAR_BIT * sizeof (INT_TYPE)));
-            Require (((endOffset - startOffset) - 1 + startOffset) < (CHAR_BIT * sizeof (INT_TYPE)));
-            return (bitField >> startOffset) & ((1 << (endOffset - startOffset)) - 1);
-        }
+    /*
+     ********************************************************************************
+     ********************************* BitSubstring *********************************
+     ********************************************************************************
+     */
+    template <typename INT_TYPE>
+    inline constexpr INT_TYPE BitSubstring (INT_TYPE bitField, unsigned int startOffset, unsigned int endOffset)
+    {
+        Require (startOffset < endOffset);
+        Require (startOffset < (CHAR_BIT * sizeof (INT_TYPE)));
+        Require (((endOffset - startOffset) - 1 + startOffset) < (CHAR_BIT * sizeof (INT_TYPE)));
+        return (bitField >> startOffset) & ((1 << (endOffset - startOffset)) - 1);
     }
 }
 #endif /*_Stroika_Foundation_Memory_Bits_inl_*/
