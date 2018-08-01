a836cc9248046a9571677b308643b208503229f3
Lewis Pringle <lewis@sophists.com>
2018-08-01 11:09:46 -0400

    cosmetic

--- Library/Sources/Stroika/Frameworks/NetworkMonitor/Ping.h
+++ Library/Sources/Stroika/Frameworks/NetworkMonitor/Ping.h
@@ -20,156 +20,151 @@
 #include "../../Foundation/Traversal/Range.h"
 
 /**
+ *  \file
  *
- *  \version    <a href="Code-Status.md#Alpha-Early">Alpha-Early</a>
+ *  \version    <a href="Code-Status.md#Alpha-Late">Alpha-Late</a>
  *
- * TODO:
- *      @todo
  */
 
-namespace Stroika::Frameworks {
-    namespace NetworkMonitor {
-        namespace Ping {
+namespace Stroika::Frameworks::NetworkMonitor::Ping {
 
-            using namespace Stroika::Foundation;
+    using namespace Stroika::Foundation;
 
-            using IO::Network::InternetAddress;
-            using Time::Duration;
-            using Time::DurationSecondsType;
+    using IO::Network::InternetAddress;
+    using Time::Duration;
+    using Time::DurationSecondsType;
 
-            /**
-             */
-            struct Options {
-                /**
-                 */
-                static constexpr unsigned int kDefaultMaxHops = 64;
+    /**
+     */
+    struct Options {
+        /**
+         */
+        static constexpr unsigned int kDefaultMaxHops = 64;
 
-                /**
-                 */
-                optional<unsigned int> fMaxHops;
+        /**
+         */
+        optional<unsigned int> fMaxHops;
 
-                /**
-                 */
-                static const Duration kDefaultTimeout;
+        /**
+         */
+        static const Duration kDefaultTimeout;
 
-                /**
-                 *  time after a single ping is sent before we treat the ping as having timed out (so not total time if multiple samples taken).
-                 */
-                optional<Duration> fTimeout;
+        /**
+         *  time after a single ping is sent before we treat the ping as having timed out (so not total time if multiple samples taken).
+         */
+        optional<Duration> fTimeout;
 
-                /*
-                 * No standard for this, but just what this library does.
-                 */
-                static constexpr size_t kDefaultPayloadSize = 32;
+        /*
+         * No standard for this, but just what this library does.
+         */
+        static constexpr size_t kDefaultPayloadSize = 32;
 
-                /**
-                 *  The range of supported payload (not including ICMP and IP packet headers)
-                 *
-                 *      @see http://stackoverflow.com/questions/9449837/maximum-legal-size-of-icmp-echo-packet
-                 *
-                 *      This does NOT include the IP header, nor the ICMP Header
-                 */
-                static constexpr Traversal::Range<size_t> kAllowedICMPPayloadSizeRange{0, numeric_limits<uint16_t>::max () - (sizeof (IO::Network::InternetProtocol::ICMP::V4::PacketHeader) + sizeof (IO::Network::InternetProtocol::IP::V4::PacketHeader)), Traversal::Openness::eClosed, Traversal::Openness::eClosed};
+        /**
+         *  The range of supported payload (not including ICMP and IP packet headers)
+         *
+         *      @see http://stackoverflow.com/questions/9449837/maximum-legal-size-of-icmp-echo-packet
+         *
+         *      This does NOT include the IP header, nor the ICMP Header
+         */
+        static constexpr Traversal::Range<size_t> kAllowedICMPPayloadSizeRange{0, numeric_limits<uint16_t>::max () - (sizeof (IO::Network::InternetProtocol::ICMP::V4::PacketHeader) + sizeof (IO::Network::InternetProtocol::IP::V4::PacketHeader)), Traversal::Openness::eClosed, Traversal::Openness::eClosed};
 
-                /**
-                 *  \not including ICMP nor IP header overhead.
-                 */
-                optional<size_t> fPacketPayloadSize;
+        /**
+         *  \not including ICMP nor IP header overhead.
+         */
+        optional<size_t> fPacketPayloadSize;
 
-                /**
-                 *  @see Characters::ToString ();
-                 */
-                nonvirtual Characters::String ToString () const;
-            };
+        /**
+         *  @see Characters::ToString ();
+         */
+        nonvirtual Characters::String ToString () const;
+    };
 
-            /**
-             *
-             *  \note   \em Thread-Safety   <a href="thread_safety.html#C++-Standard-Thread-Safety">C++-Standard-Thread-Safety</a>
-             */
-            class Pinger {
-            public:
-                Pinger ()              = delete;
-                Pinger (const Pinger&) = delete;
-                Pinger (const InternetAddress& addr, const Options& options = {});
+    /**
+     *
+     *  \note   \em Thread-Safety   <a href="thread_safety.html#C++-Standard-Thread-Safety">C++-Standard-Thread-Safety</a>
+     */
+    class Pinger {
+    public:
+        Pinger ()              = delete;
+        Pinger (const Pinger&) = delete;
+        Pinger (const InternetAddress& addr, const Options& options = {});
 
-            public:
-                struct ResultType;
+    public:
+        struct ResultType;
 
-            public:
-                /**
-                 *  Can throw:
-                 *      TimeoutException
-                 *      InternetProtocol::ICMP::DestinationUnreachableException
-                 *      InternetProtocol::ICMP::UnknownICMPPacket
-                 *      InternetProtocol::ICMP::TTLExpiredException
-                 *      others...
-                 */
-                nonvirtual ResultType RunOnce (const optional<unsigned int>& ttl = {});
+    public:
+        /**
+         *  Can throw:
+         *      TimeoutException
+         *      InternetProtocol::ICMP::DestinationUnreachableException
+         *      InternetProtocol::ICMP::UnknownICMPPacket
+         *      InternetProtocol::ICMP::TTLExpiredException
+         *      others...
+         */
+        nonvirtual ResultType RunOnce (const optional<unsigned int>& ttl = {});
 
-            private:
-                nonvirtual ResultType RunOnce_ICMP_ (unsigned int ttl);
+    private:
+        nonvirtual ResultType RunOnce_ICMP_ (unsigned int ttl);
 
-            private:
-                std::uniform_int_distribution<std::mt19937::result_type> fAllUInt16Distribution_{0, numeric_limits<uint16_t>::max ()};
-                std::mt19937                                             fRng_{std::random_device () ()};
-                InternetAddress                                          fDestination_;
-                Options                                                  fOptions_;
-                size_t                                                   fICMPPacketSize_;
-                Memory::SmallStackBuffer<Memory::Byte>                   fSendPacket_;
-                IO::Network::ConnectionlessSocket::Ptr                   fSocket_;
-                uint16_t                                                 fNextSequenceNumber_;
-                Time::DurationSecondsType                                fPingTimeout_;
-            };
+    private:
+        std::uniform_int_distribution<std::mt19937::result_type> fAllUInt16Distribution_{0, numeric_limits<uint16_t>::max ()};
+        std::mt19937                                             fRng_{std::random_device () ()};
+        InternetAddress                                          fDestination_;
+        Options                                                  fOptions_;
+        size_t                                                   fICMPPacketSize_;
+        Memory::SmallStackBuffer<Memory::Byte>                   fSendPacket_;
+        IO::Network::ConnectionlessSocket::Ptr                   fSocket_;
+        uint16_t                                                 fNextSequenceNumber_;
+        Time::DurationSecondsType                                fPingTimeout_;
+    };
 
-            /**
-             */
-            struct Pinger::ResultType {
-                Duration     fPingTime;
-                unsigned int fHopCount{};
+    /**
+     */
+    struct Pinger::ResultType {
+        Duration     fPingTime;
+        unsigned int fHopCount{};
 
-                /**
-                 *  @see Characters::ToString ();
-                 */
-                nonvirtual Characters::String ToString () const;
-            };
+        /**
+         *  @see Characters::ToString ();
+         */
+        nonvirtual Characters::String ToString () const;
+    };
 
-            /**
-             */
-            struct SampleOptions {
-                Duration     fInterval;
-                unsigned int fSampleCount{};
+    /**
+     */
+    struct SampleOptions {
+        Duration     fInterval;
+        unsigned int fSampleCount{};
 
-                /**
-                 *  @see Characters::ToString ();
-                 */
-                nonvirtual Characters::String ToString () const;
-            };
+        /**
+         *  @see Characters::ToString ();
+         */
+        nonvirtual Characters::String ToString () const;
+    };
 
-            /**
-             */
-            struct SampleResults {
-                optional<Duration>     fMedianPingTime; // excludes timeouts
-                optional<unsigned int> fMedianHopCount;
-                unsigned int           fExceptionCount{};
+    /**
+     */
+    struct SampleResults {
+        optional<Duration>     fMedianPingTime; // excludes timeouts
+        optional<unsigned int> fMedianHopCount;
+        unsigned int           fExceptionCount{};
 
-                /**
-                 *  @see Characters::ToString ();
-                 */
-                nonvirtual Characters::String ToString () const;
-            };
+        /**
+         *  @see Characters::ToString ();
+         */
+        nonvirtual Characters::String ToString () const;
+    };
 
-            /**
-             *  \breif Send network packets to the argument internet address (govered by sample Options and optins) - and return the sampled results (e.g. averages)
-             *
-             *  'options' govern how the pinging is done.
-             *
-             *  'sampleOptions' govern how the results are statistically processed.
-             *
-             *  Sample () will summarize most exceptions in Results.
-             */
-            SampleResults Sample (const InternetAddress& addr, const SampleOptions& sampleOptions = {}, const Options& options = {});
-        }
-    }
+    /**
+     *  \breif Send network packets to the argument internet address (govered by sample Options and optins) - and return the sampled results (e.g. averages)
+     *
+     *  'options' govern how the pinging is done.
+     *
+     *  'sampleOptions' govern how the results are statistically processed.
+     *
+     *  Sample () will summarize most exceptions in Results.
+     */
+    SampleResults Sample (const InternetAddress& addr, const SampleOptions& sampleOptions = {}, const Options& options = {});
 
 }
 
--- Library/Sources/Stroika/Frameworks/WebServer/Router.h
+++ Library/Sources/Stroika/Frameworks/WebServer/Router.h
@@ -39,78 +39,76 @@
  *          just having a C++ method on RequestHandler().
  */
 
-namespace Stroika::Frameworks {
-    namespace WebServer {
+namespace Stroika::Frameworks::WebServer {
 
-        using namespace Stroika::Foundation;
+    using namespace Stroika::Foundation;
 
-        using Characters::RegularExpression;
-        using Characters::String;
-        using Containers::Sequence;
-        using IO::Network::URL;
+    using Characters::RegularExpression;
+    using Characters::String;
+    using Containers::Sequence;
+    using IO::Network::URL;
 
-        class Router;
+    class Router;
 
+    /**
+     *      @todo - we probably want to add ability to generically parse out arguments from url, and include them to handler (as rails does - handy for ID in REST)
+     *
+     *      @todo need more generic matchign to fit in (maybe optional matcher that takes URL?, or even full Request).
+     *
+     *        NOTE - may verb match and path match each OPTIONALS in class and have maybe a LIST of THINGS we know how to match.
+     *             VERB
+     *             RELPATH
+     *             HTTP HEADER (like SOAPACTION)
+     *             RequestObject????
+     *         then we use "Route" to make generic the mapping of a request to a Handler.
+     *
+     *       @todo NEED to support NESTED Routes (or aggregated).
+     *               Key is need stuff like 'default error handling' - and just to somehow inherit/copy that.
+     *
+     * \note - this must be EXTERNALLY synchronized - except that all read only methods are safe from any thread,
+     *         because these are usually stored in a strucutre where they wont be updated.
+     *         Just be sure the HANLDER argument is safe when called from multiple threads at the same time!
+     */
+    class Route {
+    public:
         /**
-         *      @todo - we probably want to add ability to generically parse out arguments from url, and include them to handler (as rails does - handy for ID in REST)
-         *
-         *      @todo need more generic matchign to fit in (maybe optional matcher that takes URL?, or even full Request).
-         *
-         *        NOTE - may verb match and path match each OPTIONALS in class and have maybe a LIST of THINGS we know how to match.
-         *             VERB
-         *             RELPATH
-         *             HTTP HEADER (like SOAPACTION)
-         *             RequestObject????
-         *         then we use "Route" to make generic the mapping of a request to a Handler.
-         *
-         *       @todo NEED to support NESTED Routes (or aggregated).
-         *               Key is need stuff like 'default error handling' - and just to somehow inherit/copy that.
-         *
-         * \note - this must be EXTERNALLY synchronized - except that all read only methods are safe from any thread,
-         *         because these are usually stored in a strucutre where they wont be updated.
-         *         Just be sure the HANLDER argument is safe when called from multiple threads at the same time!
+         *  Any route to apply the handler, must match ALL argument constraints.
          */
-        class Route {
-        public:
-            /**
-             *  Any route to apply the handler, must match ALL argument constraints.
-             */
-            Route (const RegularExpression& verbMatch, const RegularExpression& pathMatch, const RequestHandler& handler);
-            Route (const RegularExpression& pathMatch, const RequestHandler& handler);
-            Route (const function<bool(const Request&)>& requestMatcher, const RequestHandler& handler);
+        Route (const RegularExpression& verbMatch, const RegularExpression& pathMatch, const RequestHandler& handler);
+        Route (const RegularExpression& pathMatch, const RequestHandler& handler);
+        Route (const function<bool(const Request&)>& requestMatcher, const RequestHandler& handler);
 
-        private:
-            optional<RegularExpression>              fVerbMatch_;
-            optional<RegularExpression>              fPathMatch_;
-            optional<function<bool(const Request&)>> fRequestMatch_;
-            RequestHandler                           fHandler_;
+    private:
+        optional<RegularExpression>              fVerbMatch_;
+        optional<RegularExpression>              fPathMatch_;
+        optional<function<bool(const Request&)>> fRequestMatch_;
+        RequestHandler                           fHandler_;
 
-        private:
-            friend class Router;
-        };
+    private:
+        friend class Router;
+    };
 
-        /**
-         *  THREAD: must be externally synchronized, but all const methods are safe from any thread.
-         */
-        class Router : public Interceptor {
-        private:
-            using inherited = Interceptor;
+    /**
+     *  THREAD: must be externally synchronized, but all const methods are safe from any thread.
+     */
+    class Router : public Interceptor {
+    private:
+        using inherited = Interceptor;
 
-        public:
-            Router (const Sequence<Route>& routes);
+    public:
+        Router (const Sequence<Route>& routes);
 
-        public:
-            // typically just examine host-relative part of URL
-            nonvirtual optional<RequestHandler> Lookup (const Request& request) const;
+    public:
+        // typically just examine host-relative part of URL
+        nonvirtual optional<RequestHandler> Lookup (const Request& request) const;
 
-            //public:
-            // typically just examine host-relative part of URL
-            //    nonvirtual  optional<Handler>   Lookup (const String& method, const URL& url) const;
+        //public:
+        // typically just examine host-relative part of URL
+        //    nonvirtual  optional<Handler>   Lookup (const String& method, const URL& url) const;
 
-        private:
-            struct Rep_;
-        };
-    }
+    private:
+        struct Rep_;
+    };
 
 }
 
