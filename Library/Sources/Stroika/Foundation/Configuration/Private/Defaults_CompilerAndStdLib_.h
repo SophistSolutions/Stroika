83a83bfb7533564fb15407a0bfeb763cf8f1891f
Lewis Pringle <lewis@sophists.com>
2018-10-02 14:59:03 -0400

    tested with gcc 8.2

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -78,7 +78,7 @@
 #if __GNUC__ < 7
 #define _STROIKA_CONFIGURATION_WARNING_ "Warning: Stroika v2.1 does not support versions prior to GCC 7 (v2.0 supports g++5 and g++6)"
 #endif
-#if __GNUC__ > 8 || (__GNUC__ == 8 && (__GNUC_MINOR__ > 1))
+#if __GNUC__ > 8 || (__GNUC__ == 8 && (__GNUC_MINOR__ > 2))
 #define _STROIKA_CONFIGURATION_WARNING_ "Info: Stroika untested with this version of GCC - USING PREVIOUS COMPILER VERSION BUG DEFINES"
 #define CompilerAndStdLib_AssumeBuggyIfNewerCheck_(X) 1
 #endif
