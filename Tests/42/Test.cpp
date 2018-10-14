24ebe75c73a367b85e1f7c4c374f74d3289a0650
Lewis Pringle <lewis@sophists.com>
2018-10-13 23:23:10 -0400

    added (partly disabled cuz still broken) Cookie expires data regression test

--- Tests/42/Test.cpp
+++ Tests/42/Test.cpp
@@ -49,6 +49,19 @@ namespace {
                 c.fDomain.reset ();
                 VerifyTestResult (c.GetAttributes ().empty ());
             }
+            {
+                Cookie c = Cookie::Decode (L"lang=en-US; Expires=Wed, 09 Jun 2021 10:18:14 GMT");
+                VerifyTestResult (c.fKey == L"lang" and c.fValue == L"en-US");
+                using Time::Date;
+                using Time::DateTime;
+                using Time::DayOfMonth;
+                using Time::MonthOfYear;
+                using Time::TimeOfDay;
+                //@todo re-enable this test once we fix Cookie date parsing
+                //VerifyTestResult (c.fExpires == (DateTime{Date{Time::Year{2021}, MonthOfYear::eJune, DayOfMonth{9}}, TimeOfDay{10, 18, 14}}));
+                c.fExpires.reset ();
+                VerifyTestResult (c.GetAttributes ().empty ());
+            }
         }
     }
 }
