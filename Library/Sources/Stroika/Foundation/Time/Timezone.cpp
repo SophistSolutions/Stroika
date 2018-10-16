3a07c14d4413d23c2a9c98bb6bb2bf8512389d81
Lewis Pringle <lewis@sophists.com>
2018-10-16 13:12:49 -0400

    USE_NOISY_TRACE_IN_THIS_MODULE_ comments

--- Library/Sources/Stroika/Foundation/Time/Timezone.cpp
+++ Library/Sources/Stroika/Foundation/Time/Timezone.cpp
@@ -32,6 +32,11 @@ using namespace Stroika::Foundation;
 using namespace Stroika::Foundation::Characters;
 using namespace Stroika::Foundation::Time;
 
+
+// Comment this in to turn on aggressive noisy DbgTrace in this module
+//#define   USE_NOISY_TRACE_IN_THIS_MODULE_       1
+
+
 namespace {
     tm Date2TM_ (const Date& d, const optional<TimeOfDay>& tod)
     {
