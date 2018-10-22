b34f71279eec96889ce76cc6e0b1f6e5a59084e0
Lewis Pringle <lewis@sophists.com>
2018-10-21 21:57:12 -0400

    CheckFileExits.sh script now supports multiple arguments

--- ScriptsLib/CheckFileExists.sh
+++ ScriptsLib/CheckFileExists.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-file2Check=$1
-if [ ! -e $file2Check ] ; then
-	echo "[FAILED]: $file2Check does not exist";
-	exit 1;
-fi
+for file2Check in "$@"
+do
+	if [ ! -e $file2Check ] ; then
+		echo "[FAILED]: $file2Check does not exist";
+		exit 1;
+	fi
+done
