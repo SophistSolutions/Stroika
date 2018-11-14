035df955c13b8d31ef1563a48c218f61009b5155
Lewis Pringle <lewis@sophists.com>
2018-11-14 08:44:53 -0500

    warning silecning

--- Tests/31/Test.cpp
+++ Tests/31/Test.cpp
@@ -702,8 +702,8 @@ namespace {
         {
             ObjectVariantMapper mapper;
             using IO::Network::CIDR;
-            mapper.Add<CIDR> ([](const ObjectVariantMapper& mapper, const CIDR* obj) -> VariantValue { return obj->ToString (); },
-                              [](const ObjectVariantMapper& mapper, const VariantValue& d, CIDR* intoObj) -> void { *intoObj = CIDR{d.As<String> ()}; });
+            mapper.Add<CIDR> ([]([[maybe_unused]]const ObjectVariantMapper& mapper, const CIDR* obj) -> VariantValue { Lambda_Arg_Unused_BWA (mapper); return obj->ToString (); },
+                              []([[maybe_unused]]const ObjectVariantMapper& mapper, const VariantValue& d, CIDR* intoObj) -> void { Lambda_Arg_Unused_BWA (mapper); *intoObj = CIDR{d.As<String> ()}; });
         }
         {
             struct RGBColor {
@@ -772,10 +772,9 @@ namespace {
     DISABLE_COMPILER_GCC_WARNING_START ("GCC diagnostic ignored \"-Winvalid-offsetof\"");
     const ObjectVariantMapper dev::kMapper_ = []() {
         ObjectVariantMapper mapper;
-
         using IO::Network::CIDR;
-        mapper.Add<CIDR> ([](const ObjectVariantMapper& mapper, const CIDR* obj) -> VariantValue { return obj->ToString (); },
-                          [](const ObjectVariantMapper& mapper, const VariantValue& d, CIDR* intoObj) -> void { *intoObj = CIDR{d.As<String> ()}; });
+        mapper.Add<CIDR> ([]([[maybe_unused]]const ObjectVariantMapper& mapper, const CIDR* obj) -> VariantValue { return obj->ToString (); },
+                          []([[maybe_unused]]const ObjectVariantMapper& mapper, const VariantValue& d, CIDR* intoObj) -> void { *intoObj = CIDR{d.As<String> ()}; });
         return mapper;
     }();
 }
