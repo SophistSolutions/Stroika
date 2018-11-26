057b06bd20671de20b7f358b2675bf9882816c04
Lewis Pringle <lewis@sophists.com>
2018-11-25 21:29:28 -0500

    cosmetic

--- Library/Sources/Stroika/Frameworks/WebService/Server/VariantValue.h
+++ Library/Sources/Stroika/Frameworks/WebService/Server/VariantValue.h
@@ -43,49 +43,49 @@ namespace Stroika::Frameworks::WebService::Server::VariantValue {
     using WebServer::Response;
 
     /**
-        * Convert URL query argumetns to a mapping of name to value pairs (so they can be mapped to objects)
-        *
-        *  @see PickoutParamValuesFromBody () to just pickout params from Body
-        *  @see PickoutParamValues () to pickout params from both url arg and body
-        */
+     * Convert URL query arguments to a mapping of name to value pairs (so they can be mapped to objects)
+     *
+     *  @see PickoutParamValuesFromBody () to just pickout params from Body
+     *  @see PickoutParamValues () to pickout params from both url arg and body
+     */
     Mapping<String, VariantValue> PickoutParamValuesFromURL (Request* request, const optional<Iterable<String>>& namedParameters = {});
     Mapping<String, VariantValue> PickoutParamValuesFromURL (const URL& url, const optional<Iterable<String>>& namedParameters = {});
 
     /**
-        * Convert body to a mapping of name to value pairs (so they can be mapped to objects)
-        *
-        *  @see PickoutParamValuesFromURL () to just pickout params from URL
-        *  @see PickoutParamValues () to pickout params from both url arg and body
-        */
+     * Convert body to a mapping of name to value pairs (so they can be mapped to objects)
+     *
+     *  @see PickoutParamValuesFromURL () to just pickout params from URL
+     *  @see PickoutParamValues () to pickout params from both url arg and body
+     */
     Mapping<String, VariantValue> PickoutParamValuesFromBody (Request* request, const optional<Iterable<String>>& namedParameters);
     Mapping<String, VariantValue> PickoutParamValuesFromBody (const BLOB& body, const optional<InternetMediaType>& bodyContentType, const optional<Iterable<String>>& namedParameters);
 
     /**
-        * Combine params from URL (@see PickoutParamValuesFromURL) and PickoutParamValuesFromBody - optionally restricting which params we grab from URL/body.
-        *
-        *  If parameters given in both places, use the BODY -provided value.
-        *
-        *  @see PickoutParamValuesFromURL () to just pickout params from URL
-        *  @see PickoutParamValuesFromBody () to just pickout params from Body
-        *  @see PickoutParamValues () to pickout params from both url arg and body
-        */
+     * Combine params from URL (@see PickoutParamValuesFromURL) and PickoutParamValuesFromBody - optionally restricting which params we grab from URL/body.
+     *
+     *  If parameters given in both places, use the BODY -provided value.
+     *
+     *  @see PickoutParamValuesFromURL () to just pickout params from URL
+     *  @see PickoutParamValuesFromBody () to just pickout params from Body
+     *  @see PickoutParamValues () to pickout params from both url arg and body
+     */
     Mapping<String, VariantValue> PickoutParamValues (Request* request, const optional<Iterable<String>>& namedURLParams = {}, const optional<Iterable<String>>& namedBodyParams = {});
 
     /**
-        */
+     */
     VariantValue GetWebServiceArgsAsVariantValue (Request* request, const optional<String>& fromInMessage = {});
 
     /**
-        *  Take the ordered list of param names, and produce an ordered list of variant values (with the same ordering).
-        *  This is useful if you know the order of named parameters, and just want to pass them as ordered parameters.
-        */
+     *  Take the ordered list of param names, and produce an ordered list of variant values (with the same ordering).
+     *  This is useful if you know the order of named parameters, and just want to pass them as ordered parameters.
+     */
     Sequence<VariantValue> OrderParamValues (const Iterable<String>& paramNames, const Mapping<String, VariantValue>& paramValues);
 
     /**
-        *  \brief Apply the arguments in Mapping<String,VariantValue> in the order specified by paramNames, to function f, using objVariantMapper to transform them, and return the result
-        *
-        *  @todo - figure out how to rewrite this using variadic template
-        */
+     *  \brief Apply the arguments in Mapping<String,VariantValue> in the order specified by paramNames, to function f, using objVariantMapper to transform them, and return the result
+     *
+     *  @todo - figure out how to rewrite this using variadic template
+     */
     template <typename RETURN_TYPE>
     RETURN_TYPE ApplyArgs (const Mapping<String, VariantValue>& variantValueArgs, const DataExchange::ObjectVariantMapper& objVarMapper, const Traversal::Iterable<String>& paramNames, const function<RETURN_TYPE (void)>& f);
     template <typename RETURN_TYPE, typename ARG_TYPE_0>
@@ -98,40 +98,40 @@ namespace Stroika::Frameworks::WebService::Server::VariantValue {
     RETURN_TYPE ApplyArgs (const Mapping<String, VariantValue>& variantValueArgs, const DataExchange::ObjectVariantMapper& objVarMapper, const Traversal::Iterable<String>& paramNames, const function<RETURN_TYPE (ARG_TYPE_0, ARG_TYPE_1, ARG_TYPE_2, ARG_TYPE_3)>& f);
 
     /**
-        */
+     */
     template <typename RETURN_TYPE, typename... IN_ARGS>
     void CallFAndWriteConvertedResponse (Response* response, const WebServiceMethodDescription& webServiceDescription, const DataExchange::ObjectVariantMapper& objVarMapper, const function<RETURN_TYPE (IN_ARGS...)>& f, IN_ARGS... inArgs);
 
     /**
-        */
+     */
     void WriteResponse (Response* response, const WebServiceMethodDescription& webServiceDescription);
     void WriteResponse (Response* response, const WebServiceMethodDescription& webServiceDescription, const VariantValue& responseValue);
 
     /**
-        *  The variants of mkRequestHandler () which take paramNames will eventually be replaced with variadic templates. So the calls should remain
-        *  unchanged, but the exact parameters passed to the template instantiation will.
-        *
-        *  Each named parameter (in paramNames) must correspond to the JSON param arg to the funtion and must correspond exactly in type and
-        *  order to the parameters of the function.
-        *
-        *  \par Example Usage
-        *      \code
-        *           Route{
-        *              RegularExpression{L"plus"},
-        *              mkRequestHandler (
-        *                  kPlusWSDescriptor_,
-        *                  kObjectVariantMapper,
-        *                  Sequence<String>{L"lhs", L"rhs"},
-        *                  function<float (float, float)>{[=](float lhs, float rhs) { return lhs + rhs }})},
-        *      \endcode
-        *
-        *  The overload with f (void) as argument, takes no arguments (and so omits paramNames), and just returns the given result.
-        *
-        *  The overload with no 'f' just takes a simple message handler (and wraps a bit of WebServiceMethodDescription checking around its call).
-        *
-        *
-        *  @todo REWRITE USING PickoutParamValues, and ApplyArgs (allowing intermeidate varsions 
-        */
+     *  The variants of mkRequestHandler () which take paramNames will eventually be replaced with variadic templates. So the calls should remain
+     *  unchanged, but the exact parameters passed to the template instantiation will.
+     *
+     *  Each named parameter (in paramNames) must correspond to the JSON param arg to the funtion and must correspond exactly in type and
+     *  order to the parameters of the function.
+     *
+     *  \par Example Usage
+     *      \code
+     *           Route{
+     *              RegularExpression{L"plus"},
+     *              mkRequestHandler (
+     *                  kPlusWSDescriptor_,
+     *                  kObjectVariantMapper,
+     *                  Sequence<String>{L"lhs", L"rhs"},
+     *                  function<float (float, float)>{[=](float lhs, float rhs) { return lhs + rhs }})},
+     *      \endcode
+     *
+     *  The overload with f (void) as argument, takes no arguments (and so omits paramNames), and just returns the given result.
+     *
+     *  The overload with no 'f' just takes a simple message handler (and wraps a bit of WebServiceMethodDescription checking around its call).
+     *
+     *
+     *  @todo REWRITE USING PickoutParamValues, and ApplyArgs (allowing intermeidate varsions 
+     */
     template <typename RETURN_TYPE, typename ARG_TYPE_COMBINED>
     WebServer::RequestHandler mkRequestHandler (const WebServiceMethodDescription& webServiceDescription, const DataExchange::ObjectVariantMapper& objVarMapper, const function<RETURN_TYPE (ARG_TYPE_COMBINED)>& f);
     template <typename RETURN_TYPE, typename ARG_TYPE_0>
