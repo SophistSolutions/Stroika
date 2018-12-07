9e19ed9edb8b6b4710f46b82205d6b2a31c6a03d
Lewis Pringle <lewis@sophists.com>
2018-12-07 17:14:56 -0500

    Comments

--- ScriptsLib/Makefile-Common.mk
+++ ScriptsLib/Makefile-Common.mk
@@ -9,8 +9,12 @@ endif
 
 FUNCTION_CAPITALIZE_WORD=$(shell echo $1 | tr '[:lower:]' '[:upper:]' | cut -c 1-1)$(shell echo $1 | cut -c 2-99)
 
+# intentionally export just as a performance hack (avoid call to getdefaultshellvariable)
 export ECHO?=	$(shell $(StroikaRoot)ScriptsLib/GetDefaultShellVariable.sh ECHO)
 
+# intentionally export cuz sometimes MAKE_INDENT_LEVEL doesnt go up as fast as MAKELEVEL, and so submakes work out
+# looking better with less explicit setting
 export MAKE_INDENT_LEVEL?=$(MAKELEVEL)
 
+# intentionally DONT set export for this cuz it could vary from makefile to makefile
 SHELL?=/bin/bash
