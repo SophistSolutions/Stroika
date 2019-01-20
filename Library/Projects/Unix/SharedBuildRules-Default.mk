559ad07832b7a3bc0aa2bb4950ca0994fc33640e
Lewis Pringle <lewis@sophists.com>
2019-01-20 17:23:04 -0500

    cosmetic cleanup of SharedBuildRules-Default.mk

--- Library/Projects/Unix/SharedBuildRules-Default.mk
+++ Library/Projects/Unix/SharedBuildRules-Default.mk
@@ -15,16 +15,6 @@ $(ObjDir):
 $(Objs):	| $(ObjDir)
 
 
-
-### As of Stroika v2.1d15 disabled
-#%.o : %.cpp
-#	@$(StroikaRoot)ScriptsLib/PrintProgressLine $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables `realpath $<`) ... "
-#	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
-#	    $(StroikaRoot)ScriptsLib/PrintProgressLine $$(($(MAKE_INDENT_LEVEL)+1)) "$(CXX) $(CXXFLAGS) -c $< -o $@";\
-#	fi
-#	@mkdir -p `dirname $@`
-#	@$(CXX) $(CXXFLAGS) -c $< -o $@
-
 $(ObjDir)%.o : %.cpp
 	@$(StroikaRoot)ScriptsLib/PrintProgressLine $(MAKE_INDENT_LEVEL) "Compiling $(shell $(StroikaRoot)ScriptsLib/SubstituteBackVariables `realpath $<`) ... "
 	@if [ $(ECHO_BUILD_LINES) -eq 1 ]; then\
