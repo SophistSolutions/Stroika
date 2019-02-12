088c1c122555c43c1ace934c83e61b2c3e54f5e5
Lewis Pringle <lewis@sophists.com>
2019-02-12 18:48:20 -0500

    Support vs2k17 15.9.7

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -121,6 +121,7 @@
 #define _MS_VS_2k17_15Pt9Pt2_ 191627024
 #define _MS_VS_2k17_15Pt9Pt4_ 191627025
 #define _MS_VS_2k17_15Pt9Pt5_ 191627026
+#define _MS_VS_2k17_15Pt9Pt7_ 191627027
 
 // _MSC_VER=1920
 #define _MSC_VER_2k19_16Pt0_ 1920
@@ -137,7 +138,7 @@
 #endif
 #elif _MSC_VER <= _MSC_VER_2k17_15Pt9_
 // check which pointer-version of MSVC2k17 (15.9.x)
-#if _MSC_FULL_VER > _MS_VS_2k17_15Pt9Pt5_
+#if _MSC_FULL_VER > _MS_VS_2k17_15Pt9Pt7_
 // @todo figure out how to add arg to message
 #define _STROIKA_CONFIGURATION_WARNING_ "Info: This version (#_MSC_FULL_VER ) - 15.9.x - of Stroika is untested with this Update of of Microsoft Visual Studio.net / Visual C++ - USING PREVIOUS COMPILER VERSION BUG DEFINES"
 #define CompilerAndStdLib_AssumeBuggyIfNewerCheck_(X) 1
