0240ceedc636373b08751ed0afa5c73401df735d
Lewis Pringle <lewis@sophists.com>
2019-02-26 11:58:33 -0500

    cosmetic

--- Library/Sources/Stroika/Foundation/Debug/BackTrace.cpp
+++ Library/Sources/Stroika/Foundation/Debug/BackTrace.cpp
@@ -37,9 +37,9 @@ wstring Debug::BackTrace ([[maybe_unused]] unsigned int maxFrames)
      */
     constexpr size_t kMaxStackSize_ = 100; // could look at return size and re-run if equals exactly...
     // @todo combine maxFrames with trial and error on backtrace() calls
-    void* stackTraceBuf[kMaxStackSize_]{};
-    int   nptrs = ::backtrace (stackTraceBuf, NEltsOf (stackTraceBuf));
-    char** syms = ::backtrace_symbols (stackTraceBuf, nptrs);
+    void*  stackTraceBuf[kMaxStackSize_]{};
+    int    nptrs = ::backtrace (stackTraceBuf, NEltsOf (stackTraceBuf));
+    char** syms  = ::backtrace_symbols (stackTraceBuf, nptrs);
     if (syms == NULL) {
         //DbgTrace ("%d errno", errno); // perror("backtrace_symbols");
         return wstring{};
