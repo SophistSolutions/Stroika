b6e70e49d37adb5d34d8d7cc96cc5fecbf69442f
Lewis Pringle <lewis@sophists.com>
2019-03-05 14:51:48 -0500

    rename  _ISO8601 to _duration (operator" for duration)

--- Library/Sources/Stroika/Foundation/Time/Duration.h
+++ Library/Sources/Stroika/Foundation/Time/Duration.h
@@ -412,13 +412,13 @@ namespace Stroika::Foundation::Time {
     /**
      *  \brief user defined literal for Duration, specified in ISO8601 format.
      */
-    Duration operator"" _ISO8601 (const char* str, size_t len) noexcept;
-    Duration operator"" _ISO8601 (const wchar_t* str, size_t len) noexcept;
+    Duration operator"" _duration (const char* str, size_t len) noexcept;
+    Duration operator"" _duration (const wchar_t* str, size_t len) noexcept;
 #if __cpp_char8_t >= 201811L
-    Duration operator"" _ISO8601 (const char8_t* str, size_t len) noexcept;
+    Duration operator"" _duration (const char8_t* str, size_t len) noexcept;
 #endif
-    Duration operator"" _ISO8601 (const char16_t* str, size_t len) noexcept;
-    Duration operator"" _ISO8601 (const char32_t* str, size_t len) noexcept;
+    Duration operator"" _duration (const char16_t* str, size_t len) noexcept;
+    Duration operator"" _duration (const char32_t* str, size_t len) noexcept;
 
     /**
      *  operator indirects to Duration::Compare()
