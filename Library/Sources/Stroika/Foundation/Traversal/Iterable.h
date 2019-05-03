0c022332cf6217a25f025dd3a539920de5b0fb3d
Lewis Pringle <lewis@sophists.com>
2019-05-02 21:44:47 -0400

    docs

--- Library/Sources/Stroika/Foundation/Traversal/Iterable.h
+++ Library/Sources/Stroika/Foundation/Traversal/Iterable.h
@@ -182,13 +182,11 @@ namespace Stroika::Foundation::Traversal {
      *      sequence of numbers each time you make an iterate and run.
      *
      *  *Design Note*:
-     *      Chose NOT to include an Equals (Iterable<T> rhs) const method here, but instead duplicatively in
+     *      Chose NOT to include an equal_to<Iterable<T>> partial specialization here, but instead duplicatively in
      *      each subclass, so that it could more easily be implemented efficiently (not a biggie), but more
      *      importantly because it doesn't appear to me to make sense so say that a Stack<T> == Set<T>, even if
-     *      their values were the same.
-     *
-     *      Also, Equals() meaning is just wrong if you implement it the only way you can for an Iterable,
-     *      (iterating over elements) if the container has a differnt logical notion of equals (like Set).
+     *      their values were the same. In other words, the meaning of 'equals' varies between different kinds of
+     *      iterables (over the same type).
      *
      *      We DO have methods SetEquals/MultiSetEquals/SequnceEquals (see below).
      *
