0e9f8b996865ba6ebf2fb9b725a8e0d806b6ee21
Lewis Pringle <lewis@sophists.com>
2019-06-26 14:17:29 -0400

    very rough (not yet usable) draft of IO::Transfer::Cache code

---
+++ Library/Sources/Stroika/Foundation/IO/Network/Transfer/Cache.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2019.  All rights reserved
+ */
+#include "../../../StroikaPreComp.h"
+
+#include "../../../Cache/SynchronizedLRUCache.h"
+
+#include "Cache.h"
+
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Characters;
+using namespace Stroika::Foundation::Containers;
+using namespace Stroika::Foundation::IO;
+using namespace Stroika::Foundation::IO::Network;
+using namespace Stroika::Foundation::IO::Network::Transfer;
+using namespace Stroika::Foundation::Time;
+
+using Stroika::Foundation::Cache::SynchronizedLRUCache;
+
+namespace {
+
+    struct DefaultCacheRep_ : Transfer::Cache::Rep {
+
+        using Element = Transfer::Cache::Element;
+
+        /**
+         */
+        virtual optional<Response> OnBeforeFetch (Request* request) override
+        {
+            // @todo -- see if its in cache, and if so, maybe add conditional if-modfied-since or -if-etag
+            // and under somecircumstances, just return the correct answer if its cachable without any call
+            return nullopt;
+        }
+
+        virtual void OnAfterFetch (Request* request, Response* response) override
+        {
+            RequireNotNull (request);
+            RequireNotNull (response);
+            switch (response->GetStatus ()) {
+                case HTTP::StatusCodes::kOK: {
+					// @todo add result to cache
+					// but must look at headers closely first
+                } break;
+                case HTTP::StatusCodes::kNotModified: {
+                    // @todo replaces response value with right answer on 304
+                    // lookup cache value and return it - updating any needed http headers stored in cache
+                } break;
+                default: {
+					// ignored
+                } break;
+            }
+        }
+
+        /**
+         * if not cleared, external cache can be re-used
+         */
+        virtual void ClearCache () override
+        {
+        }
+
+        virtual optional<Element> Lookup (const URI& url) const override
+        {
+            return fCache_.Lookup (url);
+        }
+
+#if 0
+     *  \par Example Usage
+     *      \code
+     *          LRUCache<string, string> tmp (3);
+     *          tmp.Add ("a", "1");
+     *          tmp.Add ("b", "2");
+     *          tmp.Add ("c", "3");
+     *          tmp.Add ("d", "4");
+     *          VerifyTestResult (not tmp.Lookup ("a").has_value ());
+     *          VerifyTestResult (tmp.Lookup ("b") == "2");
+     *          VerifyTestResult (tmp.Lookup ("d") == "4");
+     *      \endcode
+#endif
+        SynchronizedLRUCache<URI, Element> fCache_{101};
+	 // we want to use hash stuff but then need hash<URI>
+        ///pair<string, string>{}, 3, 10, hash<string>{}
+    };
+
+}
+/*
+ ********************************************************************************
+ **************************** Transfer::Cache ***********************************
+ ********************************************************************************
+ */
+Transfer::Cache::Ptr Transfer::Cache::CreateDefault ()
+{
+    return Ptr{make_shared<DefaultCacheRep_> ()};
+}
---
+++ Library/Sources/Stroika/Foundation/IO/Network/Transfer/Cache.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2019.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_IO_Network_Transfer_Cache_h_
+#define _Stroika_Foundation_IO_Network_Transfer_Cache_h_ 1
+
+#include "../../../StroikaPreComp.h"
+
+#include "../../../Containers/Set.h"
+#include "../../../Time/DateTime.h"
+
+#include "Request.h"
+#include "Response.h"
+
+/**
+ *
+ *  \version    <a href="Code-Status.md#Alpha-Late">Alpha-Late</a>
+ *
+ *
+ * TODO:
+ */
+
+namespace Stroika::Foundation::IO::Network::Transfer {
+
+    /**
+     *  This cache is a generic API, allowing users of the IO::Transfer::Connection code to
+     *  use different cache implementations (e.g. one storing to disk, and one to RAM).
+     *
+     *  It is hopefully done in such a way that you can do alot of code sharing among these implementations,
+     *  but do not need to.
+     *
+     *  This Cache class also comes with an opaque, one-size-fits all (who they kidding) implementation, with a
+     *  handful of configuration options.
+     */
+    class Cache {
+    public:
+        struct Rep;
+
+    public:
+        struct Ptr;
+
+    public:
+        struct Element;
+
+    public:
+        /**
+         *  Creates a default-styled HTTP cache object. Note - you still must associate this cache object with
+         *  any cache connections you create (typically by assigning it to the Connection creation factory)
+         *
+         *  NOTE - thoguh the smart ptr Ptr is not fully re-entrant, the letter class created by CreateDefault is fully
+         *  internally synchonized. This means you can re-use it with multiple connections and run those connection requests from as
+         *  many threads as desired.
+         */
+        static Ptr CreateDefault ();
+    };
+
+    /**
+     *  This is the basic element of data stored in the cache for any cached URL.
+     */
+    struct Cache::Element {
+        optional<String>                          fETag;
+        optional<Time::DateTime>                  fExpires;
+        optional<Time::DateTime>                  fLastModified;
+        optional<Containers::Set<String>>         fCacheControl; // parsed/split rep
+        Memory::BLOB                              fBody;         // no point in a cache entry without this
+        optional<DataExchange::InternetMediaType> fContentType;
+        Mapping<String, String>                   fOtherHeaders; // headers not captured/extracted above
+    };
+
+    /**
+     */
+    struct Cache::Rep {
+
+        /**
+         *  was called BeforeGet - but can decide internally - and callers can decide to only use on get
+         */
+        virtual optional<Response> OnBeforeFetch (Request* request) = 0;
+
+        /**
+         * replaces response value with right answer on 304
+         // after any possibly cachable calls (typiucally just Get) - was called AfterGet
+         */
+        virtual void OnAfterFetch (Request* request, Response* response) = 0;
+
+        /**
+         * if not cleared, external cache can be re-used
+         */
+        virtual void ClearCache () = 0;
+
+        /**
+         */
+        virtual optional<Element> Lookup (const URI& url) const = 0;
+    };
+
+    /**
+     */
+    struct Cache::Ptr : shared_ptr<Cache::Rep> {
+    };
+
+}
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include "Cache.inl"
+
+#endif /*_Stroika_Foundation_IO_Network_Transfer_Cache_h_*/
