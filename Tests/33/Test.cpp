abe93aea2e1d9b8abed69591d6c698b88101fc85
Lewis Pringle <lewis@sophists.com>
2019-07-19 11:49:15 -0400

    cosmetic

--- Tests/33/Test.cpp
+++ Tests/33/Test.cpp
@@ -58,7 +58,7 @@ namespace {
         };
         OptionsFile of{
             L"MyModule",
-            []() -> ObjectVariantMapper {
+            [] () -> ObjectVariantMapper {
                 ObjectVariantMapper mapper;
                 mapper.AddClass<MyData_> (initializer_list<ObjectVariantMapper::StructFieldInfo>{
                     {L"Enabled", Stroika_Foundation_DataExchange_StructFieldMetaInfo (MyData_, fEnabled)},
@@ -67,7 +67,7 @@ namespace {
                 return mapper;
             }(),
             OptionsFile::kDefaultUpgrader,
-            [](const String& moduleName, const String& fileSuffix) -> String {
+            [] (const String& moduleName, const String& fileSuffix) -> String {
                 return IO::FileSystem::WellKnownLocations::GetTemporary () + moduleName + fileSuffix;
             }};
         MyData_ m = of.Read<MyData_> (MyData_ ()); // will return default values if file not present
@@ -84,7 +84,7 @@ namespace {
         ModuleGetterSetter_Implementation_MyData_ ()
             : fOptionsFile_{
                   L"MyModule",
-                  []() -> ObjectVariantMapper {
+                  [] () -> ObjectVariantMapper {
                       ObjectVariantMapper mapper;
                       mapper.AddClass<MyData_> (initializer_list<ObjectVariantMapper::StructFieldInfo>{
                           {L"Enabled", Stroika_Foundation_DataExchange_StructFieldMetaInfo (MyData_, fEnabled)},
@@ -92,7 +92,7 @@ namespace {
                       });
                       return mapper;
                   }(),
-                  OptionsFile::kDefaultUpgrader, [](const String& moduleName, const String& fileSuffix) -> String {
+                  OptionsFile::kDefaultUpgrader, [] (const String& moduleName, const String& fileSuffix) -> String {
                 // for regression tests write to /tmp
                 return  IO::FileSystem::WellKnownLocations::GetTemporary () + moduleName + fileSuffix; }}
             , fActualCurrentConfigData_ (fOptionsFile_.Read<MyData_> (MyData_ ()))
