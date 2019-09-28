94cac6af97cb4ced90ec6efe8d3dea686053c0e4
Lewis Pringle <lewis@sophists.com>
2019-09-28 00:53:35 -0400

    lose obsolete commented out var-setting in makefile

--- Tests/Makefile-Test-Template.mk
+++ Tests/Makefile-Test-Template.mk
@@ -39,32 +39,6 @@ check:
 	@$(ECHO) "[SUCCEEDED]";
 
 
-#tmphack - til we can properly amend configure (and to experiment HOW we need to amend confiugure)
-# ifeq (VisualStudio.Net,$(findstring VisualStudio.Net,$(ProjectPlatformSubdir)))
-
-# EXTRA_PREFIX_LINKER_ARGS+= /MACHINE:${WIN_LIBCOMPATIBLE_ARCH}
-
-# #FOR RELEASE BUIILDS 
-# #	/OPT:REF eliminates functions and or data that are never referenced
-# #	/OPT:ICF[=iterations] to perform identical COMDAT folding
-# EXTRA_PREFIX_LINKER_ARGS+= /OPT:REF
-# EXTRA_PREFIX_LINKER_ARGS+= /OPT:ICF
-
-# EXTRA_PREFIX_LINKER_ARGS+= /DEBUG
-# #EXTRA_PREFIX_LINKER_ARGS+= /DYNAMICBASE:NO
-
-# #EXTRA_SUFFIX_LINKER_ARGS+= /SAFESEH 
-# #EXTRA_SUFFIX_LINKER_ARGS+= /INCREMENTAL:NO
-# #EXTRA_SUFFIX_LINKER_ARGS+= /PGD:"C:\Sandbox\Stroika\DevRoot\Tests\Projects\VisualStudio.Net-2019\34\..\..\..\..\Builds\Release-U-32\Tests\Test34.pgd" 
-
-# # 
-# # /LTCG linktime code gen
-# # /LTCG:incremental incremental linktime codegen (or many other : variations)
-# #EXTRA_PREFIX_LINKER_ARGS+= /LTCG
-# endif
-
-
-
 # Cannot be done generically through configuration cuz a given build of Stroika might contain EXEs targetting multiple subsystems
 ifeq (VisualStudio.Net,$(findstring VisualStudio.Net,$(ProjectPlatformSubdir)))
 EXTRA_SUFFIX_LINKER_ARGS+= /SUBSYSTEM:CONSOLE
