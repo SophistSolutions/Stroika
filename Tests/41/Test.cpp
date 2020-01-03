b18fb6407c495d5779ad890a9c0226698aa6425a
Lewis Pringle <lewis@sophists.com>
2020-01-03 13:45:26 -0500

    change name of ignored variable, just to avoid a name conflict (maybe bug in MSVC2k17 but not worth checking)

--- Tests/41/Test.cpp
+++ Tests/41/Test.cpp
@@ -48,11 +48,11 @@ namespace {
         RequireNotNull (relPath);
         RequireNotNull (query);
 
-        DWORD   ignore = 0;
+        DWORD   ignr = 0;
         wchar_t outBuf[10 * 1024];
 
         String canonical;
-        ThrowIfErrorHRESULT (::CoInternetParseUrl (CComBSTR (w.c_str ()), PARSE_CANONICALIZE, 0, outBuf, static_cast<DWORD> (NEltsOf (outBuf)), &ignored, 0));
+        ThrowIfErrorHRESULT (::CoInternetParseUrl (CComBSTR (w.c_str ()), PARSE_CANONICALIZE, 0, outBuf, static_cast<DWORD> (NEltsOf (outBuf)), &ignr, 0));
         canonical = outBuf;
 
         {
@@ -62,7 +62,7 @@ namespace {
             }
         }
 
-        if (SUCCEEDED (::CoInternetParseUrl (CComBSTR (canonical.c_str ()), PARSE_DOMAIN, 0, outBuf, static_cast<DWORD> (NEltsOf (outBuf)), &ignored, 0))) {
+        if (SUCCEEDED (::CoInternetParseUrl (CComBSTR (canonical.c_str ()), PARSE_DOMAIN, 0, outBuf, static_cast<DWORD> (NEltsOf (outBuf)), &ignr, 0))) {
             *host = outBuf;
         }
 
