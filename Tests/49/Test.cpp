54f99c1fe441e4b158eec5362ef64ba2ef96cfba
Lewis Pringle <lewis@sophists.com>
2020-04-04 18:18:12 -0400

    cleanup/enhance regressiontests for recent bug with Iterable<> closure handling in getNext CreateGenerator usage - all fixed now I htink

--- Tests/49/Test.cpp
+++ Tests/49/Test.cpp
@@ -829,46 +829,71 @@ namespace {
 
             VerifyTestResult (fds.size () == 2);
             VerifyTestResult (seeIfReady.size () == 2);
-            DbgTrace (L"fds=%s", Characters::ToString (fds).c_str ()); // not this is critical step in reproducing old bug - iterating over fds
+            DbgTrace (L"fds=%s", Characters::ToString (fds).c_str ()); // note this is critical step in reproducing old bug - iterating over fds
             VerifyTestResult (fds.size () == 2);
             VerifyTestResult (seeIfReady.size () == 2);
         };
         t1 ();
+        auto t11 = [] () {
+            Containers::Bijection<int, int> seeIfReady;
+            seeIfReady.Add (1, 1);
+            seeIfReady.Add (2, 2);
+            seeIfReady.Add (3, 3);
+            VerifyTestResult (seeIfReady.size () == 3);
+            DbgTrace (L"seeIfReady=%s", Characters::ToString (seeIfReady).c_str ());
+            VerifyTestResult (seeIfReady.size () == 3);
+
+            Traversal::Iterable<int> fds = seeIfReady.Image ();
+
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
+            DbgTrace (L"fds=%s", Characters::ToString (fds).c_str ()); // note this is critical step in reproducing old bug - iterating over fds
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
+            DbgTrace (L"fds=%s", Characters::ToString (fds).c_str ()); // note this is critical step in reproducing old bug - iterating over fds
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
+        };
+        t11 ();
         auto t2 = [] () {
             Containers::Bijection<int, int> seeIfReady;
             seeIfReady.Add (1, 1);
             seeIfReady.Add (2, 2);
-            VerifyTestResult (seeIfReady.size () == 2);
+            seeIfReady.Add (3, 3);
+            VerifyTestResult (seeIfReady.size () == 3);
             DbgTrace (L"seeIfReady=%s", Characters::ToString (seeIfReady).c_str ());
-            VerifyTestResult (seeIfReady.size () == 2);
+            VerifyTestResult (seeIfReady.size () == 3);
 
             const bool               kFails_ = true;
-            Traversal::Iterable<int> fds     = kFails_ ? seeIfReady.Image () : Traversal::Iterable<int>{1, 2};
+            Traversal::Iterable<int> fds     = kFails_ ? seeIfReady.Image () : Traversal::Iterable<int>{1, 2, 3};
 
-            VerifyTestResult (fds.size () == 2);
-            VerifyTestResult (seeIfReady.size () == 2);
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
             DbgTrace (L"fds=%s", Characters::ToString (fds).c_str ()); // not this is critical step in reproducing old bug - iterating over fds
-            VerifyTestResult (fds.size () == 2);
-            VerifyTestResult (seeIfReady.size () == 2);
+            VerifyTestResult (fds.size () == 3);
+            VerifyTestResult (seeIfReady.size () == 3);
 
             const bool               kFails2_ = true;
             Traversal::Iterable<int> o1       = kFails2_ ? fds.Select<int> ([&] (const int& t) { return t; }) : fds;
 
-            VerifyTestResult (seeIfReady.size () == 2);
-            VerifyTestResult (o1.size () == 2);
+            VerifyTestResult (seeIfReady.size () == 3);
+            VerifyTestResult (o1.size () == 3);
 
             // this iteration seems to break the list o1 if kFails_
-            DbgTrace (L"o1=%s", Characters::ToString (o1).c_str ());
+            int a = o1.size ();
+            //DbgTrace (L"o1=%s", Characters::ToString (o1).c_str ());
 
-            VerifyTestResult (o1.size () == 2);
+            VerifyTestResult (o1.size () == 3);
 
             auto c1 = Containers::Collection<int>{o1};
-            //DbgTrace (L"****CTOR2 top1: o1=%s", Characters::ToString (o1).c_str ());
-            VerifyTestResult (o1.size () == 2);
-            //DbgTrace (L"****CTOR2 top1: c1=%s", Characters::ToString (c1).c_str ());
-            VerifyTestResult (c1.size () == 2);
+            (void)Characters::ToString (o1);
+            VerifyTestResult (o1.size () == 3);
+            (void)Characters::ToString (c1);
+            VerifyTestResult (c1.size () == 3);
         };
-        //t2 ();    -- @todo debug still failing
+        t2 ();
     }
 }
 
