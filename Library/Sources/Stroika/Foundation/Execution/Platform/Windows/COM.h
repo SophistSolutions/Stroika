62473c941e799efd27118b9be3d7bc919d7a16a3
Lewis Pringle <lewis@sophists.com>
2020-05-24 20:17:58 -0400

    Added files forgot to checkin

---
+++ Library/Sources/Stroika/Foundation/Execution/Platform/Windows/COM.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2020.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Execution_Platform_Windows_COM_h_
+#define _Stroika_Foundation_Execution_Platform_Windows_COM_h_ 1
+
+#include "../../../StroikaPreComp.h"
+
+#if qPlatform_Windows
+#include <Windows.h>
+
+#include <objbase.h>
+#else
+#error "WINDOWS REQUIRED FOR THIS MODULE"
+#endif
+
+#include "../../../Configuration/Common.h"
+
+namespace Stroika::Foundation::Execution::Platform::Windows {
+
+    /**
+     */
+    class COMInitializer {
+    public:
+        /**
+         *  Declare one of these objects to have the scope/lifetime for which you want COM initialized.
+         *
+         *  if (worksWithAnyCoInitFlag) then ignore RPC_E_CHANGED_MODE, and allow any successful COM initialization
+         *
+         *  \par Example Usage
+         *      \code
+         *          COMInitializer initCOMForScope { COINIT_APARTMENTTHREADED };
+         *      \endcode
+         *
+         *  \par Example Usage
+         *      \code
+         *          COMInitializer initCOMForScope { COINIT_MULTITHREADED };
+         *      \endcode
+         *
+         *  \note - if using MFC and/or OleInitialize(), users may wish to create a COMInitializer instance (with COINIT_APARTMENTTHREADED)
+         *      very early, to assure application reliably sets up use of COINIT_APARTMENTTHREADED initialization.
+         */
+        COMInitializer () = delete;
+        COMInitializer (COINIT initFlag = COINIT_APARTMENTTHREADED, bool worksWithAnyCoInitFlag = true);
+        COMInitializer (const COMInitializer&) = delete;
+        ~COMInitializer ();
+
+    public:
+        nonvirtual COMInitializer& operator= (const COMInitializer&) = delete;
+
+    private:
+        bool fNeedsShutdown_{false};
+    };
+
+}
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include "COM.inl"
+
+#endif /*_Stroika_Foundation_Execution_Platform_Windows_COM_h_*/
---
+++ Library/Sources/Stroika/Foundation/Execution/Platform/Windows/COM.inl
@@ -0,0 +1,58 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2020.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Execution_Platform_Windows_COM_inl_
+#define _Stroika_Foundation_Execution_Platform_Windows_COM_inl_ 1
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+#include "../../../Debug/Trace.h"
+
+#include "HRESULTErrorException.h"
+
+namespace Stroika::Foundation::Execution::Platform::Windows {
+
+    /*
+     ********************************************************************************
+     ***************************** Implementation Details ***************************
+     ********************************************************************************
+     */
+    inline COMInitializer::COMInitializer (COINIT initFlag, bool worksWithAnyCoInitFlag)
+    {
+        Debug::TraceContextBumper ctx{L"Execution::Platform::Windows::COMInitializer::CTOR"};
+        HRESULT                   hr = ::CoInitializeEx (nullptr, initFlag);
+        switch (hr) {
+            case S_OK:
+                fNeedsShutdown_ = true;
+                return;
+            case S_FALSE:
+                fNeedsShutdown_ = true;
+                return; // harmless, already initialized
+            case RPC_E_CHANGED_MODE: {
+                if (worksWithAnyCoInitFlag) {
+                    fNeedsShutdown_ = true;
+                    DbgTrace ("RPC_E_CHANGED_MODE being ignored");
+                }
+                else {
+                    ThrowIfErrorHRESULT (RPC_E_CHANGED_MODE);
+                }
+            }
+            default:
+                ThrowIfErrorHRESULT (hr);
+        }
+    }
+    inline COMInitializer::~COMInitializer ()
+    {
+        Debug::TraceContextBumper ctx{L"Execution::Platform::Windows::COMInitializer::~COMInitializer"};
+        if (fNeedsShutdown_) {
+            ::CoUninitialize ();
+        }
+    }
+
+}
+
+#endif /*_Stroika_Foundation_Execution_Platform_Windows_COM_inl_*/
