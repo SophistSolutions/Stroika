9ae318836186ddf1e1202d8a2599007ebab00dcd
Lewis Pringle <lewis@sophists.com>
2020-05-31 19:50:08 -0400

    lose deprecated files Library/Projects/Unix/SharedBuildRules-Default.mk and Library/Projects/Unix/SharedMakeVariables-Default.mk

--- Library/Projects/Unix/SharedMakeVariables-Default.mk
+++
@@ -1,5 +0,0 @@
-$(info "This file deprecated  (as of Stroika 2.1a2) - include ScriptsLib/SharedMakeVariables-Default.mk instead")
-ifndef StroikaRoot
-$(error("StroikaRoot must be defined and included before this file"))
-endif
-include $(StroikaRoot)/ScriptsLib/SharedMakeVariables-Default.mk
