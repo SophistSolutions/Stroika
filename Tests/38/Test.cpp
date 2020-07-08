b3b5c95d5eb56894af27d91ea99ca5bce0e91275
Lewis Pringle <lewis@sophists.com>
2020-07-08 12:09:26 -0400

    Comments

--- Tests/38/Test.cpp
+++ Tests/38/Test.cpp
@@ -774,6 +774,7 @@ namespace {
          *      2018-03-10 - saw happen on Linux, but still very rare
          *      2018-12-21 - saw happen on Linux (WSL), but still very rare
          *      2019-08-28 - saw happen on Linux (WSL), but still very rare
+         *      2020-07-07 - saw happen on Windows, but still very rare
          */
         VerifyTestResult (WaitableEvent::WaitForAny (Sequence<WaitableEvent*> ({&we1, &we2})) == set<WaitableEvent*> ({&we2})); // may not indicate a real problem if triggered rarely - just threads ran in queer order, but can happen
         Time::DurationSecondsType timeTaken = Time::GetTickCount () - startAt;
