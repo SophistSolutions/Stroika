1e3a4e806b32053c8f8ad0265244992c23f80120
Lewis Pringle <lewis@sophists.com>
2020-08-30 11:23:24 -0400

    PortType well known ports now as namespace with constexpr instead of enum cuz cannot implicitly assign to PortType otehrwise (sad)

--- Library/Sources/Stroika/Foundation/IO/Network/Port.h
+++ Library/Sources/Stroika/Foundation/IO/Network/Port.h
@@ -6,26 +6,21 @@
 
 #include "../../StroikaPreComp.h"
 
-#if defined(__cpp_impl_three_way_comparison)
-#include <compare>
-#endif
+#include <cstdint>
 
-#include "../../Characters/String.h"
-
-#include "InternetAddressRange.h"
 
 /**
  *  \file
  *
- *  \version    <a href="Code-Status.md#Alpha">Alphawi</a>
+ *  \version    <a href="Code-Status.md#Alpha">Alpha</a>
  *
  * TODO:
  */
 
 namespace Stroika::Foundation::IO::Network {
 
-    using Characters::String;
-
+    /**
+     */
     using PortType = uint16_t;
 
     /**
@@ -34,23 +29,23 @@ namespace Stroika::Foundation::IO::Network {
     namespace WellKnownPorts {
 
         namespace TCP {
-            constexpr PortType kSSH            = 22;
-            constexpr PortType kSMPT = 25;
-            constexpr PortType kDNS            = 53;
-            constexpr PortType kHTTP           = 80;
-            constexpr PortType kPOP3           = 110;
-            constexpr PortType kNTP            = 123;
-            constexpr PortType kNETBIOS        = 137;
-            constexpr PortType kSMB            = 139; // NetBIOS Session Service
-            constexpr PortType kHTTPS          = 443;
-            constexpr PortType kMicrosoftDS    = 445; // https://en.wikipedia.org/wiki/Active_Directory, https://en.wikipedia.org/wiki/Server_Message_Block
-            constexpr PortType kModbus         = 502;
-            constexpr PortType kLPD            = 515;  // Line Printer Daemon
-            constexpr PortType kIPP            = 631;  // internet printing protocol
-            constexpr PortType kDNSOverTLS     = 853;  // https://tools.ietf.org/html/rfc7858
-            constexpr PortType kMSSQL          = 1433; // https://en.wikipedia.org/wiki/Microsoft_SQL_Server
-            constexpr PortType kRDP            = 3389;
-            constexpr PortType kSIP            = 5060; // SIP (https://isc.sans.edu/diary/Cyber+Security+Awareness+Month+-+Day+20+-+Ports+5060+%26+5061+-+SIP+%28VoIP%29/7405
+            constexpr PortType kSSH         = 22;
+            constexpr PortType kSMPT        = 25;
+            constexpr PortType kDNS         = 53;
+            constexpr PortType kHTTP        = 80;
+            constexpr PortType kPOP3        = 110;
+            constexpr PortType kNTP         = 123;
+            constexpr PortType kNETBIOS     = 137;
+            constexpr PortType kSMB         = 139; // NetBIOS Session Service
+            constexpr PortType kHTTPS       = 443;
+            constexpr PortType kMicrosoftDS = 445; // https://en.wikipedia.org/wiki/Active_Directory, https://en.wikipedia.org/wiki/Server_Message_Block
+            constexpr PortType kModbus      = 502;
+            constexpr PortType kLPD         = 515;  // Line Printer Daemon
+            constexpr PortType kIPP         = 631;  // internet printing protocol
+            constexpr PortType kDNSOverTLS  = 853;  // https://tools.ietf.org/html/rfc7858
+            constexpr PortType kMSSQL       = 1433; // https://en.wikipedia.org/wiki/Microsoft_SQL_Server
+            constexpr PortType kRDP         = 3389;
+            constexpr PortType kSIP         = 5060; // SIP (https://isc.sans.edu/diary/Cyber+Security+Awareness+Month+-+Day+20+-+Ports+5060+%26+5061+-+SIP+%28VoIP%29/7405
         };
 
     }
