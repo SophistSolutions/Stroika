6be89abecab9b56af60dd509b394b53d2fe96b83
Lewis Pringle <lewis@sophists.com>
2020-11-07 10:54:22 -0500

    Cleanup macros/defines in Debug/Sanitizer.h

--- Library/Sources/Stroika/Foundation/Debug/Sanitizer.h
+++ Library/Sources/Stroika/Foundation/Debug/Sanitizer.h
@@ -76,7 +76,7 @@ namespace Stroika::Foundation::Debug {
 #if __STK_DBG_SAN_HAS_ASAN
     constexpr bool kBuiltWithAddressSanitizer = true;
 #else
-    constexpr bool kBuiltWithAddressSanitizer = false;
+    constexpr bool kBuiltWithAddressSanitizer           = false;
 #endif
 
     /**
@@ -86,7 +86,7 @@ namespace Stroika::Foundation::Debug {
 #if __STK_DBG_SAN_HAS_TSAN
     constexpr bool kBuiltWithThreadSanitizer = true;
 #else
-    constexpr bool kBuiltWithThreadSanitizer  = false;
+    constexpr bool kBuiltWithThreadSanitizer            = false;
 #endif
 
     /**
