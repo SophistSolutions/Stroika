26071ca157f0a85c2d7c520cd898a6848e7ffb08
Lewis Pringle <lewis@sophists.com>
2020-11-07 20:24:27 -0500

    qCompiler_SanitizerDoubleLockWithConditionVariables_Buggy workaround

--- Tests/48/Test.cpp
+++ Tests/48/Test.cpp
@@ -257,6 +257,11 @@ namespace {
 
         void Tests_ ()
         {
+            if constexpr (qCompiler_SanitizerDoubleLockWithConditionVariables_Buggy and Debug::kBuiltWithThreadSanitizer) {
+                //	we get bogus lock inversion / deadlock warning because unlocks not recognized
+				DbgTrace ("Skipping this test cuz double locks cause TSAN to die and cannot be easily suppressed");
+                return;
+            }
             Private_::T1_ ();
         }
     }
