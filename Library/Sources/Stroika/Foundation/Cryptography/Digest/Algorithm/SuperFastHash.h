255ebe87d0c74a430da4c29a4105371b1d296426
Lewis Pringle <lewis@sophists.com>
2021-02-25 20:09:21 -0500

    tweak SuperFastHash algorithm to avoid warning

--- Library/Sources/Stroika/Foundation/Cryptography/Digest/Algorithm/SuperFastHash.h
+++ Library/Sources/Stroika/Foundation/Cryptography/Digest/Algorithm/SuperFastHash.h
@@ -67,7 +67,7 @@ namespace Stroika::Foundation::Cryptography::Digest::Algorithm {
 
     private:
         uint32_t       fHash_{0}; // original algorithm set this to len, but we cannot and be windowed
-        int            fRemainder_{};
+        size_t         fRemainder_{};
         array<byte, 3> fFinalBytes_{};
     };
 
