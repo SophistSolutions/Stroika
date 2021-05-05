a42ff3029d8283a268b881900ae7ee85c13222d2
Lewis Pringle <lewis@sophists.com>
2021-05-05 20:41:08 +0100

    Tweak sqlite sample

--- Samples/SQLite/Sources/Main.cpp
+++ Samples/SQLite/Sources/Main.cpp
@@ -30,17 +30,16 @@ namespace {
         // Example roughly from https://www.tutorialspoint.com/sqlite/sqlite_insert_query.htm
         auto initializeDB = [] (const Connection::Ptr& c) {
             c.Exec (
-                L"CREATE TABLE COMPANY("
+                L"CREATE TABLE DEPARTMENT(ID INT PRIMARY KEY NOT NULL,"
+                L"NAME CHAR (50) NOT NULL"
+                L");");
+            c.Exec (
+                L"CREATE TABLE EMPLOYEES("
                 L"ID INT PRIMARY KEY     NOT NULL,"
                 L"NAME           TEXT    NOT NULL,"
                 L"AGE            INT     NOT NULL,"
                 L"ADDRESS        CHAR(50),"
                 L"SALARY         REAL"
-                L");");
-            c.Exec (
-                L"CREATE TABLE DEPARTMENT(ID INT PRIMARY KEY NOT NULL,"
-                L"DEPT CHAR (50) NOT NULL,"
-                L" EMP_ID INT NOT     NULL"
                 L");");
         };
         auto dbPath = filesystem::current_path () / "testdb.db";
@@ -67,62 +66,67 @@ namespace {
         /*
          ***** INSERT ROWS ****
          */
-        Statement addCompanyStatement{conn, L"INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) values (:ID, :NAME, :AGE, :ADDRESS, :SALARY);"};
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        Statement addDepartment{conn, L"INSERT INTO DEPARTMENT (ID, NAME) values (:ID, :NAME);"};
+        addDepartment.Execute (initializer_list<Statement::ParameterDescription>{
+            {L":ID", 1},
+            {L":NAME", L"Washing machines"},
+        });
+        Statement addEmployeeStatement{conn, L"INSERT INTO EMPLOYEES (ID,NAME,AGE,ADDRESS,SALARY) values (:ID, :NAME, :AGE, :ADDRESS, :SALARY);"};
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 1},
             {L":NAME", L"Paul"},
             {L":AGE", 32},
             {L":ADDRESS", L"California"},
             {L":SALARY", 20000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 2},
             {L":NAME", L"Allen"},
             {L":AGE", 25},
             {L":ADDRESS", L"Texas"},
             {L":SALARY", 15000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 3},
             {L":NAME", L"Teddy"},
             {L":AGE", 23},
             {L":ADDRESS", L"Norway"},
             {L":SALARY", 20000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 4},
             {L":NAME", L"Mark"},
             {L":AGE", 25},
             {L":ADDRESS", L"Rich-Mond"},
             {L":SALARY", 65000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 5},
             {L":NAME", L"David"},
             {L":AGE", 27},
             {L":ADDRESS", L"Texas"},
             {L":SALARY", 85000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 6},
             {L":NAME", L"Kim"},
             {L":AGE", 22},
             {L":ADDRESS", L"South-Hall"},
             {L":SALARY", 45000.00},
         });
-        addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+        addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
             {L":ID", 7},
             {L":NAME", L"James"},
             {L":AGE", 24},
             {L":ADDRESS", L"Houston"},
             {L":SALARY", 10000.00},
         });
-/*
+        /*
          ***** INSERT ROWS (ERROR CHECKING) ****
          */
 #if 0
             // This call will generate a REQUIRE assertion error - terminating your program. Don't violate assertions!
-            addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+            addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
                 {L":BAD-ARGUMENT", 7},
                 {L":NAME", L"James"},
                 {L":AGE", 24},
@@ -132,7 +136,7 @@ namespace {
             AssertNotReached ();
 #endif
         try {
-            addCompanyStatement.Execute (initializer_list<Statement::ParameterDescription>{
+            addEmployeeStatement.Execute (initializer_list<Statement::ParameterDescription>{
                 {L":ID", 7},
                 {L":NAME", L"James"},
                 {L":AGE", 24},
@@ -148,13 +152,13 @@ namespace {
         /*
          ***** SIMPLE QUERIES ****
          */
-        Statement   getAllNames{conn, L"Select NAME from COMPANY;"};
+        Statement   getAllNames{conn, L"Select NAME from EMPLOYEES;"};
         Set<String> allNames = getAllNames.GetAllRows (0).Select<String> ([] (VariantValue v) { return v.As<String> (); }).As<Set<String>> ();
         Assert ((allNames == Set<String>{L"Paul", L"Allen", L"Kim", L"David", L"Mark", L"James", L"Teddy"}));
 
-        Statement               sumAllSalarys{conn, L"select SUM(SALARY) from COMPANY;"};
+        Statement               sumAllSalarys{conn, L"select SUM(SALARY) from EMPLOYEES;"};
         [[maybe_unused]] double sumSalaryUsingSQL = sumAllSalarys.GetAllRows (0)[0].As<double> ();
-        Statement               getAllSalarys{conn, L"select SALARY from COMPANY;"};
+        Statement               getAllSalarys{conn, L"select SALARY from EMPLOYEES;"};
         [[maybe_unused]] double sumSalaryUsingIterableApply = getAllSalarys.GetAllRows (0).Select<double> ([] (VariantValue v) { return v.As<double> (); }).SumValue ();
         Assert (Math::NearlyEquals (sumSalaryUsingSQL, sumSalaryUsingIterableApply));
 #endif
