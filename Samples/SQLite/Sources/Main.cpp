1835a6aafd24c12addfe61c1eb0e1c8137473aa8
Lewis Pringle <lewis@sophists.com>
2021-05-05 16:15:45 +0100

    Silence warnings

--- Samples/SQLite/Sources/Main.cpp
+++ Samples/SQLite/Sources/Main.cpp
@@ -153,9 +153,9 @@ namespace {
         Assert ((allNames == Set<String>{L"Paul", L"Allen", L"Kim", L"David", L"Mark", L"James", L"Teddy"}));
 
         Statement sumAllSalarys{conn, L"select SUM(SALARY) from COMPANY;"};
-        double    sumSalaryUsingSQL = sumAllSalarys.GetAllRows (0)[0].As<double> ();
+        [[maybe_unused]]double    sumSalaryUsingSQL = sumAllSalarys.GetAllRows (0)[0].As<double> ();
         Statement getAllSalarys{conn, L"select SALARY from COMPANY;"};
-        double    sumSalaryUsingIterableApply = getAllSalarys.GetAllRows (0).Select<double> ([] (VariantValue v) { return v.As<double> (); }).SumValue ();
+        [[maybe_unused]] double   sumSalaryUsingIterableApply = getAllSalarys.GetAllRows (0).Select<double> ([] (VariantValue v) { return v.As<double> (); }).SumValue ();
         Assert (Math::NearlyEquals (sumSalaryUsingSQL, sumSalaryUsingIterableApply));
 #endif
     }
