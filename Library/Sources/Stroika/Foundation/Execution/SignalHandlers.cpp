d160a8739a8609d15903326fce9a351f1874ba71
Lewis Pringle <lewis@sophists.com>
2021-06-07 02:20:32 +0100

    vs2k17 bwa

--- Library/Sources/Stroika/Foundation/Execution/SignalHandlers.cpp
+++ Library/Sources/Stroika/Foundation/Execution/SignalHandlers.cpp
@@ -323,7 +323,7 @@ SignalHandlerRegistry::~SignalHandlerRegistry ()
 #if __cpp_lib_atomic_shared_ptr >= 201711
     Assert (SafeSignalsManager::sTheRep_.load () == nullptr); // must be cleared first
 #else
-    Assert (atomic_load (&SafeSignalsManager::sTheRep_) == nullptr); // must be cleared first
+    Assert (atomic_load (&SafeSignalsManager::sTheRep_) == nullptr);                         // must be cleared first
 #endif
 }
 
@@ -395,7 +395,7 @@ void SignalHandlerRegistry::SetSignalHandlers (SignalID signal, const Set<Signal
     }
 
     auto sigSetHandler = [] (SignalID signal, [[maybe_unused]] void (*fun) (int)) {
-#if qCompilerAndStdLib_maybe_unused_b4_auto_in_for_loop_Buggy
+#if qCompilerAndStdLib_maybe_unused_in_lambda_ignored_Buggy
         &fun;
 #endif
 #if qPlatform_POSIX
