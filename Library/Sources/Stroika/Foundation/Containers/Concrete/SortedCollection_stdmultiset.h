251a336b0a95e8cad9d2dde174987365f8f21eb7
Lewis Pringle <lewis@sophists.com>
2021-12-08 22:23:08 -0500

    fixed typo

--- Library/Sources/Stroika/Foundation/Containers/Concrete/SortedCollection_stdmultiset.h
+++ Library/Sources/Stroika/Foundation/Containers/Concrete/SortedCollection_stdmultiset.h
@@ -35,6 +35,8 @@ namespace Stroika::Foundation::Containers::Concrete {
         using inherited = SortedCollection<T>;
 
     public:
+        template <typename POTENTIALLY_ADDABLE_T>
+        static constexpr bool IsAddable_v = inherited::template IsAddable_v<POTENTIALLY_ADDABLE_T>;
         using value_type          = typename inherited::value_type;
         using InOrderComparerType = typename SortedCollection<T>::InOrderComparerType;
 
