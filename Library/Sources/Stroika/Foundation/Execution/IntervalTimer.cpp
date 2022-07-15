1ce5c0eef427d258067366a91af35ab1fd4462bb
Lewis Pringle <lewis@sophists.com>
2022-07-15 05:07:04 -0400

    First draft implementation of Execution/IntervalTimer

---
+++ Library/Sources/Stroika/Foundation/Execution/IntervalTimer.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2022.  All rights reserved
+ */
+#include "../StroikaPreComp.h"
+
+#include "../Containers/Collection.h"
+#include "../Time/Realtime.h"
+
+#include "Synchronized.h"
+#include "Thread.h"
+
+#include "IntervalTimer.h"
+
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Containers;
+using namespace Stroika::Foundation::Execution;
+using namespace Stroika::Foundation::Time;
+
+/*
+ ********************************************************************************
+ ******************** IntervalTimer::Manager::DefaultRep ************************
+ ********************************************************************************
+ */
+struct IntervalTimer::Manager::DefaultRep ::Rep_ {
+    Rep_ ()
+    {
+    }
+    void AddOneShot (const TimerCallback& intervalTimer, const Time::Duration& when)
+    {
+        auto lk = fData_.rwget ();
+        lk->Add (Elt_{intervalTimer, Time::GetTickCount () + when.As<DurationSecondsType> ()});
+        DataChanged_ ();
+    }
+    void AddRepeating (const TimerCallback& intervalTimer, const Time::Duration& repeatInterval, const optional<Time::Duration>& histeresis)
+    {
+        auto lk = fData_.rwget ();
+        lk->Add (Elt_{intervalTimer, Time::GetTickCount () + repeatInterval.As<DurationSecondsType> (), repeatInterval, histeresis});
+        DataChanged_ ();
+    }
+    void RemoveRepeating (const TimerCallback& intervalTimer) noexcept
+    {
+        auto lk = fData_.rwget ();
+        Verify (lk->RemoveIf ([&] (const Elt_& i) { return i.fCallback == intervalTimer; }));
+        DataChanged_ ();
+    }
+
+    struct Elt_ {
+        Function<void (void)> fCallback;
+        DurationSecondsType   fCallNextAt;
+        optional<Duration>    fFrequency;
+        optional<Duration>    fHisteresis;
+    };
+    // @todo - re-implement using priority q, with next time at top of q
+    Synchronized<Collection<Elt_>>   fData_;
+    Synchronized<Thread::CleanupPtr> sThread_{Execution::Thread::CleanupPtr::eAbortBeforeWaiting};
+    WaitableEvent                    fDataChanged_{WaitableEvent::ResetType::eAutoReset};
+
+    // this is where a priorityq would be better
+    DurationSecondsType GetNextWakeupTime_ ()
+    {
+        auto dataLock = fData_.cget ();
+        Assert (not dataLock->empty ());
+        DurationSecondsType r = kInfinite;
+        for (Elt_ i : dataLock.cref ()) {
+            r = min (r, i.fCallNextAt);
+        }
+        return r;
+    }
+    void RunnerLoop_ ()
+    {
+        // keep checking for timer events to run
+        while (true) {
+            DurationSecondsType wakeupAt = GetNextWakeupTime_ ();
+            fDataChanged_.WaitUntilQuietly (wakeupAt);
+            // now process any timer events that are ready (could easily be more than one).
+            // if we had a priority q, we would do them in order, but for now, just do all that are ready
+            // NOTE - to avoid holding a lock (in case these guys remove themselves or whatever) - lock/run through list twice
+            DurationSecondsType now      = Time::GetTickCount ();
+            Collection<Elt_>    elts2Run = fData_.cget ()->Where ([=] (const Elt_& i) { return i.fCallNextAt <= now; });
+            for (Elt_ i : elts2Run) {
+                IgnoreExceptionsExceptThreadAbortForCall (i.fCallback ());
+            }
+            // now reset the 'next' time for each run element
+            auto rwDataLock = fData_.rwget ();
+            now             = Time::GetTickCount ();
+            for (Iterator<Elt_> i = rwDataLock->begin (); i != rwDataLock->end (); ++i) {
+                if (i->fFrequency.has_value ()) {
+                    Elt_ newE = *i;
+                    // @todo add histeresis
+                    newE.fCallNextAt = now + i->fFrequency->As<DurationSecondsType> ();
+                }
+                else {
+                    rwDataLock->Remove (i, &i);
+                }
+            }
+        }
+    }
+    void DataChanged_ ()
+    {
+        auto rwThreadLk = sThread_.rwget ();
+        if (fData_.cget ()->empty ()) {
+            rwThreadLk->operator= (nullptr);
+        }
+        else {
+            sThread_.rwget ()->operator= (Thread::New ([this] () {
+                RunnerLoop_ ();
+            }));
+        }
+    }
+};
+
+IntervalTimer::Manager::DefaultRep::DefaultRep ()
+    : fHiddenRep_{make_shared<Rep_> ()}
+{
+}
+void IntervalTimer::Manager::DefaultRep::AddOneShot (const TimerCallback& intervalTimer, const Time::Duration& when)
+{
+    fHiddenRep_->AddOneShot (intervalTimer, when);
+}
+
+void IntervalTimer::Manager::DefaultRep::AddRepeating (const TimerCallback& intervalTimer, const Time::Duration& repeatInterval, const optional<Time::Duration>& histeresis)
+{
+    fHiddenRep_->AddRepeating (intervalTimer, repeatInterval, histeresis);
+}
+
+void IntervalTimer::Manager::DefaultRep::RemoveRepeating (const TimerCallback& intervalTimer) noexcept
+{
+    fHiddenRep_->RemoveRepeating (intervalTimer);
+}
---
+++ Library/Sources/Stroika/Foundation/Execution/IntervalTimer.inl
@@ -0,0 +1,60 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2022.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Execution_IntervalTimer_inl_
+#define _Stroika_Foundation_Execution_IntervalTimer_inl_ 1
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+namespace Stroika::Foundation::Execution {
+
+    /*
+     ********************************************************************************
+     *************************** IntervalTimer::Manager *****************************
+     ********************************************************************************
+     */
+    inline IntervalTimer::Manager::Manager (const shared_ptr<IRep>& rep)
+        : fRep_{rep}
+    {
+        RequireNotNull (rep);
+    }
+    inline void IntervalTimer::Manager::AddOneShot (const TimerCallback& intervalTimer, const Time::Duration& when)
+    {
+        fRep_->AddOneShot (intervalTimer, when);
+    }
+    inline void IntervalTimer::Manager ::AddRepeating (const TimerCallback& intervalTimer, const Time::Duration& repeatInterval, const optional<Time::Duration>& histeresis)
+    {
+        fRep_->AddRepeating (intervalTimer, repeatInterval, histeresis);
+    }
+    void IntervalTimer::Manager::RemoveRepeating (const TimerCallback& intervalTimer) noexcept
+    {
+        fRep_->RemoveRepeating (intervalTimer);
+    }
+
+    /*
+     ********************************************************************************
+     ***************************** IntervalTimer::Adder *****************************
+     ********************************************************************************
+     */
+    inline IntervalTimer::Adder::Adder (IntervalTimer::Manager& manager, const Function<void (void)>& f, const Time::Duration& repeatInterval, const optional<Time::Duration>& histeresis)
+        : fManager_{manager}
+        , fFunction_{f}
+    {
+        Manager::sThe.AddRepeating (fFunction_, repeatInterval, histeresis);
+    }
+    inline IntervalTimer::Adder::Adder (const Function<void (void)>& f, const Time::Duration& repeatInterval, const optional<Time::Duration>& histeresis)
+        : Adder{Manager::sThe, f, repeatInterval, histeresis}
+    {
+    }
+    inline IntervalTimer::Adder::~Adder ()
+    {
+        Manager::sThe.RemoveRepeating (fFunction_);
+    }
+
+}
+
+#endif /*_Stroika_Foundation_Execution_IntervalTimer_inl_*/
