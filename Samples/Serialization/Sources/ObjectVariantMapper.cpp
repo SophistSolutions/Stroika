8c390b845723ee5c3142bf312d3a5286f9aafdcb
Lewis Pringle <lewis@sophists.com>
2022-11-17 10:28:42 -0500

    further simplify sample

--- Samples/Serialization/Sources/ObjectVariantMapper.cpp
+++ Samples/Serialization/Sources/ObjectVariantMapper.cpp
@@ -117,7 +117,8 @@ namespace {
             bool newEnabled = true;
             tmp.fEnabled    = newEnabled;
             tmp.fThisPHRsIDToSharedContactID.Add (L"A", L"B");
-            tmp.fLastSynchronizedAt = DateTime{Time::Date{Time::Year{1998}, Time::April, Time::day{11}}, Time::TimeOfDay::Parse (L"3pm", locale::classic ())};
+            tmp.fLastSynchronizedAt = DateTime{1998y/Time::April/11d, Time::TimeOfDay::Parse (L"3pm", locale::classic ())
+        };
         }
 
         /// Map that object to a VariantValue
