61d2a1cf55b4dea28024154628d1aa0a306f2f7c
Lewis Pringle <lewis@sophists.com>
2022-11-28 08:05:08 -0500

    use sequence of numbers rather than random number for Execution::Function Execution::Private_::FunctionObjectOrderingType_ since faster, and more reliable

--- Library/Sources/Stroika/Foundation/Execution/Function.cpp
+++ Library/Sources/Stroika/Foundation/Execution/Function.cpp
@@ -27,16 +27,16 @@ Characters::String Execution::Private_::ToString_ (Private_::FunctionObjectOrder
 Execution::Private_::FunctionObjectOrderingType_ Execution::Private_::mkFunctionObjectOrderingType_ (const void* p)
 {
     if constexpr (is_same_v<Execution::Private_::FunctionObjectOrderingType_, const void*>) {
-        return FunctionObjectOrderingType_ (p);             // cannot use {} if type differs error
+        return FunctionObjectOrderingType_ (p); // cannot use {} if type differs error
     }
     if constexpr (is_same_v<Execution::Private_::FunctionObjectOrderingType_, uint32_t>) {
         static atomic<uint32_t> sNextPtr_{1};
-        return FunctionObjectOrderingType_ (++sNextPtr_);   // cannot use {} if type differs error
-        #if 0
+        return FunctionObjectOrderingType_ (++sNextPtr_); // cannot use {} if type differs error
+#if 0
         random_device                      rd;
         mt19937                            gen{rd ()};
         uniform_int_distribution<uint32_t> dis{numeric_limits<uint32_t>::min (), numeric_limits<uint32_t>::max ()};
         return FunctionObjectOrderingType_ (dis (gen)); // cannot use {} if type differs error
-        #endif
+#endif
     }
 }
