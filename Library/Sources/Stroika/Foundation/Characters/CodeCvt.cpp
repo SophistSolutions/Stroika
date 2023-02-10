af8465ab7c9c694e1092a159d86f74ac09cfcf18
Lewis Pringle <lewis@sophists.com>
2023-02-09 22:29:27 -0500

    completed first draft of Characters::CodeCvt (wrapper on std::code_cvt) - PITA - crazy non-movable/non-deletable, pita to use; still untested

--- Library/Sources/Stroika/Foundation/Characters/CodeCvt.cpp
+++ Library/Sources/Stroika/Foundation/Characters/CodeCvt.cpp
@@ -15,7 +15,7 @@ using namespace Stroika::Foundation::Characters;
 namespace {
     // crazy - from https://en.cppreference.com/w/cpp/locale/codecvt
     template <typename FACET>
-    struct deletable_facet_ final : FACET {
+    struct deletable_facet_  : FACET {
         template <typename... Args>
         deletable_facet_ (Args&&... args)
             : FACET{forward<Args> (args)...}
@@ -28,22 +28,21 @@ namespace {
 namespace {
     using std::byte;
 
-    template <typename CHAR_T, typename STD_CODE_CVT_T = deletable_facet_<codecvt<CHAR_T, char8_t, std::mbstate_t>>>
-    struct CodeCvt_BasicUnicodeRep_CHAR_T : CodeCvt<CHAR_T>::IRep {
-        STD_CODE_CVT_T fCodeCvt_;
+    /*
+     *  This is crazy complicated because codecvt goes out of its way to be hard to copy, hard to move, but with
+     *  a little care, can be made to work with unique_ptr.
+     */
+    template <typename STD_CODE_CVT_T, typename CHAR_T = typename STD_CODE_CVT_T::intern_type>
+    struct CodeCvt_WrapStdCodeCvt_CHAR_T : CodeCvt<CHAR_T>::IRep {
+        unique_ptr < STD_CODE_CVT_T> fCodeCvt_;
         using result      = typename CodeCvt<CHAR_T>::result;
         using MBState     = typename CodeCvt<CHAR_T>::MBState;
         using extern_type = typename STD_CODE_CVT_T::extern_type;
 
-        CodeCvt_BasicUnicodeRep_CHAR_T ()
-            : fCodeCvt_{}
+        CodeCvt_WrapStdCodeCvt_CHAR_T (unique_ptr<STD_CODE_CVT_T>&& codeCvt)
+            : fCodeCvt_{move (codeCvt)}
         {
         }
-        CodeCvt_BasicUnicodeRep_CHAR_T (STD_CODE_CVT_T&& codeCtt)
-            : fCodeCvt_{move<STD_CODE_CVT_T> (codeCtt)}
-        {
-        }
-
         virtual result in (MBState* state, span<const byte>* from, span<CHAR_T>* to) const override
         {
             const extern_type* _First1 = reinterpret_cast<const extern_type*> (from->data ());
@@ -52,7 +51,7 @@ namespace {
             CHAR_T*            _First2 = to->data ();
             CHAR_T*            _Last2  = _First2 + to->size ();
             CHAR_T*            _Mid2   = _First2; // DOUBLE CHECK SPEC - NOT SURE IF THIS IS USED ON INPUT
-            auto               r       = fCodeCvt_.in (*state, _First1, _Last1, _Mid1, _First2, _Last2, _Mid2);
+            auto               r       = fCodeCvt_->in (*state, _First1, _Last1, _Mid1, _First2, _Last2, _Mid2);
             *from                      = from->subspan (_Mid1 - _First1);  // point to remaining to use data - typically none
             *to                        = to->subspan (0, _Mid2 - _First2); // point ACTUAL copied data
             return r;
@@ -65,7 +64,7 @@ namespace {
             extern_type*  _First2 = reinterpret_cast<extern_type*> (to->data ());
             extern_type*  _Last2  = _First2 + to->size ();
             extern_type*  _Mid2   = _First2; // DOUBLE CHECK SPEC - NOT SURE IF THIS IS USED ON INPUT
-            auto          r       = fCodeCvt_.out (*state, _First1, _Last1, _Mid1, _First2, _Last2, _Mid2);
+            auto          r       = fCodeCvt_->out (*state, _First1, _Last1, _Mid1, _First2, _Last2, _Mid2);
             *from                 = from->subspan (_Mid1 - _First1);  // point to remaining to use data - typically none
             *to                   = to->subspan (0, _Mid2 - _First2); // point ACTUAL copied data
             return r;
@@ -82,27 +81,24 @@ namespace {
 template <>
 shared_ptr<CodeCvt<char16_t>::IRep> Characters::Private_::mk_UNICODECodeCvt_<char16_t> ()
 {
-    return Memory::MakeSharedPtr<CodeCvt_BasicUnicodeRep_CHAR_T<char16_t>> ();
+    using CODE_CVT = codecvt<char16_t, char8_t, std::mbstate_t>;
+    using D_CODE_CVT = deletable_facet_<CODE_CVT>;
+    using BASIC_UNICODE_WRAPPER = CodeCvt_WrapStdCodeCvt_CHAR_T<D_CODE_CVT>;
+    return Memory::MakeSharedPtr<BASIC_UNICODE_WRAPPER> (make_unique<D_CODE_CVT> ());
 }
 template <>
 shared_ptr<CodeCvt<char32_t>::IRep> Characters::Private_::mk_UNICODECodeCvt_<char32_t> ()
 {
-    return Memory::MakeSharedPtr<CodeCvt_BasicUnicodeRep_CHAR_T<char32_t>> ();
+    using CODE_CVT              = codecvt<char32_t, char8_t, std::mbstate_t>;
+    using D_CODE_CVT            = deletable_facet_<CODE_CVT>;
+    using BASIC_UNICODE_WRAPPER = CodeCvt_WrapStdCodeCvt_CHAR_T<D_CODE_CVT>;
+    return Memory::MakeSharedPtr<BASIC_UNICODE_WRAPPER> (make_unique<D_CODE_CVT> ());
 }
 template <>
 shared_ptr<CodeCvt<wchar_t>::IRep> Characters::Private_::mk_UNICODECodeCvt_<wchar_t> (const locale& l)
 {
-    // next todo is add COPY CTOR given codecvt facet, and extract from locale and wrap in this class
-    // CodeCvt_BasicUnicodeRep_CHAR_T
-
-    using Destructible_codecvt_byname = deletable_facet_<codecvt_byname<wchar_t, char, mbstate_t>>;
-    Destructible_codecvt_byname cvt{l.name ()}; // only works to wchar_t.
-
-    Destructible_codecvt_byname&& a = move (cvt);
-
-    //CodeCvt_BasicUnicodeRep_CHAR_T<wchar_t, Destructible_codecvt_byname> aa{cvt};   // this is what I need to get to compile
-    return nullptr;
-    // debug why this fails and do that instead of above...
-    // todo check if sizes match and if so cast, and otherwise need adapter from 32_t
-    // return Memory::MakeSharedPtr<CodeCvt_BasicUnicodeRep_CHAR_T<wchar_t, Destructible_codecvt_byname>> (move (cvt));
+    using CODE_CVT              = codecvt_byname<wchar_t, char, mbstate_t>;
+    using D_CODE_CVT            = deletable_facet_<CODE_CVT>;
+    using BASIC_UNICODE_WRAPPER = CodeCvt_WrapStdCodeCvt_CHAR_T<D_CODE_CVT>;
+    return Memory::MakeSharedPtr<BASIC_UNICODE_WRAPPER> (make_unique<D_CODE_CVT> (l.name ()));
 }
