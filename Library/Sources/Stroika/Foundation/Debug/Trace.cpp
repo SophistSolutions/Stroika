b1f5b46030d7039f19e892ac30299f60f134ce43
Lewis Pringle <lewis@sophists.com>
2023-03-25 11:34:45 -0400

    also emit flags kBuiltWithThreadSanitizer kBuiltWithUndefinedBehaviorSanitizer in Trace startup

--- Library/Sources/Stroika/Foundation/Debug/Trace.cpp
+++ Library/Sources/Stroika/Foundation/Debug/Trace.cpp
@@ -169,6 +169,8 @@ void Debug::Private_::EmitFirstTime (Emitter& emitter)
     }
     emitter.EmitTraceMessage (L"<debug-state {>");
     emitter.EmitTraceMessage (L"  Debug::kBuiltWithAddressSanitizer = %s", Characters::ToString (Debug::kBuiltWithAddressSanitizer).c_str ());
+    emitter.EmitTraceMessage (L"  Debug::kBuiltWithThreadSanitizer = %s", Characters::ToString (Debug::kBuiltWithThreadSanitizer).c_str ());
+    emitter.EmitTraceMessage (L"  Debug::kBuiltWithUndefinedBehaviorSanitizer = %s", Characters::ToString (Debug::kBuiltWithUndefinedBehaviorSanitizer).c_str ());
     emitter.EmitTraceMessage (L"  Debug::IsRunningUnderValgrind () = %s", Characters::ToString (Debug::IsRunningUnderValgrind ()).c_str ());
     emitter.EmitTraceMessage (L"</debug-state>");
 }
