e8f2222e9b8c9e40bc1e997847c5060d9620ee6e
Lewis Pringle <lewis@sophists.com>
2023-04-17 12:23:14 -0400

    docs

--- Library/Sources/Stroika/Foundation/Containers/Factory/Association_Factory.h
+++ Library/Sources/Stroika/Foundation/Containers/Factory/Association_Factory.h
@@ -21,7 +21,7 @@ namespace Stroika::Foundation::Containers {
 namespace Stroika::Foundation::Containers::Factory {
 
     /**
-     *  \brief   Singleton factory object - Used to create the default backend implementation of a Association<> container
+     *  \brief   Singleton factory object - Used to create the default backend implementation of a Association<> container; typically not called directly.
      *
      *  Note - you can override the underlying factory dynamically by calling Association_Factory<T,TRAITS>::Register (), or
      *  replace it statically by template-specializing Association_Factory<T,TRAITS>::New () - though the later is trickier.
