16c7430469e96e1c267eac4988c5c5a9cb05b782
Lewis Pringle <lewis@sophists.com>
2023-05-23 16:45:43 -0400

    test possible xcode bwa

--- Library/Sources/Stroika/Foundation/Execution/Function.h
+++ Library/Sources/Stroika/Foundation/Execution/Function.h
@@ -92,7 +92,8 @@ namespace Stroika::Foundation::Execution {
         Function (Function&&)      = default;
         template <typename CTOR_FUNC_SIG>
         Function (CTOR_FUNC_SIG&& f)
-            requires (is_convertible_v<CTOR_FUNC_SIG, function<FUNCTION_SIGNATURE>> and
+            requires (
+                //tmphack to test macos - is_convertible_v<CTOR_FUNC_SIG, function<FUNCTION_SIGNATURE>> and
                       not is_base_of_v<Function<FUNCTION_SIGNATURE>, remove_cvref_t<CTOR_FUNC_SIG>>)
 #if qCompilerAndStdLib_RequiresNotMatchInlineOutOfLineForTemplateClassBeingDefined_Buggy
             : fFun_{forward<CTOR_FUNC_SIG> (f)}
