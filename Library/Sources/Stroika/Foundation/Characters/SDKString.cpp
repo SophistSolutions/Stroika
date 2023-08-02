fab9b6e9e5de6ef47110d1ee5230173e3f935c99
Lewis Pringle <lewis@sophists.com>
2023-08-01 22:28:10 -0400

    bad checkin fixed missing file

---
+++ Library/Sources/Stroika/Foundation/Characters/SDKString.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2023.  All rights reserved
+ */
+#include "../StroikaPreComp.h"
+
+#include "CodeCvt.h"
+
+#include "SDKString.h"
+
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Characters;
+
+
+/*
+ ********************************************************************************
+ ******************************* Characters::SDKString2Wide *********************
+ ********************************************************************************
+ */
+
+
+#if !qPlatform_Windows
+ wstring SDKString2Wide (const SDKString& s)
+{
+#if qPlatform_MacOS
+    return CodeCvt<wchar_t>{UnicodeExternalEncodings::eUTF8}.Bytes2String<wstring> (Memory::SpanReInterpretCast<const byte> (span{s}));
+#else
+    return CodeCvt<wchar_t>{locale{}}.Bytes2String<wstring> (Memory::SpanReInterpretCast<const byte>  (span{s}));
+#endif
+}
+#endif
+
+/*
+ ********************************************************************************
+ ******************************* Characters::WideString2SDK *********************
+ ********************************************************************************
+ */
+#if !qPlatform_Windows
+SDKString WideString2SDK (const wstring& s)
+{
+#if qPlatform_MacOS
+    return CodeCvt<wchar_t>{UnicodeExternalEncodings::eUTF8}.String2Bytes<SDKString> (span{s});
+#else
+    return CodeCvt<wchar_t>{locale{}}.String2Bytes<SDKString> (span{s});
+#endif
+}
+#endif
