61700cf5f4e1a149682af7a9b5bc3e905ddcf19f
Lewis Pringle <lewis@sophists.com>
2023-10-17 23:51:24 -0400

    cosmetic

--- Library/Sources/Stroika/Foundation/Execution/WaitableEvent.cpp
+++ Library/Sources/Stroika/Foundation/Execution/WaitableEvent.cpp
@@ -63,7 +63,7 @@ auto WaitableEvent::WE_::WaitUntilQuietly (Time::DurationSecondsType timeoutAt) 
     // https://stroika.atlassian.net/browse/STK-993
     constexpr bool kMagicWorkaroundMaybe_ = true;
     if (kMagicWorkaroundMaybe_) {
-         std::atomic_thread_fence(std::memory_order::seq_cst);
+        std::atomic_thread_fence (std::memory_order::seq_cst);
         if (fTriggered) {
             return WaitStatus::eTriggered;
         }
