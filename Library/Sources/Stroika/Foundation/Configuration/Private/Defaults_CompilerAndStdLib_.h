44aaa082a96ee143d98c3448ad07bcbd90d170b0
Lewis G. Pringle, Jr <lewis@sophists.com>
2023-10-19 20:02:45 -0400

    just check __GNUC__ not __GNUC_MINOR__ in compiler bug dversion detector

--- Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Configuration/Private/Defaults_CompilerAndStdLib_.h
@@ -94,7 +94,7 @@
 #define _STROIKA_CONFIGURATION_WARNING_                                                                                                    \
     "Warning: Stroika v3 does not support versions prior to GCC 11 (v2.1 supports g++7 and later, v2.0 supports g++5 and g++6 and g++-7)"
 #endif
-#if __GNUC__ > 13 || (__GNUC__ == 13 && (__GNUC_MINOR__ > 1))
+#if __GNUC__ > 13
 #define _STROIKA_CONFIGURATION_WARNING_ "Info: Stroika untested with this version of GCC - USING PREVIOUS COMPILER VERSION BUG DEFINES"
 #define CompilerAndStdLib_AssumeBuggyIfNewerCheck_(X) 1
 #endif
