e89cb29c23c5951ec0ed6a8f09c22e95bf0fc676
Lewis Pringle <lewis@sophists.com>
2023-10-28 21:51:26 -0400

    silcence gcc warning

--- Library/Sources/Stroika/Foundation/Memory/Common.h
+++ Library/Sources/Stroika/Foundation/Memory/Common.h
@@ -172,12 +172,14 @@ namespace Stroika::Foundation::Memory {
     template <class T>
     inline T byteswap (T n) noexcept
     {
+        DISABLE_COMPILER_GCC_WARNING_START ("GCC diagnostic ignored \"-Warray-bounds=\"");
         static_assert (std::has_unique_object_representations_v<T>, "T may not have padding bits");
         auto value_representation = bit_cast<array<byte, sizeof (T)>> (n);
         for (size_t i = 0; i < value_representation.size () / 2; ++i) {
             swap (value_representation[i], value_representation[value_representation.size () - i]);
         }
         return bit_cast<T> (value_representation);
+        DISABLE_COMPILER_GCC_WARNING_END ("GCC diagnostic ignored \"-Warray-bounds=\"");
     }
 #endif
 }
