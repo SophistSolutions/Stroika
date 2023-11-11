0c9a99425048ced365c1ceb68a4fb218f4e5de58
Lewis Pringle <lewis@sophists.com>
2023-11-11 07:20:46 -0500

    remove unneeded CPP file from foundation

--- Library/Sources/Stroika/Foundation/Math/LinearAlgebra/Matrix.cpp
+++
@@ -1,8 +0,0 @@
-/*
-* Copyright(c) Sophist Solutions, Inc. 1990-2023.  All rights reserved
-*/
-#include "../../StroikaPreComp.h"
-
-#include "Matrix.h"
-
-using namespace Stroika::Foundation;
--- Library/Sources/Stroika/Foundation/Math/LinearAlgebra/Vector.cpp
+++
@@ -1,8 +0,0 @@
-/*
-* Copyright(c) Sophist Solutions, Inc. 1990-2023.  All rights reserved
-*/
-#include "../../StroikaPreComp.h"
-
-#include "Vector.h"
-
-using namespace Stroika::Foundation;
