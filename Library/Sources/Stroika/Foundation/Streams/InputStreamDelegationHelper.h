31f3509236c133834f1871e9cd9d5e6ab64eac53
Lewis Pringle <lewis@sophists.com>
2024-01-03 12:01:14 -0500

    Added InputStreamDelegationHelper helper

---
+++ Library/Sources/Stroika/Foundation/Streams/InputStreamDelegationHelper.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2023.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Streams_InputStreamDelegationHelper_h_
+#define _Stroika_Foundation_Streams_InputStreamDelegationHelper_h_ 1
+
+#include "../StroikaPreComp.h"
+
+#include "InputStream.h"
+
+/**
+ *  \file
+ *
+ *  \version    <a href="Code-Status.md#Alpha-Late">Alpha-Late</a>
+ */
+
+namespace Stroika::Foundation::Streams {
+
+    /**
+     *  This does nothing useful, in and of itself: it just takes an input stream and wraps it into another input stream.
+     *  But it provides a useful helper to reduce typing when you want to 'wrap'/'hook' an existing input stream and just react
+     *  to a few things (like progress/reads).
+     */
+    template <typename ELEMENT_TYPE>
+    struct InputStreamDelegationHelper : public InputStream::IRep<ELEMENT_TYPE> {
+        InputStreamDelegationHelper (const typename InputStream::Ptr<ELEMENT_TYPE>& realIn)
+            : fRealIn{realIn}
+        {
+            RequireNotNull (realIn);    // no point in a wrapping a null pointer!
+        }
+        // Stream::IRep
+        virtual bool IsSeekable () const override
+        {
+            return fRealIn.IsSeekable ();
+        }
+        // InputStream::IRep
+        virtual void CloseRead () override
+        {
+            fRealIn.Close ();
+        }
+        virtual bool IsOpenRead () const override
+        {
+            fRealIn.IsOpenRead ();
+        }
+        virtual SeekOffsetType GetReadOffset () const override
+        {
+            return fRealIn.GetOffset ();
+        }
+        virtual SeekOffsetType SeekRead (Whence whence, SignedSeekOffsetType offset)
+        {
+            return fRealIn.SeekRead (whence, offset);
+        }
+        virtual optional<size_t> AvailableToRead () override
+        {
+            return fRealIn.AvailableToRead ();
+        }
+        virtual optional<span<ELEMENT_TYPE>> Read (span<ELEMENT_TYPE> intoBuffer, NoDataAvailableHandling blockFlag) override
+        {
+            return fRealIn.Read (intoBuffer, blockFlag);
+        }
+        typename InputStream::Ptr<ELEMENT_TYPE> fRealIn;
+    };
+
+}
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+#include "InputStreamDelegationHelper.inl"
+
+#endif /*_Stroika_Foundation_Streams_InputStreamDelegationHelper_h_*/
---
+++ Library/Sources/Stroika/Foundation/Streams/InputStreamDelegationHelper.inl
@@ -0,0 +1,17 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2023.  All rights reserved
+ */
+#ifndef _Stroika_Foundation_Streams_InputStreamDelegationHelper_inl_
+#define _Stroika_Foundation_Streams_InputStreamDelegationHelper_inl_ 1
+
+/*
+ ********************************************************************************
+ ***************************** Implementation Details ***************************
+ ********************************************************************************
+ */
+
+namespace Stroika::Foundation::Streams {
+
+}
+
+#endif /*_Stroika_Foundation_Streams_InputStreamDelegationHelper_inl_*/
