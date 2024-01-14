d50e09dbb59243cb58c99c3fffeb2166eb5038ad
Lewis G. Pringle, Jr <lewis@sophists.com>
2024-01-14 10:04:52 -0500

    cleanup qStroika_Foundation_DataExchange_XML_Supportxxx flags

--- Library/Sources/Stroika/Foundation/DataExchange/XML/Common.h
+++ Library/Sources/Stroika/Foundation/DataExchange/XML/Common.h
@@ -15,31 +15,21 @@
 namespace Stroika::Foundation::DataExchange::XML {
 
 /**
- *  As of Stroika v3.0d4, we only support XML DOM if Xerces is builtin (could use libxml2 as well in the future).
  */
 #ifndef qStroika_Foundation_DataExchange_XML_SupportDOM
-#define qStroika_Foundation_DataExchange_XML_SupportDOM qHasFeature_Xerces
+#define qStroika_Foundation_DataExchange_XML_SupportDOM qHasFeature_Xerces or qFeature_HasFeature_libxml2
 #endif
 
 /**
- *  As of Stroika v3.0d4, we only support XML Schema if Xerces is builtin (could use libxml2 as well in the future).
  */
 #ifndef qStroika_Foundation_DataExchange_XML_SupportSchema
 #define qStroika_Foundation_DataExchange_XML_SupportSchema qHasFeature_Xerces or qFeature_HasFeature_libxml2
 #endif
 
 /**
- *  As of Stroika v3.0d4, we only support XML parsing if Xerces is builtin (could use libxml2 as well in the future).
  */
 #ifndef qStroika_Foundation_DataExchange_XML_SupportParsing
-#define qStroika_Foundation_DataExchange_XML_SupportParsing qHasFeature_Xerces
-#endif
-
-/**
- *  As of Stroika v3.0d4, we don't support XPath, but could re-include via XQilla (or less likely xalan or libxml2 in the future).
- */
-#ifndef qStroika_Foundation_DataExchange_XML_SupportXPath
-#define qStroika_Foundation_DataExchange_XML_SupportXPath 0
+#define qStroika_Foundation_DataExchange_XML_SupportParsing qHasFeature_Xerces or qFeature_HasFeature_libxml2
 #endif
 
 /**
