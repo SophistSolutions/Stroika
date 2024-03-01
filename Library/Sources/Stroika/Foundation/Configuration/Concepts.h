a426422d291ed25bada68597fb75ddc8426eabbf
Lewis G. Pringle, Jr <lewis@sophists.com>
2024-02-29 21:05:47 -0500

    Added IStdOptional and ExtractStdOptionalOf_t utilities/concepts

--- Library/Sources/Stroika/Foundation/Configuration/Concepts.h
+++ Library/Sources/Stroika/Foundation/Configuration/Concepts.h
@@ -111,6 +111,19 @@ namespace Stroika::Foundation::Configuration {
     template <typename...>
     using True = true_type;
 
+
+     template <typename OT>
+    concept IStdOptional = 
+        same_as<OT,std::optional<typename OT::value_type>>;
+        static_assert(same_as<int,std::optional<int>::value_type>);
+        static_assert(same_as<std::optional<int>,std::optional<std::optional<int>::value_type>>);
+   static_assert(IStdOptional<std::optional<int>>);
+    static_assert(not IStdOptional<int>);
+
+ template < typename OT>
+    using ExtractStdOptionalOf_t = typename std::optional<OT>::value_type  ;
+
+
     /**
      * Concepts let you construct a 'template' of one arg from one with two args, but class, and variable templates don't allow
      * this; but this magic trick of double indirection does allow it. And cannot use concepts as template arguments to another template
