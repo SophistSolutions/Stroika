42ce53a7b5ec5812c810acd0849b03e04f0bd2fa
Lewis Pringle <lewis@sophists.com>
2024-03-16 07:22:23 -0400

    fixed typo

--- Tests/02/Test.cpp
+++ Tests/02/Test.cpp
@@ -1953,7 +1953,7 @@ namespace {
             }
         };
 
-        using FmtSupport::format; // only needed while supporting fmtlib/ cuz not std::format not available
+        using namespace StdCompat; // only needed while supporting fmtlib/ cuz not std::format not available
 
         string  a1 = format ("{}", 1);
         wstring a2 = format (L"{}", 1);
