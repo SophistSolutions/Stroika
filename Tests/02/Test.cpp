720e0216ba79f8cc55e9786d1188eba0c1b87d01
Lewis G. Pringle, Jr <lewis@sophists.com>
2024-03-19 10:41:12 -0400

    adjust test case so compiles with clang++15

--- Tests/02/Test.cpp
+++ Tests/02/Test.cpp
@@ -1953,7 +1953,7 @@ namespace {
             }
         };
 
-        using namespace Configuration::StdCompat; // only needed while supporting fmtlib/ cuz not std::format not available
+        using Configuration::StdCompat::format;          // only needed while supporting fmtlib/ cuz not std::format not available
 
         string  a1 = format ("{}", 1);
         wstring a2 = format (L"{}", 1);
