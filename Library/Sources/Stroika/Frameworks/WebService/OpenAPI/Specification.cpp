3b2918edfe40f5d1884f8de665844892caa52051
Lewis Pringle <lewis@sophists.com>
2024-03-19 08:59:21 -0400

    Added OpenAPI Specification Get/Set Servers methods

--- Library/Sources/Stroika/Frameworks/WebService/OpenAPI/Specification.cpp
+++ Library/Sources/Stroika/Frameworks/WebService/OpenAPI/Specification.cpp
@@ -16,11 +16,22 @@
 
 using namespace Stroika::Foundation;
 using namespace Stroika::Foundation::Containers;
+using namespace Stroika::Foundation::Characters;
 using namespace Stroika::Foundation::DataExchange;
 
 using namespace Stroika::Frameworks;
 using namespace Stroika::Frameworks::WebService::OpenAPI;
 
+namespace {
+    const String kServersElt_ = "servers"sv;
+    const String kURLElt_     = "url"sv;
+}
+
+/*
+ ********************************************************************************
+ *********************************** Specification ******************************
+ ********************************************************************************
+ */
 Specification::Specification (const Streams::InputStream::Ptr<byte>& b, const optional<DataExchange::InternetMediaType>& mediaType)
     : Specification{DataExchange::Variant::JSON::Reader{}.Read (b)}
 {
@@ -30,10 +41,27 @@ Memory::BLOB Specification::As (const DataExchange::InternetMediaType& mediaType
 {
     if (mediaType == kMediaType) {
         Streams::MemoryStream::Ptr<byte> b = Streams::MemoryStream::New<byte> ();
-        DataExchange::Variant::JSON::Writer{}.Write (fValue_);
+        DataExchange::Variant::JSON::Writer{}.WriteAsBLOB (fValue_);
         return b.As<Memory::BLOB> ();
     }
     else {
         Execution::Throw (Execution::RuntimeErrorException{"Type not supported"sv});
     }
+}
+
+Sequence<URI> Specification::GetServers () const
+{
+    // @todo store in better internal rep assuring structure already has all these fields... - so the below cannot fail (as is it can)
+    return fValue_.As<Mapping<String, VariantValue>> ().LookupValue (kServersElt_, {}).As<Sequence<VariantValue>> ().Map<Sequence<URI>> ([] (auto vv) {
+        return URI{vv.As<Mapping<String, VariantValue>> ().LookupValue (kURLElt_).As<String> ()};
+    });
+}
+
+void Specification::SetServers (const Sequence<URI>& s)
+{
+    Mapping<String, VariantValue> vv = fValue_.As<Mapping<String, VariantValue>> ();
+    vv.Add (kServersElt_, VariantValue{s.Map<Sequence<VariantValue>> ([] (URI u) {
+                return VariantValue{Mapping<String, VariantValue>{{kURLElt_, u.As<String> ()}}};
+            })});
+    fValue_ = VariantValue{vv};
 }
--- Library/Sources/Stroika/Frameworks/WebService/OpenAPI/Specification.h
+++ Library/Sources/Stroika/Frameworks/WebService/OpenAPI/Specification.h
@@ -6,8 +6,10 @@
 
 #include "../../StroikaPreComp.h"
 
+#include "Stroika/Foundation/Containers/Sequence.h"
 #include "Stroika/Foundation/DataExchange/InternetMediaType.h"
 #include "Stroika/Foundation/DataExchange/VariantValue.h"
+#include "Stroika/Foundation/IO/Network/URI.h"
 #include "Stroika/Foundation/Memory/BLOB.h"
 #include "Stroika/Foundation/Streams/InputStream.h"
 
@@ -24,29 +26,38 @@ namespace Stroika::Frameworks::WebService::OpenAPI {
 
     using namespace Stroika::Foundation;
 
+    using Containers::Sequence;
     using DataExchange::VariantValue;
+    using IO::Network::URI;
 
     // https://stackoverflow.com/questions/52541842/what-is-the-media-type-of-an-openapi-schema
     inline const DataExchange::InternetMediaType kMediaType{"application/openapi+json"sv};
 
     // very early rough draft - for now - represnet as a VariantValue
     class Specification {
-
-        // @
     public:
+        /**
+         */
         Specification (const VariantValue& v);
         Specification (const Streams::InputStream::Ptr<byte>& b, const optional<DataExchange::InternetMediaType>& mediaType = {});
         Specification (const Specification&) = default;
 
-        // public add accessor (properties) for ServerList??? And other aspects
+    public:
+        /**
+         */
+        nonvirtual Sequence<URI> GetServers () const;
+
+    public:
+        /**
+         */
+        nonvirtual void SetServers (const Sequence<URI>& s);
 
     public:
         /**
          * So far only kMediaType supported.
          */
-        Memory::BLOB As (const DataExchange::InternetMediaType& mediaType);
+        nonvirtual Memory::BLOB As (const DataExchange::InternetMediaType& mediaType);
 
-    private:
     private:
         VariantValue fValue_;
     };
