f4a02f757edc407faeb94083cc308539318229fb
Lewis Pringle <lewis@sophists.com>
2024-03-23 09:09:22 -0400

    tweaked skel sample app to use Execution::CommandLine and new _f string for tracecontnextdumper

--- ScriptsLib/Shared/Skel-Templates/MyApp/Sources/Main.cpp
+++ ScriptsLib/Shared/Skel-Templates/MyApp/Sources/Main.cpp
@@ -4,15 +4,17 @@
 #include "Stroika/Frameworks/StroikaPreComp.h"
 
 #include "Stroika/Foundation/Characters/ToString.h"
+#include "Stroika/Foundation/Execution/CommandLine.h"
 #include "Stroika/Foundation/Debug/Trace.h"
 
 using namespace std;
 
 using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Characters::Literals;
 
 
 int main ([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
 {
-    Debug::TraceContextBumper ctx{Stroika_Foundation_Debug_OptionalizeTraceArgs (L"main", L"argv=%s", Characters::ToString (vector<const char*>{argv, argv + argc}).c_str ())};
+    Debug::TraceContextBumper ctx{"main", "argv={}"_f, Execution::CommandLine{argc, argv}};
     return EXIT_SUCCESS;
 }
