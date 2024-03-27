b7f0e0d5434d859208d8c2cc6be580b52856c1e3
Lewis Pringle <lewis@sophists.com>
2024-03-27 09:53:02 -0400

    some regtests for recent FormatString sematnics changes git push

--- Tests/02/Test.cpp
+++ Tests/02/Test.cpp
@@ -1981,10 +1981,10 @@ namespace {
         DbgTrace (L"yippie: {}"_f, foo{});
         EXPECT_EQ (a7, "foo as a string"sv);
         DbgTrace (L"t2:  cidr= {}, s1={}, s2={}"_f, IO::Network::CIDR{"192.168.244.0/24"}, L"s1", String{"s2"});
-        DbgTrace ("t2:  cidr= {}, s1={}, s2={}"_f, IO::Network::CIDR{"192.168.244.0/24"}, "s1", String{"s2"});
+        DbgTrace ("t2:  cidr= {}, s1={}, s2={}"_f, IO::Network::CIDR{"192.168.244.0/24"}, L"s1", String{"s2"});
         // @todo add mapping for wstring/string even for std  case using my ToString mechansim (details elude me - must be careful to avoid introducing ambiguity)
         //        DbgTrace2 (L"t2:  s1={}, s2={}, s3={}",  "s1", String{"s2"}, string{"s3"});
-        DbgTrace ("t2: cidr= {}, s1={}, s2={}, s3={}"_f, IO::Network::CIDR{"192.168.244.0/24"}, "s1", String{"s2"}, string{"s3"});
+        DbgTrace ("t2: cidr= {}, s1={}, s2={}, s3={}"_f, IO::Network::CIDR{"192.168.244.0/24"}, L"s1", String{"s2"}, wstring{L"s3"});
         //         DbgTrace2 (L"t2: cidr= {}, s1={}, s2={}, s3={}", IO::Network::CIDR{"192.168.244.0/24"}, "s1", String{"s2"}, string{"s3"});
 
         auto pp = "{}"_f(1);
