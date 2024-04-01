3fd1b91121c4ea8291fba808c6b3c1f543f79889
Lewis Pringle <lewis@sophists.com>
2024-04-01 14:51:25 -0400

    add missing checkin

---
+++ Library/Sources/Stroika/Foundation/Execution/Synchronized.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright(c) Sophist Solutions, Inc. 1990-2024.  All rights reserved
+ */
+#include "../StroikaPreComp.h"
+
+#include "../Characters/Format.h"
+#include "../Debug/Trace.h"
+
+#include "Synchronized.h"
+
+using namespace Stroika::Foundation;
+using namespace Stroika::Foundation::Characters;
+using namespace Stroika::Foundation::Execution;
+using namespace Stroika::Foundation::Memory;
+
+#if qStroika_Foundation_Debug_Trace_DefaultTracingOn
+void Execution::Private_::DbgTraceHelper_ (const wchar_t* w1, const optional<std::wstring>& w2)
+{
+    if (w2) {
+        DbgTrace ("{}: {}"_f, w1, *w2);
+    }
+    else {
+        DbgTrace ("{}"_f, w1);
+    }
+}
+#endif
