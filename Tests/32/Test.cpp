14380ccc63f22b567b28996d81987b5e93066dc2
Lewis Pringle <lewis@sophists.com>
2024-05-22 17:17:03 +0200

    cleanup regtests - cannot repro problem I thiought I saw

--- Tests/32/Test.cpp
+++ Tests/32/Test.cpp
@@ -1117,22 +1117,21 @@ namespace {
     {
         using namespace Characters::Literals;
         using namespace Memory::Literals;
-#if 0
-        // asserts out - SB OK - and seekable!
-        static VariantValue kTestVariantxxx_ = DataExchange::Variant::JSON::Reader{}.Read ("{"
-                                                                                        "\"foo\" : [ \"bar\", \"baz\" ]"
-                                                                                        ",\"\" : 0"
-                                                                                        ",\"a/b\" : 1"
-                                                                                        ",\"c%d\" : 2"
-                                                                                        ",\"e^f\" : 3"
-                                                                                        ",\"g|h\" : 4"
-                                                                                        ",\"i\\\\j\" : 5" // double double backslash cuz interpreted by C and then json parser
-                                                                                        ",\"k\\\"l\": 6" // 2 C quotes, and one json quote
-                                                                                        ",\" \": 7"
-                                                                                        ",\"m~n\" : 8"
-                                                                                        "}"_k);
-#endif
-        static VariantValue kTestVariant_ = DataExchange::Variant::JSON::Reader{}.Read ("{"
+        // Saw assertion error cuz not seekable here, but doesn't seem to happen anymore - not sure what was happening? --LGP 2024-05-22
+        // I THINK an issue with code in DataExchange::JSON::Reader::BoostRep_::Read - but cannot see anything wrong and cannot repro issue
+        VariantValue readAsString = DataExchange::Variant::JSON::Reader{}.Read ("{"
+                                                                                       "\"foo\" : [ \"bar\", \"baz\" ]"
+                                                                                       ",\"\" : 0"
+                                                                                       ",\"a/b\" : 1"
+                                                                                       ",\"c%d\" : 2"
+                                                                                       ",\"e^f\" : 3"
+                                                                                       ",\"g|h\" : 4"
+                                                                                       ",\"i\\\\j\" : 5" // double double backslash cuz interpreted by C and then json parser
+                                                                                       ",\"k\\\"l\": 6" // 2 C quotes, and one json quote
+                                                                                       ",\" \": 7"
+                                                                                       ",\"m~n\" : 8"
+                                                                                       "}"_k);
+         VariantValue readAsBLOB = DataExchange::Variant::JSON::Reader{}.Read ("{"
                                                                                         "\"foo\" : [ \"bar\", \"baz\" ]"
                                                                                         ",\"\" : 0"
                                                                                         ",\"a/b\" : 1"
@@ -1144,7 +1143,7 @@ namespace {
                                                                                         ",\" \": 7"
                                                                                         ",\"m~n\" : 8"
                                                                                         "}"_blob);
-        //   EXPECT_EQ (JSON::PointerType{""}.Apply (kTestVariant_), kTestVariant_);
+         EXPECT_EQ (readAsString, readAsBLOB);
     }
 }
 
