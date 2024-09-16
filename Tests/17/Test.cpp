072dce2f0ae47a013e2d5a4e34640730281039cc
Lewis Pringle <lewis@sophists.com>
2024-09-16 09:11:50 -0400

    cleanup Mapping regtests for googletest

--- Tests/17/Test.cpp
+++ Tests/17/Test.cpp
@@ -113,115 +113,30 @@ namespace {
 }
 
 namespace {
-    namespace ExampleCTORS_Test_5_ {
-        void DoTest ()
-        {
-            Debug::TraceContextBumper ctx{"{}::ExampleCTORS_Test_5_"};
-            // From Mapping<> CTOR docs
-            Collection<pair<int, int>> c;
-            std::map<int, int>         m;
-
-            Mapping<int, int> m1 = {pair<int, int>{1, 1}, pair<int, int>{2, 2}, pair<int, int>{3, 2}};
-            Mapping<int, int> m2 = m1;
-            Mapping<int, int> m3{m1};
-            Mapping<int, int> m4{m1.begin (), m1.end ()};
-            Mapping<int, int> m5{c};
-            Mapping<int, int> m6{m};
-            Mapping<int, int> m7{m.begin (), m.end ()};
-            Mapping<int, int> m8{move (m1)};
-            Mapping<int, int> m9{Common::DeclareEqualsComparer ([] (int l, int r) { return l == r; })};
-
-            {
-                using Characters::String;
-                const auto kReference1a_ = Mapping<String, String>{{KeyValuePair<String, String>{"Content-Length", "3"}}};
-                const auto kReference1b_ =
-                    Mapping<String, String>{{KeyValuePair<String, String>{"Content-Length", "3"}, KeyValuePair<String, String>{"xx", "3"}}};
-                const auto kReference1c_ = Mapping<String, String>{KeyValuePair<String, String>{L"Content-Length", L"3"}};
-                const auto kReference2a_ = Mapping<String, String>{{pair<String, String>{L"Content-Length", "3"}}};
-                const auto kReference2b_ = Mapping<String, String>{{pair<String, String>{"Content-Length", "3"}, pair<String, String>{"xx", "3"}}};
-                const auto kReference2c_ = Mapping<String, String>{pair<String, String>{"Content-Length", "3"}, pair<String, String>{"xx", "3"}};
-                const auto kReference3a_ = Mapping<String, String>{{{"Content-Length", "3"}}};
-                EXPECT_EQ (kReference3a_.size (), 1u);
-                using Characters::operator""_k;
-                const auto kReference3b_ = Mapping<String, String>{{{"Content-Length"_k, "3"_k}, {"x"_k, "3"_k}}}; // need _k on some compilers to avoid error due to invoke explicit String/2 (g++10) - not sure if bug or not but easy to avoid ambiguity
-                EXPECT_EQ (kReference3b_.size (), 2u);
-                const auto kReference3c_ = Mapping<String, String>{{"Content-Length", "3"}, {"x", "3"}};
-                EXPECT_EQ (kReference3c_.size (), 2u);
-            }
-        }
-    }
-}
-
-namespace {
-    namespace Where_Test_6_ {
-        void DoAll ()
-        {
-            Mapping<int, int> m{{1, 3}, {2, 4}, {3, 5}, {4, 5}, {5, 7}};
-            EXPECT_EQ (m.Where ([] (const KeyValuePair<int, int>& value) { return Math::IsPrime (value.fKey); }),
-                       (Mapping<int, int>{{2, 4}, {3, 5}, {5, 7}}));
-            EXPECT_EQ (m.Where ([] (int key) { return Math::IsPrime (key); }), (Mapping<int, int>{{2, 4}, {3, 5}, {5, 7}}));
-        }
-    }
-}
-
-namespace {
-    namespace WithKeys_Test_7_ {
-        void DoAll ()
-        {
-            Mapping<int, int> m{{1, 3}, {2, 4}, {3, 5}, {4, 5}, {5, 7}};
-            EXPECT_EQ (m.WithKeys ({2, 5}), (Mapping<int, int>{{2, 4}, {5, 7}}));
-        }
-    }
-}
-
-namespace {
-    namespace ClearBug_Test_8_ {
-        void DoAll ()
-        {
-            // http://stroika-bugs.sophists.com/browse/STK-541
-            Mapping<int, int> m{KeyValuePair<int, int>{1, 3}, KeyValuePair<int, int>{2, 4}, KeyValuePair<int, int>{3, 5},
-                                KeyValuePair<int, int>{4, 5}, KeyValuePair<int, int>{5, 7}};
-            Mapping<int, int> mm{move (m)};
-            // SEE http://stroika-bugs.sophists.com/browse/STK-541  - this call to clear is ILLEGAL - after m has been moved from
-            //m.clear ();
-        }
-    }
-}
-
-namespace {
-    namespace AddVsAddIf_Test_9_ {
-        void DoAll ()
-        {
-            {
-                Mapping<int, int> m;
-                m.Add (1, 2);
-                EXPECT_TRUE (m[1] == 2);
-                m.Add (1, 3);
-                EXPECT_TRUE (m[1] == 3);
-                EXPECT_TRUE (not m.Add (1, 4, AddReplaceMode::eAddIfMissing));
-                EXPECT_TRUE (m[1] == 3);
-                EXPECT_TRUE (m.Add (2, 3, AddReplaceMode::eAddIfMissing));
-                EXPECT_TRUE (m[2] == 3);
-            }
-        }
-    }
-}
-
-namespace {
-    GTEST_TEST (Foundation_Containers_Mapping, all)
+    GTEST_TEST (Foundation_Containers_Mapping, FACTORY_DEFAULT)
     {
         DoTestForConcreteContainer_<Mapping<size_t, size_t>> ();
         DoTestForConcreteContainer_<Mapping<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> ();
         DoTestForConcreteContainer_<Mapping<OnlyCopyableMoveable, OnlyCopyableMoveable>> (
             [] () { return Mapping<OnlyCopyableMoveable, OnlyCopyableMoveable>{AsIntsEqualsComparer<OnlyCopyableMoveable>{}}; },
             AsIntsEqualsComparer<OnlyCopyableMoveable>{});
+    }
+}
 
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, Mapping_Array)
+    {
         DoTestForConcreteContainer_<Mapping_Array<size_t, size_t>> ();
         DoTestForConcreteContainer_<Mapping_Array<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> ();
         DoTestForConcreteContainer_<Mapping_Array<OnlyCopyableMoveable, OnlyCopyableMoveable>> (
             [] () { return Mapping_Array<OnlyCopyableMoveable, OnlyCopyableMoveable>{AsIntsEqualsComparer<OnlyCopyableMoveable>{}}; },
             AsIntsEqualsComparer<OnlyCopyableMoveable>{});
+    }
+}
 
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, SortedMapping_SkipList)
+    {
         DoTestForConcreteContainer_<SortedMapping_SkipList<size_t, size_t>> ();
         DoTestForConcreteContainer_<SortedMapping_SkipList<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> ();
         DoTestForConcreteContainer_<SortedMapping_SkipList<OnlyCopyableMoveable, OnlyCopyableMoveable>> (
@@ -231,13 +146,23 @@ namespace {
                 }};
             },
             AsIntsEqualsComparer<OnlyCopyableMoveable>{});
+    }
+}
 
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, Mapping_LinkedList)
+    {
         DoTestForConcreteContainer_<Mapping_LinkedList<size_t, size_t>> ();
         DoTestForConcreteContainer_<Mapping_LinkedList<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> ();
         DoTestForConcreteContainer_<Mapping_LinkedList<OnlyCopyableMoveable, OnlyCopyableMoveable>> (
             [] () { return Mapping_LinkedList<OnlyCopyableMoveable, OnlyCopyableMoveable> (AsIntsEqualsComparer<OnlyCopyableMoveable>{}); },
             AsIntsEqualsComparer<OnlyCopyableMoveable>{});
+    }
+}
 
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, SortedMapping_stdmap)
+    {
         DoTestForConcreteContainer_<SortedMapping_stdmap<size_t, size_t>> ();
         DoTestForConcreteContainer_<SortedMapping_stdmap<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> ();
         {
@@ -254,53 +179,132 @@ namespace {
                 },
                 AsIntsEqualsComparer<OnlyCopyableMoveable>{});
         }
+    }
+}
 
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, Mapping_stdhashmap)
+    {
         {
-            {
-                Mapping_stdhashmap<size_t, size_t> fred{};
-                fred.Add (1, 2);
-                fred.Add (1, 3);
-                EXPECT_TRUE (fred.size () == 1);
-            }
-            {
-                Mapping_stdhashmap<size_t, size_t> fred{};
-                fred.Add (1, 2);
-                fred.Add (2, 4);
-                fred.Add (3, 9);
-                EXPECT_TRUE (fred.size () == 3);
-                EXPECT_TRUE ((fred == Mapping<size_t, size_t>{{1, 2}, {2, 4}, {3, 9}}));
-            }
-            {
-                static_assert (Cryptography::Digest::IHashFunction<std::hash<size_t>, size_t>);
-                Mapping_stdhashmap<size_t, size_t> fred{std::hash<size_t>{}, std::equal_to<size_t>{}};
-                EXPECT_TRUE ((fred == Mapping_stdhashmap<size_t, size_t>{}));
-            }
-            {
-                using Characters::String;
-                Mapping_stdhashmap<String, String> m{};
-                m.Add ("a", "alpha");
-                m.Add ("b", "beta");
-                m.Add ("c", "gamma");
-                m.Add ("d", "delta");
-                EXPECT_TRUE (m.size () == 4);
-                EXPECT_TRUE ((m == Mapping<String, String>{{"a", "alpha"}, {"b", "beta"}, {"c", "gamma"}, {"d", "delta"}}));
-            }
+            Mapping_stdhashmap<size_t, size_t> fred{};
+            fred.Add (1, 2);
+            fred.Add (1, 3);
+            EXPECT_EQ (fred.size (), 1u);
+        }
+        {
+            Mapping_stdhashmap<size_t, size_t> fred{};
+            fred.Add (1, 2);
+            fred.Add (2, 4);
+            fred.Add (3, 9);
+            EXPECT_EQ (fred.size (), 3u);
+            EXPECT_EQ (fred, (Mapping<size_t, size_t>{{1, 2}, {2, 4}, {3, 9}}));
+        }
+        {
+            static_assert (Cryptography::Digest::IHashFunction<std::hash<size_t>, size_t>);
+            Mapping_stdhashmap<size_t, size_t> fred{std::hash<size_t>{}, std::equal_to<size_t>{}};
+            EXPECT_EQ (fred, (Mapping_stdhashmap<size_t, size_t>{}));
+        }
+        {
+            using Characters::String;
+            Mapping_stdhashmap<String, String> m{};
+            m.Add ("a", "alpha");
+            m.Add ("b", "beta");
+            m.Add ("c", "gamma");
+            m.Add ("d", "delta");
+            EXPECT_EQ (m.size (), 4u);
+            EXPECT_EQ (m, (Mapping<String, String>{{"a", "alpha"}, {"b", "beta"}, {"c", "gamma"}, {"d", "delta"}}));
+        }
 
-            DoTestForConcreteContainer_<Mapping_stdhashmap<size_t, size_t>> ();
-            //DoTestForConcreteContainer_<Mapping_stdhashmap<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> (); // -- wont work cuz not hashable
+        DoTestForConcreteContainer_<Mapping_stdhashmap<size_t, size_t>> ();
+        //DoTestForConcreteContainer_<Mapping_stdhashmap<OnlyCopyableMoveableAndTotallyOrdered, OnlyCopyableMoveableAndTotallyOrdered>> (); // -- wont work cuz not hashable
 #if 0
             DoTestForConcreteContainer_<Mapping<OnlyCopyableMoveable, OnlyCopyableMoveable>> (
                 [] () { return Mapping<OnlyCopyableMoveable, OnlyCopyableMoveable> (AsIntsEqualsComparer<OnlyCopyableMoveable>{}); },
                 AsIntsEqualsComparer<OnlyCopyableMoveable>{});
 #endif
+    }
+}
+
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, ExampleCTORS_)
+    {
+        Debug::TraceContextBumper ctx{"{}::ExampleCTORS_"};
+        // From Mapping<> CTOR docs
+        Collection<pair<int, int>> c;
+        std::map<int, int>         m;
+
+        Mapping<int, int> m1 = {pair<int, int>{1, 1}, pair<int, int>{2, 2}, pair<int, int>{3, 2}};
+        Mapping<int, int> m2 = m1;
+        Mapping<int, int> m3{m1};
+        Mapping<int, int> m4{m1.begin (), m1.end ()};
+        Mapping<int, int> m5{c};
+        Mapping<int, int> m6{m};
+        Mapping<int, int> m7{m.begin (), m.end ()};
+        Mapping<int, int> m8{move (m1)};
+        Mapping<int, int> m9{Common::DeclareEqualsComparer ([] (int l, int r) { return l == r; })};
+
+        {
+            using Characters::String;
+            const auto kReference1a_ = Mapping<String, String>{{KeyValuePair<String, String>{"Content-Length", "3"}}};
+            const auto kReference1b_ =
+                Mapping<String, String>{{KeyValuePair<String, String>{"Content-Length", "3"}, KeyValuePair<String, String>{"xx", "3"}}};
+            const auto kReference1c_ = Mapping<String, String>{KeyValuePair<String, String>{L"Content-Length", L"3"}};
+            const auto kReference2a_ = Mapping<String, String>{{pair<String, String>{L"Content-Length", "3"}}};
+            const auto kReference2b_ = Mapping<String, String>{{pair<String, String>{"Content-Length", "3"}, pair<String, String>{"xx", "3"}}};
+            const auto kReference2c_ = Mapping<String, String>{pair<String, String>{"Content-Length", "3"}, pair<String, String>{"xx", "3"}};
+            const auto kReference3a_ = Mapping<String, String>{{{"Content-Length", "3"}}};
+            EXPECT_EQ (kReference3a_.size (), 1u);
+            using Characters::operator""_k;
+            const auto kReference3b_ = Mapping<String, String>{{{"Content-Length"_k, "3"_k}, {"x"_k, "3"_k}}}; // need _k on some compilers to avoid error due to invoke explicit String/2 (g++10) - not sure if bug or not but easy to avoid ambiguity
+            EXPECT_EQ (kReference3b_.size (), 2u);
+            const auto kReference3c_ = Mapping<String, String>{{"Content-Length", "3"}, {"x", "3"}};
+            EXPECT_EQ (kReference3c_.size (), 2u);
         }
+    }
+}
 
-        ExampleCTORS_Test_5_::DoTest ();
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, Where_Test_6_)
+    {
+        Mapping<int, int> m{{1, 3}, {2, 4}, {3, 5}, {4, 5}, {5, 7}};
+        EXPECT_EQ (m.Where ([] (const KeyValuePair<int, int>& value) { return Math::IsPrime (value.fKey); }),
+                   (Mapping<int, int>{{2, 4}, {3, 5}, {5, 7}}));
+        EXPECT_EQ (m.Where ([] (int key) { return Math::IsPrime (key); }), (Mapping<int, int>{{2, 4}, {3, 5}, {5, 7}}));
+    }
+}
 
-        Where_Test_6_::DoAll ();
-        WithKeys_Test_7_::DoAll ();
-        ClearBug_Test_8_::DoAll ();
-        AddVsAddIf_Test_9_::DoAll ();
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, WithKeys_Test_7_)
+    {
+        Mapping<int, int> m{{1, 3}, {2, 4}, {3, 5}, {4, 5}, {5, 7}};
+        EXPECT_EQ (m.WithKeys ({2, 5}), (Mapping<int, int>{{2, 4}, {5, 7}}));
+    }
+}
+
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, ClearBug_Test_8_)
+    {
+        // http://stroika-bugs.sophists.com/browse/STK-541
+        Mapping<int, int> m{KeyValuePair<int, int>{1, 3}, KeyValuePair<int, int>{2, 4}, KeyValuePair<int, int>{3, 5},
+                            KeyValuePair<int, int>{4, 5}, KeyValuePair<int, int>{5, 7}};
+        Mapping<int, int> mm{move (m)};
+        // SEE http://stroika-bugs.sophists.com/browse/STK-541  - this call to clear is ILLEGAL - after m has been moved from
+        //m.clear ();
+    }
+}
+
+namespace {
+    GTEST_TEST (Foundation_Containers_Mapping, AddVsAddIf_)
+    {
+        Mapping<int, int> m;
+        m.Add (1, 2);
+        EXPECT_EQ (m[1], 2);
+        m.Add (1, 3);
+        EXPECT_EQ (m[1], 3);
+        EXPECT_TRUE (not m.Add (1, 4, AddReplaceMode::eAddIfMissing));
+        EXPECT_EQ (m[1], 3);
+        EXPECT_TRUE (m.Add (2, 3, AddReplaceMode::eAddIfMissing));
+        EXPECT_EQ (m[2], 3);
     }
 }
 
