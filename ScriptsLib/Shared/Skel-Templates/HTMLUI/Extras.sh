d8aee6f78a25ef2b77feb8daca777883d3536946
Lewis Pringle <lewis@sophists.com>
2024-10-24 11:36:17 -0400

    progress refactoring Skel script to support new templates

---
+++ ScriptsLib/Shared/Skel-Templates/HTMLUI/Extras.sh
@@ -0,0 +1,5 @@
+
+# shell script after base Skel run
+targetSkelDir=./
+oldStroikaDir=../../Stroika/DevRoot/
+cp -r ${oldStroikaDir}Samples/HTMLUI/{Backend,Docker,Installers,OpenAPI,QuasarBasedHTMLApp} ${targetSkelDir}
