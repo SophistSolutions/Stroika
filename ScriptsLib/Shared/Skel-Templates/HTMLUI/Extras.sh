b61093b13e353d1fa1df7128fc6472bec82dfffd
Lewis Pringle <lewis@sophists.com>
2024-10-24 18:04:49 -0400

    more progress on Skel support for HTMLUI template

--- ScriptsLib/Shared/Skel-Templates/HTMLUI/Extras.sh
+++ ScriptsLib/Shared/Skel-Templates/HTMLUI/Extras.sh
@@ -10,9 +10,11 @@ sed s/\$\{APP_NAME\}/${APP_NAME}/g < $TEMPLATE_SRC_DIR/Makefile > $APP_ROOT/Make
 sed s/\$\{APP_NAME\}/${APP_NAME}/g < $TEMPLATE_SRC_DIR/Makefile > $APP_ROOT/Makefile
 
 # Patch TOP_ROOT and StroikaRoot, and HTMLUI->AppName in Makefiles
+echo "Patching for HTMLUI Template::Makefiles..."
 for i in Backend Installers QuasarBasedHTMLApp;
 do
-    sed 's/export TOP_ROOT:=.*/export TOP_ROOT:=$(abspath ..)\//' < $oldStroikaDir/Samples/HTMLUI/$i/Makefile |
+    cat $oldStroikaDir/Samples/HTMLUI/$i/Makefile  |
+    sed 's/export TOP_ROOT:=.*/export TOP_ROOT:=$(abspath ..)\//' |
     sed 's/StroikaRoot:=.*/StroikaRoot:=$(TOP_ROOT)ThirdPartyComponents\/Stroika\/StroikaRoot\//' |
     sed 's/SrcDir.*:=.*/SrcDir:=$(TOP_ROOT)Backend\//' |
     sed 's/VERSION_FILE_IN=$(StroikaRoot)STROIKA_VERSION/VERSION_FILE_IN=$(TOP_ROOT)VERSION/' |
@@ -20,3 +22,18 @@ do
     sed "s/Samples\/HTMLUI/${APP_NAME}/g" |
     sed "s/HTMLUI/${APP_NAME}/g" > $APP_ROOT/$i/Makefile
 done
+
+
+# Patch HTMLUI => {APP_NAME} in various source files
+echo "Patching for HTMLUI Template::Sources..."
+for i in ${APP_ROOT}/Installers/*.wix ${APP_ROOT}/QuasarBasedHTMLApp/src/pages/*.vue ${APP_ROOT}/Backend/Sources/*.{h,inl,cpp};
+do
+    echo PATCHING HTMLUI NAME in: $i
+    cat $i |
+    sed "s/Samples-HTMLUI/${APP_NAME}/g" |
+    sed "s/Samples\/HTMLUI/${APP_NAME}/g" |
+    sed "s/Sample HTMLUI/${APP_NAME}/g" |
+    sed "s/HTMLUI/${APP_NAME}/g" > $i.tmp
+    mv  $i.tmp $i
+done
+
