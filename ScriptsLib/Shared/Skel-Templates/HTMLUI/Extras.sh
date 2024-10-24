173b47e01e03105e5368b06c868f316851ea2a6d
Lewis Pringle <lewis@sophists.com>
2024-10-24 17:23:54 -0400

    more progress on HTMLUI Skel template support

--- ScriptsLib/Shared/Skel-Templates/HTMLUI/Extras.sh
+++ ScriptsLib/Shared/Skel-Templates/HTMLUI/Extras.sh
@@ -7,3 +7,16 @@ cp -r ${oldStroikaDir}Samples/HTMLUI/{Backend,Docker,Installers,OpenAPI,QuasarBa
 
 sed s/\$\{APP_NAME\}/${APP_NAME}/g < $TEMPLATE_SRC_DIR/Makefile > $APP_ROOT/Makefile
 
+sed s/\$\{APP_NAME\}/${APP_NAME}/g < $TEMPLATE_SRC_DIR/Makefile > $APP_ROOT/Makefile
+
+# Patch TOP_ROOT and StroikaRoot, and HTMLUI->AppName in Makefiles
+for i in Backend Installers QuasarBasedHTMLApp;
+do
+    sed 's/export TOP_ROOT:=.*/export TOP_ROOT:=$(abspath ..)\//' < $oldStroikaDir/Samples/HTMLUI/$i/Makefile |
+    sed 's/StroikaRoot:=.*/StroikaRoot:=$(TOP_ROOT)ThirdPartyComponents\/Stroika\/StroikaRoot\//' |
+    sed 's/SrcDir.*:=.*/SrcDir:=$(TOP_ROOT)Backend\//' |
+    sed 's/VERSION_FILE_IN=$(StroikaRoot)STROIKA_VERSION/VERSION_FILE_IN=$(TOP_ROOT)VERSION/' |
+    sed "s/Samples-HTMLUI/${APP_NAME}/g" |
+    sed "s/Samples\/HTMLUI/${APP_NAME}/g" |
+    sed "s/HTMLUI/${APP_NAME}/g" > $APP_ROOT/$i/Makefile
+done
