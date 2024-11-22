7c64b0ddbcebddfb12b154cb993c97f986eb431a
Lewis Pringle <lewis@sophists.com>
2024-11-22 07:29:51 -0500

    fixed typo

--- Samples/WebService/Sources/WebServer.cpp
+++ Samples/WebService/Sources/WebServer.cpp
@@ -108,7 +108,7 @@ public:
                              // demo getting argument from the body
                              if (not number) {
                                  // read if content-type is text (not json)
-                                 if (m->request ().contentType () and
+                                 if (m.request ().contentType () and
                                      InternetMediaTypeRegistry::sThe->IsA (InternetMediaTypes::kText_PLAIN, *m.request ().contentType ())) {
                                      String argsAsString = Streams::TextReader::New (m.rwRequest ().GetBody ()).ReadAll ();
                                      number              = kMapper.ToObject<Number> (DataExchange::VariantValue{argsAsString});
