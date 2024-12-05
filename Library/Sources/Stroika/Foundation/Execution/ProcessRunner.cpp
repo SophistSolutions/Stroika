01ad4c8a985ce7e3661323d6d14edc9d6e70c32e
Lewis Pringle <lewis@sophists.com>
2024-12-05 11:33:44 -0500

    fixed typo

--- Library/Sources/Stroika/Foundation/Execution/ProcessRunner.cpp
+++ Library/Sources/Stroika/Foundation/Execution/ProcessRunner.cpp
@@ -885,7 +885,7 @@ namespace {
                         stderrMsg << String::FromLatin1 (Memory::ConstSpan (span{trailingStderrBufNextByte2WriteAt, end (trailingStderrBuf)}));
                     }
                     stderrMsg << String::FromLatin1 (Memory::ConstSpan (span{begin (trailingStderrBuf), trailingStderrBufNextByte2WriteAt}));
-                    Throw (ProcessRunner::Exception{effectiveCmdLine, "Spawned program"sv, stderrMsg.str (),
+                    Throw (ProcessRunner::Exception{cmdLine.As<String> (), "Spawned program"sv, stderrMsg.str (),
                                                     WIFEXITED (status) ? WEXITSTATUS (status) : optional<uint8_t>{},
                                                     WIFSIGNALED (status) ? WTERMSIG (status) : optional<uint8_t>{},
                                                     WIFSTOPPED (status) ? WSTOPSIG (status) : optional<uint8_t>{}});
