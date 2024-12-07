f1940003e4744baf756c981d9a6827136d57493b
Lewis Pringle <lewis@sophists.com>
2024-12-07 13:04:05 -0500

    disable some maybe unwanted/unhelpful quoting in CommandLine::As<String> ()

--- Library/Sources/Stroika/Foundation/Execution/CommandLine.cpp
+++ Library/Sources/Stroika/Foundation/Execution/CommandLine.cpp
@@ -446,12 +446,16 @@ String CommandLine::As<String> () const
 template <>
 String CommandLine::As<String> (optional<CommandLine::StringShellQuoting> shellStyle) const
 {
+    // UNCLEAR how to handle quoting of elements inside string - so for now, do (less) harm? DOnt try to
+    // quote the quotes (but still wrap items in quotes).
+    //  --LGP 2024-12-07
     return fArgs_.Join (
         [&] (const String& i) {
             // default in Stroika is wrap in double-quotes, and \-quote double-quote characters, and rest leave alone
             if (shellStyle == nullopt) {
                 if (i.ContainsAny ({' ', '\"'})) {
-                    return "\"{}\""_f(i.ReplaceAll ("\""sv, "\\\""sv));
+                    return "\"{}\""_f(i);
+                    //return "\"{}\""_f(i.ReplaceAll ("\""sv, "\\\""sv));
                 }
                 else {
                     return i;
@@ -460,7 +464,8 @@ String CommandLine::As<String> (optional<CommandLine::StringShellQuoting> shellS
             else if (shellStyle == StringShellQuoting::eWindowsCMD) {
                 // @todo - NO IDEA - I think "" replaces ", in cmd shell?
                 if (i.ContainsAny ({' ', '\"'})) {
-                    return "\"{}\""_f(i.ReplaceAll ("\""sv, "\\\""sv));
+                    return "\"{}\""_f(i);
+                    //                    return "\"{}\""_f(i.ReplaceAll ("\""sv, "\"\""sv));
                 }
                 else {
                     return i;
@@ -469,7 +474,8 @@ String CommandLine::As<String> (optional<CommandLine::StringShellQuoting> shellS
             else if (shellStyle == StringShellQuoting::eBash) {
                 // @todo more complex - think I need to quote other stuff, but unclear
                 if (i.ContainsAny ({' ', '\"'})) {
-                    return "\"{}\""_f(i.ReplaceAll ("\""sv, "\\\""sv));
+                    return "\"{}\""_f(i);
+                    //return "\"{}\""_f(i.ReplaceAll ("\""sv, "\\\""sv));
                 }
                 else {
                     return i;
