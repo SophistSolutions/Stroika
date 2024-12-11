0151e98f377bae16b3dba7ed8866955aaa272166
Lewis Pringle <lewis@sophists.com>
2024-12-11 12:25:49 -0500

    split webservice/webservice regtest into two regtests

--- Tests/52/Test.cpp
+++ Tests/52/Test.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright(c) Sophist Solutions, Inc. 1990-2024.  All rights reserved
  */
-//  TEST    Frameworks::WebServer && Frameworks::WebService
+//  TEST    Frameworks::WebServe
 #include "Stroika/Foundation/StroikaPreComp.h"
 
 #include <iostream>
@@ -30,8 +30,6 @@
 #include "Stroika/Frameworks/WebServer/ConnectionManager.h"
 #include "Stroika/Frameworks/WebServer/FileSystemRequestHandler.h"
 #include "Stroika/Frameworks/WebServer/Router.h"
-#include "Stroika/Frameworks/WebService/Server/ObjectRequestHandler.h"
-#include "Stroika/Frameworks/WebService/Server/VariantValue.h"
 
 using namespace Stroika::Foundation;
 using namespace Stroika::Foundation::Containers;
@@ -59,18 +57,6 @@ namespace {
     }
 }
 
-#if 0
-using CALLBACK_FUNCTION = decltype([] (int) -> String { return String{}; });
-
-using RR = invoke_result_t<CALLBACK_FUNCTION,int>;
-static_assert (same_as<RR, String>);
-
-ObjectVariantMapper kMapper;
-auto                iiii = ObjectRequestHandler::Factory{ObjectRequestHandler::Options{kMapper},
-                                        [] () -> String { return String{}; }};
-auto iiii1 = ObjectRequestHandler::Factory<String, int>{ObjectRequestHandler::Options{kMapper}, [] (int) -> String { return String{}; }};
-auto iiii1a = ObjectRequestHandler::Factory{ObjectRequestHandler::Options{kMapper}, [] (int) -> String { return String{}; }};
-#endif
 
 namespace {
     /*
@@ -182,103 +168,6 @@ namespace {
                                       .LookupChecked ("AppState", RuntimeErrorException{"oops"})
                                       .As<String> ();
             message.rwResponse ().write (argsAsString);
-        }
-    };
-}
-
-namespace {
-    // simple object to create keyed collection of, to test
-    struct ObjMapperableObj_ {
-        GUID id;
-
-        static const inline ObjectVariantMapper kMapper = [] () {
-            ObjectVariantMapper mapper;
-            mapper.AddCommonType<GUID> ();
-            mapper.AddClass<ObjMapperableObj_> ({
-                {"id", &ObjMapperableObj_::id},
-            });
-            return mapper;
-        }();
-    };
-
-    /*
-     */
-    struct MyObjectWebServiceWebServer_ {
-
-        using MyKeyedCollection_ = KeyedCollection<
-            ObjMapperableObj_, GUID,
-            KeyedCollection_DefaultTraits<ObjMapperableObj_, GUID, decltype ([] (const ObjMapperableObj_& t) -> GUID { return t.id; })>>;
-
-        static const inline ObjectVariantMapper kMapper = [] () {
-            ObjectVariantMapper mapper;
-            mapper += ObjMapperableObj_::kMapper;
-            mapper.AddCommonType<MyKeyedCollection_> ();
-            mapper.AddCommonType<Sequence<ObjMapperableObj_>> ();
-            mapper.AddCommonType<GUID> ();
-            mapper.AddCommonType<Sequence<GUID>> ();
-            return mapper;
-        }();
-
-        static inline Synchronized<MyKeyedCollection_> sData_;
-
-        const Sequence<Route> kRoutes_;
-        ConnectionManager     fConnectionMgr_;
-
-        MyObjectWebServiceWebServer_ (uint16_t portNumber)
-            : kRoutes_{
-
-                Route{"api/objs/?"_RegEx,
-                             ObjectRequestHandler::Factory{
-                                 {kMapper},
-                                 [] () -> Sequence<GUID> {
-                                     return sData_.cget ().cref ().Map<Sequence<GUID>> ([] (const ObjMapperableObj_& r) { return r.id; });
-                                 }}}
-
-                , Route{"api/objs-context/?"_RegEx, ObjectRequestHandler::Factory{{kMapper},
-                                                                        [] ([[maybe_unused]]const ObjectRequestHandler::Context& c) -> Sequence<GUID> {
-                                                                            return sData_.cget ().cref ().Map<Sequence<GUID>> (
-                                                                                [] (const ObjMapperableObj_& r) { return r.id; });
-                                                                        }}}
-
-                // PATCH could be implemented using ObjectRequestHandler::Factory, but it adds little value, and good to show
-                // mixing direct RequestHandlers with ObjectRequestHandler based ones
-                 ,  Route{IO::Network::HTTP::MethodsRegEx::kPatch, "api/objs/(.+)"_RegEx,
-                        [] (Message& m, const String& id) {
-                    using DataExchange::VariantValue;
-                            using JSON::Patch::OperationItemsType;
-                            OperationItemsType patch = ClientErrorException::TreatExceptionsAsClientError ([&] () {return OperationItemsType::kMapper.ToObject<OperationItemsType> (m.rwRequest ().GetBodyVariantValue()); });
-                            // automatic / generic patch implemented using the VariantValue representation - if that's good enuf for your purposes, easy to use
-                            ObjMapperableObj_ obj2Patch = sData_.cget ().cref ().LookupChecked (id, ClientErrorException{"obj with that ID not found"sv});
-                            VariantValue obj2PatchVV = patch.Apply (kMapper.FromObject (obj2Patch));
-                            obj2Patch                = kMapper.ToObject<ObjMapperableObj_> (obj2PatchVV);
-                            sData_.rwget ().rwref ().Add (obj2Patch);
-                            m.rwResponse ().status = IO::Network::HTTP::StatusCodes::kNoContent;
-                        }}
-
-                , Route{IO::Network::HTTP::MethodsRegEx::kPost, "api/objs/?"_RegEx,
-                      // redo so can POST raw data and arguments as query-args!
-                      // break ObjectRequestHandler into parts/phases so can be used directly from regular message handler
-                      ObjectRequestHandler::Factory{{kMapper},
-                                                    [] (const ObjMapperableObj_& r) -> GUID {
-                                                        ObjMapperableObj_ rr = r;
-                                                        rr.id                = GUID::GenerateNew ();
-                                                        sData_.rwget ().rwref ().Add (rr);
-                                                        return rr.id;
-                                                    }}}
-
-                , Route{ IO::Network::HTTP::MethodsRegEx::kPost, "api/objs-context/?"_RegEx,
-                    ObjectRequestHandler::Factory { {kMapper},
-                        [] (const ObjMapperableObj_& r, [[maybe_unused]] const ObjectRequestHandler::Context& c) -> GUID {
-                        ObjMapperableObj_ rr = r;
-                        rr.id                = GUID::GenerateNew ();
-                        sData_.rwget ().rwref ().Add (rr);
-                        return rr.id;
-                    }}}
-
-        }
-
-        , fConnectionMgr_{SocketAddresses (InternetAddresses_Any (), portNumber), kRoutes_}
-        {
         }
     };
 }
@@ -427,69 +316,6 @@ namespace {
         }
     }
 }
-
-namespace {
-    GTEST_TEST (Frameworks_WebService, TestVariantValueSupport)
-    {
-        using namespace WebService::Server::VariantValue;
-        // @todo - move this to some framework-specific regtests...
-        using VariantValue = DataExchange::VariantValue;
-        Sequence<VariantValue> tmp =
-            OrderParamValues (Iterable<String>{"page", "xxx"}, PickoutParamValuesFromURL (URI{"http://www.sophist.com?page=5"}));
-        Assert (tmp.size () == 2);
-        Assert (tmp[0].ConvertTo (VariantValue::eInteger) == 5);
-        Assert (tmp[1] == nullptr);
-    }
-}
-
-namespace {
-    GTEST_TEST (Frameworks_WebService, TestWebServiceObjectRequestHandler1)
-    {
-        EXPECT_EQ (Compression::Deflate::Compress::New ().Transform (TestDeflateEnc1_::kDecoded), TestDeflateEnc1_::kEncoded);
-        const IO::Network::PortType  portNumber = 8083;
-        MyObjectWebServiceWebServer_ myWebServer{portNumber}; // listen and dispatch while this object exists
-        try {
-            auto c = IO::Network::Transfer::Connection::New ();
-        }
-        catch (const RequiredComponentMissingException&) {
-            DbgTrace ("ignore RequiredComponentMissingException cuz no IO::Network::Transfer::Connection factory"_f);
-        }
-
-        // @todo do some calls to test api...
-        //
-        //
-
-        //IO::Network::Transfer::Response r = c.GET (URI{"http", URI::Authority{URI::Host{"localhost"}, portNumber}, "/TEST"sv});
-        //EXPECT_TRUE (r.GetSucceeded ());
-        //EXPECT_GT (r.GetData ().size (), 1u);
-        //String response = r.GetDataTextInputStream ().ReadAll ();
-        ////DbgTrace (L"response={}"_f, response);
-        //EXPECT_EQ (response, "TEST");
-        //// @todo enhance this test so we force accept-encoding none, and force accept-encoding : deflate, and check raw
-        //// result???
-    }
-}
-
-namespace {
-    GTEST_TEST (Frameworks_WebService, DurationPrecision)
-    {
-        Debug::TraceContextBumper ctx{"DurationPrecision"};
-        using DataExchange::VariantValue;
-        {
-            ObjectVariantMapper m;
-            m.AddCommonType<Duration> ();
-            VariantValue vv = m.FromObject (Duration{numbers::pi});
-            EXPECT_EQ (Variant::JSON::Writer{}.WriteAsString (vv), "\"PT3.14159S\"");
-        }
-        {
-            ObjectVariantMapper m;
-            m.AddCommonType<Duration> (FloatConversion::Precision{2});
-            VariantValue vv = m.FromObject (Duration{numbers::pi});
-            EXPECT_EQ (Variant::JSON::Writer{}.WriteAsString (vv), "\"PT3.1S\"");
-        }
-    }
-}
-
 #endif
 
 int main (int argc, const char* argv[])
