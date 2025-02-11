ef0a41dae866f317fa089a1e663e62c4c84ecd15
Lewis Pringle <lewis@sophists.com>
2025-02-11 11:19:39 -0500

    docs

--- Library/Sources/Stroika/Foundation/DataExchange/TypedBLOB.h
+++ Library/Sources/Stroika/Foundation/DataExchange/TypedBLOB.h
@@ -21,6 +21,29 @@ namespace Stroika::Foundation::DataExchange {
     /**
      *  \brief TypedBLOB is a named tuple<Memory::BLOB, optional<InternetMediaType>> - with friendlier names, and
      *         serialization properties.
+     * 
+     *  \par Example Usage
+     *      \code
+     *          return TypedBLOB{
+     *              .fData = GetOpenAPISpecification ().As (Frameworks::WebService::OpenAPI::kMediaType),
+     *              .fType = Frameworks::WebService::OpenAPI::kMediaType,
+     *          };
+     *      \endcode
+     *  
+     *  \par Example Usage
+     *      \code
+     *          TypedBLOB{someBLOBData, DataExchange::InternetMediaTypes::kOctetStream}
+     *      \endcode
+     *
+     *  \par Example Usage
+     *      \code
+     *          TypedBLOB{someBLOBData}
+     *      \endcode
+     *
+     *  \par Example Usage
+     *      \code
+     *          return TypedBLOB{.fData = GetData (), .fType = GetContentType ()};
+     *      \endcode
      */
     class [[nodiscard]] TypedBLOB {
     public:
