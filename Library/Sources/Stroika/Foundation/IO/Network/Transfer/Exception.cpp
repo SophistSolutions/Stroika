cc4b6fba2fe24319117e7e8c34e99f62a05346ed
Lewis Pringle <lewis@sophists.com>
2025-02-22 08:48:04 -0500

    experimental change for IO/Network/Transfer/Exception to strip out html tags in message gen

--- Library/Sources/Stroika/Foundation/IO/Network/Transfer/Exception.cpp
+++ Library/Sources/Stroika/Foundation/IO/Network/Transfer/Exception.cpp
@@ -5,6 +5,7 @@
 
 #include "Stroika/Foundation/Characters/Format.h"
 #include "Stroika/Foundation/Characters/RegularExpression.h"
+#include "Stroika/Foundation/Containers/Set.h"
 #include "Stroika/Foundation/DataExchange/InternetMediaTypeRegistry.h"
 #include "Stroika/Foundation/IO/Network/HTTP/Headers.h"
 #include "Stroika/Foundation/Streams/BinaryToText.h"
@@ -13,6 +14,7 @@
 
 using namespace Stroika::Foundation;
 using namespace Stroika::Foundation::Characters;
+using namespace Stroika::Foundation::Containers;
 using namespace Stroika::Foundation::DataExchange;
 using namespace Stroika::Foundation::IO::Network;
 using namespace Stroika::Foundation::IO::Network::Transfer;
@@ -20,6 +22,7 @@ using namespace Stroika::Foundation::IO::Network::Transfer;
 namespace {
     String ExtractReasonFromResponse_ (const Response& response)
     {
+        using namespace Streams;
         //DbgTrace ("response headers={}"_f, response.GetHeaders ());
         //DbgTrace ("response body={}"_f, Streams::BinaryToText::Reader::New (response.GetData()).ReadAll());
         if (auto ctHdr = response.GetHeaders ().Lookup ("Content-Type")) {
@@ -32,23 +35,28 @@ namespace {
                     // The sample JSON - I have in mind:
                     //      {   "error": "unsupported_grant_type",   "error_description": "Invalid grant_type: "
                     // Try to just grab the first few 'words', and hope it makes some sense in an error message
-                    Character                      buf[512];
-                    auto                           textStream = Streams::BinaryToText::Reader::New (response.GetData ());
-                    String                         roughText  = textStream.ReadBlocking (buf);
-                    static const RegularExpression kBunchaWords_{"([\\w]+)"};
-                    StringBuilder                  sb;
-                    auto                           words = roughText.FindEachMatch (kBunchaWords_);
+                    Character                   buf[512];
+                    InputStream::Ptr<Character> textStream = BinaryToText::Reader::New (response.GetData ());
+                    String                      roughText  = textStream.ReadAll (span{buf});
+                    //static const RegularExpression kBunchaWords_{"([\\w]+)"};
+                    static const RegularExpression kBunchaWordsOrTags_{"(<?[\\w]+)/?>?"};
+                    auto                           words = roughText.FindEachMatch (kBunchaWordsOrTags_);
                     if (not textStream.IsAtEOF ()) {
                         if (words.size () > 2) {
-                            words.erase (words.end () - 1); // last word cut-off, so don't include
+                            words.Remove (words.size () - 1); // last word cut-off, so don't include
                         }
                     }
-                    for (auto i : words) {
-                        sb << i.GetFullMatch () << " ";
+                    StringBuilder    sb;
+                    Iterable<String> wordText = words.Map<Iterable<String>> ([] (auto i) { return i.GetFullMatch (); });
+                    for (const String& w : wordText) {
+                        // remove html tags
+                        if (not w.StartsWith ("<"sv)) {
+                            sb << w << " ";
+                        }
                     }
                     sb = sb.str ().RTrim ();
                     if (not textStream.IsAtEOF ()) {
-                        sb += "...";
+                        sb += "..."sv;
                     }
                     return sb;
                 }
