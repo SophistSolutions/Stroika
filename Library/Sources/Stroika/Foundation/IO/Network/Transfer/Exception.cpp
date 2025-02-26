11d594bbce99ed9dd0f95a59eb1fdbda3b09504d
Lewis Pringle <lewis@sophists.com>
2025-02-26 11:05:44 -0500

    fixed (and some testing infrastructure) for IO/Network/Transfer/Exception message extraction

--- Library/Sources/Stroika/Foundation/IO/Network/Transfer/Exception.cpp
+++ Library/Sources/Stroika/Foundation/IO/Network/Transfer/Exception.cpp
@@ -27,7 +27,7 @@ namespace {
         // Try to just grab the first few 'words', and hope it makes some sense in an error message
 
         //static const RegularExpression kBunchaWords_{"([\\w]+)"};
-        static const RegularExpression kBunchaWordsOrTags_{"(<?[\\w]+/?>?)"};
+        static const RegularExpression kBunchaWordsOrTags_{"(<?/?[\\w]+/?>?)"sv};
         auto                           words = responseBody.FindEachMatch (kBunchaWordsOrTags_);
         if (atEOF) {
             if (words.size () > 2) {
@@ -53,7 +53,7 @@ namespace {
         using namespace Streams;
         //DbgTrace ("response headers={}"_f, response.GetHeaders ());
         //DbgTrace ("response body={}"_f, Streams::BinaryToText::Reader::New (response.GetData()).ReadAll());
-        if (auto ctHdr = response.GetHeaders ().Lookup ("Content-Type")) {
+        if (auto ctHdr = response.GetHeaders ().Lookup ("Content-Type"sv)) {
             InternetMediaType ct{*ctHdr};
             if (InternetMediaTypeRegistry::sThe->IsA (InternetMediaTypes::Wildcards::kText, ct)) {
                 if (ct == InternetMediaTypes::kText_PLAIN) {
@@ -69,6 +69,25 @@ namespace {
         }
         return String{};
     }
+
+    // Would like to test in regression tests, but not easy due to API taking Response&
+    constexpr bool kIncludeTest_ = false;
+    struct Test_ {
+        Test_ ()
+        {
+            if constexpr (kIncludeTest_) {
+                Assert (ExtractReasonFromResponse_NonPlainText_ ("<html> <head> <title>502 Bad Gateway</title></head><body><center> <h1> "
+                                                                 "502 Bad Gateway </h1></center></body></html>",
+                                                                 true) == "502 Bad Gateway 502 Bad Gateway");
+                Assert (ExtractReasonFromResponse_NonPlainText_ ("<html> <head> <title>502 Bad Gateway</title></head><body><center> <h1> "
+                                                                 "502 Bad Gateway </h1></center></body></html>",
+                                                                 false) == "502 Bad Gateway 502 Bad Gateway...");
+                Assert (ExtractReasonFromResponse_NonPlainText_ (
+                            "{  \"error\": \"unsupported_grant_type\",  \"error_description\": \"Invalid grant_type\", ", false) ==
+                        "error unsupported_grant_type error_description Invalid grant_type...");
+            }
+        }
+    } _RegTest;
 }
 
 /*
