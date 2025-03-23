aa16d0cf4ca7bfe0e4ad17a0201e40e8ca061be6
Lewis Pringle <lewis@sophists.com>
2025-03-23 15:13:03 -0400

    cosmetic

--- Tests/35/Test.cpp
+++ Tests/35/Test.cpp
@@ -60,7 +60,7 @@ namespace {
      *  docker run --rm --name mongodb -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=pass mongo:latest
      */
     [[maybe_unused]] optional<String> sMongoConnectionString_;
-    [[maybe_unused]] const String     kDefaultMongoConnectionString_    = "mongodb://admin:pass@localhost:27017";
+    [[maybe_unused]] const String     kDefaultMongoConnectionString_    = "mongodb://admin:pass@localhost:27017"sv;
     [[maybe_unused]] bool             sAlreadyWarnedFailedMongoDBServer = false;
 }
 
