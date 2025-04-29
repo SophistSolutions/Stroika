aba31c00d9b375ec3a52ef666907e9f6927130fe
Lewis Pringle <lewis@sophists.com>
2025-04-29 11:35:22 -0400

    Cleanups to AppSettings sample, and fixedup error handling so doesn't crash

--- Samples/AppSettings/Sources/Main.cpp
+++ Samples/AppSettings/Sources/Main.cpp
@@ -6,6 +6,8 @@
 #include "Stroika/Foundation/Debug/Trace.h"
 #include "Stroika/Foundation/Debug/Visualizations.h"
 #include "Stroika/Foundation/Execution/CommandLine.h"
+#include "Stroika/Foundation/Execution/Logger.h"
+#include "Stroika/Foundation/IO/FileSystem/FileOutputStream.h"
 
 #include "AppConfiguration.h"
 #include "MoreConfiguration.h"
@@ -14,6 +16,8 @@ using namespace std;
 
 using namespace Stroika::Foundation;
 using namespace Stroika::Foundation::Characters;
+using namespace Stroika::Foundation::Execution;
+using namespace Stroika::Foundation::IO::FileSystem;
 
 using namespace StroikaSample;
 using namespace StroikaSample::AppSettings;
@@ -23,7 +27,13 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
     Debug::TraceContextBumper ctx{
         Stroika_Foundation_Debug_OptionalizeTraceArgs ("main", "argv={}"_f, Characters::ToString (vector<const char*>{argv, argv + argc}))};
 
-    // Simplest example
+    Logger::Activator logMgrActivator; // OptionsFile uses this by default...'
+
+    // for a test app, writing to stdout, seems a plausible default...
+    Logger::sThe.AddAppender (
+        make_shared<Logger::StreamAppender> (FileOutputStream::New (STDOUT_FILENO, FileStream::AdoptFDPolicy::eDisconnectOnDestruction)));
+
+    // Simple example using OpensFile, and ModuleGetterSetter
     [[maybe_unused]] uint16_t usePort = gAppConfiguration->WebServerPort.value_or (AppConfigurationType::kWebServerPort_Default);
 
     // Fancier examples
