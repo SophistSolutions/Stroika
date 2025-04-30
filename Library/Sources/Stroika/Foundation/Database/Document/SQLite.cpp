d637eb8bb754d67fa70946e0dff660e754057ff5
Lewis Pringle <lewis@sophists.com>
2025-04-30 12:29:51 -0400

    undo some of the JSON sqlite changes - not working yet (documentdb)

--- Library/Sources/Stroika/Foundation/Database/Document/SQLite.cpp
+++ Library/Sources/Stroika/Foundation/Database/Document/SQLite.cpp
@@ -230,7 +230,8 @@ namespace {
                  *      none that efficient and clean and simple. I guess this is clean and simple and efficient, just probably a race
                  */
                 if (fAddStatement_ == nullptr) [[unlikely]] {
-                    fAddStatement_ = mkPreparedStatement_ (fConnectionRep_->fDB_, "insert into {} (json) values(json(?));"_f(fTableName_));
+                    //fAddStatement_ = mkPreparedStatement_ (fConnectionRep_->fDB_, "insert into {} (json) values(json(?));"_f(fTableName_));
+                    fAddStatement_ = mkPreparedStatement_ (fConnectionRep_->fDB_, "insert into {} (json) values(?);"_f(fTableName_));
                 }
                 string jsonText = Variant::JSON::Writer{}.WriteAsString (VariantValue{v}).AsUTF8<string> ();
                 ThrowSQLiteErrorIfNotOK_ (::sqlite3_reset (fAddStatement_), fConnectionRep_->fDB_);
@@ -338,7 +339,8 @@ namespace {
                 String r = Variant::JSON::Writer{}.WriteAsString (VariantValue{d2Update});
                 ThrowSQLiteErrorIfNotOK_ (
                     ::sqlite3_exec (fConnectionRep_->fDB_,
-                                    "update {} SET json='json({})' where id='{}';"_f(fTableName_, r, id).AsUTF8<string> ().c_str (),
+                                    "update {} SET json='{}' where id='{}';"_f(fTableName_, r, id).AsUTF8<string> ().c_str (),
+//                                    "update {} SET json='json({})' where id='{}';"_f(fTableName_, r, id).AsUTF8<string> ().c_str (),
                                     nullptr, nullptr, nullptr),
                     fConnectionRep_->fDB_);
             }
