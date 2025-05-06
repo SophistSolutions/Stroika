bfa7000e78b64f4661de5412e2a3034f3ac56e8e
Lewis Pringle <lewis@sophists.com>
2025-05-05 22:22:13 -0400

    fixed qCompilerAndStdLib_explicitly_defaulted_threeway_warning_Buggy bug define

--- Library/Sources/Stroika/Foundation/Common/Private/CompilerAndStdLib_.h
+++ Library/Sources/Stroika/Foundation/Common/Private/CompilerAndStdLib_.h
@@ -2243,11 +2243,12 @@ In file included from Namespace.cpp:10:
 */
 #ifndef qCompilerAndStdLib_explicitly_defaulted_threeway_warning_Buggy
 
-#if defined(_LIBCPP_VERSION)
+#if defined(__clang__)
 // Reproduced using clang++15, libcpp 15007, and Ubunutu 22.04
 // Reproduced using xcode 15 with _LIBCPP_VERSION == 160006 (yes they appear to have added an extra digit recently) (note some warnings only happen on XCode 15)
 // new case whith this happening with 200100  (was just checking < 170000)
-#define qCompilerAndStdLib_explicitly_defaulted_threeway_warning_Buggy (_LIBCPP_VERSION < 210000)
+// and     {clang++-15-release-libstdc++}:
+#define qCompilerAndStdLib_explicitly_defaulted_threeway_warning_Buggy (__clang_major__ < 20)
 #else
 #define qCompilerAndStdLib_explicitly_defaulted_threeway_warning_Buggy 0
 #endif
