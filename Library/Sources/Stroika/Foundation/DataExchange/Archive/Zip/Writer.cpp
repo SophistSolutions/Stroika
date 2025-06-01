eb21289da0ba5e524f11b3a387aa0b80b12ada76
Lewis Pringle <lewis@sophists.com>
2025-06-01 10:11:12 -0400

    progress on DataExchange/Archive/Zip/Writer - almost testable

--- Library/Sources/Stroika/Foundation/DataExchange/Archive/Zip/Writer.cpp
+++ Library/Sources/Stroika/Foundation/DataExchange/Archive/Zip/Writer.cpp
@@ -16,20 +16,125 @@ using namespace Stroika::Foundation::DataExchange::Archive;
 using namespace Stroika::Foundation::DataExchange::Archive::Writer;
 using namespace Stroika::Foundation::Execution;
 using namespace Stroika::Foundation::Memory;
+using namespace Stroika::Foundation::Streams;
 
 #if qStroika_HasComponent_zlib
 using Memory::BLOB;
 using std::byte;
+using namespace Stroika::Foundation::DataExchange::Archive::Zip::PrivateMinizip_;
+
+namespace {
+    struct MyZipLibOutStream_ : zlib_filefunc64_def {
+        OutputStream::Ptr<byte> fOutSteram_;
+#if qStroika_Foundation_Debug_AssertionsChecked
+        bool fOpened_{false};
+#endif
+        MyZipLibOutStream_ (const OutputStream::Ptr<byte>& in)
+            : fOutSteram_{in}
+        {
+            this->zopen64_file = [] (voidpf opaqueStream, const void* /*filename*/, int /*mode*/) -> voidpf {
+                MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Assert (not myThis->fOpened_);
+                myThis->fOpened_ = true;
+#endif
+                return myThis;
+            };
+            this->zread_file = [] ([[maybe_unused]] voidpf opaqueStream, [[maybe_unused]] voidpf stream, [[maybe_unused]] void* buf,
+                                   [[maybe_unused]] uLong size) -> uLong {
+                RequireNotReached (); // read only zip
+                return static_cast<uLong> (UNZ_PARAMERROR);
+            };
+            this->zwrite_file = [] (voidpf opaqueStream, [[maybe_unused]] voidpf stream, const void* buf, uLong size) -> uLong {
+                Require (opaqueStream == stream); // our use is one stream per zlib_filefunc64_def object
+                MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Assert (myThis->fOpened_);
+#endif
+                myThis->fOutSteram_.Write (span{reinterpret_cast<const byte*> (buf), size});
+                return static_cast<uLong> (size);
+                
+             };
+            this->ztell64_file = [] (voidpf opaqueStream, [[maybe_unused]] voidpf stream) -> ZPOS64_T {
+                Require (opaqueStream == stream); // our use is one stream per zlib_filefunc64_def object
+                MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Assert (myThis->fOpened_);
+#endif
+                return myThis->fOutSteram_.GetOffset ();
+            };
+            this->zseek64_file = [] (voidpf opaqueStream, [[maybe_unused]] voidpf stream, ZPOS64_T offset, int origin) -> long {
+                Require (opaqueStream == stream); // our use is one stream per zlib_filefunc64_def object
+                MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Assert (myThis->fOpened_);
+#endif
+                switch (origin) {
+                    case ZLIB_FILEFUNC_SEEK_SET:
+                        myThis->fOutSteram_.Seek (offset);
+                        break;
+                    case ZLIB_FILEFUNC_SEEK_CUR:
+                        myThis->fOutSteram_.Seek (Streams::eFromCurrent, offset);
+                        break;
+                    case ZLIB_FILEFUNC_SEEK_END:
+                        myThis->fOutSteram_.Seek (Streams::eFromEnd, offset);
+                        break;
+                    default:
+                        AssertNotReached ();
+                        return UNZ_PARAMERROR;
+                }
+                return UNZ_OK;
+            };
+            this->zclose_file = [] ([[maybe_unused]] voidpf opaqueStream, [[maybe_unused]] voidpf stream) -> int {
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Require (opaqueStream == stream); // our use is one stream per zlib_filefunc64_def object
+                MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+                Assert (myThis->fOpened_);
+                myThis->fOutSteram_.Flush ();
+                myThis->fOpened_ = false;
+#endif
+                return UNZ_OK;
+            };
+            this->zerror_file = [] (voidpf opaqueStream, [[maybe_unused]] voidpf stream) -> int {
+                Require (opaqueStream == stream); // our use is one stream per zlib_filefunc64_def object
+                [[maybe_unused]] MyZipLibOutStream_* myThis = reinterpret_cast<MyZipLibOutStream_*> (opaqueStream);
+#if qStroika_Foundation_Debug_AssertionsChecked
+                Assert (myThis->fOpened_);
+#endif
+                return UNZ_OK; // @todo - see what this means?
+            };
+            this->opaque = this;
+        }
+        ~MyZipLibOutStream_ ()
+        {
+#if qStroika_Foundation_Debug_AssertionsChecked
+            Assert (not fOpened_);
+#endif
+        }
+    };
+}
 
 namespace {
     struct MyRep_ : Archive::Writer::IRep {
-        Streams::OutputStream::Ptr<byte> fOutputTo_;
-        MyRep_ (const Streams::OutputStream::Ptr<byte>& writeTo)
-            : fOutputTo_{writeTo}
+        MyZipLibOutStream_ fInSeekStream_;
+        unzFile           fZipFile_;
+        MyRep_ (const OutputStream::Ptr<byte>& in)
+            : fInSeekStream_{in}
+            , fZipFile_{unzOpen2_64 ("", &fInSeekStream_)}
         {
+            if (fZipFile_ == nullptr) [[unlikely]] {
+                static const RuntimeErrorException kException_{"failed to open zipfile"sv};
+                Throw (kException_);
+            }
+        }
+        ~MyRep_ ()
+        {
+            AssertNotNull (fZipFile_);
+            unzClose (fZipFile_);
         }
         virtual void Add (const String& fileName, const BLOB& data) override
         {
+            // NYI
         }
     };
 }
@@ -39,7 +144,7 @@ namespace {
  ********************** DataExchange::Archive::Writer::Ptr **********************
  ********************************************************************************
  */
-Archive::Writer::Ptr Archive::Zip::Writer::New (const Streams::OutputStream::Ptr<byte>& writeTo)
+Archive::Writer::Ptr Archive::Zip::Writer::New (const OutputStream::Ptr<byte>& writeTo)
 {
     Require (writeTo.IsSeekable ());
     return Archive::Writer::Ptr{make_shared<MyRep_> (writeTo)};
