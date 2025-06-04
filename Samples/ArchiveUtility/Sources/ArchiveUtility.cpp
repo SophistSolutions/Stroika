f4ad80d921e1082c88dd77bdbc3e1cdda58a7b0e
Lewis Pringle <lewis@sophists.com>
2025-06-04 08:56:27 -0400

    slight cleanup to Archive Utility sample (and fixed build error on no-thrid-party compoennts config)

--- Samples/ArchiveUtility/Sources/ArchiveUtility.cpp
+++ Samples/ArchiveUtility/Sources/ArchiveUtility.cpp
@@ -144,6 +144,19 @@ namespace {
 }
 
 namespace {
+    DataExchange::Archive::Writer::Ptr CreateWritingArchive_ (const filesystem::path& archiveName)
+    {
+#if qStroika_HasComponent_zlib
+        // for now require just zip
+        if (String{archiveName}.EndsWith (".zip"sv, Characters::eCaseInsensitive)) {
+            return DataExchange::Archive::Zip::Writer::New (IO::FileSystem::FileOutputStream::New (archiveName));
+        }
+#endif
+        Throw(Exception{"Unrecognized format"sv});
+    }
+}
+
+namespace {
     void ListArchive_ (const filesystem::path& archiveName)
     {
         for (String i : OpenArchive_ (archiveName).GetContainedFiles ()) {
@@ -152,8 +165,7 @@ namespace {
     }
     void ExtractArchive_ (const filesystem::path& archiveName, const filesystem::path& toDirectory)
     {
-        Debug::TraceContextBumper ctx{"ExtractArchive_"};
-        DbgTrace ("(archiveName={}, toDir={})"_f, archiveName, toDirectory);
+        Debug::TraceContextBumper ctx{"ExtractArchive_", "(archiveName={}, toDir={})"_f, archiveName, toDirectory);
         DataExchange::Archive::Reader::Ptr archive{OpenArchive_ (archiveName)};
         for (String i : archive.GetContainedFiles ()) {
             String           srcFileName = i;
@@ -168,18 +180,10 @@ namespace {
     }
     void CreateArchive_ (const filesystem::path& archiveName, const Sequence<String>& files2Add)
     {
-        Debug::TraceContextBumper ctx{"CreateArchive_"};
-        DbgTrace ("(archiveName={}, files2Add={})"_f, archiveName, files2Add);
-        // for now require just zip
-        if (String{archiveName}.EndsWith (".zip"sv, Characters::eCaseInsensitive)) {
-            DataExchange::Archive::Writer::Ptr archive =
-                DataExchange::Archive::Zip::Writer::New (IO::FileSystem::FileOutputStream::New (archiveName));
-            for (auto f2a : files2Add) {
-                archive.Add (f2a, IO::FileSystem::FileInputStream::New (f2a.As<filesystem::path> ()).ReadAll ());
-            }
-        }
-        else {
-            cerr << "that format NYI" << endl;
+        Debug::TraceContextBumper ctx{"CreateArchive_", "(archiveName={}, files2Add={})"_f, archiveName, files2Add);
+        DataExchange::Archive::Writer::Ptr archive = CreateWritingArchive_ (archiveName);
+        for (String f2a : files2Add) {
+            archive.Add (f2a, IO::FileSystem::FileInputStream::New (f2a.As<filesystem::path> ()).ReadAll ());
         }
     }
 }
