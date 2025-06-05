1fc25883a40c69d6d8545226919b9e2d5f360040
Lewis Pringle <lewis@sophists.com>
2025-06-05 12:07:17 -0400

    maybe fix warning about bad reg key in WarnIfNotWindowsDeveloperMode/Docker windows

--- ScriptsLib/WarnIfNotWindowsDeveloperMode.ps1
+++ ScriptsLib/WarnIfNotWindowsDeveloperMode.ps1
@@ -1,8 +1,21 @@
 #!/usr/bin/env powershell 
 
+function Test-RegistryValue {
+    param (
+        [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]$Path,
+        [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]$Value
+    )
+    try {
+        return Get-ItemPropertyValue -Path $Path -Name $Value -ErrorAction Stop 
+    } catch {
+        return $true
+    }
+}
+
+
 # Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
 $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock\"
-if (-not(Get-ItemPropertyValue -Path $RegistryKeyPath -name "AllowDevelopmentWithoutDevLicense")) {
+if (-not(Test-RegistryValue -Path $RegistryKeyPath -Value "AllowDevelopmentWithoutDevLicense")) {
     #New-Item -Path $RegistryKeyPath -ItemType Directory -Force
     echo "Warning: Developer Mode not enabled - goto Settings, and search for Developer Mode, and enable, else ln -s calls might not work"
     echo "Settings: For Developers: Enable Developer Mode"
