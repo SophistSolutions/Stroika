6b02f4f40089e043ba642dd72cb914bfc364c17e
Lewis Pringle <lewis@sophists.com>
2026-01-04 12:37:28 -0500

    appears to be cosmetic reformatting from editor

--- Samples/HTMLUI/QuasarBasedHTMLApp/src/pages/AuthTestPage.vue
+++ Samples/HTMLUI/QuasarBasedHTMLApp/src/pages/AuthTestPage.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
-import { defineComponent, computed, toRaw, watch, ref, Ref, onMounted, getCurrentInstance, onUnmounted } from "vue";
-import { QSelectOption } from 'quasar';
+import {
+    defineComponent,
+    computed,
+    toRaw,
+    watch,
+    ref,
+    Ref,
+    onMounted,
+    getCurrentInstance,
+    onUnmounted,
+} from "vue";
+import { QSelectOption } from "quasar";
 
 import { useRouter, useRoute } from "vue-router";
 import { useConfigurationStore } from "../stores/Configuration-Store";
@@ -22,27 +32,36 @@ const router = useRouter();
 const configurationStore = useConfigurationStore();
 let auth: IAuthService = getCurrentInstance()?.appContext.config.globalProperties.$auth;
 
-const availableProviders: Ref<IOAuthProviderConfig[] | undefined> = auth.availableProviders;
-const availableProviderOptions = computed(() => availableProviders.value?.map((i) => { return { 'label': capitalize_(i.provider), 'value': i.provider }; }));
+const availableProviders: Ref<IOAuthProviderConfig[] | undefined> =
+    auth.availableProviders;
+const availableProviderOptions = computed(() =>
+    availableProviders.value?.map((i) => {
+        return { label: capitalize_(i.provider), value: i.provider };
+    })
+);
 const activeProvider: Ref<IOAuthProviderConfig | undefined> = auth.activeProvider;
 
-const loginWith: Ref<QSelectOption> = ref(availableProviderOptions.value?.length ? availableProviderOptions.value[0] : { 'label': "Unavailable", 'value': "unavailable" });
+const loginWith: Ref<QSelectOption> = ref(
+    availableProviderOptions.value?.length
+        ? availableProviderOptions.value[0]
+        : { label: "Unavailable", value: "unavailable" }
+);
 // availableProviders not immediately available always, and not sure how to specify loginWith to have its default react this way
 // other than watch --LGP 2025-02-06
 watch(availableProviders, () => {
     if (loginWith.value.value == "unavailable" && availableProviderOptions.value?.length) {
         loginWith.value = availableProviderOptions.value[0];
     }
-})
+});
 
 let iterervalTimer: NodeJS.Timeout | undefined = undefined;
 
 onMounted(() => {
     console.log("AuthTest page, on-mounted Event: auth=", toRaw(auth));
 
-    recomputeExpiresIn_()
+    recomputeExpiresIn_();
     iterervalTimer = setInterval(function () {
-        recomputeExpiresIn_()
+        recomputeExpiresIn_();
     }, 30 * 1000);
 });
 
@@ -60,11 +79,16 @@ const personEMail = computed(() => auth.user.value?.email);
 const personImageURL = computed(() => auth.user.value?.personImageURL);
 
 async function login() {
-    const useProvider: IOAuthProviderConfig | undefined = availableProviders.value?.find((i) => { if (loginWith.value.value == i.provider) { return i; } });
+    const useProvider: IOAuthProviderConfig | undefined = availableProviders.value?.find(
+        (i) => {
+            if (loginWith.value.value == i.provider) {
+                return i;
+            }
+        }
+    );
     try {
         await auth.login({ useProvider });
-    }
-    catch (e) {
+    } catch (e) {
         alert(`Error during login: ${e?.message}`);
     }
 }
@@ -94,13 +118,15 @@ function logout() {
         <div class="justify-center row">
             <q-card class="col-11 pageCard">
                 <q-card-section>
-                    <div class="row">Configuration (availableProviders - from Backend webservices and its
-                        configuration):</div>
+                    <div class="row">
+                        Configuration (availableProviders - from Backend webservices and its
+                        configuration):
+                    </div>
                     <pre v-if="auth.availableProviders.value">{{
                         JSON.stringify(auth.availableProviders.value, null, 2)
                     }}</pre>
                     <div v-if="!auth.availableProviders.value">
-                        <div class="row" style="color:red">No OAuth configuration.</div>
+                        <div class="row" style="color: red">No OAuth configuration.</div>
                         <div class="row">See /api/v1/auth/oauth/configurations (backend api)</div>
                         <div class="row">
                             See C:/ProgramData/Stroika-Sample-HTMLUI/Server.json (or
@@ -131,12 +157,12 @@ function logout() {
 
             <q-card class="col-11 pageCard" v-if="activeProvider">
                 <q-card-section>
-                    <div class="row">Configuration (activeProvider - if logged in, will be valid, but can be valid even
-                        if not logged in):</div>
                     <div class="row">
-                        <pre v-if="activeProvider">{{
-                            JSON.stringify(activeProvider, null, 2)
-                        }}</pre>
+                        Configuration (activeProvider - if logged in, will be valid, but can be valid
+                        even if not logged in):
+                    </div>
+                    <div class="row">
+                        <pre v-if="activeProvider">{{ JSON.stringify(activeProvider, null, 2) }}</pre>
                     </div>
                 </q-card-section>
             </q-card>
@@ -147,19 +173,16 @@ function logout() {
                     <div class="q-gutter-y-sm">
                         <div class="row" v-if="isAuthenticated">
                             <div class="col-1"></div>
-                            <div class="col-1">
-                                User Info:
-                            </div>
+                            <div class="col-1">User Info:</div>
                             <div class="col">
                                 You are logged in as "{{ personName }}" (email: {{ personEMail }})
-                                <img v-if="personImageURL" :src="personImageURL" style="height: .25in; width: .25in;" />
+                                <img v-if="personImageURL" :src="personImageURL"
+                                    style="height: 0.25in; width: 0.25in" />
                             </div>
                         </div>
                         <div class="row" v-if="auth.authorizationTokens.value">
                             <div class="col-1"></div>
-                            <div class="col-1">
-                                Access Token:
-                            </div>
+                            <div class="col-1">Access Token:</div>
                             <div class="col">
                                 <pre>{{ auth.authorizationTokens.value.accessToken }}</pre>
                             </div>
@@ -167,8 +190,9 @@ function logout() {
                         <div class="row" v-if="auth.authorizationTokens.value?.expiresAt">
                             <div class="col-1"></div>
                             <div class="col-1">ExpiresAt</div>
-                            <div class="col">{{ auth.authorizationTokens.value.expiresAt }} ({{
-                                expiresIn }})</div>
+                            <div class="col">
+                                {{ auth.authorizationTokens.value.expiresAt }} ({{ expiresIn }})
+                            </div>
                         </div>
                         <div class="row" v-if="auth.authorizationTokens.value?.idToken">
                             <div class="col-1"></div>
@@ -184,9 +208,7 @@ function logout() {
                                 <pre>{{ auth.authorizationTokens.value.refreshToken }}</pre>
                             </div>
                         </div>
-                        <div class="row" v-if="!isAuthenticated">
-                            NOT CURRENTLY LOGGED IN.
-                        </div>
+                        <div class="row" v-if="!isAuthenticated">NOT CURRENTLY LOGGED IN.</div>
                         <div class="row">
                             <div class="col-1"></div>
                             <div class="col-2 self-center" v-if="isAuthenticated">
