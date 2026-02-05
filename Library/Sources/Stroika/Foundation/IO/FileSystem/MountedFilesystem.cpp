3d393d75c872f8f3c3bc8e1c819298b04e69813d
Lewis Pringle <lewis@sophists.com>
2026-02-05 08:55:01 -0500

    use new qStroika_Foundation_INDETERMINATE

--- Library/Sources/Stroika/Foundation/IO/FileSystem/MountedFilesystem.cpp
+++ Library/Sources/Stroika/Foundation/IO/FileSystem/MountedFilesystem.cpp
@@ -256,7 +256,7 @@ namespace {
                 v.fVolumeID       = String::FromSDKString (volumeNameBuf);
                 v.fDevicePaths    = GetDisksForVolume_ (volumeNameBuf);
 
-                TCHAR volPathsBuf[10 * 1024]; // intentionally uninitialized
+                TCHAR volPathsBuf[10 * 1024] qStroika_Foundation_INDETERMINATE;
                 DWORD retLen = 0;
                 DWORD x = ::GetVolumePathNamesForVolumeName (volumeNameBuf, volPathsBuf, static_cast<DWORD> (std::size (volPathsBuf)), &retLen);
                 if (x == 0) {
