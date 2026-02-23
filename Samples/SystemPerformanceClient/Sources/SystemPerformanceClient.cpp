3379cf271e4451a8ef92dfd5d79598c602088e0a
Lewis Pringle <lewis@sophists.com>
2026-02-23 18:48:06 -0500

    Simplify/enahance BWA for qCompilerAndStdLib_template_template_auto_deduced_Buggy

--- Samples/SystemPerformanceClient/Sources/SystemPerformanceClient.cpp
+++ Samples/SystemPerformanceClient/Sources/SystemPerformanceClient.cpp
@@ -56,16 +56,14 @@ namespace {
         }
         return result.AsNarrowSDKString ();
     }
-#if qCompilerAndStdLib_template_template_argument_as_different_template_paramters_Buggy
+#if qCompilerAndStdLib_template_template_argument_as_different_template_paramters_Buggy or qCompilerAndStdLib_template_template_auto_deduced_Buggy
     template <typename X>
     using RANGE_TEMPLATE_BWA = Range<X>;
 #endif
     Range<DisplayedRealtimeClock::time_point> toDisp_ (Range<TimePointSeconds> tpRange)
     {
-#if qCompilerAndStdLib_template_template_argument_as_different_template_paramters_Buggy
+#if qCompilerAndStdLib_template_template_argument_as_different_template_paramters_Buggy or qCompilerAndStdLib_template_template_auto_deduced_Buggy
         return Time::clock_cast<DisplayedRealtimeClock, RANGE_TEMPLATE_BWA> (tpRange);
-#elif qCompilerAndStdLib_template_template_auto_deduced_Buggy
-        return Time::clock_cast<DisplayedRealtimeClock, Range> (tpRange);
 #else
         return Time::clock_cast<DisplayedRealtimeClock> (tpRange);
 #endif
